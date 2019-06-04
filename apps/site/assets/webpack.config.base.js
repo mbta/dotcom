2bb23ed0ad5cca7f13893600e0fcea76b3b81c67
Kate Hedgpeth <katehedgpeth@gmail.com>
2019-06-04 14:44:32 -0400

    add interactions to trip planner leaflet map (#4)

--- apps/site/assets/webpack.config.base.js
+++ apps/site/assets/webpack.config.base.js
@@ -12,6 +12,7 @@ module.exports = {
     core: ["./js/core.js"], // For core.css only, not js
     map: ["./js/leaflet-css.js"], // For leaflet.css only, not js
     tnm: ["./ts/transit-near-me-entry.ts"],
+    tripplanner: ["./ts/trip-planner-entry.ts"],
     stop: ["./ts/stop-entry.ts"],
     leaflet: ["./ts/leaflet-entry.ts"],
     schedule: ["./ts/schedule-entry.ts"]
