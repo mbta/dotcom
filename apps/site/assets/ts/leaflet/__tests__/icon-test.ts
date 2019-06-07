a8ee51608e3a344594c4b7c98e8ae77b806161bb
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2019-06-07 13:06:58 -0400

    format

--- apps/site/assets/ts/leaflet/__tests__/icon-test.ts
+++ apps/site/assets/ts/leaflet/__tests__/icon-test.ts
@@ -15,7 +15,7 @@ it("returns default icon opts if options are missing", () => {
   expect(iconClass!.options).toEqual({
     ...defaultIconOpts,
     iconRetinaUrl: "/images/icon-string.svg",
-    iconUrl: "/images/icon-string.svg",
+    iconUrl: "/images/icon-string.svg"
   });
 });
 
@@ -25,6 +25,6 @@ it("returns default icon opts if some options are missing", () => {
     ...defaultIconOpts,
     iconRetinaUrl: "/images/icon-string.svg",
     iconSize: [50, 50],
-    iconUrl: "/images/icon-string.svg",
+    iconUrl: "/images/icon-string.svg"
   });
 });
