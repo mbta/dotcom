be00ca439aa3fc826ee1d56b663badd998e5dd21
Ryan Mahoney <ryan.mahoney@outlook.com>
2019-06-28 09:57:04 -0400

    upload fonts/images with max_age

--- upload_assets.sh
+++ upload_assets.sh
@@ -12,9 +12,13 @@ unzip $BUILD_ARTIFACT "lib/site-$VERSION/priv/static/*" -d $TEMP_DIR
 # sync the digested files with a cache control header
 aws s3 sync $STATIC_DIR/css s3://mbta-dotcom/css --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
 aws s3 sync $STATIC_DIR/js s3://mbta-dotcom/js --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
+aws s3 sync $STATIC_DIR/fonts s3://mbta-dotcom/fonts --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
+aws s3 sync $STATIC_DIR/images s3://mbta-dotcom/images --size-only --exclude "*" --include "*-*" --cache-control=$CACHE_CONTROL
 
 # sync everything else normally
 aws s3 sync $STATIC_DIR/css s3://mbta-dotcom/css --size-only
 aws s3 sync $STATIC_DIR/js s3://mbta-dotcom/js --size-only
+aws s3 sync $STATIC_DIR/fonts s3://mbta-dotcom/fonts --size-only
+aws s3 sync $STATIC_DIR/images s3://mbta-dotcom/images --size-only
 
 rm -rf $TEMP_DIR
