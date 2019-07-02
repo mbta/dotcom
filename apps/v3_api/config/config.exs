fdd02642bc77f8d39baf3b7e8df46bfd3125a543
Paul Swartz <paul@paulswartz.net>
2019-07-02 08:08:52 -0400

    feat: don't require WIREMOCK_PROXY_URL to be set
    The current code requires WIREMOCK_PROXY_URL to be set to a particular value
    for the tests to pass. This pulls the configuration into the test so that the
    environment isn't required to be in a particular state.
    
    Also removes `async: true` since it modifies the global state.

--- apps/v3_api/config/config.exs
+++ apps/v3_api/config/config.exs
@@ -5,7 +5,7 @@ use Mix.Config
 config :v3_api,
   base_url: {:system, "V3_URL", System.get_env("V3_URL")},
   api_key: {:system, "V3_API_KEY"},
-  wiremock_proxy_url: System.get_env("WIREMOCK_PROXY_URL"),
+  wiremock_proxy_url: {:system, "WIREMOCK_PROXY_URL"},
   wiremock_proxy: {:system, "WIREMOCK_PROXY", "false"},
   default_timeout: 5_000,
   cache_size: 10_000,
