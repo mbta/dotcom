1c79a335f0221465a7cc355ad83d7932885c6fe3
Meaghan Harty <1829246+meagonqz@users.noreply.github.com>
2019-07-09 14:50:38 -0400

    test multiple weekdays for service selector

--- apps/site/assets/ts/schedule/__tests__/ServiceSelectorTest.tsx
+++ apps/site/assets/ts/schedule/__tests__/ServiceSelectorTest.tsx
@@ -23,6 +23,19 @@ const services: ServiceWithServiceDate[] = [
     added_dates: []
   },
   {
+    valid_days: [5],
+    typicality: "typical_service",
+    type: "weekday",
+    start_date: "2019-07-05",
+    service_date: "2019-07-09",
+    name: "Weekday",
+    id: "BUS319-D-Wdy-02",
+    end_date: "2019-08-30",
+    description: "Weekday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
     valid_days: [6],
     typicality: "typical_service",
     type: "saturday",
@@ -54,6 +67,69 @@ const services: ServiceWithServiceDate[] = [
   }
 ] as ServiceWithServiceDate[];
 const serviceSchedules: ServiceSchedule = serviceData as ServiceSchedule;
+
+const multipleWeekdays = [
+  {
+    valid_days: [1, 2, 3, 4],
+    typicality: "typical_service",
+    type: "weekday",
+    start_date: "2019-07-02",
+    service_date: "2019-07-09",
+    removed_dates_notes: { "2019-07-04": "Independence Day" },
+    removed_dates: ["2019-07-04"],
+    name: "Weekday",
+    id: "BUS319-F-Wdy-02",
+    end_date: "2019-08-29",
+    description: "Weekday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
+    valid_days: [5],
+    typicality: "typical_service",
+    type: "weekday",
+    start_date: "2019-07-05",
+    service_date: "2019-07-09",
+    removed_dates_notes: {},
+    removed_dates: [],
+    name: "Weekday",
+    id: "BUS319-D-Wdy-02",
+    end_date: "2019-08-30",
+    description: "Weekday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
+    valid_days: [6],
+    typicality: "typical_service",
+    type: "saturday",
+    start_date: "2019-07-06",
+    service_date: "2019-07-09",
+    removed_dates_notes: {},
+    removed_dates: [],
+    name: "Saturday",
+    id: "BUS319-B-Sa-02",
+    end_date: "2019-08-31",
+    description: "Saturday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
+    valid_days: [7],
+    typicality: "typical_service",
+    type: "sunday",
+    start_date: "2019-07-07",
+    service_date: "2019-07-09",
+    removed_dates_notes: {},
+    removed_dates: [],
+    name: "Sunday",
+    id: "BUS319-C-Su-02",
+    end_date: "2019-08-25",
+    description: "Sunday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  }
+];
 
 describe("ServiceSelector", () => {
   it("it renders", () => {
