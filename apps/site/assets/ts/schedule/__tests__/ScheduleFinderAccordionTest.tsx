85f393117178932ff8646d7581357ac0db0fcf14
Ryan Mahoney <ryan.mahoney@outlook.com>
2019-07-11 16:56:09 -0400

    add test for uncovered component

---
+++ apps/site/assets/ts/schedule/__tests__/ScheduleFinderAccordionTest.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { createReactRoot } from "../../app/helpers/testUtils";
+import ScheduleFinderAccordion from "../components/ScheduleFinderAccordion";
+import { EnhancedRoute, ServiceWithServiceDate } from "../../__v3api";
+import serviceData from "./serviceData.json";
+import { ServiceSchedule } from "../components/__schedule";
+
+const services: ServiceWithServiceDate[] = [
+  {
+    valid_days: [1, 2, 3, 4, 5],
+    typicality: "typical_service",
+    type: "weekday",
+    start_date: "2019-07-02",
+    service_date: "2019-07-09",
+    removed_dates_notes: { "2019-07-04": "Independence Day" },
+    removed_dates: ["2019-07-04"],
+    name: "Weekday",
+    id: "BUS319-O-Wdy-02",
+    end_date: "2019-08-30",
+    description: "Weekday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
+    valid_days: [6],
+    typicality: "typical_service",
+    type: "saturday",
+    start_date: "2019-07-06",
+    service_date: "2019-07-09",
+    removed_dates_notes: {},
+    removed_dates: [],
+    name: "Saturday",
+    id: "BUS319-P-Sa-02",
+    end_date: "2019-08-31",
+    description: "Saturday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  },
+  {
+    valid_days: [7],
+    typicality: "typical_service",
+    type: "sunday",
+    start_date: "2019-07-07",
+    service_date: "2019-07-09",
+    removed_dates_notes: {},
+    removed_dates: [],
+    name: "Sunday",
+    id: "BUS319-Q-Su-02",
+    end_date: "2019-08-25",
+    description: "Sunday schedule",
+    added_dates_notes: {},
+    added_dates: []
+  }
+] as ServiceWithServiceDate[];
+const serviceSchedules: ServiceSchedule = serviceData as ServiceSchedule;
+// the enzyme test was done as one test because there was
+// an issue mounting it more than once due to the focus-trap
+// dependency that the Modal component depends on
+
+/* eslint-disable @typescript-eslint/camelcase */
+const body = '<div id="react-root"></div>';
+const route: EnhancedRoute = {
+  alert_count: 0,
+  description: "",
+  direction_destinations: { 0: "Oak Grove", 1: "Forest Hills" },
+  direction_names: { 0: "Inbound", 1: "Outbound" },
+  header: "",
+  id: "Orange",
+  long_name: "Orange Line",
+  name: "Orange",
+  type: 1
+};
+const stops = [
+  {
+    name: "SL",
+    id: "741",
+    is_closed: false,
+    zone: "1"
+  },
+  {
+    name: "Abc",
+    id: "123",
+    is_closed: false,
+    zone: null
+  },
+  {
+    name: "Wellington",
+    id: "place-welln",
+    is_closed: true,
+    zone: null
+  }
+];
+
+it("renders", () => {
+  createReactRoot();
+  const tree = renderer
+    .create(
+      <ScheduleFinderAccordion
+        route={route}
+        stops={stops}
+        directionId={0}
+        services={services}
+        serviceSchedules={serviceSchedules}
+      />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
