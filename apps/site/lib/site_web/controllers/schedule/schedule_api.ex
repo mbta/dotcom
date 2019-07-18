0d31909c3c77533002edb8afd0ef54e6ac61a9aa
Phil Darnowsky <pdarnowsky@mbta.com>
2019-07-18 12:14:09 -0400

    Rename var

--- apps/site/lib/site_web/controllers/schedule/schedule_api.ex
+++ apps/site/lib/site_web/controllers/schedule/schedule_api.ex
@@ -10,18 +10,18 @@ defmodule SiteWeb.ScheduleController.ScheduleApi do
 
   @spec get_schedules(binary, any, any) :: %{by_trip: map, trip_order: [any]}
   def get_schedules(route_id, date, direction_id) do
-    services =
+    schedules =
       Enum.map(
         Schedules.Repo.by_route_ids([route_id], date: date, direction_id: direction_id),
         &Map.update!(&1, :route, fn route -> Routes.Route.to_json_safe(route) end)
       )
 
-    services_by_trip =
-      services
+    schedules_by_trip =
+      schedules
       |> Enum.map(&Map.update!(&1, :time, fn time -> Timex.format!(time, "{0h12}:{m} {AM}") end))
       |> Enum.group_by(& &1.trip.id)
 
-    ordered_trips = services |> Enum.sort_by(& &1.time) |> Enum.map(& &1.trip.id) |> Enum.uniq()
-    %{by_trip: services_by_trip, trip_order: ordered_trips}
+    ordered_trips = schedules |> Enum.sort_by(& &1.time) |> Enum.map(& &1.trip.id) |> Enum.uniq()
+    %{by_trip: schedules_by_trip, trip_order: ordered_trips}
   end
 end
