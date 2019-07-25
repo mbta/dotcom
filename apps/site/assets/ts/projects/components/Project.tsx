90b5ba734e9cfaecef73d041f5006757b22052a4
Phil Darnowsky <pdarnowsky@mbta.com>
2019-07-25 15:26:12 -0400

    Remove unneeded fields from Project

--- apps/site/assets/ts/projects/components/Project.tsx
+++ apps/site/assets/ts/projects/components/Project.tsx
@@ -3,14 +3,11 @@ import { Route } from "./Route";
 
 export interface Project {
   date: string;
-  date_end: string;
   id: number;
   image: Image | null;
-  location: string | null;
   path: string;
   routes: Route[];
   status: string | null;
   text: string;
   title: string;
-  topic: string;
 }
