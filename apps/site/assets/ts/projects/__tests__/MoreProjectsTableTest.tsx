7177f062b1cc66e1c907317699cf68376d37a454
Phil Darnowsky <pdarnowsky@mbta.com>
2019-07-25 15:43:30 -0400

    Take snapshot of MoreProjectsTable

---
+++ apps/site/assets/ts/projects/__tests__/MoreProjectsTableTest.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { createReactRoot } from "../../app/helpers/testUtils";
+import { MoreProjectsTable } from "../components/MoreProjectsTable";
+import { Project } from "../components/Project";
+
+const projects: Project[] = [
+  {
+    date: '2018-06-02',
+    id: 1234,
+    image: { url: "http://www.example.com/some.gif", alt: "Photo of stuff" },
+    path: "http://www.mbta.com/awesome_project",
+    routes: [
+      { mode: "subway", id: "Red", group: "line" },
+      { mode: "bus", id: "16", group: "route" }
+    ],
+    status: null,
+    text: "This project will make everything perfect forever.",
+    title: "The Awesome Project"
+  }
+]
+
+it("renders", () => {
+  createReactRoot();
+  const tree = renderer.create(
+    <MoreProjectsTable projects={projects}></MoreProjectsTable>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+})
