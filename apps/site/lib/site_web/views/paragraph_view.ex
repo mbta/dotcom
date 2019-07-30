2e8c903f52e10f51a9af1fd7ef21b60e76833fb0
Adamo Maisano <amaisano@mbta.com>
2019-07-30 09:06:42 -0400

    adjust/add View modules for different sub-folders
    - breaks paragraph, page, and generic content ops into 3 view files

--- apps/site/lib/site_web/views/paragraph_view.ex
+++ apps/site/lib/site_web/views/paragraph_view.ex
@@ -1,20 +1,16 @@
-defmodule SiteWeb.ContentView do
+defmodule SiteWeb.CMS.ParagraphView do
   @moduledoc """
   Handles rendering of partial content from the CMS.
   """
   use SiteWeb, :view
 
-  import SiteWeb.TimeHelpers
-
-  alias Content.CMS
-  alias Content.Field.{File, Image, Link}
-  alias Content.Paragraph
-  alias Content.Paragraph.{Callout, ColumnMulti, ContentList, FareCard}
-  alias Content.Teaser
+  alias CMS.API
+  alias CMS.Field.{Image, Link}
+  alias CMS.Paragraph
+  alias CMS.Paragraph.{Callout, ColumnMulti, ContentList, DescriptiveLink, FareCard}
+  alias CMS.Teaser
   alias Plug.Conn
   alias Site.ContentRewriter
-
-  defdelegate fa_icon_for_file_type(mime), to: Site.FontAwesomeHelpers
 
   @doc "Map paragraph module names to their templates"
   for type <- Paragraph.get_types() do
@@ -46,10 +42,10 @@ defmodule SiteWeb.ContentView do
   Intelligently choose and render paragraph template based on type, except
   for certain types which either have no template or require special values.
   """
-  @spec render_content(Paragraph.t(), Conn.t()) :: Phoenix.HTML.safe()
-  def render_content(paragraph, conn)
+  @spec render_paragraph_content(Paragraph.t(), Conn.t()) :: Phoenix.HTML.safe()
+  def render_paragraph_content(page_or_paragraph, conn)
 
-  def render_content(%ColumnMulti{display_options: "grouped"} = paragraph, conn) do
+  def render_paragraph_content(%ColumnMulti{display_options: "grouped"} = paragraph, conn) do
     fare_cards =
       paragraph.columns
       |> nested_paragraphs()
@@ -65,7 +61,7 @@ defmodule SiteWeb.ContentView do
     )
   end
 
-  def render_content(%FareCard{} = paragraph, conn) do
+  def render_paragraph_content(%FareCard{} = paragraph, conn) do
     render(
       "_fare_card.html",
       fare_card: paragraph,
@@ -74,42 +70,11 @@ defmodule SiteWeb.ContentView do
     )
   end
 
-  def render_content(paragraph, conn) do
+  def render_paragraph_content(paragraph, conn) do
     paragraph
     |> get_template()
     |> render(content: paragraph, conn: conn)
   end
-
-  def file_description(%File{description: desc}) when not is_nil(desc) and desc != "" do
-    desc
-  end
-
-  def file_description(%File{url: url}) do
-    url |> Path.basename() |> URI.decode()
-  end
-
-  @doc "Nicely renders the duration of an event, given two DateTimes."
-  @spec render_duration(NaiveDateTime.t() | DateTime.t(), NaiveDateTime.t() | DateTime.t() | nil) ::
-          String.t()
-  def render_duration(start_time, end_time)
-
-  def render_duration(start_time, nil) do
-    start_time
-    |> maybe_shift_timezone
-    |> do_render_duration(nil)
-  end
-
-  def render_duration(start_time, end_time) do
-    start_time
-    |> maybe_shift_timezone
-    |> do_render_duration(maybe_shift_timezone(end_time))
-  end
-
-  @doc "Sets CMS content wrapper classes based on presence of sidebar elements {left, right}"
-  @spec sidebar_classes({boolean, boolean}) :: String.t()
-  def sidebar_classes({true, _}), do: "c-cms--with-sidebar c-cms--sidebar-left"
-  def sidebar_classes({false, true}), do: "c-cms--with-sidebar c-cms--sidebar-right"
-  def sidebar_classes({false, false}), do: "c-cms--no-sidebar"
 
   @spec grid(ColumnMulti.t()) :: integer
   def grid(%ColumnMulti{columns: columns}) do
@@ -135,7 +100,7 @@ defmodule SiteWeb.ContentView do
     end
   end
 
-  @spec list_cta?(CMS.type(), map(), [Teaser.t()], [String.t()]) :: boolean()
+  @spec list_cta?(API.type(), map(), [Teaser.t()], [String.t()]) :: boolean()
   # No results
   def list_cta?(_type, _cta, [], _path) do
     false
@@ -177,39 +142,6 @@ defmodule SiteWeb.ContentView do
     end
   end
 
-  defp maybe_shift_timezone(%NaiveDateTime{} = time) do
-    time
-  end
-
-  defp maybe_shift_timezone(%DateTime{} = time) do
-    Util.to_local_time(time)
-  end
-
-  defp do_render_duration(start_time, nil) do
-    "#{format_date(start_time)} at #{format_time(start_time)}"
-  end
-
-  defp do_render_duration(
-         %{year: year, month: month, day: day} = start_time,
-         %{year: year, month: month, day: day} = end_time
-       ) do
-    "#{format_date(start_time)} at #{format_time(start_time)} - #{format_time(end_time)}"
-  end
-
-  defp do_render_duration(start_time, end_time) do
-    "#{format_date(start_time)} #{format_time(start_time)} - #{format_date(end_time)} #{
-      format_time(end_time)
-    }"
-  end
-
-  defp format_time(%{minute: 0} = time) do
-    Timex.format!(time, "{h12} {AM}")
-  end
-
-  defp format_time(time) do
-    Timex.format!(time, "{h12}:{m} {AM}")
-  end
-
   defp nested_paragraphs(columns), do: columns |> Enum.flat_map(& &1.paragraphs)
 
   defp grouped_fare_card_data(paragraphs) when is_list(paragraphs) do
@@ -238,7 +170,7 @@ defmodule SiteWeb.ContentView do
   defp paragraph_classes(_), do: []
 
   # Automatically map each list to a destination page based on content type
-  @spec default_list_cta([CMS.type()], String.t()) :: Link.t()
+  @spec default_list_cta([API.type()], String.t()) :: Link.t()
   defp default_list_cta([:project_update], current_path) do
     %Link{
       title: "View all project updates",
@@ -268,7 +200,7 @@ defmodule SiteWeb.ContentView do
   end
 
   # Override one or both of the url/text values for the list CTA
-  @spec custom_list_cta([CMS.type()], map, String.t()) :: Link.t()
+  @spec custom_list_cta([API.type()], map, String.t()) :: Link.t()
   defp custom_list_cta(type, %{text: nil, url: url}, current_path) do
     type
     |> default_list_cta(current_path)
