188e4055de3aad9f65583c30c92879392a471a4b
Phil Darnowsky <pdarnowsky@mbta.com>
2019-08-12 12:25:17 -0400

    Redirect routes eliminated by Better Bus Project

--- apps/site/test/site_web/router_test.exs
+++ apps/site/test/site_web/router_test.exs
@@ -7,7 +7,7 @@ defmodule Phoenix.Router.RoutingTest do
       assert redirected_to(conn, 301) == "/stops/Yawkey"
     end
 
-    test "SL", %{conn: conn} do
+    test "SL buses", %{conn: conn} do
       conn = get(conn, "/schedules/SL1")
       assert redirected_to(conn, 301) == "/schedules/741"
 
@@ -37,13 +37,9 @@ defmodule Phoenix.Router.RoutingTest do
 
       conn = get(conn, "/schedules/sl5")
       assert redirected_to(conn, 301) == "/schedules/749"
+    end
 
-      conn = get(conn, "/schedules/CT1")
-      assert redirected_to(conn, 301) == "/schedules/701"
-
-      conn = get(conn, "/schedules/ct1")
-      assert redirected_to(conn, 301) == "/schedules/701"
-
+    test "CT buses", %{conn: conn} do
       conn = get(conn, "/schedules/CT2")
       assert redirected_to(conn, 301) == "/schedules/747"
 
@@ -55,6 +51,50 @@ defmodule Phoenix.Router.RoutingTest do
 
       conn = get(conn, "/schedules/ct3")
       assert redirected_to(conn, 301) == "/schedules/708"
+    end
+
+    test "routes eliminated by Better Bus Project", %{conn: conn} do
+      conn = get(conn, "/schedules/CT1")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/CT1/tab")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/ct1")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/ct1/tab")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/701")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/701/tab")
+      assert redirected_to(conn, 301) == "/betterbus-CT1"
+
+      conn = get(conn, "/schedules/5")
+      assert redirected_to(conn, 301) == "/betterbus-5"
+
+      conn = get(conn, "/schedules/5/tab")
+      assert redirected_to(conn, 301) == "/betterbus-5"
+
+      conn = get(conn, "/schedules/459")
+      assert redirected_to(conn, 301) == "/betterbus-455-459"
+
+      conn = get(conn, "/schedules/459/tab")
+      assert redirected_to(conn, 301) == "/betterbus-455-459"
+
+      conn = get(conn, "/schedules/448")
+      assert redirected_to(conn, 301) == "/betterbus-440s"
+
+      conn = get(conn, "/schedules/448/tab")
+      assert redirected_to(conn, 301) == "/betterbus-440s"
+
+      conn = get(conn, "/schedules/449")
+      assert redirected_to(conn, 301) == "/betterbus-440s"
+
+      conn = get(conn, "/schedules/449/tab")
+      assert redirected_to(conn, 301) == "/betterbus-440s"
     end
   end
 end
