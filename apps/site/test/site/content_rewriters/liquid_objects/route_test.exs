794a3c5b501e05a245353c95e0c013ca53853e1a
Adamo Maisano <amaisano@mbta.com>
2019-08-19 17:03:26 -0400

    support {{ route:x }} liquid replacements
    - creates a new replacement plugin for dealing with Route.t() requests

---
+++ apps/site/test/site/content_rewriters/liquid_objects/route_test.exs
@@ -0,0 +1,25 @@
+defmodule Site.ContentRewriters.LiquidObjects.RouteTest do
+  use ExUnit.Case, async: true
+
+  import Site.ContentRewriters.LiquidObjects.Route
+
+  alias Routes.{Repo, Route}
+
+  describe "route_request/1" do
+    test "it handles route requests for a valid/existing route ID" do
+      assert "83"
+             |> Repo.get()
+             |> Map.get(:long_name) == "Rindge Avenue - Central Square, Cambridge"
+
+      assert route_request("83") == {:ok, "Rindge Avenue - Central Square, Cambridge"}
+    end
+
+    test "it reports when there are no results (valid request, but no repo matches)" do
+      assert route_request("999") == {:error, {:unmatched, "no route match"}}
+    end
+
+    test "it handles a blank request (no filters provided)" do
+      assert route_request("") == {:error, {:empty, "no input"}}
+    end
+  end
+end
