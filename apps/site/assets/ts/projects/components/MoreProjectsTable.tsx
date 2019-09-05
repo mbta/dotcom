d4b179056912e03633fa7f60c1dae708a4a6e881
Phil Darnowsky <pdarnowsky@mbta.com>
2019-09-05 09:47:55 -0400

    Only render more-projects table if there are projects to show

--- apps/site/assets/ts/projects/components/MoreProjectsTable.tsx
+++ apps/site/assets/ts/projects/components/MoreProjectsTable.tsx
@@ -32,56 +32,62 @@ const MoreProjectsTable = ({
   state,
   setState,
   fetchMoreProjects
-}: Props): ReactElement<HTMLElement> => (
-  <div className="container">
-    <h2>{tableHeaderText(state)} Projects</h2>
-    <div>
-      <table className="c-more-projects-table" aria-label="More Projects">
-        <thead className="c-more-projects-table__thead hidden-md-down">
-          <tr>
-            <th
-              scope="col"
-              className="c-more-projects-table__th c-more-projects-table__th-project"
-            >
-              Project
-            </th>
-            <th
-              scope="col"
-              className="c-more-projects-table__th c-more-projects-table__th-last-updated"
-            >
-              Last Updated
-            </th>
-            <th
-              scope="col"
-              className="c-more-projects-table__th c-more-projects-table__th-status"
-            >
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody className="c-more-projects-table__tbody">
-          {state.projects.map(project => (
-            <MoreProjectsRow
-              key={project.id}
-              placeholderImageUrl={placeholderImageUrl}
-              {...project}
-            />
-          ))}
-        </tbody>
-      </table>
+}: Props): ReactElement<HTMLElement> | null => {
+  if (state.projects.length === 0) {
+    return null;
+  }
 
-      <div className="c-more-projects__show-more-button-wrapper">
-        <button
-          className="btn btn-secondary c-more-projects__show-more-button"
-          type="button"
-          disabled={state.fetchInProgress}
-          onClick={() => fetchMoreProjects(state, setState)}
-        >
-          Show more
-        </button>
+  return (
+    <div className="container">
+      <h2>{tableHeaderText(state)} Projects</h2>
+      <div>
+        <table className="c-more-projects-table" aria-label="More Projects">
+          <thead className="c-more-projects-table__thead hidden-md-down">
+            <tr>
+              <th
+                scope="col"
+                className="c-more-projects-table__th c-more-projects-table__th-project"
+              >
+                Project
+              </th>
+              <th
+                scope="col"
+                className="c-more-projects-table__th c-more-projects-table__th-last-updated"
+              >
+                Last Updated
+              </th>
+              <th
+                scope="col"
+                className="c-more-projects-table__th c-more-projects-table__th-status"
+              >
+                Status
+              </th>
+            </tr>
+          </thead>
+          <tbody className="c-more-projects-table__tbody">
+            {state.projects.map(project => (
+              <MoreProjectsRow
+                key={project.id}
+                placeholderImageUrl={placeholderImageUrl}
+                {...project}
+              />
+            ))}
+          </tbody>
+        </table>
+
+        <div className="c-more-projects__show-more-button-wrapper">
+          <button
+            className="btn btn-secondary c-more-projects__show-more-button"
+            type="button"
+            disabled={state.fetchInProgress}
+            onClick={() => fetchMoreProjects(state, setState)}
+          >
+            Show more
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MoreProjectsTable;
