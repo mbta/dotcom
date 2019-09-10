59767f1472f0d006ca2230226dd018c0b90f42fc
Phil Darnowsky <pdarnowsky@mbta.com>
2019-09-10 14:19:30 -0400

    Design tweaks

--- apps/site/assets/ts/projects/components/SubwayFilter.tsx
+++ apps/site/assets/ts/projects/components/SubwayFilter.tsx
@@ -30,7 +30,7 @@ const LineButton = ({
   const iconType = `${line}_line`;
   const firstLetter = line[0].toUpperCase();
   const rest = line.slice(1);
-  const name = `${firstLetter}${rest} Line`;
+  const name = `${firstLetter}${rest}`;
 
   return (
     <FilterButton
@@ -54,44 +54,50 @@ const SubwayFilter = ({
   }
 
   return (
-    <div className="subway-filter">
-      <div className="c-mode-filter__filter-btn-group">
-        <LineButton
-          line="red"
-          updateSelectedLine={updateSelectedLine}
-          state={state}
-          setState={setState}
-        />
-        <LineButton
-          line="orange"
-          updateSelectedLine={updateSelectedLine}
-          state={state}
-          setState={setState}
-        />
+    <>
+      <div className="subway-filter__top-hairline" />
+      <span className="m-tnm-sidebar__filter-header u-small-caps">
+        Subway Lines
+      </span>
+      <div className="subway-filter">
+        <div className="c-mode-filter__filter-btn-group">
+          <LineButton
+            line="red"
+            updateSelectedLine={updateSelectedLine}
+            state={state}
+            setState={setState}
+          />
+          <LineButton
+            line="orange"
+            updateSelectedLine={updateSelectedLine}
+            state={state}
+            setState={setState}
+          />
+        </div>
+        <div className="c-mode-filter__filter-btn-group">
+          <LineButton
+            line="blue"
+            updateSelectedLine={updateSelectedLine}
+            state={state}
+            setState={setState}
+          />
+          <LineButton
+            line="green"
+            updateSelectedLine={updateSelectedLine}
+            state={state}
+            setState={setState}
+          />
+        </div>
+        <div className="c-mode-filter__filter-btn-group">
+          <LineButton
+            line="mattapan"
+            updateSelectedLine={updateSelectedLine}
+            state={state}
+            setState={setState}
+          />
+        </div>
       </div>
-      <div className="c-mode-filter__filter-btn-group">
-        <LineButton
-          line="blue"
-          updateSelectedLine={updateSelectedLine}
-          state={state}
-          setState={setState}
-        />
-        <LineButton
-          line="green"
-          updateSelectedLine={updateSelectedLine}
-          state={state}
-          setState={setState}
-        />
-      </div>
-      <div className="c-mode-filter__filter-btn-group">
-        <LineButton
-          line="mattapan"
-          updateSelectedLine={updateSelectedLine}
-          state={state}
-          setState={setState}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
