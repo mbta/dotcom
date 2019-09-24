e4fcb5def3b8c2715010aa0f88b66e210461005f
Adamo Maisano <amaisano@mbta.com>
2019-09-24 16:18:39 -0400

    correction: field may not be present
    - depending on the CMS field instance used, not all images will have caption data. use the typical CMS field parser and pass through NIL if not present

--- apps/cms/lib/field/image.ex
+++ apps/cms/lib/field/image.ex
@@ -5,7 +5,12 @@ defmodule CMS.Field.Image do
   """
   alias Phoenix.HTML
 
-  import CMS.Helpers, only: [handle_html: 1, rewrite_url: 1]
+  import CMS.Helpers,
+    only: [
+      field_value: 2,
+      handle_html: 1,
+      rewrite_url: 1
+    ]
 
   defstruct url: "",
             alt: "",
@@ -18,11 +23,15 @@ defmodule CMS.Field.Image do
         }
 
   @spec from_api(map) :: t
-  def from_api(%{"alt" => alt, "url" => url, "caption" => caption}) do
+  def from_api(%{"alt" => alt, "url" => url} = data) do
     %__MODULE__{
       url: rewrite_url(url),
       alt: alt,
-      caption: caption && handle_html(caption)
+      caption: data |> field_value("field_image_caption") |> handle_caption()
     }
   end
+
+  @spec handle_caption(String.t() | nil) :: HTML.safe() | nil
+  defp handle_caption(nil), do: nil
+  defp handle_caption(caption), do: handle_html(caption)
 end
