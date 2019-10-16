dc4114b78b88661fbb5275269dc642f908052f90
Adamo Maisano <amaisano@mbta.com>
2019-10-16 11:19:16 -0400

    include alerts in js tests
    should only render one of the two alerts (high priority)

--- apps/site/assets/ts/schedule/__tests__/LineDiagramTest.tsx
+++ apps/site/assets/ts/schedule/__tests__/LineDiagramTest.tsx
@@ -249,6 +249,252 @@ export const lineDiagram = [
       branch: null
     },
     alerts: []
+  },
+  {
+    stop_data: [{ type: "terminus", branch: null }],
+    route_stop: {
+      zone: null,
+      stop_features: ["bus", "access", "parking_lot"],
+      station_info: {
+        type: "station",
+        "station?": true,
+        platform_name: null,
+        platform_code: null,
+        parking_lots: [
+          {
+            utilization: null,
+            payment: {
+              monthly_rate: "Monthly passes are not available at MBTA garages.",
+              mobile_app: null,
+              methods: ["Tap Card", "Credit/Debit Card", "Cash"],
+              daily_rate:
+                "Mon-Fri: $9 | Sat-Sun: $3 (Prices are valid for up to 14 hours of parking. After the first 14 hours, the daily rate is $15.)"
+            },
+            note: null,
+            name: "Alewife Garage",
+            manager: {
+              url: "http://www.parkmbta.com/",
+              phone: "617-222-3200",
+              name: "Republic Parking System",
+              contact: "MBTA Customer Support"
+            },
+            longitude: -71.142483,
+            latitude: 42.395428,
+            capacity: {
+              type: "Garage",
+              total: 2471,
+              overnight: "Available",
+              accessible: 32
+            },
+            address: null
+          }
+        ],
+        parent_id: null,
+        note: null,
+        name: "Alewife",
+        longitude: -71.142483,
+        latitude: 42.395428,
+        "is_child?": false,
+        id: "place-alfcl",
+        "has_fare_machine?": true,
+        "has_charlie_card_vendor?": true,
+        fare_facilities: ["fare_media_assistant", "fare_vending_machine"],
+        description: null,
+        closed_stop_info: null,
+        child_ids: [
+          "141",
+          "70061",
+          "Alewife-01",
+          "Alewife-02",
+          "door-alfcl-alewife",
+          "door-alfcl-busway",
+          "door-alfcl-cambridgepark",
+          "door-alfcl-russell",
+          "door-alfcl-steel"
+        ],
+        bike_storage: ["bike_storage_cage"],
+        address:
+          "Alewife Brook Pkwy and Cambridge Park Dr, Cambridge, MA 02140",
+        accessibility: ["accessible", "escalator_both", "elevator", "ramp"]
+      },
+      route: {
+        type: 1,
+        name: "Red Line",
+        long_name: "Red Line",
+        id: "Red",
+        direction_names: { "0": "Southbound", "1": "Northbound" },
+        direction_destinations: { "0": "Ashmont/Braintree", "1": "Alewife" },
+        description: "rapid_transit",
+        "custom_route?": false
+      },
+      name: "Alewife",
+      "is_terminus?": true,
+      "is_beginning?": true,
+      id: "place-alfcl",
+      connections: [
+        {
+          type: 3,
+          name: "62",
+          long_name: "Bedford VA Hospital - Alewife",
+          id: "62",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: {
+            "0": "Bedford VA Hospital",
+            "1": "Alewife"
+          },
+          description: "local_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "67",
+          long_name: "Turkey Hill - Alewife",
+          id: "67",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: { "0": "Turkey Hill", "1": "Alewife" },
+          description: "local_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "76",
+          long_name: "Lincoln Lab/Hanscom Air Force Base - Alewife",
+          id: "76",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: {
+            "0": "Lincoln Lab/Hanscom Air Force Base",
+            "1": "Alewife"
+          },
+          description: "local_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "79",
+          long_name: "Arlington Heights - Alewife",
+          id: "79",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: { "0": "Arlington Heights", "1": "Alewife" },
+          description: "local_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "84",
+          long_name: "Arlmont Village - Alewife",
+          id: "84",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: { "0": "Arlmont Village", "1": "Alewife" },
+          description: "commuter_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "350",
+          long_name: "North Burlington - Alewife",
+          id: "350",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: { "0": "North Burlington", "1": "Alewife" },
+          description: "local_bus",
+          "custom_route?": false
+        },
+        {
+          type: 3,
+          name: "351",
+          long_name: "Oak Park/Bedford Woods - Alewife",
+          id: "351",
+          direction_names: { "0": "Outbound", "1": "Inbound" },
+          direction_destinations: {
+            "0": "Oak Park/Bedford Woods",
+            "1": "Alewife"
+          },
+          description: "commuter_bus",
+          "custom_route?": false
+        }
+      ],
+      closed_stop_info: null,
+      branch: null
+    },
+    alerts: [
+      {
+        updated_at: "Updated: 10/2/2019 06:58P",
+        severity: 1,
+        priority: "low",
+        lifecycle: "upcoming",
+        informed_entity: {
+          trip: [null],
+          stop: ["70061", "place-alfcl"],
+          route_type: [1],
+          route: ["Red"],
+          entities: [
+            {
+              trip: null,
+              stop: "70061",
+              route_type: 1,
+              route: "Red",
+              direction_id: null,
+              activities: ["board"]
+            },
+            {
+              trip: null,
+              stop: "place-alfcl",
+              route_type: 1,
+              route: "Red",
+              direction_id: null,
+              activities: ["board"]
+            }
+          ],
+          direction_id: [null],
+          activities: ["board"]
+        },
+        id: "335740",
+        header:
+          "On Saturday, October 19, the main entrance to the Alewife parking garage will close and temporarily relocate for repairs and upgrades. Access to the station will be modified during the closure.",
+        effect: "station_issue",
+        description:
+          "Signs will be placed around the facility to direct motorists to the temporary entrance.",
+        active_period: [["2019-10-19 4:30", null]]
+      },
+      {
+        updated_at: "Updated: 10/2/2019 06:58P",
+        severity: 1,
+        priority: "high",
+        lifecycle: "ongoing",
+        informed_entity: {
+          trip: [null],
+          stop: ["70061", "place-alfcl"],
+          route_type: [1],
+          route: ["Red"],
+          entities: [
+            {
+              trip: null,
+              stop: "70061",
+              route_type: 1,
+              route: "Red",
+              direction_id: null,
+              activities: ["board"]
+            },
+            {
+              trip: null,
+              stop: "place-alfcl",
+              route_type: 1,
+              route: "Red",
+              direction_id: null,
+              activities: ["board"]
+            }
+          ],
+          direction_id: [null],
+          activities: ["board"]
+        },
+        id: "335741",
+        header:
+          "On Saturday, October 19, the main entrance to the Alewife parking garage will close and temporarily relocate for repairs and upgrades. Access to the station will be modified during the closure.",
+        effect: "station_issue",
+        description:
+          "Signs will be placed around the facility to direct motorists to the temporary entrance.",
+        active_period: [["2019-10-19 4:30", null]]
+      }
+    ]
   }
 ] as LineDiagramStop[];
 
