eef921a650741fdf2fc1ac5a4ae227c8e04c9760
Phil Darnowsky <pdarnowsky@mbta.com>
2019-10-22 10:31:22 -0400

    make tslint pass

--- apps/site/assets/ts/schedule/components/LineDiagram.tsx
+++ apps/site/assets/ts/schedule/components/LineDiagram.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { LineDiagramStop, RouteStopRoute, StopData } from "./__schedule";
+import { LineDiagramStop, RouteStopRoute } from "./__schedule";
 import {
   alertIcon,
   modeIcon,
@@ -16,17 +16,17 @@ const filteredConnections = (
   connections: RouteStopRoute[]
 ): RouteStopRoute[] => {
   const firstCRIndex = connections.findIndex(
-    connection => connection.type == 2
+    connection => connection.type === 2
   );
   const firstFerryIndex = connections.findIndex(
-    connection => connection.type == 4
+    connection => connection.type === 4
   );
 
   return connections.filter(
     (connection, index) =>
-      (connection.type != 2 && connection.type != 4) ||
-      (connection.type == 2 && index == firstCRIndex) ||
-      (connection.type == 4 && index == firstFerryIndex)
+      (connection.type !== 2 && connection.type !== 4) ||
+      (connection.type === 2 && index === firstCRIndex) ||
+      (connection.type === 4 && index === firstFerryIndex)
   );
 };
 
@@ -42,9 +42,6 @@ const maybeAlert = (alerts: Alert[]): ReactElement<HTMLElement> | null => {
   );
 };
 
-const maybeTerminus = (stopData: StopData[]): StopData | undefined =>
-  stopData.find((stop: StopData) => stop.type === "terminus");
-
 const busBackgroundClass = (connection: Route): string =>
   connection.name.startsWith("SL") ? "u-bg--silver-line" : "u-bg--bus";
 
@@ -53,26 +50,21 @@ const LineDiagram = ({
 }: Props): ReactElement<HTMLElement> | null => (
   <>
     {lineDiagram.map(
-      ({
-        route_stop: routeStop,
-        stop_data: stopData,
-        alerts: stopAlerts
-      }: LineDiagramStop) => (
+      ({ route_stop: routeStop, alerts: stopAlerts }: LineDiagramStop) => (
         <div key={routeStop.id} className="m-schedule-line-diagram__stop">
           <div className="m-schedule-line-diagram__card-left">
             <div className="m-schedule-line-diagram__stop-name">
               {maybeAlert(stopAlerts)}
-              <a href={"/stops/" + routeStop.id}>{routeStop.name}</a>
+              <a href={`/stops/${routeStop.id}`}>{routeStop.name}</a>
             </div>
             <div className="m-schedule-line-diagram__connections">
               {filteredConnections(routeStop.connections).map((route: Route) =>
                 route.type === 3 ? (
                   <span
                     key={route.id}
-                    className={
-                      "c-icon__bus-pill m-schedule-line-diagram__connection " +
-                      busBackgroundClass(route)
-                    }
+                    className={`c-icon__bus-pill m-schedule-line-diagram__connection ${busBackgroundClass(
+                      route
+                    )}`}
                   >
                     {route.name}
                   </span>
