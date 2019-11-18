60e4400811b535ac2b11d7c8e2abbce8c940a7b9
Adamo Maisano <amaisano@mbta.com>
2019-11-18 18:18:07 -0500

    fixup! adjust stacking breakpoint, add row wrapper
    - Boostrap demands a row wrapper
    - Keep child columns together in .eex template

--- apps/site/assets/ts/shuttles/components/ShuttlesPage.tsx
+++ apps/site/assets/ts/shuttles/components/ShuttlesPage.tsx
@@ -1,7 +1,5 @@
 import React, { ReactElement } from "react";
 
-const ShuttlesPage = (): ReactElement<HTMLElement> => (
-  <div className="shuttles__main col-xs-10 col-xs-offset-1 col-sm-offset-0 col-sm-12 col-lg-7" />
-);
+const ShuttlesPage = (): ReactElement<HTMLElement> => <div />;
 
 export default ShuttlesPage;
