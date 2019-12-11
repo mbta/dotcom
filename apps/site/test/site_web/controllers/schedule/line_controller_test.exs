373a561f3bf58f3cadd1004c7ca6c8561efecd85
Phil Darnowsky <pdarnowsky@mbta.com>
2019-12-11 14:05:35 -0500

    Omit no-school services
    Due to the way we already call out stops that are skipped on non-school
    days in the regular weekday service, it's redundant to show the
    no-school services in the service picker as well; leave them out.

--- apps/site/test/site_web/controllers/schedule/line_controller_test.exs
+++ apps/site/test/site_web/controllers/schedule/line_controller_test.exs
@@ -34,5 +34,32 @@ defmodule SiteWeb.Schedule.LineControllerTest do
       assert length(services) == 2
       refute Enum.member?(services, past_service)
     end
+
+    test "omits no-school weekday services" do
+      service_date = ~D[2019-05-01]
+
+      school_service = %Service{
+        start_date: ~D[2019-12-11],
+        end_date: ~D[2019-12-11],
+        name: "Weekday"
+      }
+
+      no_school_service = %Service{
+        start_date: ~D[2019-12-12],
+        end_date: ~D[2019-12-12],
+        name: "Weekday (no school)"
+      }
+
+      repo_fn = fn _ ->
+        [
+          school_service,
+          no_school_service
+        ]
+      end
+
+      services = LineController.services("1", service_date, repo_fn)
+      assert length(services) == 1
+      refute Enum.member?(services, no_school_service)
+    end
   end
 end
