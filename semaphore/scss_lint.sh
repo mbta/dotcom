16812f7a3a853b2acb891da2162526816d2695fd
Cora Grant <394835+x@users.noreply.github.com>
2020-01-06 17:45:22 -0500

    Fix linter false positives on CI
    Since we were diffing the branch against the current tip of master,
    rather than the point where it diverged from master, linting would flag
    issues that had actually been resolved in master.

--- semaphore/scss_lint.sh
+++ semaphore/scss_lint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-changed=$(git diff --name-only origin/master apps/site/assets/css)
+fork_point=$(git merge-base --fork-point origin/master)
+changed=$(git diff --name-only $fork_point "*.ex" "*.exs")
 
 if [ -z "$changed" ]; then
-  echo "No CSS files changed from origin/master."
+  echo "No files changed relative to origin/master fork point."
 else
   rbenv local 2.4.1
   gem install scss_lint -v 0.59.0
