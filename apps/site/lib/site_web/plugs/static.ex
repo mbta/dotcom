54aa58901e7e8ff3b1569b5d24731352258b79df
Phil Darnowsky <pdarnowsky@mbta.com>
2020-02-12 15:23:56 -0500

    Add Loader.io token for dev-green

--- apps/site/lib/site_web/plugs/static.ex
+++ apps/site/lib/site_web/plugs/static.ex
@@ -12,7 +12,7 @@ defmodule SiteWeb.Plugs.Static do
     headers: %{"access-control-allow-origin" => "*"},
     cache_control_for_etags: "public, max-age=86400",
     only:
-      ~w(css fonts images js robots.txt google778e4cfd8ca77f44.html loaderio-b4c35b9431db61b4421fcf03e17c3818),
+      ~w(css fonts images js robots.txt google778e4cfd8ca77f44.html loaderio-b4c35b9431db61b4421fcf03e17c3818 loaderio-392aa6a67825336b70393c6be61d56ce),
     only_matching: ~w(favicon apple-touch-icon)
   )
 
