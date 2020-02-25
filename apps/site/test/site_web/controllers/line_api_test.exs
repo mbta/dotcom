3169e9c4547dbd7ea8f262d3fe89f9193408bfca
Phil Darnowsky <pdarnowsky@mbta.com>
2020-02-25 17:09:41 -0500

    Revert "fix: disable the realtime LineAPI"
    This reverts commit 5ff14ffd21bb251d5c8691d866c1655de97ad78f. It also
    moves the endpoint to a different route. This is so that any browsers
    out there that still have the polling code will not be hitting this
    endpoint.

--- apps/site/test/site_web/controllers/line_api_test.exs
+++ apps/site/test/site_web/controllers/line_api_test.exs
@@ -9,11 +9,15 @@ defmodule SiteWeb.LineApiTest do
     end
   end
 
-  describe "realtime" do
+  describe "predictions_and_vehicles" do
     test "success response", %{conn: conn} do
-      conn = get(conn, line_api_path(conn, :realtime, %{"id" => "Red", "direction_id" => "1"}))
+      conn =
+        get(
+          conn,
+          line_api_path(conn, :predictions_and_vehicles, %{"id" => "Red", "direction_id" => "1"})
+        )
 
-      assert %{} = json_response(conn, 200)
+      assert %{"place-brdwy" => %{"headsigns" => _, "vehicles" => _}} = json_response(conn, 200)
     end
   end
 end
