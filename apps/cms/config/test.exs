b74ad730b7b7dc9bb0e9fb967d1d03266fac6a64
Phil Darnowsky <pdarnowsky@mbta.com>
2020-02-27 11:50:15 -0500

    Remove dead email test code
    This code was discovered while searching for uses of Mailgun in the
    codebase, and is not used anymore.

--- apps/cms/config/test.exs
+++ apps/cms/config/test.exs
@@ -5,9 +5,3 @@ config :cms, :drupal,
   static_path: "/sites/default/files"
 
 config :cms, :cms_api, CMS.API.Static
-
-config :cms, :mailgun,
-  domain: "https://test-domain.com",
-  key: "key-test",
-  mode: :test,
-  test_file_path: "/tmp/mailgun.json"
