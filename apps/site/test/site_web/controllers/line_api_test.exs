029a8daa8ad0c086f1e96e3f7129bf7faeb926b8
Phil Darnowsky <pdarnowsky@mbta.com>
2020-03-05 15:57:21 -0500

    Adjust one test to pass

--- apps/site/test/site_web/controllers/line_api_test.exs
+++ apps/site/test/site_web/controllers/line_api_test.exs
@@ -14,7 +14,7 @@ defmodule SiteWeb.LineApiTest do
       conn =
         get(
           conn,
-          line_api_path(conn, :realtime, %{"id" => "Red", "direction_id" => "1"})
+          line_api_path(conn, :realtime, %{"id" => "Red", "direction_id" => "1", "v" => "2"})
         )
 
       assert %{"place-brdwy" => %{"headsigns" => _, "vehicles" => _}} = json_response(conn, 200)
