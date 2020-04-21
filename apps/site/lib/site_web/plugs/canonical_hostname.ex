3d479fcba1aa86dfe6d35bd43ddf8c8c3de7c5e3
Phil Darnowsky <pdarnowsky@mbta.com>
2020-04-21 12:06:10 -0400

    Consider private IPs to be canonical hostnames
    The health checker runs on the same hosts as the application servers and
    uses a private IP address to access the /_health endpoint. Whitelist local
    IP addresses as well.

--- apps/site/lib/site_web/plugs/canonical_hostname.ex
+++ apps/site/lib/site_web/plugs/canonical_hostname.ex
@@ -23,7 +23,9 @@ defmodule SiteWeb.Plugs.CanonicalHostname do
   def call(%Plug.Conn{host: requested_hostname} = conn, _) do
     canonical_hostname = SiteWeb.Endpoint.config(:url)[:host]
 
-    if requested_hostname != canonical_hostname do
+    if requested_hostname == canonical_hostname or is_private_ip(requested_hostname) do
+      conn
+    else
       rewritten_url =
         Plug.Conn.request_url(conn)
         |> String.replace(requested_hostname, canonical_hostname, global: false)
@@ -32,8 +34,18 @@ defmodule SiteWeb.Plugs.CanonicalHostname do
       |> put_status(:moved_permanently)
       |> redirect(external: rewritten_url)
       |> halt()
-    else
-      conn
+    end
+  end
+
+  # The health checker uses a private IP for a hostname
+  defp is_private_ip(hostname) do
+    case hostname |> String.to_charlist() |> :inet.parse_ipv4_address() do
+      {:ok, {byte0, byte1, _, _}} ->
+        byte0 == 10 or (byte0 == 172 and byte1 >= 16 and byte1 <= 31) or
+          (byte0 == 192 and byte1 == 168)
+
+      _ ->
+        false
     end
   end
 end
