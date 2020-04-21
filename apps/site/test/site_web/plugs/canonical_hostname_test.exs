26ceb681af1c5270f5dade278a03bd48c91deeb0
Phil Darnowsky <pdarnowsky@mbta.com>
2020-04-21 12:06:10 -0400

    Consider private IPs to be canonical hostnames
    The health checker runs on the same hosts as the application servers and
    uses a private IP address to access the /_health endpoint. Whitelist local
    IP addresses as well.

--- apps/site/test/site_web/plugs/canonical_hostname_test.exs
+++ apps/site/test/site_web/plugs/canonical_hostname_test.exs
@@ -11,6 +11,26 @@ defmodule SiteWeb.Plugs.CanonicalHostnameTest do
       assert conn.status != 301
     end
 
+    test "with a local IP address, does nothing" do
+      # Class A
+      conn = %Plug.Conn{default_conn() | host: "10.127.127.127"}
+      assert conn.status != 301
+      conn = SiteWeb.Plugs.CanonicalHostname.call(conn, nil)
+      assert conn.status != 301
+
+      # Class B
+      conn = %Plug.Conn{default_conn() | host: "172.24.127.127"}
+      assert conn.status != 301
+      conn = SiteWeb.Plugs.CanonicalHostname.call(conn, nil)
+      assert conn.status != 301
+
+      # Class C
+      conn = %Plug.Conn{default_conn() | host: "192.168.127.127"}
+      assert conn.status != 301
+      conn = SiteWeb.Plugs.CanonicalHostname.call(conn, nil)
+      assert conn.status != 301
+    end
+
     test "when the hostname doesn't match the canonical hostname, redirects" do
       conn = %Plug.Conn{default_conn() | host: "example.com"}
       assert conn.status != 301
