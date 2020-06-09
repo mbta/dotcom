c817bd942eeda0984fd070145d466401b0060ed4
Cristen Jones <cjones3@mbta.com>
2020-06-09 13:06:11 -0400

    Add <CrowdingPill /> component and styles

---
+++ apps/site/assets/ts/schedule/components/line-diagram/CrowdingPill.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { CrowdingType } from "../../components/__schedule";
+import { crowdingIcon } from "../../../helpers/icon";
+
+const crowdingDescriptions = {
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  not_crowded: "Not crowded",
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  some_crowding: "Some crowding",
+  crowded: "Crowded"
+};
+
+const CrowdingPill = ({
+  crowding
+}: {
+  crowding: CrowdingType;
+}): JSX.Element | null => {
+  if (!crowding) return null;
+
+  return (
+    <div className={`c-crowding-pill c-crowding-pill--${crowding}`}>
+      {crowdingIcon(`c-icon__crowding--${crowding}`)}
+      <span className="c-crowding-pill__label">
+        {crowdingDescriptions[crowding]}
+      </span>
+    </div>
+  );
+};
+
+export default CrowdingPill;
