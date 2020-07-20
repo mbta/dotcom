f7ddcb211229201f7dc66d62422d326f8a9d336a
Cristen Jones <cjones3@mbta.com>
2020-07-20 10:39:34 -0400

    Fix alert priority for TNM to match other areas

--- apps/site/lib/site_web/views/mode_view.ex
+++ apps/site/lib/site_web/views/mode_view.ex
@@ -144,7 +144,8 @@ defmodule SiteWeb.ModeView do
     entity = %Alerts.InformedEntity{route_type: route.type, route: route.id}
 
     Enum.any?(alerts, fn alert ->
-      alert.priority == :high and Match.match([alert], entity, date) == [alert]
+      Alerts.Alert.is_high_severity_or_high_priority(alert) and
+        Match.match([alert], entity, date) == [alert]
     end)
   end
 
