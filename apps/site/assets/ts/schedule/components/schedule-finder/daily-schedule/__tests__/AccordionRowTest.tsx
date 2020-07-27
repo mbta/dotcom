d42636b37e8875a65b3859293992e0658244cd37
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-07-27 18:34:46 -0400

    Fix test data for AccordionRowTest

--- apps/site/assets/ts/schedule/components/schedule-finder/daily-schedule/__tests__/AccordionRowTest.tsx
+++ apps/site/assets/ts/schedule/components/schedule-finder/daily-schedule/__tests__/AccordionRowTest.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { mount, ReactWrapper } from "enzyme";
 import { DirectionId, RouteType } from "../../../../../__v3api";
 import * as KeyboardEvents from "../../../../../helpers/keyboard-events";
-import { TripInfo } from "../../../__trips";
+import { TripInfo, Journey } from "../../../__trips";
 import tripData from "../../__tests__/test-data/tripInfo.json";
 import AccordionRow from "../AccordionRow";
 import { FetchState, FetchStatus } from "../../../../../helpers/use-fetch";
@@ -14,81 +14,44 @@ jest.mock("../../../../../helpers/keyboard-events", () => ({
   handleReactEnterKeyPress: jest.fn()
 }));
 
-const journey = {
+const journey: Journey = {
   trip: {
     shape_id: "010070",
     route_pattern_id: "1-_-0",
     name: "",
-    id: "41894293",
+    id: "45030860",
     headsign: "Harvard",
-    direction_id: 0 as DirectionId,
+    direction_id: 0,
     "bikes_allowed?": true
   },
   route: {
-    type: 3 as RouteType,
+    type: 3,
+    sort_order: 50010,
     name: "1",
-    long_name: "Harvard - Dudley via Massachusetts Avenue",
+    long_name: "Harvard Square - Nubian Station",
     id: "1",
     direction_names: {
       "0": "Outbound",
       "1": "Inbound"
     },
     direction_destinations: {
-      "0": "Harvard",
-      "1": "Dudley"
+      "0": "Harvard Square",
+      "1": "Nubian Station"
     },
     description: "key_bus_route",
-    "custom_route?": false,
     color: "FFC72C"
   },
-  realtime: {
-    scheduled_time: ["1:47", " ", "PM"],
-    prediction: {
-      track: null,
-      time: ["8", " ", "min"],
-      status: null,
-      seconds: 491
-    },
-    delay: 7
-  },
   departure: {
-    time: "01:47 PM",
+    time: "04:54 AM",
     schedule: {
-      time: "2019-12-04T13:47:00-05:00",
-      stop_sequence: 17,
-      stop: null,
+      time: "2020-08-14T04:54:00-04:00",
+      stop_sequence: 19,
       pickup_type: 0,
       "last_stop?": false,
       "flag?": false,
       "early_departure?": true
     },
-    prediction: {
-      track: null,
-      time: "2019-12-04T13:54:17-05:00",
-      stop_sequence: 17,
-      stop: null,
-      status: null,
-      schedule_relationship: null,
-      id: "prediction-41894293-99-17",
-      direction_id: 0 as DirectionId,
-      "departing?": true,
-      delay: 5
-    }
-  },
-  tripInfo: {
-    route_type: 3 as RouteType,
-    vehicle_stop_name: "",
-    vehicle: null,
-    times: [],
-    stop_count: 25,
-    status: "operating at normal schedule",
-    origin_id: "place-dudly",
-    fare: {
-      fare_link: "/fares/bus-fares",
-      price: "$1.70"
-    },
-    duration: 21,
-    destination_id: "110"
+    prediction: null
   }
 };
 
