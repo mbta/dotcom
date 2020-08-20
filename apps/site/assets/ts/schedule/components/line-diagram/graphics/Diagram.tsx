109c7b5bfd4d5a1c21f587ea7e7f484f9446f506
Cristen Jones <cjones3@mbta.com>
2020-08-20 12:31:00 -0400

    Add title and description to the SVG

--- apps/site/assets/ts/schedule/components/line-diagram/graphics/Diagram.tsx
+++ apps/site/assets/ts/schedule/components/line-diagram/graphics/Diagram.tsx
@@ -5,7 +5,8 @@ import {
   hasBranchLines,
   isBranchTerminusStop,
   areOnDifferentBranchLines,
-  diagramWidth
+  diagramWidth,
+  isMergeStop
 } from "../line-diagram-helpers";
 import Line from "./Line";
 import Merges from "./Merge";
@@ -13,14 +14,46 @@ import Stop from "./Stop";
 import { routeToModeName } from "../../../../helpers/css";
 import { LiveDataByStop } from "../__line-diagram";
 import VehicleIcons from "../VehicleIcons";
+import { getCurrentState } from "../../../store/ScheduleStore";
+import { DirectionId } from "../../../../__v3api";
+import { isAGreenLineRoute } from "../../../../models/route";
 
 interface DiagramProps {
   lineDiagram: LineDiagramStop[];
   liveData: LiveDataByStop | null;
 }
 
+const branchingDescription = (lineDiagram: LineDiagramStop[]): string => {
+  const mergeStops = lineDiagram.filter(isMergeStop);
+  const branchText = mergeStops.map(mergeStop => {
+    const mergeStopName = mergeStop.route_stop.name;
+    const branchNames = mergeStop.stop_data
+      .filter(sd => sd.branch)
+      .map(sd => sd.branch);
+    return `Use ${mergeStopName} to transfer to trains on ${branchNames.join(
+      " or "
+    )} branches. `;
+  });
+  return branchText.join("");
+};
+const diagramDescription = (
+  lineDiagram: LineDiagramStop[],
+  direction: DirectionId
+): string => {
+  const text = `${lineDiagram.length} stops`;
+  const {
+    direction_destinations: destinations,
+    direction_names: names
+  } = lineDiagram[0].route_stop.route!;
+  if (destinations) {
+    return `${text} going ${names[direction]} to ${destinations[direction]}`;
+  }
+  return text;
+};
+
 const Diagram = (props: DiagramProps): ReactElement<HTMLElement> | null => {
   const { lineDiagram, liveData } = props;
+  const { selectedDirection } = getCurrentState();
   const width = diagramWidth(
     max(lineDiagram.map(ld => ld.stop_data.length)) || 1
   );
@@ -52,12 +85,25 @@ const Diagram = (props: DiagramProps): ReactElement<HTMLElement> | null => {
       {vehicleIcons}
       <svg
         xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-labelledby="diagram-title diagram-desc"
         className={`line-diagram-svg ${routeToModeName(
           lineDiagram[0].route_stop.route!
         )}`}
         width={`${width + 4}px`}
         height="100%"
       >
+        <title id="diagram-title">
+          Line diagram for{" "}
+          {isAGreenLineRoute(lineDiagram[0].route_stop.route!)
+            ? "Green Line"
+            : lineDiagram[0].route_stop.route!.name}
+        </title>
+        <desc id="diagram-desc">
+          {diagramDescription(lineDiagram, selectedDirection)}
+          {hasBranchLines(lineDiagram) && branchingDescription(lineDiagram)}
+        </desc>
+
         {/* If there are multiple branches, draw the lines and curves for those */
         hasBranchLines(lineDiagram) && <Merges lineDiagram={lineDiagram} />}
 
