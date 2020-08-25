a6af53e684baf156e4556029a161b8e19caa2f45
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-08-25 16:19:55 -0400

    Describe the functions being tested

--- apps/zones/test/repo_test.exs
+++ apps/zones/test/repo_test.exs
@@ -2,20 +2,24 @@ defmodule Zones.RepoTest do
   use ExUnit.Case, async: true
   alias Zones.Repo
 
-  test "it finds the zone of a commuter rail stop" do
-    assert Repo.get("South Acton") == "6"
+  describe "get/1" do
+    test "it finds the zone of a commuter rail stop" do
+      assert Repo.get("South Acton") == "6"
+    end
+
+    test "it finds the zone of a zone 1a stop" do
+      assert Repo.get("place-sstat") == "1A"
+    end
+
+    test "it finds the zone of a 'combo' zone stop (e.g. 1A-1 or 1A-2)" do
+      assert Repo.get("place-qnctr") == "1A-1"
+      assert Repo.get("place-ER-0115") == "1A-2"
+    end
   end
 
-  test "it finds the zone of a zone 1a stop" do
-    assert Repo.get("place-sstat") == "1A"
-  end
-
-  test "it finds the zone of a 'combo' zone stop (e.g. 1A-1 or 1A-2)" do
-    assert Repo.get("place-qnctr") == "1A-1"
-    assert Repo.get("place-ER-0115") == "1A-2"
-  end
-
-  test "it finds the zones for all stops" do
-    assert length(Map.keys(Repo.all())) == length(Map.values(Repo.all()))
+  describe "all/0" do
+    test "it finds the zones for all stops" do
+      assert length(Map.keys(Repo.all())) == length(Map.values(Repo.all()))
+    end
   end
 end
