52ffc699498143f4ee1a8f2873baca5826d96149
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-08-25 16:14:14 -0400

    Set combo zones
    Update Quincy Center to be zone 1A-1.
    
    Update Lynn to be zone 1A-2.
    
    Update River Works to be zone 1A-2.

--- apps/zones/test/repo_test.exs
+++ apps/zones/test/repo_test.exs
@@ -10,6 +10,11 @@ defmodule Zones.RepoTest do
     assert Repo.get("place-sstat") == "1A"
   end
 
+  test "it finds the zone of a 'combo' zone stop (e.g. 1A-1 or 1A-2)" do
+    assert Repo.get("place-qnctr") == "1A-1"
+    assert Repo.get("place-ER-0115") == "1A-2"
+  end
+
   test "it finds the zones for all stops" do
     assert length(Map.keys(Repo.all())) == length(Map.values(Repo.all()))
   end
