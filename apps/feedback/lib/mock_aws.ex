337d2994b276907873c14e9a159d9291bfc07971
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-09-09 15:21:44 -0400

    Add a mock version of ExAws.Operation.perform for dev
    NB to use, set the `LOGGER_LEVEL` env var: `LOGGER_LEVEL=info`.

---
+++ apps/feedback/lib/mock_aws.ex
@@ -0,0 +1,35 @@
+defmodule Feedback.MockAws do
+  @moduledoc """
+  Mock ExAws functions so that we aren't dependent on AWS in development.
+  """
+  require Logger
+
+  def config(:ses) do
+    :ok
+  end
+
+  def perform(%{params: %{"RawMessage.Data" => raw_message}}, _config) do
+    _ =
+      raw_message
+      |> Base.decode64!()
+      |> Mail.Parsers.RFC2822.parse()
+      |> log_email()
+
+    {:ok, :status_info_that_gets_ignored_by_caller}
+  end
+
+  defp log_email(%Mail.Message{headers: %{"to" => to}, body: body}) do
+    Logger.info(fn ->
+      """
+
+      MOCK EMAIL
+
+      To: #{to}
+
+      Body:
+      #{body}
+
+      """
+    end)
+  end
+end
