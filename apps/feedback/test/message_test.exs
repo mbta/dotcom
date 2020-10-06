8b31ebe25ff456080df7b87bf013fe90ed7a7298
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2020-10-06 11:15:39 -0400

    Add back-end support for a new Subject field (#637)
    Submit subject to Heat in `CATEGORY` field.
    
    Generate Heat `TOPIC` based on the service and subject.

--- apps/feedback/test/message_test.exs
+++ apps/feedback/test/message_test.exs
@@ -17,4 +17,27 @@ defmodule MessageTest do
       refute valid_service?("nonsense")
     end
   end
+
+  describe "valid_subject_for_service?/2" do
+    test "is true when the subject is in the allowed subject values for the given service option" do
+      assert valid_subject_for_service?("Bus Stop", "Complaint")
+      assert valid_subject_for_service?("Other", "Complaint")
+      assert valid_subject_for_service?("MBTA Projects/Programs", "Suggestion")
+      assert valid_subject_for_service?("Other", "Suggestion")
+      assert valid_subject_for_service?("CharlieCards & Tickets", "Inquiry")
+      assert valid_subject_for_service?("Other", "Inquiry")
+      assert valid_subject_for_service?("Employee", "Commendation")
+      assert valid_subject_for_service?("Other", "Commendation")
+    end
+
+    test "is false when the subject is not in the allowed subject values for the given service option" do
+      refute valid_subject_for_service?("", "Complaint")
+      refute valid_subject_for_service?(nil, "Complaint")
+      refute valid_subject_for_service?(nil, "Complaint")
+      refute valid_subject_for_service?("Fare Policy", "Complaint")
+      refute valid_subject_for_service?("TAlerts/Countdowns/Apps", "Suggestion")
+      refute valid_subject_for_service?("TAlerts/Countdowns/Apps", "Inquiry")
+      refute valid_subject_for_service?("CTD", "Commendation")
+    end
+  end
 end
