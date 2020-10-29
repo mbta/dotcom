a9bfce3b05df8e1b458d5a4d889c74811f90a81e
fjlanasa@gmail.com <fjlanasa@gmail.com>
2020-10-29 15:31:15 -0400

    fix: redirect /Vote to /vote

--- apps/site/lib/site_web/router.ex
+++ apps/site/lib/site_web/router.ex
@@ -184,6 +184,7 @@ defmodule SiteWeb.Router do
     resources("/alerts", AlertController, only: [:index, :show])
     get("/trip-planner", TripPlanController, :index)
     get("/vote", TripPlanController, :vote)
+    get("/Vote", TripPlanController, :vote)
     get("/customer-support", CustomerSupportController, :index)
     get("/customer-support/thanks", CustomerSupportController, :thanks)
     post("/customer-support", CustomerSupportController, :submit)
