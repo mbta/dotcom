0d579c8ec6884298f0fe83b6a075985eb170b2f1
Cristen Jones <cjones3@mbta.com>
2020-11-12 23:50:04 -0500

    Add correct branch name for rockport test

--- apps/site/test/site_web/controllers/schedule/line/helpers_test.exs
+++ apps/site/test/site_web/controllers/schedule/line/helpers_test.exs
@@ -669,7 +669,7 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
 
       assert [
                %RouteStops{branch: nil, stops: trunk_route_stops},
-               %RouteStops{branch: "North Station - Rockport", stops: rockport_route_stops},
+               %RouteStops{branch: "North Station - Manchester", stops: rockport_route_stops},
                %RouteStops{branch: "North Station - Newburyport", stops: newburyport_route_stops}
              ] = Helpers.get_branch_route_stops(newburyport_route, 0)
 
@@ -707,7 +707,7 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
                  false
                ]
 
-      assert Enum.all?(rockport_route_stops, &(&1.branch == "North Station - Rockport"))
+      assert Enum.all?(rockport_route_stops, &(&1.branch == "North Station - Manchester"))
 
       assert_stop_ids(rockport_route_stops, [
         "place-GB-0198",
