95e7345492eced38da3c5d256eddf42bf285d73a
Cristen Jones <cjones3@mbta.com>
2021-02-11 17:19:33 -0500

    fix: Actually exit/fail after break;s
    Clean up a few other things

--- semaphore/run_mock_server.sh
+++ semaphore/run_mock_server.sh
@@ -17,18 +17,17 @@ set -e
 # When script exits, clean up processes
 function cleanup () {
   error_code=$?
-  printf -- '\033[35m :( Killing running processes... \033[0m\n'
+  printf -- '\033[31m z_z Time to go! Closing the mock server and killing leftover running processes... \033[0m\n'
   pkill -TERM -f "wiremock"
   pkill -TERM -f "mix phx.server"
   pkill -TERM -f "sleep"
-  printf -- '\033[35m DONE.\nExiting....\n\n \033[0m\n'
   exit "${error_code}";
 }
-trap cleanup ERR EXIT SIGINT SIGTERM
+trap cleanup EXIT
 
 # Require these variables to proceed
 if [[ -z "${WIREMOCK_PROXY_URL}" ]]; then
-  echo "Must provide WIREMOCK_PROXY_URL in environment" 1>&2
+  echo "Unable to start: Must provide WIREMOCK_PROXY_URL in environment"
   exit 1
 fi
 
@@ -38,7 +37,7 @@ fi
 
 export WIREMOCK_PROXY=true
 export WIREMOCK_PROXY_URL="${WIREMOCK_PROXY_URL}"
-export WIREMOCK_TRIP_PLAN_PROXY_URL="${WIREMOCK_TRIP_PLAN_PROXY_URL}"
+export WIREMOCK_TRIP_PLAN_PROXY_URL="${WIREMOCK_TRIP_PLAN_PROXY_URL:-}"
 export V3_URL=http://localhost:8080
 export OPEN_TRIP_PLANNER_URL=http://localhost:8080
 
@@ -73,13 +72,13 @@ export DRUPAL_ROOT="${DRUPAL_ROOT:=http://www.cms.example}"
 
 cd apps/site
 if [[ "${ENABLE_RECORDING:-}" = "true" ]]; then
-  printf -- '\033[36m [Wiremock] Starting (recording responses) \033[0m\n'
+  printf -- '\033[33m [Wiremock] Starting (recording responses) \033[0m\n'
   java -jar "${WIREMOCK_PATH}" --root-dir=test --global-response-templating \
-    --record-mappings 1>/dev/null 2>/dev/null &
+    --record-mappings >> /dev/null 2>&1 &
 else
-  printf -- '\033[36m [Wiremock] Starting \033[0m\n'
+  printf -- '\033[33m [Wiremock] Starting \033[0m\n'
   java -jar "${WIREMOCK_PATH}" --root-dir=test --global-response-templating \
-    1>/dev/null 2>/dev/null &
+    >> /dev/null 2>&1 &
 fi
 cd - 1>/dev/null
 
@@ -87,8 +86,8 @@ cd - 1>/dev/null
 # Running the Phoenix server, supressing the output
 #===============================================================================
 
-printf -- '\033[36m [Phoenix] Starting server \033[0m\n'
-mix phx.server 1>/dev/null 2>/dev/null &
+printf -- '\033[33m [Phoenix] Starting server \033[0m\n'
+mix phx.server >> /dev/null 2>&1 &
 
 #===============================================================================
 # Wait for both to be running before proceeding
@@ -97,23 +96,28 @@ wiremock_iterations=0
 printf '\033[34m [Wiremock] Waiting on port 8080 .\033[0m'
 until curl -X GET -H "X-WM-Proxy-Url: ${WIREMOCK_PROXY_URL}" \
   --output /dev/null --silent --head --fail http://localhost:8080/alerts/; do
-  if [ "$wiremock_iterations" -ge 4 ]; then break; fi
+  if [ "$wiremock_iterations" -ge 5 ]; then 
+    printf -- '\033[31m x_x failed \033[0m\n'
+    exit 1
+  fi
   ((wiremock_iterations++))
   printf '.'
   sleep 1
 done
-printf -- '\033[32m \342\234\224 mock API is running \033[0m\n'
+printf -- '\033[92m ^_^ running \033[0m\n'
 
 phoenix_iterations=0
 printf '\033[34m [Phoenix] Waiting on port 8082 .\033[0m'
 until curl --output /dev/null --silent --head --fail \
   http://localhost:8082/_health; do
-  if [ "$phoenix_iterations" -ge 20 ]; then break; fi
+  if [ "$phoenix_iterations" -ge 20 ]; then
+    printf -- '\033[31m [Phoenix] x_x failed \033[0m\n'
+    exit 1
+  fi
   ((phoenix_iterations++))
   printf '.'
   sleep 1
 done
-printf -- '\033[32m \342\234\224 application server is running \033[0m\n'
+printf -- '\033[92m ^_^ running \033[0m\n'
 
-sleep 10
-printf -- '\033[36m Setup complete! \033[0m\n'
+printf -- '\033[95m \^_^/ Setup complete! \n Mocked server up and ready for requests on http://localhost:8082\033[0m\n\n'
