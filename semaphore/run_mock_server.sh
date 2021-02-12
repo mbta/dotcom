1dc8c222ed94d99eff6a529fb74c8c53d240bb65
Cristen Jones <cjones3@mbta.com>
2021-02-12 13:30:11 -0500

    docs: Show usage, edit comment doc to match

--- semaphore/run_mock_server.sh
+++ semaphore/run_mock_server.sh
@@ -10,7 +10,7 @@ set -e
 # with new API responses from the endpoint specified in WIREMOCK_PROXY_URL
 #
 # Run it like
-# bash ./semaphore/run_mock_server.sh &
+# source ./semaphore/run_mock_server.sh
 # or else it'll immediately exit and shut down the server.
 #===============================================================================
 
