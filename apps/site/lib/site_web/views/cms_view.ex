f5436bd1446a4ab1b816b4d2e0c68bed284d3853
Hannah Purcell <hpurcell@mbta.com>
2021-02-17 09:12:06 -0500

    Event listing redesign. Time rendering has a new parameter:  format type

--- apps/site/lib/site_web/views/cms_view.ex
+++ apps/site/lib/site_web/views/cms_view.ex
@@ -2,6 +2,9 @@ defmodule SiteWeb.CMSView do
   @moduledoc """
   Handles rendering of partial content from the CMS.
   """
+
+  @type format_type :: atom()
+
   use SiteWeb, :view
 
   import SiteWeb.TimeHelpers
@@ -22,20 +25,20 @@ defmodule SiteWeb.CMSView do
   end
 
   @doc "Nicely renders the duration of an event, given two DateTimes."
-  @spec render_duration(NaiveDateTime.t() | DateTime.t(), NaiveDateTime.t() | DateTime.t() | nil) ::
-          String.t()
-  def render_duration(start_time, end_time)
+  @spec render_duration(NaiveDateTime.t() | DateTime.t(), NaiveDateTime.t() | DateTime.t() | nil,
+          format_type()) :: String.t()
+  def render_duration(start_time, end_time, format_type \\ nil)
 
-  def render_duration(start_time, nil) do
+  def render_duration(start_time, nil, format_type) do
     start_time
     |> maybe_shift_timezone
-    |> do_render_duration(nil)
+    |> do_render_duration(nil, format_type)
   end
 
-  def render_duration(start_time, end_time) do
+  def render_duration(start_time, end_time, format_type) do
     start_time
     |> maybe_shift_timezone
-    |> do_render_duration(maybe_shift_timezone(end_time))
+    |> do_render_duration(maybe_shift_timezone(end_time), format_type)
   end
 
   defp maybe_shift_timezone(%NaiveDateTime{} = time) do
@@ -46,19 +49,22 @@ defmodule SiteWeb.CMSView do
     Util.to_local_time(time)
   end
 
-  defp do_render_duration(start_time, nil) do
-    "#{format_date(start_time)} at #{format_time(start_time)}"
+  defp do_render_duration(start_time, nil, format_type) do
+    joiner = if (format_type == :event), do: "\u2022", else: "at"
+    "#{format_date(start_time, format_type)} #{joiner} #{format_time(start_time)}"
   end
 
   defp do_render_duration(
          %{year: year, month: month, day: day} = start_time,
-         %{year: year, month: month, day: day} = end_time
+         %{year: year, month: month, day: day} = end_time,
+         format_type
        ) do
-    "#{format_date(start_time)} at #{format_time(start_time)} - #{format_time(end_time)}"
+    joiner = if (format_type == :event), do: "\u2022", else: "at"
+    "#{format_date(start_time, format_type)} #{joiner} #{format_time(start_time)} - #{format_time(end_time)}"
   end
 
-  defp do_render_duration(start_time, end_time) do
-    "#{format_date(start_time)} #{format_time(start_time)} - #{format_date(end_time)} #{
+  defp do_render_duration(start_time, end_time, format_type) do
+    "#{format_date(start_time, format_type)} #{format_time(start_time)} - #{format_date(end_time, format_type)} #{
       format_time(end_time)
     }"
   end
