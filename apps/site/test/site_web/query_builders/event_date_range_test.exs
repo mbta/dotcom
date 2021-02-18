8957c56c4bfffe84b26ee0d553bf38d7066a1054
Cristen Jones <cjones3@mbta.com>
2021-02-18 15:31:30 -0500

    Put all the months on the same page (show all events for the year) (#801)

--- apps/site/test/site_web/query_builders/event_date_range_test.exs
+++ apps/site/test/site_web/query_builders/event_date_range_test.exs
@@ -3,22 +3,13 @@ defmodule SiteWeb.EventDateRangeTest do
   alias SiteWeb.EventDateRange
 
   describe "build/2" do
-    test "returns a date range for the given a month" do
+    test "returns a date range for the given month" do
       params = %{"month" => "2017-02-01"}
       current_month = ~D[2017-04-15]
 
       assert EventDateRange.build(params, current_month) == %{
                start_time_gt: "2017-02-01",
                start_time_lt: "2017-03-01"
-             }
-    end
-
-    test "returns a date range for the current month when a month is not provided" do
-      current_month = ~D[2017-04-15]
-
-      assert EventDateRange.build(%{}, current_month) == %{
-               start_time_gt: "2017-04-01",
-               start_time_lt: "2017-05-01"
              }
     end
 
@@ -41,6 +32,35 @@ defmodule SiteWeb.EventDateRangeTest do
                start_time_lt: "2017-05-01"
              }
     end
+
+    test "returns a date range for the current year when given an invalid date" do
+      params = %{"year" => "nope"}
+      current_month = ~D[2017-04-15]
+
+      assert EventDateRange.build(params, current_month) == %{
+               start_time_gt: "2017-01-01",
+               start_time_lt: "2018-01-01"
+             }
+    end
+
+    test "returns a date range for the given year" do
+      params = %{"year" => "2017-02-01"}
+      current_year = ~D[2020-04-15]
+
+      assert EventDateRange.build(params, current_year) == %{
+               start_time_gt: "2017-01-01",
+               start_time_lt: "2018-01-01"
+             }
+    end
+
+    test "returns a date range for the current year when a year is not provided" do
+      current_year = ~D[2019-04-15]
+
+      assert EventDateRange.build(%{}, current_year) == %{
+               start_time_gt: "2019-01-01",
+               start_time_lt: "2020-01-01"
+             }
+    end
   end
 
   describe "for_month/1" do
@@ -50,6 +70,17 @@ defmodule SiteWeb.EventDateRangeTest do
       assert EventDateRange.for_month(date) == %{
                start_time_gt: "2017-04-01",
                start_time_lt: "2017-05-01"
+             }
+    end
+  end
+
+  describe "for_year/1" do
+    test "returns query params for the beginning and end of the given month" do
+      date = ~D[2017-04-10]
+
+      assert EventDateRange.for_year(date) == %{
+               start_time_gt: "2017-01-01",
+               start_time_lt: "2018-01-01"
              }
     end
   end
