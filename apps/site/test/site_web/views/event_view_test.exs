7b2d59c15122b9df73c41ea78fe54c5b4ea75899
Hannah Purcell <hpurcell@mbta.com>
2021-02-19 09:09:16 -0500

    Finally, added tests!

--- apps/site/test/site_web/views/event_view_test.exs
+++ apps/site/test/site_web/views/event_view_test.exs
@@ -1,6 +1,7 @@
 defmodule SiteWeb.EventViewTest do
   use Site.ViewCase, async: true
   import SiteWeb.EventView
+  import CMS.Helpers, only: [parse_iso_datetime: 1]
 
   describe "index.html" do
     test "includes links to the previous and next month", %{conn: conn} do
@@ -125,6 +126,49 @@ defmodule SiteWeb.EventViewTest do
         |> Floki.parse()
 
       assert title == "June"
+    end
+  end
+
+  describe "render_event_duration/2" do
+    test "with no end time, only renders start time" do
+      actual = render_event_duration(~N[2016-11-15T10:00:00], nil)
+      expected = "Tue, Nov 15, 2016 \u2022 10 AM"
+      assert expected == actual
+    end
+
+    test "with start/end on same day, only renders date once" do
+      actual = render_event_duration(~N[2016-11-14T12:00:00], ~N[2016-11-14T14:30:00])
+      expected = "Mon, Nov 14, 2016 \u2022 12 PM - 2:30 PM"
+      assert expected == actual
+    end
+
+    test "with start/end on different days, renders both dates" do
+      actual = render_event_duration(~N[2016-11-14T12:00:00], ~N[2016-12-01T14:30:00])
+      expected = "Mon, Nov 14, 2016 12 PM - Thu, Dec 1, 2016 2:30 PM"
+      assert expected == actual
+    end
+
+    test "with DateTimes, shifts them to America/New_York" do
+      actual =
+        render_event_duration(
+          Timex.to_datetime(~N[2016-11-05T05:00:00], "Etc/UTC"),
+          Timex.to_datetime(~N[2016-11-06T06:00:00], "Etc/UTC")
+        )
+
+      # could also be November 6th, 1:00 AM (test daylight savings)
+      expected = "Sat, Nov 5, 2016 1 AM - Sun, Nov 6, 2016 2 AM"
+      assert expected == actual
+    end
+
+    test "with ISO:Extended DateTimes, does not shift timezone" do
+      actual =
+        render_event_duration(
+          parse_iso_datetime("2016-11-06T01:00:00-04:00"),
+          parse_iso_datetime("2016-11-06T02:00:00-04:00")
+        )
+
+      expected = "Sun, Nov 6, 2016 \u2022 1 AM - 2 AM"
+      assert expected == actual
     end
   end
 end
