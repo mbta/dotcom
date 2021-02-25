68a8eca81be8ac0ae7c82500a2b3b71839571a4c
Cristen Jones <cjones3@mbta.com>
2021-02-25 13:08:23 -0500

    Rename "Info & Maps" tab to "Schedules & Maps"

--- apps/site/test/site_web/views/schedule_view_test.exs
+++ apps/site/test/site_web/views/schedule_view_test.exs
@@ -898,7 +898,7 @@ defmodule SiteWeb.ScheduleViewTest do
         |> safe_to_string()
 
       assert tabs =~ "schedule-tab"
-      assert tabs =~ "info-&amp;-maps-tab"
+      assert tabs =~ "schedules-&amp;-maps-tab"
       assert tabs =~ "alerts-tab"
       refute tabs =~ "info-tab"
       refute tabs =~ "timetable-tab"
