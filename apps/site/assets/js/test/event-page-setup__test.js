1c03a8f2c75e504ea1f0d12e805e4e8ed083d2c0
Cristen Jones <cjones3@mbta.com>
2021-02-26 15:51:59 -0500

    tests: Add remaining JS tests!

--- apps/site/assets/js/test/event-page-setup__test.js
+++ apps/site/assets/js/test/event-page-setup__test.js
@@ -1,4 +1,4 @@
-import { assert, expect } from "chai";
+import { assert } from "chai";
 import sinon from "sinon";
 import jsdom from "mocha-jsdom";
 import { setupEventsPage } from "../event-page-setup";
@@ -39,9 +39,37 @@ const eventsHubHTML = `
     </div>
     `;
 
+function triggerScrollEvent(clock) {
+  document.dispatchEvent(new window.Event("scroll"));
+  clock.tick(); // fast forward set timeout (gets past window.requestAnimationFrame)
+}
+
 describe("setupEventsPage", () => {
   let $;
+  let scrollIntoViewSpy;
+  let getComputedStyleSpy;
+  let getBoundingClientRectSpy;
+  let toggleAttributeSpy;
   jsdom({ url: testURL });
+
+  before(() => {
+    /**
+     * Set up sinon stubs to watch DOM functions we want to assert.
+     *
+     * JSDOM lacks various DOM API features, so for those functions we have to
+     * manually assign the stubs to window.HTMLElement.prototype
+     */
+    scrollIntoViewSpy = sinon.stub();
+    window.HTMLElement.prototype.scrollIntoView = scrollIntoViewSpy;
+
+    toggleAttributeSpy = sinon.stub();
+    window.HTMLElement.prototype.toggleAttribute = toggleAttributeSpy;
+    getBoundingClientRectSpy = sinon.stub(
+      window.HTMLElement.prototype,
+      "getBoundingClientRect"
+    );
+    getComputedStyleSpy = sinon.stub(window, "getComputedStyle");
+  });
 
   beforeEach(() => {
     $ = jsdom.rerequire("jquery");
@@ -51,11 +79,86 @@ describe("setupEventsPage", () => {
 
   afterEach(() => {
     $("#test").remove();
+    // reset all the stubs
+    scrollIntoViewSpy.resetHistory();
+    getComputedStyleSpy.resetHistory();
+    getBoundingClientRectSpy.resetHistory();
+    toggleAttributeSpy.resetHistory();
+  });
+
+  describe("scroll event listeners", () => {
+    /**
+     * Because the logic in these event listeners are inside a
+     * window.requestAnimationFrame() call, we need to let the test bypass that.
+     */
+    let clock;
+
+    beforeEach(() => {
+      clock = sinon.useFakeTimers();
+      window.requestAnimationFrame = setTimeout;
+      window.cancelAnimationFrame = clearTimeout;
+    });
+
+    afterEach(() => {
+      clock.restore();
+    });
+
+    it("will toggle 'stuck' attribute based on calculations on scroll", () => {
+      // mock the positions. making these equal triggers the attribute!
+      const t = 22;
+      getBoundingClientRectSpy.callsFake(() => ({ top: t }));
+      getComputedStyleSpy.callsFake(el => ({ top: `${t}px` }));
+
+      setupEventsPage();
+      sinon.assert.notCalled(getComputedStyleSpy);
+      sinon.assert.notCalled(getBoundingClientRectSpy);
+      sinon.assert.notCalled(toggleAttributeSpy);
+
+      triggerScrollEvent(clock);
+      sinon.assert.called(getComputedStyleSpy);
+      sinon.assert.called(getBoundingClientRectSpy);
+      sinon.assert.called(toggleAttributeSpy);
+      sinon.assert.calledWith(toggleAttributeSpy, "stuck", true);
+
+      // try not equal values. getComputedStyle().top will keep returning "22px"
+      // but now getBoundingClientRect().top will return 20!
+      getBoundingClientRectSpy.callsFake(() => ({ top: 20 }));
+
+      triggerScrollEvent(clock);
+      sinon.assert.calledWith(toggleAttributeSpy, "stuck", false);
+    });
+
+    it("will toggle 'js-nav-*' class name based on scroll direction", () => {
+      const eventsHubPage = document.querySelector(".m-events-hub");
+      getBoundingClientRectSpy.callsFake(() => ({ height: 10 }));
+
+      // mock window.Y for this test with initial scroll position
+      global.window = Object.assign(global.window, {
+        scrollY: 222
+      });
+      setupEventsPage();
+      assert.isFalse(eventsHubPage.classList.contains("js-nav-up"));
+      assert.isFalse(eventsHubPage.classList.contains("js-nav-down"));
+
+      // mock scroll up
+      global.window = Object.assign(global.window, {
+        scrollY: 20
+      });
+      triggerScrollEvent(clock);
+      assert.isTrue(eventsHubPage.classList.contains("js-nav-up"));
+      assert.isFalse(eventsHubPage.classList.contains("js-nav-down"));
+
+      // mock scroll down
+      global.window = Object.assign(global.window, {
+        scrollY: 442
+      });
+      triggerScrollEvent(clock);
+      assert.isTrue(eventsHubPage.classList.contains("js-nav-down"));
+      assert.isFalse(eventsHubPage.classList.contains("js-nav-up"));
+    });
   });
 
   it("scrolls to .m-event-list__month--active", () => {
-    const scrollIntoViewSpy = sinon.spy();
-    window.HTMLElement.prototype.scrollIntoView = scrollIntoViewSpy;
     sinon.assert.notCalled(scrollIntoViewSpy);
 
     const activeMonth = $("section.m-event-list__month--active");
