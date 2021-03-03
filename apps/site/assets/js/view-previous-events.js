0e879868a3c3cfbd3cb996cd0a07fd351c15beb2
Cristen Jones <cjones3@mbta.com>
2021-03-03 17:43:45 -0500

    fix: button should hide events for only that month
    Adjust button text to be for that month, not for now

--- apps/site/assets/js/view-previous-events.js
+++ apps/site/assets/js/view-previous-events.js
@@ -1,17 +1,21 @@
 const setupViewPreviousEventsButton = function() {
-  const viewPreviousEventsButton = document.querySelector(
-    ".m-previous-events-button"
-  );
-  const hiddenEventList = [...document.querySelectorAll(".m-hidden-event")];
+  const viewPreviousEventsButtons = [
+    ...document.querySelectorAll("a.m-previous-events-button")
+  ];
 
-  viewPreviousEventsButton &&
-    hiddenEventList &&
-    viewPreviousEventsButton.addEventListener("click", () => {
-      hiddenEventList.forEach(hiddenEvent =>
-        hiddenEvent.classList.remove("m-hidden-event")
-      );
-      viewPreviousEventsButton.classList.add("m-hidden-button");
+  viewPreviousEventsButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      // show hidden events for this button
+      // because of how the list is laid out, we can just iterate through next <li> siblings until we find one without .m-hidden-event
+      let li = button.parentElement;
+      do {
+        li.classList.remove("m-hidden-event");
+        li = li.nextElementSibling;
+      } while (li && li.classList.contains("m-hidden-event") === true);
+
+      button.parentElement.classList.add("m-hidden-button");
     });
+  });
 };
 
 export default function() {
