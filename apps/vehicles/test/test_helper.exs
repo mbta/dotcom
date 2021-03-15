16bd94344bc3a028a4ab76ef12b633b477ba48f6
Cristen Jones <cjones3@mbta.com>
2021-03-15 17:36:11 -0400

    tests(Vehicles): Ensure RepoCache before ExUnit

--- apps/vehicles/test/test_helper.exs
+++ apps/vehicles/test/test_helper.exs
@@ -1,5 +1,7 @@
-ExUnit.start()
 Application.ensure_all_started(:bypass)
+Application.ensure_all_started(:repo_cache)
+
+ExUnit.start()
 
 # Report warnings as errors
 Code.compiler_options(warnings_as_errors: true)
