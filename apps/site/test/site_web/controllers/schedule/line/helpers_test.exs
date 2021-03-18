b6a5f44805661392f67e2c4a03f1cf3c2367f62e
Cristen Jones <cjones3@mbta.com>
2021-03-18 12:58:36 -0400

    Hide line diagram branches created by multi route trips (#847)
    * tests: More Green-E test adjustments
    
    * test(ScheduleController.Line.Helpers): fix branch
    
    * fix(ScheduleController.Line.Helpers): filter route
    
    Route patterns from other routes might be returned if they are from multi route trips. We don't want to show them in the schedules page so we filter these out by making sure the route ID matches the desired route.

--- apps/site/test/site_web/controllers/schedule/line/helpers_test.exs
+++ apps/site/test/site_web/controllers/schedule/line/helpers_test.exs
@@ -42,6 +42,9 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
   end
 
   describe "get_branch_route_stops/3" do
+    @tag skip: "We'll mock route patterns soon"
+    test "does not return branches for route patterns from multi trip routes"
+
     test "returns a list of RouteStops, one for each branch of the line" do
       assert [
                %RouteStops{branch: nil, stops: trunk_route_stops},
@@ -150,8 +153,6 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
                  nil,
                  nil,
                  nil,
-                 nil,
-                 nil,
                  "Green-E",
                  "Green-E",
                  "Green-E",
@@ -166,8 +167,6 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
                ]
 
       assert_stop_ids(e_stops, [
-        "place-lech",
-        "14159",
         "place-north",
         "place-haecl",
         "place-gover",
@@ -207,16 +206,12 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
                  false,
                  false,
                  false,
-                 false,
-                 false,
                  true
                ]
 
       assert Enum.map(e_stops, & &1.is_beginning?) ==
                [
                  true,
-                 false,
-                 false,
                  false,
                  false,
                  false,
@@ -630,16 +625,12 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
                  false,
                  false,
                  false,
-                 false,
-                 false,
                  true
                ]
 
       assert Enum.map(stops, & &1.is_beginning?) ==
                [
                  true,
-                 false,
-                 false,
                  false,
                  false,
                  false,
@@ -675,8 +666,8 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
 
       assert [
                %RouteStops{branch: nil, stops: trunk_route_stops},
-               %RouteStops{branch: "North Station - Manchester", stops: rockport_route_stops},
-               %RouteStops{branch: "North Station - Newburyport", stops: newburyport_route_stops}
+               %RouteStops{branch: "North Station - Newburyport", stops: newburyport_route_stops},
+               %RouteStops{branch: "North Station - Manchester", stops: rockport_route_stops}
              ] = Helpers.get_branch_route_stops(newburyport_route, 0)
 
       assert Enum.all?(trunk_route_stops, &(&1.branch == nil))
@@ -715,11 +706,16 @@ defmodule SiteWeb.ScheduleController.Line.HelpersTest do
 
       assert Enum.all?(rockport_route_stops, &(&1.branch == "North Station - Manchester"))
 
-      assert_stop_ids(rockport_route_stops, ["place-GB-0198", "place-GB-0229", "place-GB-0254"])
+      assert_stop_ids(rockport_route_stops, [
+        "place-GB-0198",
+        "place-GB-0229",
+        "place-GB-0254",
+        "place-GB-0296"
+      ])
 
-      assert Enum.map(rockport_route_stops, & &1.is_terminus?) == [false, false, true]
+      assert Enum.map(rockport_route_stops, & &1.is_terminus?) == [false, false, false, true]
 
-      assert Enum.map(rockport_route_stops, & &1.is_beginning?) == [false, false, false]
+      assert Enum.map(rockport_route_stops, & &1.is_beginning?) == [false, false, false, false]
 
       assert Enum.all?(newburyport_route_stops, &(&1.branch == "North Station - Newburyport"))
 
