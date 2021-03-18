34fc0aaf173f1ddaa795b34138afde4662070aa4
Hannah Purcell <hpurcell@mbta.com>
2021-03-18 16:11:13 -0400

    Needed that polyline format I removed

--- apps/site/lib/leaflet/map_data/polyline.ex
+++ apps/site/lib/leaflet/map_data/polyline.ex
@@ -20,7 +20,7 @@ defmodule Leaflet.MapData.Polyline do
 
   @default_opts [color: "#000000", weight: 5, dotted?: false]
 
-  @spec new(RoutePattern.t(), Keyword.t()) :: t()
+  @spec new(RoutePattern.t() | String.t(), Keyword.t()) :: t()
   def new(polyline, user_opts \\ [])
 
   def new(%RoutePattern{representative_trip_polyline: polyline, shape_id: id}, user_opts) when is_binary(polyline) do
@@ -35,6 +35,22 @@ defmodule Leaflet.MapData.Polyline do
       color: opts[:color],
       dotted?: opts[:dotted?],
       id: id,
+      positions: positions,
+      weight: opts[:weight]
+    }
+  end
+
+  def new(polyline, user_opts) when is_binary(polyline) do
+    positions =
+      polyline
+      |> Polyline.decode()
+      |> Enum.map(fn {lng, lat} -> [lat, lng] end)
+
+    opts = Keyword.merge(@default_opts, user_opts)
+
+    %__MODULE__{
+      color: opts[:color],
+      dotted?: opts[:dotted?],
       positions: positions,
       weight: opts[:weight]
     }
