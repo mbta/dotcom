991c6e9e7e2d519b60a09d672326b873736a8e27
Hannah Purcell <hpurcell@mbta.com>
2021-04-06 12:10:12 -0400

    Test default polyline constructor

--- apps/site/test/leaflet/map_data/polyline_test.exs
+++ apps/site/test/leaflet/map_data/polyline_test.exs
@@ -17,5 +17,18 @@ defmodule Leaflet.MapData.PolylineTest do
       assert [first | _] = positions
       assert first == [42.37428, -71.119]
     end
+
+    test "makes polyline with default options" do
+      route_pattern =
+        @route_patterns_repo_api.by_route_id("77")
+        |> List.first()
+
+      assert %Polyline{color: color, positions: positions} =
+               Polyline.new(route_pattern)
+
+      assert color == "#000000"
+      assert [first | _] = positions
+      assert first == [42.37428, -71.119]
+    end
   end
 end
