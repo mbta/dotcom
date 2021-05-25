8f73dd5dbc034d0ef5d66bb8617eade5cc18a398
Hannah Purcell <hpurcell@mbta.com>
2021-05-25 12:34:46 -0400

    broke down long function chain

--- apps/site/lib/site_web/controllers/schedule/line/diagram_helpers.ex
+++ apps/site/lib/site_web/controllers/schedule/line/diagram_helpers.ex
@@ -45,13 +45,17 @@ defmodule SiteWeb.ScheduleController.Line.DiagramHelpers do
     # Get route_id to check if this is the HH Ferry
     # Most routes have a merge on the inbound side, 
     # But the HH ferry merges on the outbound end
-    route_id =
-      branches
+    stops_on_first_branch = branches
       |> List.first()
       |> Map.get(:stops)
-      |> List.first()
-      |> Map.get(:route)
-      |> Map.get(:id)
+
+    route = if stops_on_first_branch,
+      do: stops_on_first_branch
+        |> List.first()
+        |> Map.get(:route),
+      else: nil
+
+    route_id = if route, do: Map.get(route, :id), else: nil
 
     verified_direction = RouteStop.verify_direction(route_id, direction_id)
 
