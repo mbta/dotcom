2234ebe643a49ea60f58af7bc3fc069c9b0bd599
Paul Swartz <paul@paulswartz.net>
2021-07-01 09:31:46 -0400

    Revert "Revert "Sentry:  Created a "no-cache" option when a dead trip id is found (#982)""
    This reverts commit 73e43e7c61afd96b1928608ce8316a7ed0eb1440.

--- apps/site/test/site_web/controllers/customer_support_controller_test.exs
+++ apps/site/test/site_web/controllers/customer_support_controller_test.exs
@@ -332,7 +332,9 @@ defmodule SiteWeb.CustomerSupportControllerTest do
       assert "recaptcha" in conn.assigns.errors
     end
 
-    test "if the submission doesn't carry a recaptcha value, consider it an invalid recaptcha", %{conn: conn} do      
+    test "if the submission doesn't carry a recaptcha value, consider it an invalid recaptcha", %{
+      conn: conn
+    } do
       conn =
         post(
           conn,
