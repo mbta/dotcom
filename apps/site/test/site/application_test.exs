9fc2be82db57525dd1cd827c87a731f910e5aa5d
Cristen Jones <cjones3@mbta.com>
2021-07-26 15:58:39 -0400

    hack: stopping the app might not release the port

--- apps/site/test/site/application_test.exs
+++ apps/site/test/site/application_test.exs
@@ -3,9 +3,14 @@ defmodule Site.ApplicationTest do
   use ExUnit.Case
 
   describe "start/2" do
+    setup do
+      :ok = Application.stop(:site)
+      Process.sleep(5000)
+      :ok
+    end
+
     test "can start the application" do
       # ensures that our supervision tree gets coverage tracked
-      Application.stop(:site)
       assert {:ok, _} = Application.ensure_all_started(:site)
     end
   end
