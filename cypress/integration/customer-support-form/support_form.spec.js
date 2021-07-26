c6e5af67874c0db098b30480f4242549a8231f99
Cristen Jones <cjones3@mbta.com>
2021-07-26 10:18:35 -0400

    Add Cypress for integration testing (#1021)
    * npm: Improve build script, add rebuild script
    
    Turns out mix phx.digest should be after webpack builds, who knew
    
    Add a rebuild script as a “blow it all up and try again” option
    
    * dev-deps: Add faker to for generating basic random data
    
    A few other things:
    * regenerate the package-lock.json files (removes unneeded root level package-lock.json)
    * adds name to root level package.json - it fixes an obscure bug in CI later on, I promise (ref: https://stackoverflow.com/a/60112825)
    
    * dev-deps: Add cypress + plugins
    
    * dev-deps: Add start-server-and-test
    
    * feat: add test config to adjust rate limiting
    
    Defaults to true to leave rate limiting enabled at 60s.
    
    Used here to effectively disable rate limiting on the customer support form by setting the interval to 1s, so that tests can submit the form several times a minute!
    
    * tests: Add mix task to output tested info
    
    * npm: Add scripts for working with cypress
    
    * refactor: Add customer support form Cypress tests
    
    Also adds basic Cypress config
    
    * refactor: Remove customer support form Wallaby tests
    
    Also remove test files as they have been moved.
    
    * actions: Add job for Cypress tests
    
    * fix: account for rate limit config in test

---
+++ cypress/integration/customer-support-form/support_form.spec.js
@@ -0,0 +1,236 @@
+import faker from "../../../apps/site/assets/node_modules/faker/locale/en_US";
+
+describe("Customer Support Form", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4002/customer-support");
+  });
+
+  it("shows an error state for missing type and comments", () => {
+    cy.get("#support-submit").click();
+    cy.get(".has-danger #service").should("be.visible");
+    cy.get(".has-danger #comments").should("be.visible");
+  });
+
+  it("choosing service shows subject list", () => {
+    cy.get("#support_subject").should("not.be.visible");
+    cy.get("#service-Complaint").check({ force: true });
+    cy.get("#support_subject")
+      .should("be.visible")
+      .should("contain", "Employee Complaint");
+  });
+
+  it("toggles additional fields when 'would like a response' is changed", () => {
+    // these fields are visible to start
+    // checkboxes need to be targeted by their label instead of the input
+    // because of how they are rendered (the actual input has opacity: 0)
+    const additionalFields = [
+      "#first_name",
+      "#last_name",
+      "#email",
+      "#phone",
+      "#privacy_label",
+      "#promotions_label"
+    ];
+
+    additionalFields.forEach(id => {
+      cy.get(`#contactInfoForm ${id}`).should("be.visible");
+    });
+
+    cy.get("#no_request_response_label").click();
+
+    additionalFields.forEach(id => {
+      cy.get(`#contactInfoForm ${id}`).should("not.be.visible");
+    });
+  });
+
+  it("doesn't allow non-image files to be uploaded", () => {
+    cy.get("input#photo").attachFile("customer-support-form/test_file.txt");
+    cy.get("#upload-photo-error").should("be.visible");
+  });
+
+  it("Generates previews for each uploaded image", () => {
+    cy.get("input#photo").attachFile("customer-support-form/test_image1.png");
+    cy.get("input#photo").attachFile("customer-support-form/test_image2.png");
+    cy.get(".photo-preview").should("have.length", 2);
+  });
+
+  describe("Submits form successfully", () => {
+    let comment, first_name, last_name, email, phone;
+
+    beforeEach(() => {
+      cy.intercept("POST", "/customer-support").as("submitForm");
+      comment = faker.hacker.phrase();
+      first_name = faker.name.firstName();
+      last_name = faker.name.lastName();
+      email = faker.internet.email();
+      phone = faker.phone.phoneNumber();
+    });
+
+    it("With basic info", () => {
+      cy.selectRandomServiceAndSubject();
+      cy.get("#comments").type(comment);
+      cy.get("#no_request_response_label").click();
+      cy.fillRecaptcha();
+      cy.get('[type="submit"]').click();
+
+      cy.wait("@submitForm")
+        .its("response.statusCode")
+        .should("eq", 302);
+
+      cy.get(".support-confirmation--error").should("not.be.visible");
+      cy.get(".support-confirmation--success").should("be.visible");
+
+      // check the email is sent with relevant data
+      cy.wait(2000);
+      cy.mix("test_helpers.feedback_email")
+        .its("stdout")
+        .should("not.contain", "enonent")
+        .should("contain", `${comment}</DESCRIPTION>`)
+        .then(stdout => {
+          cy.get("@selectedService").then(service =>
+            cy.wrap(stdout).should("contain", `<SERVICE>${service}</SERVICE>`)
+          );
+          cy.get("@selectedSubject").then(subject =>
+            cy.wrap(stdout).should("contain", `<CATEGORY>${subject}</CATEGORY>`)
+          );
+        });
+    });
+
+    it("Filters profane words", () => {
+      comment = "Distinctio cum dolor distinctio eos deserunt.";
+
+      cy.selectRandomServiceAndSubject();
+      cy.get("#comments").type(comment);
+      cy.get("#no_request_response_label").click();
+      cy.fillRecaptcha();
+      cy.get('[type="submit"]').click();
+
+      cy.wait("@submitForm")
+        .its("response.statusCode")
+        .should("eq", 302);
+
+      const censoredComment = "Distinctio *** dolor distinctio eos deserunt.";
+      // check the email is sent with relevant data
+      cy.wait(2000);
+      cy.mix("test_helpers.feedback_email")
+        .its("stdout")
+        .should("not.contain", "enonent")
+        .should("contain", `${censoredComment}</DESCRIPTION>`);
+    });
+
+    it("With optional fields", () => {
+      cy.selectRandomServiceAndSubject();
+      cy.get("#comments").type(comment);
+      cy.get("#first_name").type(first_name);
+      cy.get("#last_name").type(last_name);
+      cy.get("#email").type(email);
+      cy.get("#phone").type(phone);
+      cy.get("#privacy_label").click();
+      cy.get("#promotions_label").click();
+      cy.fillRecaptcha();
+      cy.get('[type="submit"]').click();
+
+      cy.wait("@submitForm")
+        .its("response.statusCode")
+        .should("eq", 302);
+
+      cy.get(".support-confirmation--error").should("not.be.visible");
+      cy.get(".support-confirmation--success").should("be.visible");
+
+      // check the email is sent with relevant data
+      cy.wait(2000);
+      cy.mix("test_helpers.feedback_email")
+        .its("stdout")
+        .should("not.contain", "enonent")
+        .should("contain", `${comment}</DESCRIPTION>`)
+        .should("contain", first_name)
+        .should("contain", last_name)
+        .should("contain", email)
+        .should("contain", phone)
+        .then(stdout => {
+          cy.get("@selectedService").then(service =>
+            cy.wrap(stdout).should("contain", `<SERVICE>${service}</SERVICE>`)
+          );
+          cy.get("@selectedSubject").then(subject =>
+            cy.wrap(stdout).should("contain", `<CATEGORY>${subject}</CATEGORY>`)
+          );
+        });
+    });
+
+    /**
+     * FIXME: Returns Poison.Encoder error, maybe the test upload is getting
+     * encoded suboptimally?
+     */
+    xit("With a photo attachment", () => {
+      cy.selectRandomServiceAndSubject();
+      cy.get("#comments").type(comment);
+      cy.get("input#photo").attachFile("customer-support-form/test_image1.png");
+      cy.get("#no_request_response_label").click();
+      cy.fillRecaptcha();
+      cy.get('[type="submit"]').click();
+
+      cy.wait("@submitForm")
+        .its("response.statusCode")
+        .should("eq", 302);
+
+      cy.get(".support-confirmation--error").should("not.be.visible");
+      cy.get(".support-confirmation--success").should("be.visible");
+
+      // check the email is sent with relevant data
+      cy.wait(2000);
+      cy.mix("test_helpers.feedback_email")
+        .its("stdout")
+        .should("not.contain", "enonent")
+        .should("contain", `${comment}</DESCRIPTION>`)
+        .should("contain", "test_image1")
+        .then(stdout => {
+          cy.get("@selectedService").then(service =>
+            cy.wrap(stdout).should("contain", `<SERVICE>${service}</SERVICE>`)
+          );
+          cy.get("@selectedSubject").then(subject =>
+            cy.wrap(stdout).should("contain", `<CATEGORY>${subject}</CATEGORY>`)
+          );
+        });
+    });
+
+    /**
+     * FIXME: Always raises Elixir error, e.g.:
+     * (File.Error) could not read file "/var/folders/2v/
+     * x05g8lfd1l734_337rcx1dpwbgm0nx/T//plug-1626/
+     * multipart-1626630666-24765732541476-1": no such file or directory
+     */
+    xit("With two photo attachments", () => {
+      cy.selectRandomServiceAndSubject();
+      cy.get("#comments").type(comment);
+      cy.get("input#photo").attachFile("customer-support-form/test_image1.png");
+      cy.get("input#photo").attachFile("customer-support-form/test_image2.png");
+      cy.get("#no_request_response_label").click();
+      cy.fillRecaptcha();
+      cy.get('[type="submit"]').click();
+
+      cy.wait("@submitForm")
+        .its("response.statusCode")
+        .should("eq", 302);
+
+      cy.get(".support-confirmation--error").should("not.be.visible");
+      cy.get(".support-confirmation--success").should("be.visible");
+
+      // check the email is sent with relevant data
+      cy.wait(2000);
+      cy.mix("test_helpers.feedback_email")
+        .its("stdout")
+        .should("not.contain", "enonent")
+        .should("contain", `${comment}</DESCRIPTION>`)
+        .should("contain", "test_image1")
+        .should("contain", "test_image2")
+        .then(stdout => {
+          cy.get("@selectedService").then(service =>
+            cy.wrap(stdout).should("contain", `<SERVICE>${service}</SERVICE>`)
+          );
+          cy.get("@selectedSubject").then(subject =>
+            cy.wrap(stdout).should("contain", `<CATEGORY>${subject}</CATEGORY>`)
+          );
+        });
+    });
+  });
+});
