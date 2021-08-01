309102569e74d6fc18a2b5f19d8453a178eabf59
Cristen Jones <cjones3@mbta.com>
2021-08-01 16:41:18 -0400

    Github Actions: build cache, run all tests on master branch (#1020)
    * refactor: add build job to tests workflow
    
    The new build job also has a mix compile check.
    The outputs of the build are cached and used in subsequent jobs.
    
    * feat: Run all tests in tests.yml on push to master
    
    * feat: Have tests.yml run all tests for dependabot
    
    * refactor: name cache keys once, refine approach
    
    * actions: Add job for Cypress tests
    
    * fix: should run with other integration/unit tests
    
    * fix: always run mix phx.digest
    
    * fix: forgot to use the cached digest sometimes
    
    * fix: use cached assets in tests
    
    * feat: make test server port configurable
    
    * fix: specify unique ports for tests with server
    
    * refactor: rename cypress test to match ci tests
    
    * hack: stopping the app might not release the port
    
    * reword outputs
    
    * refactor: use shorthand css value
    
    * refactor: cache digest with webpack-built assets
    
    also fetched cached assets before the build in the js integration test step

--- apps/site/config/test.exs
+++ apps/site/config/test.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :site, SiteWeb.Endpoint,
-  http: [port: 4002],
+  http: [port: System.get_env("PORT") || 4002],
   server: true
 
 # Print only warnings and errors during test
