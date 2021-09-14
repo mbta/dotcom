4db32a8950e1b271c033039968654adb37990220
Cristen Jones <cjones3@mbta.com>
2021-09-14 13:49:37 -0400

    cleanup(Site.MapHelpers.Markers): remove vehicle/1
    It looks at though this function let vehicles be rendered on the server-rendered google map. But it's no longer in use now.

--- apps/site/lib/site/map_helpers/markers.ex
+++ apps/site/lib/site/map_helpers/markers.ex
@@ -1,43 +1,6 @@
 defmodule Site.MapHelpers.Markers do
-  alias GoogleMaps.MapData.{Marker, Symbol}
-  alias Vehicles.Vehicle
-  alias Routes.Route
+  alias GoogleMaps.MapData.Marker
   alias Stops.Stop
-
-  @z_index %{
-    vehicle: 1_000
-  }
-
-  @doc """
-  Builds marker data for a vehicle that will be displayed in a Google Map.
-  """
-  @spec vehicle(VehicleTooltip.t()) :: Marker.t()
-  def vehicle(%VehicleTooltip{vehicle: %Vehicle{}} = data) do
-    Marker.new(
-      data.vehicle.latitude,
-      data.vehicle.longitude,
-      id: vehicle_marker_id(data.vehicle.id),
-      icon: vehicle_icon(data.vehicle, data.route),
-      tooltip: VehicleHelpers.tooltip(data),
-      z_index: @z_index.vehicle
-    )
-  end
-
-  def vehicle_marker_id(vehicle_id) when is_binary(vehicle_id) do
-    "vehicle-" <> vehicle_id
-  end
-
-  @spec vehicle_icon(Vehicle.t(), Route.t()) :: Symbol.t()
-  defp vehicle_icon(%Vehicle{} = vehicle, %Route{} = route) do
-    Symbol.new(
-      fill_color: "#" <> route.color,
-      fill_opacity: 1,
-      rotation: vehicle.bearing,
-      path: :forward_closed_arrow,
-      size: :tiny,
-      stroke_weight: 1
-    )
-  end
 
   @doc """
   Builds marker data for a stop that will be displayed in a Google Map.
