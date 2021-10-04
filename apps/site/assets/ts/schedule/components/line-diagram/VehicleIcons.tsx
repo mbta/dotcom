dccd4f23cb395eb0e237394fb418bb6b715751f6
Cristen Jones <cjones3@mbta.com>
2021-10-04 13:02:41 -0400

    refactor(VehicleIcons): Use vehicle tooltip text
    Instead of composing the tooltip text from other client-side data, use the newly available tooltip attribute.
    
    Also adjust the LineDiagramVehicle type to include the tooltip, and remove the now-redundant headsign and trip_name variables from it and related components and tests.

--- apps/site/assets/ts/schedule/components/line-diagram/VehicleIcons.tsx
+++ apps/site/assets/ts/schedule/components/line-diagram/VehicleIcons.tsx
@@ -5,55 +5,18 @@ import { RouteStop, LineDiagramVehicle } from "../__schedule";
 import CrowdingPill from "./CrowdingPill";
 import { TooltipWrapper, vehicleArrowIcon } from "../../../helpers/icon";
 import { StopCoord, CoordState, CIRC_RADIUS } from "./graphics/graphic-helpers";
-import { RouteType, HeadsignWithCrowding, Prediction } from "../../../__v3api";
-import {
-  vehicleRealtimeStatusText,
-  vehicleName
-} from "../../../models/vehicle";
-import { isACommuterRailRoute } from "../../../models/route";
-import { hasPredictionTime } from "../../../models/prediction";
 
 interface VehicleIconsProps {
   stop: RouteStop;
   vehicles: LineDiagramVehicle[] | null;
-  headsigns: HeadsignWithCrowding[];
 }
-
-const tooltipText = (
-  routeType: RouteType | null,
-  stopName: string | null,
-  vehicle: LineDiagramVehicle,
-  prediction: Prediction | null
-): string => {
-  const status =
-    stopName && stopName.length
-      ? `${vehicleRealtimeStatusText(vehicle)} ${stopName}`
-      : "";
-
-  if (routeType !== null) {
-    let baseText = vehicleName(routeType);
-    if (routeType === 2) baseText += ` ${vehicle.trip_name}`;
-
-    if (vehicle.headsign) {
-      const track =
-        prediction && prediction.track ? ` on Track ${prediction.track}` : "";
-
-      return `${vehicle.headsign} ${baseText.toLowerCase()} ${status}${track}`;
-    }
-
-    return `${baseText} ${status}`;
-  }
-
-  return `Vehicle ${status}`;
-};
 
 const CrowdingIconString = (vehicle: LineDiagramVehicle): string =>
   renderToString(<CrowdingPill crowding={vehicle.crowding} />);
 
 const VehicleIcons = ({
   stop,
-  vehicles,
-  headsigns
+  vehicles
 }: VehicleIconsProps): JSX.Element | null => {
   const coords: StopCoord | null = useSelector(
     (state: CoordState) => state[stop.id]
@@ -61,7 +24,6 @@ const VehicleIcons = ({
   if (!vehicles || !coords) return null;
   const [x, y] = coords;
   const left = `${x - CIRC_RADIUS}px`;
-  const routeType = stop.route ? stop.route.type : null;
   const tooltips = vehicles.map(vehicle => {
     const top = `${{
       // eslint-disable-next-line camelcase
@@ -69,18 +31,6 @@ const VehicleIcons = ({
       incoming: y - 25,
       stopped: y - 10
     }[vehicle.status] || y}px`;
-
-    const liveHeadsigns = headsigns.filter(hasPredictionTime);
-
-    let prediction = null;
-    if (!!stop.route && isACommuterRailRoute(stop.route)) {
-      const headsign = liveHeadsigns[0];
-      if (headsign) {
-        const predictedOrScheduledTime =
-          headsign.time_data_with_crowding_list[0].time_data;
-        ({ prediction } = predictedOrScheduledTime);
-      }
-    }
 
     return (
       <div
@@ -91,7 +41,7 @@ const VehicleIcons = ({
         <TooltipWrapper
           tooltipText={`<div class="m-schedule-diagram__vehicle-tooltip">${
             vehicle.crowding ? `${CrowdingIconString(vehicle)}<br/>` : ""
-          }${tooltipText(routeType, stop.name, vehicle, prediction)}</div>`}
+          }${vehicle.tooltip}</div>`}
           tooltipOptions={{ placement: "right", animation: false, html: true }}
         >
           {vehicleArrowIcon("m-schedule-diagram__vehicle--icon")}
