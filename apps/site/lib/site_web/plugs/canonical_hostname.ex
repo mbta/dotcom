f938665c574ab55117fcd66ffc1c2c6b6109e014
Cristen Jones <cjones3@mbta.com>
2021-10-05 19:16:08 -0400

    undo CanonicalHostname change

--- apps/site/lib/site_web/plugs/canonical_hostname.ex
+++ apps/site/lib/site_web/plugs/canonical_hostname.ex
@@ -27,12 +27,8 @@ defmodule SiteWeb.Plugs.CanonicalHostname do
       conn
     else
       rewritten_url =
-        if requested_hostname == "www.mbtace.com" do
-          "https://www.mbta.com" <> conn.request_path
-        else
-          Plug.Conn.request_url(conn)
-          |> String.replace(requested_hostname, canonical_hostname, global: false)
-        end
+        Plug.Conn.request_url(conn)
+        |> String.replace(requested_hostname, canonical_hostname, global: false)
 
       conn
       |> put_status(:moved_permanently)
