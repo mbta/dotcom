90d4a5b5b2c4e1c911ee5e638b6b2699e0cc48b2
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2021-10-08 10:15:47 -0400

    Fix: should be using Date instead of DateTime function (#1098)

--- apps/site/lib/site/transit_near_me.ex
+++ apps/site/lib/site/transit_near_me.ex
@@ -144,7 +144,7 @@ defmodule Site.TransitNearMe do
       {:error, error} ->
         _ =
           Logger.warn(
-            "module=#{__MODULE__} route_id=#{route_id} date=#{DateTime.to_string(date)} Other error fetching schedule: #{
+            "module=#{__MODULE__} route_id=#{route_id} date=#{Date.to_string(date)} Other error fetching schedule: #{
               inspect(error)
             }"
           )
