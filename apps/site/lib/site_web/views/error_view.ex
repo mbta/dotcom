fc9402ea9a32d792c33a561c8ca983855ac85c02
Hannah Purcell <hpurcell@mbta.com>
2021-10-27 10:19:37 -0400

    re-introduced the css removed from _errors.scss

--- apps/site/lib/site_web/views/error_view.ex
+++ apps/site/lib/site_web/views/error_view.ex
@@ -2,23 +2,31 @@ defmodule SiteWeb.ErrorView do
   use SiteWeb, :view
 
   def render("404.html", assigns) do
-    render(__MODULE__, "error_layout.html", Map.merge(assigns, %{
-      error_code: "404",
-      error_type: "Page not found",
-      error_title: "Sorry! We missed your stop.",
-      error_description: "This page is no longer in service.",
-      error_instructions: "Try searching for what you're looking for below."
-    }))
+    render(
+      __MODULE__,
+      "error_layout.html",
+      Map.merge(assigns, %{
+        error_code: "404",
+        error_type: "Page not found",
+        error_title: "Sorry! We missed your stop.",
+        error_description: "This page is no longer in service.",
+        error_instructions: "Try searching for what you're looking for below."
+      })
+    )
   end
 
   def render("500.html", assigns) do
-    render(__MODULE__, "error_layout.html", Map.merge(assigns, %{
-      error_code: "500",
-      error_type: "Server issue",
-      error_title: "Oh no! We're experiencing delays.",
-      error_description: "Something went wrong on our end.",
-      error_instructions: "Try searching for what you're looking for below."
-    }))
+    render(
+      __MODULE__,
+      "error_layout.html",
+      Map.merge(assigns, %{
+        error_code: "500",
+        error_type: "Server issue",
+        error_title: "Oh no! We're experiencing delays.",
+        error_description: "Something went wrong on our end.",
+        error_instructions: "Try searching for what you're looking for below."
+      })
+    )
   end
 
   # Stubbed out, but untested
