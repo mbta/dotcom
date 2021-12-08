d950fa1472f2923393c12754eada51925ea91e58
Hannah Purcell <hpurcell@mbta.com>
2021-12-08 14:06:18 -0500

    Prelim 62/76 updates

--- apps/routes/lib/route.ex
+++ apps/routes/lib/route.ex
@@ -213,8 +213,6 @@ defmodule Routes.Route do
   def hidden?(%{id: "3233"}), do: true
   def hidden?(%{id: "3738"}), do: true
   def hidden?(%{id: "4050"}), do: true
-  def hidden?(%{id: "62"}), do: true
-  def hidden?(%{id: "76"}), do: true
   def hidden?(%{id: "725"}), do: true
   def hidden?(%{id: "8993"}), do: true
   def hidden?(%{id: "116117"}), do: true
