4cc219759d0af7c4facde5777154461411bdd008
Ceesay <iceesay@CTD25363ICEESAY.local>
2022-02-04 15:46:20 -0500

    note

--- apps/location_service/config/config.exs
+++ apps/location_service/config/config.exs
@@ -1,5 +1,6 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
+# TODO: add aws keys and host(https://places.geo.us-east-1.amazonaws.com)
 use Mix.Config
 
 config :location_service, :http_pool, :google_http_pool
@@ -8,7 +9,7 @@ if Mix.env() == :prod do
   config :location_service,
     google_api_key: "${GOOGLE_API_KEY}",
     google_client_id: "${GOOGLE_MAPS_CLIENT_ID}",
-    google_signing_key: "${GOOGLE_MAPS_SIGNING_KEY}"
+    google_signing_key: "${GOOGLE_MAPS_SIGNING_KEY}",
 else
   config :location_service,
     google_api_key: System.get_env("GOOGLE_API_KEY"),
