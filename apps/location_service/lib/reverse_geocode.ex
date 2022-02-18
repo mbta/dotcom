488682878ed3a11fc11a3eded215112ac947270d
Cristen Jones <cjones3@mbta.com>
2022-02-18 12:05:33 -0500

    adjust cache key to be one argument (a tuple) instead of two numbers

--- apps/location_service/lib/reverse_geocode.ex
+++ apps/location_service/lib/reverse_geocode.ex
@@ -9,7 +9,7 @@ defmodule LocationService.ReverseGeocode do
   Caches the result using the input address as key."
   @spec reverse_geocode(number, number) :: LocationService.Address.t() | GoogleMaps.Geocode.t()
   def reverse_geocode(latitude, longitude) when is_float(latitude) and is_float(longitude) do
-    cache(latitude, longitude, fn latitude, longitude ->
+    cache({latitude, longitude}, fn {latitude, longitude} ->
       case Application.get_env(:location_service, :reverse_geocode) do
         :aws -> LocationService.AWS.reverse_geocode(latitude, longitude)
         _ -> GoogleMaps.Geocode.reverse_geocode(latitude, longitude)
