924c827852a9c8fa1731b5cf93a38328c47d39e0
zovt <code@idylls.net>
2022-02-28 10:55:17 -0500

    Testing data

--- apps/site/assets/ts/schedule/components/line-diagram/LineDiagramWithStops.tsx
+++ apps/site/assets/ts/schedule/components/line-diagram/LineDiagramWithStops.tsx
@@ -3,6 +3,7 @@ import { hasBranchLines } from "./line-diagram-helpers";
 import Diagram from "./graphics/Diagram";
 import StopListWithBranches from "./StopListWithBranches";
 import { CommonLineDiagramProps } from "./__line-diagram";
+import { LineDiagramStop } from "../__schedule";
 import useStopPositions, { RefMap } from "./graphics/useStopPositions";
 import StopCard from "./StopCard";
 import { hasPredictionTime } from "../../../models/prediction";
@@ -15,7 +16,57 @@ export const StopRefContext = React.createContext<[RefMap, () => void]>([
 const LineDiagramWithStops = (
   props: CommonLineDiagramProps
 ): ReactElement<HTMLElement> => {
-  const { stops, handleStopClick, liveData } = props;
+  const { stops: stops_, handleStopClick, liveData } = props;
+  // FIXME
+  let stops = stops_;
+  const northStation = stops_.find(s => s.route_stop.name === "North Station");
+  if (northStation) {
+    const fake = (name: string, stopData: LineDiagramStop['stop_data'], addl?: Partial<LineDiagramStop['route_stop']>): LineDiagramStop => {
+      const out = { ...northStation };
+      out.alerts = [];
+      out.route_stop = {
+        ...out.route_stop,
+        ...(addl || {}),
+        name,
+        id: name.toLowerCase(),
+        zone: null,
+      };
+      out.stop_data = stopData;
+
+      return out;
+    };
+
+    const extraA: LineDiagramStop[] = [
+      fake("Test Station C", [
+          { "has_disruption?": false, branch: null, type: 'stop' },
+      ]),
+      fake("Test Station A", [
+          { "has_disruption?": false, branch: null, type: 'line' },
+          { "has_disruption?": false, branch: 'Green Test Split', type: 'terminus' },
+      ]),
+      fake("Test Station B", [
+          { "has_disruption?": false, branch: null, type: 'merge' },
+          { "has_disruption?": false, branch: null, type: 'merge' },
+      ]),
+    ];
+    const extraB: LineDiagramStop[] = [
+      fake("Test Station B", [
+          { "has_disruption?": false, branch: null, type: 'merge' },
+          { "has_disruption?": false, branch: 'Green Test Split', type: 'merge' },
+      ], { "is_terminus?": false,} ),
+      fake("Test Station A", [
+          { "has_disruption?": false, branch: null, type: 'line' },
+          { "has_disruption?": false, branch: 'Green Test Split', type: 'terminus' },
+      ], { "is_terminus?": true, branch: "Green Test Split", }),
+      fake("Test Station C", [
+          { "has_disruption?": false, branch: null, type: 'stop' },
+      ]),
+    ];
+    stops = stops_[0].route_stop.name === 'North Station'
+      ? [...extraA, ...stops_]
+      : [...stops_, ...extraB];
+    console.debug({stops});
+  }
 
   // create a ref for each stop - we will use this to track the location of the stop so we can place the line diagram bubbles
   const [stopRefsMap, updateAllStopCoords] = useStopPositions(stops);
@@ -40,7 +91,7 @@ const LineDiagramWithStops = (
       >
         <Diagram lineDiagram={stops} liveData={liveData} />
         {hasBranchLines(stops) ? (
-          <StopListWithBranches {...props} />
+          <StopListWithBranches {...props} stops={stops} />
         ) : (
           <ol>
             {stops.map(stop => (
