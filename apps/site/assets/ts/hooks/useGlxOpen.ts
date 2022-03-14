4d447fe0d88c1e33f5d3ed47debb0ded3ff874ff
Brian Fauble <bfauble@users.noreply.github.com>
2022-03-14 09:50:49 -0500

    extract helper function to be able to use in non-react code

--- apps/site/assets/ts/hooks/useGlxOpen.ts
+++ apps/site/assets/ts/hooks/useGlxOpen.ts
@@ -2,16 +2,22 @@ import { useEffect, useState } from "react";
 
 const glxStations = ["place-lech", "place-unsqu", "place-spmnl"];
 
+export const getIsGlxOpen = (stationId: string) => {
+  const glxOpen = document.querySelector(".glx-is-open");
+  console.log(glxOpen)
+  if (glxOpen instanceof HTMLElement) {
+    return glxOpen.dataset.open === "true" && glxStations.indexOf(stationId) > 0;
+  }
+  return false;
+}
+
 const useGlxOpen = (stationId: string): boolean => {
   const [isGlxOpen, setIsGlxOpen] = useState(false);
   useEffect(() => {
-    const glxOpen = document.querySelector(".glx-is-open");
-    if (glxOpen instanceof HTMLElement) {
-      setIsGlxOpen(glxOpen.dataset.open === "true");
-    }
+    setIsGlxOpen(getIsGlxOpen(stationId));
   }, []);
 
-  return isGlxOpen && glxStations.indexOf(stationId) > 0;
+  return isGlxOpen;
 };
 
 export default useGlxOpen;
