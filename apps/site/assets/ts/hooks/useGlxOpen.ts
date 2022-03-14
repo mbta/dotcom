2531eed18f4221d59e5e1a0cd127766555621487
Brian Fauble <bfauble@users.noreply.github.com>
2022-03-14 10:37:32 -0500

    lint

--- apps/site/assets/ts/hooks/useGlxOpen.ts
+++ apps/site/assets/ts/hooks/useGlxOpen.ts
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 
 const glxStations = ["place-lech", "place-unsqu", "place-spmnl"];
 
-export const getIsGlxOpen = (stationId: string) => {
+export const getIsGlxOpen = (stationId: string): boolean => {
   const glxOpen = document.querySelector(".glx-is-open");
 
   if (glxOpen instanceof HTMLElement) {
-    return glxOpen.dataset.open === "true" && glxStations.indexOf(stationId) > 0;
+    return (
+      glxOpen.dataset.open === "true" && glxStations.indexOf(stationId) > 0
+    );
   }
   return false;
-}
+};
 
 const useGlxOpen = (stationId: string): boolean => {
   const [isGlxOpen, setIsGlxOpen] = useState(false);
-  
+
   useEffect(() => {
     setIsGlxOpen(getIsGlxOpen(stationId));
   }, [stationId]);
