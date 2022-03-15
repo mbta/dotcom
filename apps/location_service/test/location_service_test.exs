6ca0deaf97043d62ab2f99a46577fd254c26a6e3
Cristen Jones <cjones3@mbta.com>
2022-03-15 16:20:02 -0400

    fixup for refactor: simplify logic for active service

--- apps/location_service/test/location_service_test.exs
+++ apps/location_service/test/location_service_test.exs
@@ -4,24 +4,25 @@ defmodule LocationServiceTest do
   import LocationService
   import Mock
 
+  setup do
+    old_value = System.get_env("LOCATION_SERVICE")
+    System.put_env("LOCATION_SERVICE", "")
+
+    on_exit(fn ->
+      System.put_env("LOCATION_SERVICE", old_value)
+    end)
+  end
+
   describe "geocode/1" do
-    setup do
-      old_value = Application.get_env(:location_service, :geocode)
-
-      on_exit(fn ->
-        Application.put_env(:location_service, :geocode, old_value)
-      end)
-    end
-
-    test "selects function based on application environment variable" do
+    test "selects function based on system environment variable" do
       with_mocks [
         {AWSLocation, [], [geocode: fn _ -> "i use the amazon one" end]},
         {GoogleMaps.Geocode, [], [geocode: fn _ -> "i use the google one" end]}
       ] do
-        Application.put_env(:location_service, :geocode, :google)
+        System.put_env("LOCATION_SERVICE", "google")
         assert "i use the google one" = geocode("a thing")
 
-        Application.put_env(:location_service, :geocode, :aws)
+        System.put_env("LOCATION_SERVICE", "aws")
         assert "i use the amazon one" = geocode("some other thing")
       end
     end
@@ -34,7 +35,7 @@ defmodule LocationServiceTest do
         {AWSLocation, [], [geocode: mock_return_address]},
         {GoogleMaps.Geocode, [], [geocode: mock_return_address]}
       ] do
-        Application.put_env(:location_service, :geocode, :aws)
+        System.put_env("LOCATION_SERVICE", "aws")
         address1 = "a first place"
         {^address1, time1} = geocode(address1)
         :timer.sleep(1000)
@@ -42,7 +43,7 @@ defmodule LocationServiceTest do
         # the second call should return the cached result
         assert DateTime.compare(time1, time2) == :eq
 
-        Application.put_env(:location_service, :geocode, :google)
+        System.put_env("LOCATION_SERVICE", "google")
         address2 = "another place"
         {^address2, time3} = geocode(address2)
         :timer.sleep(1000)
@@ -53,23 +54,15 @@ defmodule LocationServiceTest do
   end
 
   describe "reverse_geocode/1" do
-    setup do
-      old_value = Application.get_env(:location_service, :reverse_geocode)
-
-      on_exit(fn ->
-        Application.put_env(:location_service, :reverse_geocode, old_value)
-      end)
-    end
-
-    test "selects function based on application environment variable" do
+    test "selects function based on system environment variable" do
       with_mocks [
         {AWSLocation, [], [reverse_geocode: fn _, _ -> "i use the amazon one" end]},
         {GoogleMaps.Geocode, [], [reverse_geocode: fn _, _ -> "i use the google one" end]}
       ] do
-        Application.put_env(:location_service, :reverse_geocode, :google)
+        System.put_env("LOCATION_SERVICE", "google")
         assert "i use the google one" = reverse_geocode(71.56890, -68.5285)
 
-        Application.put_env(:location_service, :reverse_geocode, :aws)
+        System.put_env("LOCATION_SERVICE", "aws")
         assert "i use the amazon one" = reverse_geocode(71.56890, 40.5285)
       end
     end
@@ -82,7 +75,7 @@ defmodule LocationServiceTest do
         {AWSLocation, [], [reverse_geocode: mock_return_coordinates]},
         {GoogleMaps.Geocode, [], [reverse_geocode: mock_return_coordinates]}
       ] do
-        Application.put_env(:location_service, :reverse_geocode, :aws)
+        System.put_env("LOCATION_SERVICE", "aws")
         lat1 = 71.596
         long1 = -68.321
         {^lat1, ^long1, time1} = reverse_geocode(lat1, long1)
@@ -91,7 +84,7 @@ defmodule LocationServiceTest do
         # the second call should return the cached result
         assert DateTime.compare(time1, time2) == :eq
 
-        Application.put_env(:location_service, :reverse_geocode, :google)
+        System.put_env("LOCATION_SERVICE", "google")
         lat2 = 42.0
         long2 = 71.55
         {^lat2, ^long2, time3} = reverse_geocode(lat2, long2)
@@ -103,36 +96,23 @@ defmodule LocationServiceTest do
   end
 
   describe "autocomplete/3" do
-    setup do
-      old_value = Application.get_env(:location_service, :autocomplete)
-
-      on_exit(fn ->
-        Application.put_env(:location_service, :autocomplete, old_value)
-      end)
-    end
-
     test "selects function based on application environment variable" do
       with_mocks [
         {AWSLocation, [], [autocomplete: fn _, _ -> "i use the amazon one" end]},
         {LocationService.Wrappers, [],
          [google_autocomplete: fn _, _, _ -> "i use the google one" end]}
       ] do
-        Application.put_env(
-          :location_service,
-          :autocomplete,
-          {:system, "LOCATION_SERVICE", :google}
-        )
-
+        System.put_env("LOCATION_SERVICE", "google")
         assert "i use the google one" = autocomplete("a thing", 2, nil)
 
-        Application.put_env(:location_service, :autocomplete, {:system, "LOCATION_SERVICE", :aws})
+        System.put_env("LOCATION_SERVICE", "aws")
         assert "i use the amazon one" = autocomplete("some other thing", 2, nil)
       end
     end
 
     test "is cached" do
       with_mock AWSLocation, [], autocomplete: fn _, _ -> DateTime.utc_now() end do
-        Application.put_env(:location_service, :autocomplete, {:system, "LOCATION_SERVICE", :aws})
+        System.put_env("LOCATION_SERVICE", "aws")
         t1 = autocomplete("cached", 2, nil)
         t2 = autocomplete("cached", 2, nil)
         assert t1 == t2
