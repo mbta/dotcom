72f788ec6ebd26fe3a3c1545f3296c3232c1b5ef
Brian Fauble <bfauble@users.noreply.github.com>
2022-03-17 12:40:04 -0500

    simplify glx open component. return comma delimited list of stations instead of boolean to keep list of stations in one place. clean up css issues

--- apps/site/assets/ts/components/GlxOpen.tsx
+++ apps/site/assets/ts/components/GlxOpen.tsx
@@ -1,7 +1,18 @@
 import React, { ReactElement } from "react";
 import renderSvg from "../helpers/render-svg";
-import useGlxOpen from "../hooks/useGlxOpen";
 import glxLogo from "../../static/images/glx-logo.svg";
+
+const getIsGlxOpen = (stationId: string): boolean => {
+  const glxStationsOpen = document.querySelector(".glx-stations-open");
+
+  if (
+    glxStationsOpen instanceof HTMLElement &&
+    glxStationsOpen.dataset.stations
+  ) {
+    return glxStationsOpen.dataset.stations.split(",").indexOf(stationId) > 0;
+  }
+  return false;
+};
 
 const glxLogoElement = (): JSX.Element => renderSvg("glx-logo", glxLogo);
 
@@ -11,8 +22,8 @@ const GlxOpen = ({
 }: {
   stationPage: boolean;
   stopId: string;
-}): ReactElement<HTMLElement> => {
-  const isGlxOpen = useGlxOpen(stopId);
+}): ReactElement<HTMLElement> | null => {
+  const isGlxOpen = getIsGlxOpen(stopId);
   let textContent;
 
   if (stationPage) {
@@ -30,7 +41,7 @@ const GlxOpen = ({
     );
   }
 
-  return <div className="glx-open-container" />;
+  return null;
 };
 
 export default GlxOpen;
