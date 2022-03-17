0187c27610f2a5eba615460a9dffcd1cb2eddd21
Brian Fauble <bfauble@users.noreply.github.com>
2022-03-17 15:21:26 -0500

    add glx now open component and styling for stop page (#1223)
    * add glx now open component and styling for station pages
    
    * fix formatting issues
    
    * update snapshots
    
    * extract helper function to be able to use in non-react code
    
    * use stationId as useEffect param to make sure value updates if stationId changes
    
    * lint
    
    * lint
    
    * simplify glx open component. return comma delimited list of stations instead of boolean to keep list of stations in one place. clean up css issues
    
    * update snapshots
    
    * add GlxOpen component to react renderer
    
    * fix document reference error for server side rendering
    
    * lint
    
    Co-authored-by: Cristen Jones <cjones3@mbta.com>

---
+++ apps/site/assets/ts/components/GlxOpen.tsx
@@ -0,0 +1,52 @@
+import React, { ReactElement, useState, useEffect } from "react";
+import renderSvg from "../helpers/render-svg";
+import glxLogo from "../../static/images/glx-logo.svg";
+
+const getIsGlxOpen = (stationId: string): boolean => {
+  if (!document) return false;
+
+  const glxStationsOpen = document.querySelector(".glx-stations-open");
+
+  if (
+    glxStationsOpen instanceof HTMLElement &&
+    glxStationsOpen.dataset.stations
+  ) {
+    return glxStationsOpen.dataset.stations.split(",").indexOf(stationId) > 0;
+  }
+  return false;
+};
+
+const glxLogoElement = (): JSX.Element => renderSvg("glx-logo", glxLogo);
+
+const GlxOpen = ({
+  stationPage = false,
+  stopId
+}: {
+  stationPage: boolean;
+  stopId: string;
+}): ReactElement<HTMLElement> | null => {
+  const [isGlxOpen, setIsGlxOpen] = useState(false);
+  useEffect(() => {
+    setIsGlxOpen(getIsGlxOpen(stopId));
+  }, [stopId]);
+  let textContent;
+
+  if (stationPage) {
+    textContent = "STATION NOW OPEN";
+  } else {
+    textContent = "STATION OPEN";
+  }
+
+  if (isGlxOpen) {
+    return (
+      <div className="glx-open-container">
+        {glxLogoElement()}
+        <span className="glx-open-message">{textContent}</span>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default GlxOpen;
