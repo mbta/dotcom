042edd5ece9cfb07ed1c440024a5048f79911508
zovt <code@idylls.net>
2022-03-21 12:28:50 -0400

    Always render accessibility icon for bus stops on transit near me

--- apps/site/assets/ts/components/StopCard.tsx
+++ apps/site/assets/ts/components/StopCard.tsx
@@ -76,8 +76,12 @@ export const StopCard = ({
         <a href={stop.href} className="m-tnm-sidebar__stop-name">
           {renderStopIcon(stop)}
           {stop.name}
-          {!!stop.accessibility.length &&
-            !stop.accessibility.includes("unknown") &&
+          {(
+            // NOTE: Bus stops are always considered accessible, see
+            // https://app.asana.com/0/1201653980996886/1201894234147725/f
+            route.type === 3 ||
+            (!!stop.accessibility.length &&
+              !stop.accessibility.includes("unknown"))) &&
             accessibleIcon("m-tnm-sidebar__stop-accessible")}
         </a>
         <div className="m-tnm-sidebar__stop-distance">{distance}</div>
