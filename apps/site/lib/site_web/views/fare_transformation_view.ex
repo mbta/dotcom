839965d1a8e5a5eb0adcfdfcea377313dec06eee
Brian Fauble <bfauble@users.noreply.github.com>
2022-04-05 14:39:25 -0500

    add to fare transformation, proposed sales location view

--- apps/site/lib/site_web/views/fare_transformation_view.ex
+++ apps/site/lib/site_web/views/fare_transformation_view.ex
@@ -3,4 +3,14 @@ defmodule SiteWeb.FareTransformationView do
   View for the Fare Transformation section of the website.
   """
   use SiteWeb, :view
+
+  alias GoogleMaps.Geocode.Address
+
+  defp input_value(%Address{formatted: address}) do
+    address
+  end
+
+  defp input_value(_) do
+    ""
+  end
 end
