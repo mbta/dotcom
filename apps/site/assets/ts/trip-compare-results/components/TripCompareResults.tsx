24c5966d7de6d72f5359f29afb8f2239a287778a
Lev Boyarskiy <lboyarsky@mbta.com>
2022-05-09 12:16:21 -0700

    Reformatted

--- apps/site/assets/ts/trip-compare-results/components/TripCompareResults.tsx
+++ apps/site/assets/ts/trip-compare-results/components/TripCompareResults.tsx
@@ -1,10 +1,9 @@
 import React, { ReactElement, useState } from "react";
 import TripPlannerResults, { Itinerary } from "./TripPlannerResults";
 
-
 interface Props {
   // eslint-disable-next-line
-  itineraryData: Itinerary[],
+  itineraryData: Itinerary[];
   itineraryHeader: string;
 }
 
@@ -12,39 +11,45 @@ const TripCompareResults = ({
   itineraryData,
   itineraryHeader
 }: Props): ReactElement<HTMLElement> => {
-    const [source, setSource] = useState("NEW");
+  const [source, setSource] = useState("NEW");
 
-    let filteredData = itineraryData.filter(i => i.source == source);
-    let classRedesigned = source !== "NEW" ? "m-alerts__mode-button" : "m-alerts__mode-button m-alerts__mode-button--selected";
-    let classCurrent = source === "NEW" ? "m-alerts__mode-button" : "m-alerts__mode-button m-alerts__mode-button--selected";
+  const filteredData = itineraryData.filter(i => i.source === source);
+  const classRedesigned =
+    source !== "NEW"
+      ? "m-alerts__mode-button"
+      : "m-alerts__mode-button m-alerts__mode-button--selected";
+  const classCurrent =
+    source === "NEW"
+      ? "m-alerts__mode-button"
+      : "m-alerts__mode-button m-alerts__mode-button--selected";
 
-    return (
-      <>
-        <div>
-          <div className="m-alerts__mode-buttons">
-            <a className="m-alerts__mode-button-container" role="button">
-              <div className={classRedesigned} onClick={() => setSource("NEW")}>
-                <div className="m-alerts__mode-button-name">Redesigned</div>
-              </div>
-            </a>
-            <a className="m-alerts__mode-button-container" role="button">
-              <div className={classCurrent} onClick={() => setSource("CURRENT")}>
-                <div className="m-alerts__mode-button-name">Current</div>
-              </div>
-            </a>
+  return (
+    <>
+      <div>
+        <div className="m-alerts__mode-buttons">
+          <div className="m-alerts__mode-button-container" role="button">
+            <div className={classRedesigned} onClick={() => setSource("NEW")} >
+              <div className="m-alerts__mode-button-name">Redesigned</div>
+            </div>
           </div>
-          <p className={`no-trips page-section`}>
-            We found {filteredData.length} trips for you
-          </p>
-          <p className={`instructions page-section`}>{itineraryHeader}</p>
-          <p className={`instructions page-section`}>
-            <b>Note:</b> the following trips only reflect normal service (i.e. do not reflect 
-            current service, does not include service alert info)
-          </p>
-          <TripPlannerResults itineraryData={filteredData} />
+          <div className="m-alerts__mode-button-container" role="button">
+            <div className={classCurrent} onClick={() => setSource("CURRENT")}>
+              <div className="m-alerts__mode-button-name">Current</div>
+            </div>
+          </div>
         </div>
-      </>
-    );
-  };
+        <p className={`no-trips page-section`}>
+          We found {filteredData.length} trips for you
+        </p>
+        <p className={`instructions page-section`}>{itineraryHeader}</p>
+        <p className={`instructions page-section`}>
+          <b>Note:</b> the following trips only reflect normal service (i.e. do
+          not reflect current service, does not include service alert info)
+        </p>
+        <TripPlannerResults itineraryData={filteredData} />
+      </div>
+    </>
+  );
+};
 
 export default TripCompareResults;
