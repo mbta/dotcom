fdea79a45df4fcd51b0174b561a75a15c273c19e
Lev Boyarskiy <lboyarsky@mbta.com>
2022-05-09 17:09:59 -0700

    Addressed ESLint issues

--- apps/site/assets/ts/trip-compare-results/components/TripCompareResults.tsx
+++ apps/site/assets/ts/trip-compare-results/components/TripCompareResults.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, useState } from "react";
+import { handleReactEnterKeyPress } from "../../helpers/keyboard-events";
 import TripPlannerResults, { Itinerary } from "./TripPlannerResults";
 
 interface Props {
@@ -23,26 +24,41 @@ const TripCompareResults = ({
       ? "m-alerts__mode-button"
       : "m-alerts__mode-button m-alerts__mode-button--selected";
 
+  const onClickNew = (): void => setSource("NEW");
+  const onClickCurrent = (): void => setSource("CURRENT");
+
   return (
     <>
       <div>
         <div className="m-alerts__mode-buttons">
-          <div className="m-alerts__mode-button-container" role="button">
-            <div className={classRedesigned} onClick={() => setSource("NEW")} >
+          <div className="m-alerts__mode-button-container">
+            <div
+              className={classRedesigned}
+              onClick={onClickNew}
+              onKeyPress={e => handleReactEnterKeyPress(e, onClickNew)}
+              role="button"
+              tabIndex={0}
+            >
               <div className="m-alerts__mode-button-name">Redesigned</div>
             </div>
           </div>
-          <div className="m-alerts__mode-button-container" role="button">
-            <div className={classCurrent} onClick={() => setSource("CURRENT")}>
+          <div className="m-alerts__mode-button-container">
+            <div
+              className={classCurrent}
+              onClick={onClickCurrent}
+              onKeyPress={e => handleReactEnterKeyPress(e, onClickCurrent)}
+              role="button"
+              tabIndex={0}
+            >
               <div className="m-alerts__mode-button-name">Current</div>
             </div>
           </div>
         </div>
-        <p className={`no-trips page-section`}>
+        <p className="no-trips page-section">
           We found {filteredData.length} trips for you
         </p>
-        <p className={`instructions page-section`}>{itineraryHeader}</p>
-        <p className={`instructions page-section`}>
+        <p className="instructions page-section">{itineraryHeader}</p>
+        <p className="instructions page-section">
           <b>Note:</b> the following trips only reflect normal service (i.e. do
           not reflect current service, does not include service alert info)
         </p>
