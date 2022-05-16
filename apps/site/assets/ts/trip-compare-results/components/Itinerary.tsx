b1bb5a44f112429359743d693166851350c114dc
lboyarsky <45011335+lboyarsky@users.noreply.github.com>
2022-05-16 07:04:03 -0700

    Trip Comparison tool MVP (#1294)
    * Initial version of the new trip compare page
    
    * Hide fare and accessibility info
    
    * Reformatted
    
    * Addressed ESLint issues
    
    * Formatted elixir code
    
    * Removed alerts from trip compare
    
    * Update copy
    
    * Reformat
    
    * Added JS tests
    
    * Added elixir tests
    
    * Fixing the tests & reformatted
    
    * More test fixes
    
    * Updated trip compare views to not use trip plan views
    
    * Switching from trip_plan_path to trip_compare_path helpers
    
    * Added a new redirect route for /trip_compare
    
    * Updated trip compare controller from the latest version of trip plan controller
    
    * Added extra routes for trip compare
    
    * One more test fix :D
    
    * Follow-up on the previous fix..
    
    * PR feedback: fix typo
    
    * PR Feedback: don't crash in case of error
    
    * Update copy and link to Qualtrics survey
    
    * Use Bus Network Redesign instead of BNRD for clarity
    
    * Squashed commit of the following:
    
    commit ee473ea60e5f63fed9a6e3e12d63da2d07953115
    Author: Brian Fauble <bfauble@users.noreply.github.com>
    Date:   Mon May 16 08:22:57 2022 -0500
    
        Upgrade Font Awesome (#1291)
    
        * run sass-lang auto migrator to remove division warnings in scss files
    
        * replace fontawesome 4.6.3 files with 6.1.1
    
        * create entry file for easier inclusion of fa styles
    
        * update fa helper function to accommodate other font family types
    
        * update caret helper to include fa class
    
        * Update past uses of fa to work with the updated version
    
        * remove unnecessary added class
    
        * cleanup and add test for additional font family test case
    
        * undo package-lock.json changes
    
        * update snapshots to include newly added fa class
    
        * adjust arrow size to match text on cr timetable
    
    commit be3563e047e9c10212f9f6e8b47754322ba62d33
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 15:51:52 2022 -0400
    
        fixup: add more useful Slack message for workflow
    
    commit d284f537e6dc0ab7b30cfa63e93c1e1a8716d9c3
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 15:14:01 2022 -0400
    
        fixup: use ubuntu-latest for Slack notification
    
    commit aa439867de91a6dca0ad02c022d8ec590ae729b2
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 14:38:45 2022 -0400
    
        fixup: correct ubuntu version for workflow
    
    commit 80fd363daa1cb46504b43e9bb8952328755b0974
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 14:08:48 2022 -0400
    
        fixup: correct syntax for workflow
    
    commit bfd535964dbc1c3225709d895a41b1732a2bd676
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 14:04:57 2022 -0400
    
        ci: add ECS deploy workflow
    
        Allow users to manually select environment value
    
    commit 710fb4a07834b19f5f299f2860f9ed252d58b183
    Author: Cristen Jones <cjones3@mbta.com>
    Date:   Sat May 14 13:39:21 2022 -0400
    
        Remove "old" (#1293)
    
        * deprecate OldSiteFileController
    
        * deprecate OldSiteRedirectController
    
        * remove link to old.mbta.com
    
        * tests: update with new trip ID
    
        * tests: add shuttle child stop
    
    commit ea7eea8cda9ebcd973b7c27a4e3ef366b9cc91f0
    Author: Adamo Maisano <amaisano@mbta.com>
    Date:   Sat May 14 13:13:53 2022 -0400
    
        update structs to support incoming API changes (#1299)
    
        - projects and project_updates are getting a "redirects" field
    
    * update snapshot with new copy
    
    Co-authored-by: Steve Lund <lund5000@gmail.com>
    Co-authored-by: Cristen Jones <cjones3@mbta.com>

---
+++ apps/site/assets/ts/trip-compare-results/components/Itinerary.tsx
@@ -0,0 +1,102 @@
+import React, { ReactElement, useEffect } from "react";
+import Accordion, { AccordionNoJS } from "../../components/Accordion";
+import { Itinerary } from "./TripPlannerResults";
+import Map from "../../leaflet/components/Map";
+import {
+  addAlertItemEventHandlers,
+  removeAlertItemEventHandlers
+} from "../../../js/alert-item";
+
+/*
+The following ignored functions are vanilla JS event handlers
+that are for HTML that gets injected into itinerary rows.
+Ideally, all the HTML of this component would be built in React
+which would make it easy to test in this file. Since that HTML is
+server rendered and injected, it is difficult to test from jest.
+It is being ignored now to prevent a codecov ding.
+*/
+
+/* istanbul ignore next */
+const toggleTextContent = (e: Event): void => {
+  if ((e.target! as HTMLElement).textContent!.trim() === "(view alert)") {
+    (e.target! as HTMLElement).textContent = "(hide alert)";
+  } else {
+    (e.target! as HTMLElement).textContent = "(view alert)";
+  }
+};
+
+/* istanbul ignore next */
+export const addToggleAlertHandlers = (): void => {
+  Array.from(document.querySelectorAll(`.js-trip-plan-alert-toggle`)).forEach(
+    elem => {
+      elem.addEventListener("click", toggleTextContent);
+    }
+  );
+};
+
+/* istanbul ignore next */
+export const removeToggleAlertHandlers = (): void => {
+  Array.from(document.querySelectorAll(`.js-trip-plan-alert-toggle`)).forEach(
+    elem => {
+      elem.removeEventListener("click", toggleTextContent);
+    }
+  );
+};
+
+interface Props {
+  itinerary: Itinerary;
+}
+
+const ItineraryBody = (itinerary: Itinerary): ReactElement<HTMLElement> => {
+  useEffect(() => {
+    addAlertItemEventHandlers();
+    addToggleAlertHandlers();
+    /* istanbul ignore next */
+    return () => {
+      removeAlertItemEventHandlers();
+      removeToggleAlertHandlers();
+    };
+  }, []);
+
+  return (
+    <div className="m-trip-plan-results__itinerary-body">
+      <div className="trip-plan-map map">
+        <Map mapData={itinerary.map} boundsByMarkers />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: itinerary.html }} // eslint-disable-line react/no-danger
+      />
+    </div>
+  );
+};
+
+const ItineraryAccordion = ({
+  itinerary
+}: Props): ReactElement<HTMLElement> => (
+  <div className="m-trip-plan-results__itinerary">
+    <div className="m-trip-plan-results__itinerary-header">
+      <div className="m-trip-plan-results__itinerary-header-content">
+        <div
+          className="m-trip-plan-results__itinerary-summary"
+          dangerouslySetInnerHTML={{ __html: itinerary.tab_html }} // eslint-disable-line react/no-danger
+        />
+      </div>
+    </div>
+    <Accordion
+      id={`itinerary-${itinerary.id}`}
+      title={{
+        collapsed: "Show map and trip details",
+        expanded: "Hide map and trip details"
+      }}
+    >
+      <ItineraryBody {...itinerary} />
+    </Accordion>
+    <noscript>
+      <AccordionNoJS id={`itinerary-${itinerary.id}`}>
+        <ItineraryBody {...itinerary} />
+      </AccordionNoJS>
+    </noscript>
+  </div>
+);
+
+export default ItineraryAccordion;
