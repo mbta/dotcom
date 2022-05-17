9acb2c32499c9bcbaefb31c462042c5abd3bd5bd
Brian Fauble <bfauble@users.noreply.github.com>
2022-05-17 09:28:45 -0500

    update js for handling keyboard selection and cleanup some code.

--- apps/site/assets/js/tabbed-nav.js
+++ apps/site/assets/js/tabbed-nav.js
@@ -1,20 +1,31 @@
+const updateTabs = (tabType, navTabs, contentTabs) => e => {
+  e.preventDefault();
+
+  navTabs.forEach(tab => {
+    const clickedTab = tab.dataset.tabType === tabType;
+    tab.classList.toggle("active", clickedTab);
+    tab.toggleAttribute("aria-selected", clickedTab);
+  });
+
+  contentTabs.forEach(tab => {
+    const clickedTabContentType = tab.dataset.tabContentType === tabType;
+    tab.classList.toggle("active", clickedTabContentType);
+  });
+};
+
 const tabbedNavSetup = () => {
   const navTabs = document.querySelectorAll(".m-tabbed-nav__item");
   const contentTabs = document.querySelectorAll(".m-tabbed-nav__content-item");
 
   navTabs.forEach(item => {
     const { tabType } = item.dataset;
+    const callback = updateTabs(tabType, navTabs, contentTabs);
 
-    item.addEventListener("click", e => {
-      e.preventDefault();
-
-      navTabs.forEach(tab => tab.classList.remove("active"));
-      contentTabs.forEach(tab => tab.classList.remove("active"));
-      const contentTab = document.querySelector(
-        `.m-tabbed-nav__content-item.${tabType}`
-      );
-      item.classList.add("active");
-      contentTab.classList.add("active");
+    item.addEventListener("click", callback);
+    item.addEventListener("keyup", e => {
+      if (e.key === "Enter") {
+        callback(e);
+      }
     });
   });
 };
