738e6c9a7119927b4e94f7b043dab8d362d6dc74
zovt <code@idylls.net>
2022-05-18 10:42:37 -0400

    Use function pattern instead of case

--- apps/alerts/lib/accessibility.ex
+++ apps/alerts/lib/accessibility.ex
@@ -8,14 +8,10 @@ defmodule Alerts.Accessibility do
   def effect_types, do: [:elevator_closure, :escalator_closure, :access_issue]
 
   @spec is_accessibility_alert?(Alert.t()) :: boolean()
-  def is_accessibility_alert?(alert) do
-    case alert.effect do
-      :elevator_closure -> true
-      :escalator_closure -> true
-      :access_issue -> true
-      _ -> false
-    end
-  end
+  def is_accessibility_alert?(%Alert{effect: :elevator_closure}), do: true
+  def is_accessibility_alert?(%Alert{effect: :escalator_closure}), do: true
+  def is_accessibility_alert?(%Alert{effect: :access_issue}), do: true
+  def is_accessibility_alert?(_), do: false
 
   @spec effect_type_to_group_title(effect_type()) :: String.t()
   def effect_type_to_group_title(:access_issue), do: "Access Issues"
