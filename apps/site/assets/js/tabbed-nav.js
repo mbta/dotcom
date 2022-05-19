ed8d3d7ecbaefb8601e216df10cbeff63b576c35
Brian Fauble <bfauble@users.noreply.github.com>
2022-05-19 11:11:13 -0500

    Homepage tabs container (#1297)
    * Departure times out of order fix (#1290)
    
    * put scheduled times in order
    
    * use predictions for comparison and a test
    
    * cover all prediction formats
    
    * lint
    
    * fix tests, lint, snapshots
    
    * cleanup lint errors
    
    * consider am and pm when comparing times
    
    * prettier
    
    * parse string to date object
    
    * more tests
    
    * first pass visual updates for new tabbed homepage navigation.
    
    * fix margins
    
    * add initial js functionality
    
    * cleanup unused stylings and add some style to content section
    
    * re-init when turbo links load event is fired
    
    * remove replaced secondary nav template and css
    
    * remove redundant breakpoint padding/margin
    
    * center align xs tabs
    
    * update js for handling keyboard selection and cleanup some code.
    
    * css cleanup
    
    * make markup more accessible
    
    Co-authored-by: i0sea <91911166+i0sea@users.noreply.github.com>

---
+++ apps/site/assets/js/tabbed-nav.js
@@ -0,0 +1,37 @@
+const updateTabs = (tabType, navTabs, contentTabs) => e => {
+  e.preventDefault();
+
+  navTabs.forEach(tab => {
+    const clickedTab = tab.dataset.tabType === tabType;
+    tab.classList.toggle("active", clickedTab);
+    tab.toggleAttribute("aria-selected", clickedTab);
+  });
+
+  contentTabs.forEach(tab => {
+    const clickedTabContentType = tab.dataset.tabContentType === tabType;
+    tab.classList.toggle("active", clickedTabContentType);
+  });
+};
+
+const tabbedNavSetup = () => {
+  const navTabs = document.querySelectorAll(".m-tabbed-nav__item");
+  const contentTabs = document.querySelectorAll(".m-tabbed-nav__content-item");
+
+  navTabs.forEach(item => {
+    const { tabType } = item.dataset;
+    const callback = updateTabs(tabType, navTabs, contentTabs);
+
+    item.addEventListener("click", callback);
+    item.addEventListener("keyup", e => {
+      if (e.key === "Enter") {
+        callback(e);
+      }
+    });
+  });
+};
+
+export default function init() {
+  document.addEventListener("turbolinks:load", () => {
+    tabbedNavSetup();
+  });
+}
