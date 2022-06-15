ce198ca9dffde9e05d6734205a8a96636ef44ed1
Ceesay <ceesaythedev@gmail.com>
2022-06-15 14:09:16 -0400

    get next n events within the year

--- apps/cms/lib/repo.ex
+++ apps/cms/lib/repo.ex
@@ -355,16 +355,21 @@ defmodule CMS.Repo do
     end
   end
 
-  @doc "Get the next n events, paginating through results if needed, and caches the result"
+  @doc "Get the next n events"
   def next_n_events(n \\ []) do
+    range = [
+      min: Timex.today |> Util.convert_to_iso_format(),
+      max: Timex.end_of_year(DateTime.utc_now.year) |> Timex.shift(days: 1) |> Util.convert_to_iso_format()
+    ]
     opts = [
       type: [:event],
       items_per_page: n,
-      # date_op: "between",
-      # date: range,
-      # offset: offset * per_page,
+      date_op: "between", # should be greater than
+      date: range, # should be able to pick single date rather than range
       sort_order: :ASC
     ]
-    IO.inspect(events(opts))
+    events = teasers(opts)
+    IO.inspect(events)
+    events
   end
 end
