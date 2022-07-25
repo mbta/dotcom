a9206db2b9e9c07d6045b2cc799c4e4bf95a388e
Cristen Jones <cjones3@mbta.com>
2022-07-25 16:18:12 -0400

    cypress: update global nav tests
    - attempt to follow best practice to eliminate running into race conditions when interacting with laggy UI elements. but also add a few `cy.wait()`. now we can unskip some tests!
    - use Cypress aliases to make tests more readable
    - scope the Homepage tests to check accessibility for only the tested components

--- cypress/integration/global_navigation.spec.js
+++ cypress/integration/global_navigation.spec.js
@@ -7,18 +7,6 @@ const viewports = [
 
 const searchPlaceholderText = "Search for routes, info, and more";
 
-const SELECTORS = {
-  veil: ".m-menu--cover",
-  desktopMenu: ".m-menu-desktop__section",
-  desktopMenuButton: ".m-menu--desktop__toggle",
-  mobileMenu: ".m-menu__content",
-  mobileMenuButton: "button.m-menu__toggle",
-  searchButton: "button.header-search__toggle",
-  searchBarDesktop: "header #search-header-desktop__container",
-  searchInputDesktop: "header #search-header-desktop__input",
-  searchBarMobile: "header #search-header-mobile__container"
-}
-
 describe("Header navigation", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -28,6 +16,19 @@ describe("Header navigation", () => {
     describe(`${viewport} size`, () => {
       beforeEach(() => {
         cy.viewport(viewport);
+        cy.get("button.m-menu__toggle").as('mobileMenuButton')
+        cy.get(".m-menu__content").as('mobileMenu')
+        cy.get(".m-menu--desktop__toggle").first().as('desktopMenuButton')
+        cy.get(".m-menu-desktop__section").as('desktopMenu')
+        cy.get("button.header-search__toggle").as('searchButton');
+        cy.get("header #search-header-mobile__container").as('searchBarMobile')
+        cy.get("header #search-header-desktop__container").as('searchBarDesktop')
+        cy.get("header #search-header-desktop__input").as('searchInputDesktop')
+        cy.get(".m-menu--cover").as('veil')
+
+        if (viewport === "macbook-16") {
+          cy.wait(500); // desktop stuff takes longer to render
+        }
       });
 
       describe("has no detectable a11y violations", () => {
@@ -42,13 +43,21 @@ describe("Header navigation", () => {
         it("with menu open", () => {
           switch (viewport) {
             case "macbook-16":
-              cy.get(SELECTORS.desktopMenuButton).first().click();
-              cy.get(SELECTORS.desktopMenu).should("be.visible");
+              cy.get('@desktopMenuButton')
+                .click()
+                .wait(500) // let the menu finish expanding
+                .then(() => {
+                  cy.get('@desktopMenu').should("be.visible");
+                });
+
               break;
 
             default:
-              cy.get(SELECTORS.mobileMenuButton).click();
-              cy.get(SELECTORS.mobileMenu).should("be.visible");
+              cy.get('@mobileMenuButton')
+                .click()
+                .then(() => {
+                  cy.get('@mobileMenu').should("be.visible");
+                });
               break;
           }
           cy.checkA11y();
@@ -56,8 +65,12 @@ describe("Header navigation", () => {
 
         if (viewport == "iphone-6") {
           it("with search open", () => {
-            cy.get(SELECTORS.searchButton).click();
-            cy.get(SELECTORS.searchBarMobile).should("be.visible")
+            cy.get('@searchButton')
+              .click()
+              .then(() => {
+                cy.get('@searchBarMobile').should("be.visible");
+              })
+
             cy.checkA11y();
           });
         }
@@ -65,107 +78,152 @@ describe("Header navigation", () => {
 
       it("shows right controls at relevant viewports", () => {
         // menus start hidden
-        cy.get(SELECTORS.mobileMenu).should("not.be.visible");
-        cy.get(SELECTORS.desktopMenu).should("not.be.visible");
-        cy.get(SELECTORS.searchBarMobile).should("not.be.visible");
+        cy.get('@mobileMenu').should("not.be.visible");
+        cy.get('@desktopMenu').should("not.be.visible");
+        cy.get('@searchBarMobile').should("not.be.visible");
 
         switch (viewport) {
           case "macbook-16":
-            cy.get(SELECTORS.desktopMenuButton).first().should("be.visible");
-            cy.get(SELECTORS.searchBarDesktop).should("be.visible").get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
-            cy.get(SELECTORS.mobileMenuButton).should("not.be.visible");
-            cy.get(SELECTORS.searchButton).should("not.be.visible");
+            /// shows 4 desktop menu buttons & search bar
+            cy.get('@desktopMenuButton').should("be.visible");
+            cy.get('@searchBarDesktop').should("be.visible")
+              .get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
+            cy.get('@mobileMenuButton').should("not.be.visible");
+            cy.get('@searchBarMobile').should("not.be.visible");
             break;
 
           case "ipad-2":
-            cy.get(SELECTORS.desktopMenuButton).first().should("not.be.visible");
-            cy.get(SELECTORS.searchBarDesktop).should("be.visible").get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
-            cy.get(SELECTORS.mobileMenuButton).should("be.visible");
-            cy.get(SELECTORS.searchButton).should("not.be.visible");
+            // shows singular "Menu" button & search bar
+            cy.get('@desktopMenuButton').should("not.be.visible");
+            cy.get('@searchBarDesktop').should("be.visible")
+              .get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
+            cy.get('@mobileMenuButton').should("be.visible");
+            cy.get('@searchBarMobile').should("not.be.visible");
+            cy.get('@searchButton').should("not.be.visible");
             break;
 
           case "iphone-6":
-            cy.get(SELECTORS.desktopMenuButton).first().should("not.be.visible");
-            cy.get(SELECTORS.searchBarDesktop).should("not.be.visible");
-            cy.get(SELECTORS.mobileMenuButton).should("be.visible");
-            cy.get(SELECTORS.searchButton).should("be.visible");
+            // shows singular "Menu" button & search button
+            cy.get('@desktopMenuButton').should("not.be.visible");
+            cy.get('@searchBarDesktop').should("not.be.visible");
+            cy.get('@mobileMenuButton').should("be.visible");
+            cy.get('@searchBarMobile').should("not.be.visible");
+            cy.get('@searchButton').should("be.visible");
             break;
         }
       })
 
       if (viewport !== "macbook-16") {
         it("button toggles the mobile menu", () => {
-          cy.get(SELECTORS.mobileMenuButton).should("contain", "Menu");
-          cy.get(SELECTORS.mobileMenuButton).click();
-          cy.get(SELECTORS.mobileMenuButton).should("contain", "Close");
-          cy.get(SELECTORS.mobileMenu).should("be.visible");
-          cy.get(SELECTORS.mobileMenuButton).click();
-          cy.get(SELECTORS.mobileMenu).should("not.be.visible");
+          cy.get('@mobileMenuButton')
+            .should("contain", "Menu")
+            .click()
+            .should("contain", "Close")
+            .then(() => {
+              cy.get('@mobileMenu').should("be.visible");
+            })
+          cy.get('@mobileMenuButton')
+            .click()
+            .should("contain", "Menu")
+            .then(() => {
+              cy.get('@mobileMenu').should("not.be.visible");
+            });
         });
       } else {
-        // no idea why this test is failing in CI but here we are
-        it.skip("button toggles the desktop menu", { retries: 5 }, () => {
-          cy.get(SELECTORS.desktopMenuButton).first().click();
-          cy.get(SELECTORS.desktopMenu).should("be.visible");
-          cy.get(SELECTORS.desktopMenuButton).first().click();
-          cy.get(SELECTORS.desktopMenu).should("not.be.visible");
+        it("button toggles the desktop menu", () => {
+          cy.get('@desktopMenuButton')
+            .click()
+            .wait(500) // let the menu finish expanding
+            .then(() => {
+              cy.get('@desktopMenu').should("be.visible");
+            });
+          cy.get('@desktopMenuButton')
+            .click()
+            .then(() => {
+              cy.get('@desktopMenu').should("not.be.visible");
+            });
         });
       }
 
       if (viewport == "ipad-2") {
         it("click veil closes the mobile menu", () => {
-          cy.get(SELECTORS.mobileMenuButton).click();
-          cy.get(SELECTORS.mobileMenu).should("be.visible");
-          cy.get(SELECTORS.veil).click();
-          cy.get(SELECTORS.mobileMenu).should("not.be.visible");
-          cy.get(SELECTORS.mobileMenuButton).should("contain", "Menu");
+          cy.get('@mobileMenuButton').click();
+          cy.get('@veil')
+            .click()
+            .then(() => {
+              cy.get('@mobileMenu').should("not.be.visible");
+              cy.get('@mobileMenuButton').should("contain", "Menu");
+            })
         });
 
         it("ESC key closes the mobile menu", () => {
-          cy.get(SELECTORS.mobileMenuButton).click();
-          cy.get(SELECTORS.mobileMenu).should("be.visible");
-          cy.get("body").type("{esc}");
-          cy.get(SELECTORS.mobileMenu).should("not.be.visible");
-          cy.get(SELECTORS.mobileMenuButton).should("contain", "Menu");
+          cy.get('@mobileMenuButton').click()
+          cy.get("body")
+            .type("{esc}")
+            .then(() => {
+              cy.get('@mobileMenu').should("not.be.visible");
+              cy.get('@mobileMenuButton').should("contain", "Menu");
+            });
         });
 
-        it ("Veil should be shown when search input is focused, and hidden when blurred", () => {
-          cy.get(SELECTORS.searchInputDesktop).focus();
-          cy.get(SELECTORS.veil).should("be.visible");
-          cy.get(SELECTORS.searchInputDesktop).blur();
-          cy.get(SELECTORS.veil).should("not.be.visible");
+        it("Veil should be shown when search input is focused, and hidden when blurred", () => {
+          cy.get('@searchInputDesktop')
+            .focus()
+            .then(() => {
+              cy.get('@veil').should("be.visible");
+            });
+          cy.get('@searchInputDesktop').blur()
+            .then(() => {
+              cy.get('@veil').should("not.be.visible");
+            })
         });
       } else if (viewport == "macbook-16") {
         it("click veil closes the desktop menu", () => {
-          cy.get(SELECTORS.desktopMenuButton).first().click();
-          cy.get(SELECTORS.desktopMenu).should("be.visible");
-          cy.get(SELECTORS.veil).click();
-          cy.get(SELECTORS.desktopMenu).should("not.be.visible");
+          cy.get('@desktopMenuButton')
+            .click()
+            .wait(500); // let the menu finish expanding
+
+          cy.get('@veil')
+            .click()
+            .then(() => {
+              cy.get('@desktopMenu').should("not.be.visible");
+            });
         });
 
-        it("ESC key closes the desktop menu", { retries: 5 }, () => {
-          cy.get(SELECTORS.desktopMenuButton).first().click();
-          cy.get(SELECTORS.desktopMenu).should("be.visible");
-          cy.get("body").type("{esc}");
-          cy.get(SELECTORS.desktopMenu).should("not.be.visible");
+        it("ESC key closes the desktop menu", () => {
+          cy.get('@desktopMenuButton')
+            .click()
+            .wait(500); // let the menu finish expanding
+
+          cy.get("body")
+            .type("{esc}")
+            .then(() => {
+              cy.get('@desktopMenu').should("not.be.visible");
+            });
         });
       }
 
       /* search behaves differently between two smallest viewports */
       if (viewport == "iphone-6") {
         it("search button toggles search bar", () => {
-          cy.get(SELECTORS.searchBarMobile).should("not.be.visible");
-          cy.get(SELECTORS.searchButton).click();
-          cy.get("header").should("have.class", "search-open");
-          cy.get(SELECTORS.mobileMenuButton).should("not.be.visible");
-          cy.get(SELECTORS.searchBarDesktop)
-            .should("not.be.visible")
-          cy.get(SELECTORS.searchBarMobile)
-            .should("be.visible")
-            .get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
-          cy.get(SELECTORS.searchButton).click();
-          cy.get("header").should("not.have.class", "search-open");
-          cy.get(SELECTORS.searchBarMobile).should("not.be.visible");
+          cy.get('@searchBarMobile').should("not.be.visible");
+          cy.get('@searchButton')
+            .click()
+            .then(() => {
+              cy.get("header").should("have.class", "search-open");
+              cy.get('@mobileMenuButton').should("not.be.visible");
+              cy.get('@searchBarDesktop').should("not.be.visible")
+              cy.get('@searchBarMobile')
+                .should("be.visible")
+                .get("input.c-search-bar__-input").should("have.attr", "placeholder", searchPlaceholderText);
+            });
+
+          cy.get('@searchButton')
+            .click()
+            .then(() => {
+              cy.get("header").should("not.have.class", "search-open");
+              cy.get('@searchBarMobile').should("not.be.visible");
+            });
         });
       }
     })
@@ -173,8 +231,14 @@ describe("Header navigation", () => {
 
   it("translates languages", () => {
     cy.viewport("macbook-16");
-    cy.get(".m-homepage__shortcuts").should("contain.text", "Subway Lines");
-    cy.get(".custom-language-selector").first().select("Polish");
-    cy.get(".m-homepage__shortcuts").should("contain.text", "Linie metra");
+    cy.get(".m-homepage__shortcuts")
+      .should("contain.text", "Subway Lines");
+    cy.get(".custom-language-selector")
+      .first()
+      .select("Polish")
+      .then(() => {
+        cy.wait(1000); // translating is slow!
+        cy.get(".m-homepage__shortcuts").should("contain.text", "Linie metra");
+      })
   });
 });
