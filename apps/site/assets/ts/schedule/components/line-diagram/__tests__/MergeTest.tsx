6d541c3bcc5757d03fd7352206c70d978455847c
zovt <code@idylls.net>
2022-08-03 13:57:48 -0400

    add unit test based on test data

--- apps/site/assets/ts/schedule/components/line-diagram/__tests__/MergeTest.tsx
+++ apps/site/assets/ts/schedule/components/line-diagram/__tests__/MergeTest.tsx
@@ -4,10 +4,12 @@ import { mount, ReactWrapper } from "enzyme";
 import { LineDiagramStop } from "../../__schedule";
 import simpleLineDiagram from "./lineDiagramData/simple.json"; // not a full line diagram
 import outwardLineDiagram from "./lineDiagramData/outward.json"; // not a full line diagram
+import bidiLineDiagram_ from "./lineDiagramData/northStationBidiTest.json";
 import { createLineDiagramCoordStore } from "../graphics/graphic-helpers";
 import Merges from "../graphics/Merge";
 const lineDiagram = (simpleLineDiagram as unknown) as LineDiagramStop[];
 const lineDiagramWithBranching = (outwardLineDiagram as unknown) as LineDiagramStop[];
+const bidiLineDiagram = bidiLineDiagram_ as LineDiagramStop[];
 
 const store = createLineDiagramCoordStore(lineDiagramWithBranching);
 // mock the redux state so that snapshot has positioned stops
@@ -67,5 +69,15 @@ describe("Merge component", () => {
     expect(
       wrapperNoBranches.exists("g.line-diagram-svg__merge path")
     ).not.toBeTruthy();
+  });
+
+  it("handles bidirectional branching", () => {
+    const withBidi = mount(<Merges lineDiagram={bidiLineDiagram} />);
+    expect(
+      withBidi.findWhere(el => el.key() == "test station c-test station b-line")
+    ).toBeTruthy();
+    expect(
+      withBidi.findWhere(el => el.key() == "place-coecl-place-hymnl-line")
+    ).toBeTruthy();
   });
 });
