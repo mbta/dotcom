5af0465a2853959e91d1e4771dc75303e7323a8c
i0sea <91911166+i0sea@users.noreply.github.com>
2022-09-02 13:34:02 -0400

    Add "Trains continue to" or "coming from" row to combined timetable (#1407)
    * wip continue row and stations styling needed
    
    * short name function, first row wip
    
    * styles
    
    * move short_name to trips
    
    * remove origin row
    
    * fill nil cells
    
    * add s
    
    * spacing
    
    * refactor remove if

--- apps/v3_api/lib/trips.ex
+++ apps/v3_api/lib/trips.ex
@@ -14,4 +14,18 @@ defmodule V3Api.Trips do
     opts = put_in(opts[:route], route_id)
     get_json("/trips/", opts)
   end
+
+  def short_name(stop_name) do
+    case stop_name do
+      "Needham Heights" -> "NEED"
+      "Stoughton" -> "STOU"
+      "Providence" -> "PROV"
+      "Wickford Junction" -> "WICK"
+      "Walpole" -> "WAL"
+      "Forge Park/495" -> "FORG"
+      "Foxboro" -> "FOX"
+      "Norwood Central" -> "NOR"
+      _ -> String.upcase(String.slice(stop_name, 0..2))
+    end
+  end
 end
