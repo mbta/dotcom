b8a9d33ccfe3d69afa7001aa7ec945a1aec8b6b1
Ceesay <ceesaythedev@gmail.com>
2022-10-11 13:08:05 -0400

    test

--- apps/site/assets/ts/schedule/components/line-diagram/__tests__/StopCardTest.tsx
+++ apps/site/assets/ts/schedule/components/line-diagram/__tests__/StopCardTest.tsx
@@ -106,6 +106,8 @@ describe("StopCard", () => {
   });
 });
 
+// test for when there is no footer
+// test for footer text is View upcoming departures
 const predictionHeadsign: HeadsignWithCrowding = {
   name: "Somewhere",
   time_data_with_crowding_list: [
@@ -128,6 +130,79 @@ const predictionHeadsign: HeadsignWithCrowding = {
   ],
   train_number: null
 };
+const lineDiagramSubway = (simpleLineDiagram as unknown) as LineDiagramStop[];
+let lineDiagramBranchingOutSubway = (outwardLineDiagram as unknown) as LineDiagramStop[];
+
+const routeSubway = {
+  type: 1 as RouteType,
+  name: "route 1",
+  long_name: "route 1 long name",
+  color: "F00B42",
+  id: "route-1",
+  direction_names: {
+    0: "Outbound",
+    1: "Inbound"
+  },
+  direction_destinations: {
+    0: "Begin",
+    1: "End"
+  },
+  description: "key_bus_route",
+  "custom_route?": false,
+  header: "",
+  alerts: []
+};
+
+lineDiagramSubway.forEach(({ route_stop }) => {
+  route_stop.route = cloneDeep(routeSubway);
+});
+
+lineDiagramBranchingOutSubway.forEach(({ route_stop }) => {
+  route_stop.route = cloneDeep(routeSubway);
+});
+
+let lineDiagramBranchingInSubway = cloneDeep(
+  lineDiagramBranchingOutSubway
+).reverse();
+const CRrouteTwo = merge(cloneDeep(route), { type: 2 as RouteType });
+lineDiagramBranchingInSubway.forEach(({ route_stop }) => {
+  route_stop.route = CRrouteTwo;
+  if (route_stop["is_terminus?"]) {
+    route_stop["is_beginning?"] = !route_stop["is_beginning?"];
+  }
+});
+
+const handleStopClickSubway = () => {};
+const liveDataSubway = { headsigns: [predictionHeadsign], vehicles: [] };
+const storeSubway = createLineDiagramCoordStore(lineDiagramSubway);
+
+describe("StopCard", () => {
+  let subwayWrapper: ReactWrapper;
+  beforeEach(() => {
+    subwayWrapper = mount(
+      <redux.Provider store={storeSubway}>
+        <StopCard
+          stop={lineDiagramSubway[0]}
+          onClick={handleStopClickSubway}
+          liveData={liveDataSubway}
+        />
+      </redux.Provider>
+    );
+  });
+
+  afterEach(() => {
+    subwayWrapper.unmount();
+  });
+  it("includes a button to open Schedule Finder on each stop with departures text", () => {
+    expect(
+      subwayWrapper.exists(".m-schedule-diagram__footer > button")
+    ).toBeTruthy();
+    expect(
+      subwayWrapper.find(".m-schedule-diagram__footer > button").text()
+    ).toContain("View upcoming departures");
+  });
+});
+
 const liveDataWithPrediction = {
   headsigns: [predictionHeadsign],
   vehicles: []
