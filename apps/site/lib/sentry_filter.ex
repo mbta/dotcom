e128212eb7d92eb3aed11c9f074af0db6d726f73
Cristen Jones <cjones3@mbta.com>
2022-10-12 14:22:16 -0400

    chore: upgrade from sentry 7 to 8 on backend

--- apps/site/lib/sentry_filter.ex
+++ apps/site/lib/sentry_filter.ex
@@ -10,6 +10,14 @@ defmodule Site.SentryFilter do
     true
   end
 
+  # Ignore Phoenix route not found exception
+  def exclude_exception?(%x{}, :plug) when x in [Phoenix.Router.NoRouteError] do
+    true
+  end
+
+  # Ignore Plug route not found exception
+  def exclude_exception?(%FunctionClauseError{function: :do_match, arity: 4}, :plug), do: true
+
   def exclude_exception?(_exception, _source) do
     false
   end
