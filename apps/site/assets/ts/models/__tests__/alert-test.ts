a0c612d91f768163b5f1ffbd36c84391251642b9
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-10-12 17:05:25 -0400

    refactor: Wrap tests in describe blocks

--- apps/site/assets/ts/models/__tests__/alert-test.ts
+++ apps/site/assets/ts/models/__tests__/alert-test.ts
@@ -32,71 +32,79 @@ const alert4: Alert = {
   active_period: [["2020-09-10 12:00", "2020-09-10 20:00"]]
 } as Alert;
 
-test.each`
-  alert     | isHigh
-  ${alert1} | ${true}
-  ${alert2} | ${true}
-  ${alert3} | ${true}
-  ${alert4} | ${false}
-`(
-  "isHighSeverityOrHighPriority returns whether alert is high..",
-  ({ alert, isHigh }) => {
-    if (isHigh) {
-      expect(isHighSeverityOrHighPriority(alert)).toBeTruthy();
-    } else {
-      expect(isHighSeverityOrHighPriority(alert)).toBeFalsy();
+describe("isHighSeverityOrHighPriority", () => {
+  test.each`
+    alert     | isHigh
+    ${alert1} | ${true}
+    ${alert2} | ${true}
+    ${alert3} | ${true}
+    ${alert4} | ${false}
+  `(
+    "isHighSeverityOrHighPriority returns whether alert is high..",
+    ({ alert, isHigh }) => {
+      if (isHigh) {
+        expect(isHighSeverityOrHighPriority(alert)).toBeTruthy();
+      } else {
+        expect(isHighSeverityOrHighPriority(alert)).toBeFalsy();
+      }
     }
-  }
-);
-
-test.each`
-  alert     | isCurrent
-  ${alert1} | ${true}
-  ${alert2} | ${true}
-  ${alert3} | ${false}
-  ${alert4} | ${false}
-`(
-  "isCurrentAlert returns whether alert is current based on lifecycle and active period",
-  ({ alert, isCurrent }) => {
-    if (isCurrent) {
-      expect(isCurrentAlert(alert, testDate)).toBeTruthy();
-    } else {
-      expect(isCurrentAlert(alert, testDate)).toBeFalsy();
-    }
-  }
-);
-
-test("alertsByStop finds alerts affecting a certain stop", () => {
-  const alert_two = {
-    ...alert2,
-    informed_entity: { stop: ["place-sstat"] } as InformedEntitySet
-  };
-  const alert_three = {
-    ...alert3,
-    informed_entity: { stop: ["place-sstat"] } as InformedEntitySet
-  };
-  const alerts = [
-    { ...alert1, informed_entity: { stop: null } as InformedEntitySet },
-    alert_two,
-    alert_three,
-    { ...alert4, informed_entity: { stop: null } as InformedEntitySet }
-  ];
-  const alertsForStop = alertsByStop(alerts, "place-sstat");
-  expect(alertsForStop).toEqual([alert_two, alert_three]);
+  );
 });
 
-test("uniqueByEffect enables filtering to unique effects", () => {
-  const alerts = [
-    { effect: "shuttle" } as Alert,
-    { effect: "detour" } as Alert,
-    { effect: "detour" } as Alert,
-    { effect: "shuttle" } as Alert,
-    { effect: "unknown" } as Alert
-  ];
-  expect(alerts.filter(uniqueByEffect)).toHaveLength(3);
-  expect(alerts.filter(uniqueByEffect).map(a => a.effect)).toEqual([
-    "shuttle",
-    "detour",
-    "unknown"
-  ]);
+describe("isCurrentAlert", () => {
+  test.each`
+    alert     | isCurrent
+    ${alert1} | ${true}
+    ${alert2} | ${true}
+    ${alert3} | ${false}
+    ${alert4} | ${false}
+  `(
+    "isCurrentAlert returns whether alert is current based on lifecycle and active period",
+    ({ alert, isCurrent }) => {
+      if (isCurrent) {
+        expect(isCurrentAlert(alert, testDate)).toBeTruthy();
+      } else {
+        expect(isCurrentAlert(alert, testDate)).toBeFalsy();
+      }
+    }
+  );
+});
+
+describe("alertsByStop", () => {
+  test("alertsByStop finds alerts affecting a certain stop", () => {
+    const alert_two = {
+      ...alert2,
+      informed_entity: { stop: ["place-sstat"] } as InformedEntitySet
+    };
+    const alert_three = {
+      ...alert3,
+      informed_entity: { stop: ["place-sstat"] } as InformedEntitySet
+    };
+    const alerts = [
+      { ...alert1, informed_entity: { stop: null } as InformedEntitySet },
+      alert_two,
+      alert_three,
+      { ...alert4, informed_entity: { stop: null } as InformedEntitySet }
+    ];
+    const alertsForStop = alertsByStop(alerts, "place-sstat");
+    expect(alertsForStop).toEqual([alert_two, alert_three]);
+  });
+});
+
+describe("uniqueByEffect", () => {
+  test("uniqueByEffect enables filtering to unique effects", () => {
+    const alerts = [
+      { effect: "shuttle" } as Alert,
+      { effect: "detour" } as Alert,
+      { effect: "detour" } as Alert,
+      { effect: "shuttle" } as Alert,
+      { effect: "unknown" } as Alert
+    ];
+    expect(alerts.filter(uniqueByEffect)).toHaveLength(3);
+    expect(alerts.filter(uniqueByEffect).map(a => a.effect)).toEqual([
+      "shuttle",
+      "detour",
+      "unknown"
+    ]);
+  });
 });
