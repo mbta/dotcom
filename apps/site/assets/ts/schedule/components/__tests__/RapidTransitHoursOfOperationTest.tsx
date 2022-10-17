e604720cbde0a33a2b542b5ab676aba6d65884cd
kotva006 <ryan.kotval@gmail.com>
2022-10-17 14:25:59 -0500

    Fixed the unit tests for rapid transit display

--- apps/site/assets/ts/schedule/components/__tests__/RapidTransitHoursOfOperationTest.tsx
+++ apps/site/assets/ts/schedule/components/__tests__/RapidTransitHoursOfOperationTest.tsx
@@ -11,7 +11,7 @@ import {
   getTimezoneOffset,
   utcToZonedTime
 } from "date-fns-tz";
-import { parseISO } from "date-fns";
+import { format, parseISO } from "date-fns";
 
 describe("RapidTransitHoursOfOperation", () => {
   beforeEach(() => {
@@ -20,6 +20,14 @@ describe("RapidTransitHoursOfOperation", () => {
   });
 
   it("renders the rapid transit schedule if route rapid transit", () => {
+    // get todays date so we have the right daylight savings time offset
+    const today = new Date();
+    const dateString = formatInTimeZone(
+      today,
+      "America/New_York",
+      "yyyy-MM-dd"
+    );
+
     const offset = getTimezoneOffset("America/New_York");
     const hourOffset = offset / 3_600_000;
 
@@ -33,15 +41,15 @@ describe("RapidTransitHoursOfOperation", () => {
             {
               stop_name: "Test Stop 1",
               stop_id: "1",
-              last_departure: "2021-01-02 23:44:00" + offsetString,
-              first_departure: "2021-01-02 08:54:00" + offsetString,
+              last_departure: `${dateString}T23:44:00${offsetString}`,
+              first_departure: `${dateString}T08:54:00${offsetString}`,
               is_terminus: false
             },
             {
               stop_name: "Test Stop 2",
               stop_id: "2",
-              last_departure: "2022-01-02 22:45:00" + offsetString,
-              first_departure: "2022-01-02 07:55:00" + offsetString,
+              last_departure: `${dateString}T22:45:00${offsetString}`,
+              first_departure: `${dateString}T07:55:00${offsetString}`,
               is_terminus: true
             }
           ],
@@ -49,15 +57,15 @@ describe("RapidTransitHoursOfOperation", () => {
             {
               stop_name: "Test Stop 1",
               stop_id: "1",
-              last_departure: "2021-01-01 23:35:00" + offsetString,
-              first_departure: "2021-01-01 08:35:00" + offsetString,
+              last_departure: `${dateString}T23:35:00${offsetString}`,
+              first_departure: `${dateString}T08:35:00${offsetString}`,
               is_terminus: false
             },
             {
               stop_name: "Test Stop 2",
               stop_id: "2",
-              last_departure: "2021-01-01 23:25:00" + offsetString,
-              first_departure: "2021-01-01 08:25:00" + offsetString,
+              last_departure: `${dateString}T23:25:00${offsetString}`,
+              first_departure: `${dateString}T08:25:00${offsetString}`,
               is_terminus: true
             }
           ]
@@ -67,8 +75,8 @@ describe("RapidTransitHoursOfOperation", () => {
             {
               stop_name: "Test Stop 1",
               stop_id: "1",
-              last_departure: "2021-01-01 21:15:00" + offsetString,
-              first_departure: "2021-01-01 08:15:00" + offsetString,
+              last_departure: `${dateString}T21:15:00${offsetString}`,
+              first_departure: `${dateString}T08:15:00${offsetString}`,
               is_terminus: true
             }
           ],
@@ -76,8 +84,8 @@ describe("RapidTransitHoursOfOperation", () => {
             {
               stop_name: "Test Stop 1",
               stop_id: "1",
-              last_departure: "2021-01-01 22:15:00" + offsetString,
-              first_departure: "2021-01-01 07:15:00" + offsetString,
+              last_departure: `${dateString}T22:15:00${offsetString}`,
+              first_departure: `${dateString}T07:15:00${offsetString}`,
               is_terminus: true
             }
           ]
@@ -101,7 +109,9 @@ describe("RapidTransitHoursOfOperation", () => {
     expect(treeString).toMatch("Weekend Schedule");
     expect(treeString).toMatch("Weekday Schedule");
     expect(treeString).toMatch("Test Stop 2");
+    // only the week day stop that is a terminus stop is shown
     expect(treeString).toMatch("7:55am - 10:45pm");
+    expect(treeString).not.toMatch("8:54am - 10:44pm");
     expect(treeString).toMatch("8:15am - 9:15pm");
   });
 });
