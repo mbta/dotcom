764cf203f183911f73024a1b265b9e240e2f0cea
Cristen Jones <cjones3@mbta.com>
2022-12-16 11:02:00 -0500

    fixup(PredictionsChannel): emit predictions on join
    get whatever predictions are available upon subscribing, remove predictions in the past, push to socket immediately. this way subscribers won't have to wait until the next stream push to get any data.

--- apps/site/lib/site_web/channels/predictions_channel.ex
+++ apps/site/lib/site_web/channels/predictions_channel.ex
@@ -21,7 +21,11 @@ defmodule SiteWeb.PredictionsChannel do
     [route_id, stop_id, direction_id] = String.split(route_stop_direction, ":")
     predictions = predictions_subscribe_fn.(route_id, stop_id, direction_id)
 
-    {:ok, %{predictions: predictions}, socket}
+    # remove old predictions
+    future_predictions = Enum.filter(predictions, &is_in_future?/1)
+    # immediately push current predictions to socket
+    send(self(), {:new_predictions, future_predictions})
+    {:ok, socket}
   end
 
   @impl Channel
@@ -30,4 +34,9 @@ defmodule SiteWeb.PredictionsChannel do
     :ok = push(socket, "data", %{predictions: predictions})
     {:noreply, socket}
   end
+
+  defp is_in_future?(%Prediction{departure_time: %DateTime{} = d}),
+    do: Util.time_is_greater_or_equal?(d, Util.now())
+
+  defp is_in_future?(_), do: false
 end
