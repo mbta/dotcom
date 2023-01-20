3fb4eb556f758da1417c063d1012ee5a0c3e15a6
i0sea <91911166+i0sea@users.noreply.github.com>
2023-01-20 13:07:12 -0500

    Js test coverage (#1497)
    * check if glyphicon is removed in datepicker-input_test
    
    * token check

--- apps/site/assets/js/address-search.js
+++ apps/site/assets/js/address-search.js
@@ -1,40 +1,11 @@
-export default function() {
-  function setupLocationInput() {
-    const transitNearMeInput = document.getElementById("address-search-input");
-
-    // only load on pages that are using the location input
-    if (!transitNearMeInput) {
-      return;
-    }
-
-    transitNearMeInput.form.addEventListener("submit", ev =>
-      validateLocationForm(ev, window.location, transitNearMeInput)
-    );
-  }
-
-  document.addEventListener("turbolinks:load", setupLocationInput, {
-    passive: true
-  });
-  $(document).on(
-    "autocomplete:added",
-    "#address-search-input",
-    addLocationChangeCallback
-  );
-  $(document).on(
-    "geolocation:complete",
-    "#address-search-input",
-    geolocationCallback
-  );
-}
-
-// Functions exported for testing //
-
 export function getUrlParameter(sParam, search_string) {
-  const sPageURL = decodeURIComponent(search_string.substring(1)),
-    sURLVariables = sPageURL.split("&");
-  var sParameterName, i;
+  const sPageURL = decodeURIComponent(search_string.substring(1));
+  const sURLVariables = sPageURL.split("&");
+  let sParameterName;
+  let i;
 
-  for (i = 0; i < sURLVariables.length; i++) {
+  // eslint-disable-next-line no-plusplus
+  for (i = 0; i < sURLVariables.length; i += 1) {
     sParameterName = sURLVariables[i].split("=");
 
     if (sParameterName[0] === sParam) {
@@ -62,26 +33,16 @@ export function constructUrl(place, input) {
   const id = input.getAttribute("id");
   const name = input.getAttribute("name");
   var query_str;
-  const loc = window.location,
-    location_url = loc.protocol + "//" + loc.host + loc.pathname,
-    addr = input.value;
+  const loc = window.location;
+    const location_url = loc.protocol + "//" + loc.host + loc.pathname;
+    const addr = input.value;
 
   if (place.geometry) {
     const lat = place.geometry.location.lat();
     const lng = place.geometry.location.lng();
-    query_str =
-      "?latitude=" +
-      lat +
-      "&longitude=" +
-      lng +
-      "&" +
-      name +
-      "=" +
-      addr +
-      "#" +
-      id;
+    query_str = `?latitude=${lat}&longitude=${lng}&${name}=${addr}#${id}`;
   } else {
-    query_str = "?" + name + "=" + place.name + "#" + id;
+    query_str = `?${name}=${place.name}#${id}`;
   }
   return location_url + query_str;
 }
@@ -94,12 +55,44 @@ export function addLocationChangedEventListener(autocomplete, input) {
   google.maps.event.addListener(autocomplete, "place_changed", onPlaceChanged);
 }
 
-function addLocationChangeCallback(ev, autocomplete) {
-  addLocationChangedEventListener(autocomplete, ev.target);
-}
-
 function geolocationCallback(ev, location) {
   const path = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
   const qs = `?latitude=${location.coords.latitude}&longitude=${location.coords.longitude}`;
   window.location.href = encodeURI(path + qs + "#address-search-input");
+}
+
+export default function() {
+  function addLocationChangeCallback(ev, autocomplete) {
+    addLocationChangedEventListener(autocomplete, ev.target);
+  }
+  
+  function setupLocationInput() {
+    const transitNearMeInput = document.getElementById("address-search-input");
+
+    // only load on pages that are using the location input
+    if (!transitNearMeInput) {
+      return;
+    }
+
+    transitNearMeInput.form.addEventListener("submit", ev =>
+      // eslint-disable-next-line no-use-before-define
+      validateLocationForm(ev, window.location, transitNearMeInput)
+    );
+  }
+
+  document.addEventListener("turbolinks:load", setupLocationInput, {
+    passive: true
+  });
+  $(document).on(
+    "autocomplete:added",
+    "#address-search-input",
+    // eslint-disable-next-line no-use-before-define
+    addLocationChangeCallback
+  );
+  $(document).on(
+    "geolocation:complete",
+    "#address-search-input",
+    // eslint-disable-next-line no-use-before-define
+    geolocationCallback
+  );
 }
