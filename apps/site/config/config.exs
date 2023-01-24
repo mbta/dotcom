9c95118cd0acf51376b4ebaa7d0f37eebbb75d0a
Cristen Jones <cjones3@mbta.com>
2023-01-24 16:59:39 -0500

    fix: update PubSub usage for Phoenix 1.5

--- apps/site/config/config.exs
+++ apps/site/config/config.exs
@@ -10,7 +10,7 @@ config :site, SiteWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "yK6hUINZWlq04EPu3SJjAHNDYgka8MZqgXZykF+AQ2PvWs4Ua4IELdFl198aMvw0",
   render_errors: [accepts: ~w(html), layout: {SiteWeb.LayoutView, "app.html"}],
-  pubsub: [name: Site.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub_server: Site.PubSub
 
 config :phoenix, :gzippable_exts, ~w(.txt .html .js .css .svg)
 
