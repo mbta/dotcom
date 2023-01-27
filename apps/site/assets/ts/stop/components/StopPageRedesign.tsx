c7bb563d0433b5246eb575e6eb267d9fd76935bc
kotva006 <ryan.kotval@gmail.com>
2023-01-27 10:53:06 -0600

    Flushed out layout more

--- apps/site/assets/ts/stop/components/StopPageRedesign.tsx
+++ apps/site/assets/ts/stop/components/StopPageRedesign.tsx
@@ -8,15 +8,92 @@ const StopPageRedesign = ({
   return (
     <div>
       {/* Title Bar Div */}
-      <div>
-        <h1>{stopId}</h1>
+      <div className="d-flex justify-content-space-between">
+        <div>
+          <h1>{stopId}</h1>
+          {/* ICONS GO HERE */}
+        </div>
+        <div style={{ marginTop: "3.075rem" }}>
+          Zone Information PLACEHOLDER
+        </div>
       </div>
       {/* Route and Map Div */}
       <div className="d-flex">
-        <div>
+        <div style={{ minWidth: "50%" }}>
           <div>Route schedules & maps / Upcoming Trips PLACEHOLDER</div>
           <div>Filter Pills PLACEHOLDER</div>
-          <div>List PLACEHOLDER</div>
+          <div style={{ maxHeight: "250px", overflowY: "auto" }}>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div className="me-8">210</div>
+              <div>
+                <div>Fields Corner</div>
+                <div className="d-flex">
+                  <div>Open Schedule</div>
+                  <div>View Realtime Map</div>
+                </div>
+              </div>
+            </div>
+          </div>
           <button>Plan your Trip PLACEHOLDER</button>
         </div>
         <div className="hidden-sm-down">
