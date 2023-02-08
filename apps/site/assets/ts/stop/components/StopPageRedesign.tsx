21aa3019832d313724a25873217e396e55795511
Ryan Kotval <rkotval@mbta.com>
2023-02-08 13:59:52 -0600

    feat(StopRedesign): Route Filter Pills (#1515)
    * feat(StopRedesign): Route Filter Pills
    
    * Fixed css to rem and utils
    
    * Generated the filter buttons
    
    * Moved it to its own component and added some tests

--- apps/site/assets/ts/stop/components/StopPageRedesign.tsx
+++ apps/site/assets/ts/stop/components/StopPageRedesign.tsx
@@ -1,10 +1,51 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
+import { uniqueId } from "lodash";
+import { Mode } from "../../__v3api";
+import DeparturesFilters from "./DeparturesFilters";
+
+export const ALL = "all";
+export const BUS: Mode = "bus";
+export const SUBWAY: Mode = "subway";
+export const FERRY: Mode = "ferry";
+export const COMMUTER_RAIL: Mode = "commuter_rail";
+
+// TODO replace with real data
+const departures: any[] = [
+  {
+    headsign: "Fields Corner",
+    routeNumber: 210,
+    mode: "bus"
+  },
+  {
+    headsign: "Columbian Square",
+    routeNumber: 226,
+    mode: "commuter_rail"
+  },
+  {
+    headsign: "Quincy Center",
+    routeNumber: 230,
+    mode: "subway"
+  },
+  {
+    headsign: "Montello",
+    routeNumber: 230,
+    mode: "ferry"
+  },
+  {
+    headsign: "South Shore Plaza",
+    routeNumber: 236,
+    mode: "bus"
+  }
+];
 
 const StopPageRedesign = ({
   stopId
 }: {
   stopId: string;
 }): ReactElement<HTMLElement> => {
+  // TODO replace type with actual data type
+  const [filteredDepartures, setFilteredDepartures] = useState<any[]>([]);
+
   return (
     <article>
       {/* Title Bar Div */}
@@ -21,70 +62,27 @@ const StopPageRedesign = ({
       <div className="d-flex">
         <div style={{ minWidth: "50%" }}>
           <div>Route schedules & maps / Upcoming Trips PLACEHOLDER</div>
-          <div>Filter Pills PLACEHOLDER</div>
+          <div className="d-flex">
+            <DeparturesFilters
+              departures={departures}
+              onModeChange={setFilteredDepartures}
+            />
+          </div>
           <ul style={{ maxHeight: "250px", overflowY: "auto" }}>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
+            {filteredDepartures.map(departure => (
+              <li className="d-flex" key={uniqueId()}>
+                <div className="me-8">{departure.routeNumber}</div>
+                <div>
+                  <div>{departure.headsign}</div>
+                  <div className="d-flex">
+                    <div>Open Schedule</div>
+                    <div>View Realtime Map</div>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
-                </div>
-              </div>
-            </li>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
-                </div>
-              </div>
-            </li>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
-                </div>
-              </div>
-            </li>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
-                </div>
-              </div>
-            </li>
-            <li className="d-flex">
-              <div className="me-8">210</div>
-              <div>
-                <div>Fields Corner</div>
-                <div className="d-flex">
-                  <div>Open Schedule</div>
-                  <div>View Realtime Map</div>
-                </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
-          <button>Plan your Trip PLACEHOLDER</button>
+          <button type="button">Plan your Trip PLACEHOLDER</button>
         </div>
         <div className="hidden-sm-down">
           Map PLACEHOLDER Imageine a pretty map
