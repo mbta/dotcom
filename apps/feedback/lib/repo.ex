6ad8684b271d550390f965d74882b9001d34257a
Cristen Jones <cjones3@mbta.com>
2023-02-14 21:04:21 -0500

    feat(Feedback.Mailer): respect size/count file limit
    move photo_attachment/1 from Feedback.Repo

--- apps/feedback/lib/repo.ex
+++ apps/feedback/lib/repo.ex
@@ -4,7 +4,7 @@ defmodule Feedback.Repo do
 
   @spec send_ticket(Feedback.Message.t()) :: {:ok, any} | {:error, any}
   def send_ticket(message) do
-    result = Feedback.Mailer.send_heat_ticket(message, photo_attachment(message.photos))
+    result = Feedback.Mailer.send_heat_ticket(message, message.photos)
 
     case result do
       {:ok, _} ->
@@ -16,25 +16,4 @@ defmodule Feedback.Repo do
 
     result
   end
-
-  @spec photo_attachment([Plug.Upload.t()]) :: [%{path: String.t(), filename: String.t()}] | nil
-  def photo_attachment([%Plug.Upload{} | _rest] = photos) do
-    attachments =
-      Enum.reduce(photos, [], fn %Plug.Upload{path: path, filename: filename}, acc ->
-        case File.read(path) do
-          {:ok, uploaded_file} ->
-            [{filename, uploaded_file} | acc]
-
-          _ ->
-            # Sometimes a file isn't successfully uploaded. Ignore it here so that we can still send the email
-            _ = Logger.warn("module=#{__MODULE__} error=failed_photo_attachment")
-
-            acc
-        end
-      end)
-
-    if Enum.empty?(attachments), do: nil, else: attachments
-  end
-
-  def photo_attachment(nil), do: nil
 end
