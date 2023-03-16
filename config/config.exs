356450d0d8e4bd654da5ea66f1a01e8d878e5e75
Cristen Jones <cjones3@mbta.com>
2023-03-16 15:39:48 -0400

    config: add start_data_processes config option
    While we're here, update the config to use the newer import syntax

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,23 @@
 # This file is responsible for configuring your application
 # and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
+import Config
 
 # Enable colorization in terminal
 # https://github.com/rrrene/bunt/issues/4#issuecomment-301350784
 config :elixir, ansi_enabled: true
+
+# Used by several applications to turn off a subset of child processes in the
+# test environment.
+config :elixir, start_data_processes: config_env() != :test
 
 # By default, the umbrella project as well as each child
 # application will require this configuration file, ensuring
 # they all use the same configuration. While one could
 # configure all applications here, we prefer to delegate
 # back to each application for organization purposes.
-import_config "../apps/*/config/config.exs"
+for config <- "../apps/*/config/config.exs" |> Path.expand(__DIR__) |> Path.wildcard() do
+  import_config config
+end
 
 # Sample configuration (overrides the imported configuration above):
 #
