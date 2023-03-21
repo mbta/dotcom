b771188d65458f91292ba465d493448382191c65
kotva006 <ryan.kotval@gmail.com>
2023-03-21 07:55:42 -0500

    Fixed zone number spacing/alignment

--- apps/site/assets/ts/stop/components/icons/CommuterRailZoneIcon.tsx
+++ apps/site/assets/ts/stop/components/icons/CommuterRailZoneIcon.tsx
@@ -1,24 +1,21 @@
 import React, { ReactElement } from "react";
 
-const crZone = (zoneNumber?: string): ReactElement<HTMLElement> | false => {
-  return (
-    !!zoneNumber &&
-    zoneNumber.length > 0 && (
-      <div className="m-stop-page__header-feature">
-        <span className="m-stop-page__icon c-icon__cr-zone">
-          Zone {zoneNumber}
-        </span>
-      </div>
-    )
-  );
-};
-
 const CommuterRailZoneIcon = ({
   zoneNumber
 }: {
   zoneNumber?: string;
 }): ReactElement<HTMLElement> => {
-  return <>{crZone(zoneNumber)}</>;
+  if (!zoneNumber || zoneNumber.length === 0) {
+    return <></>;
+  }
+
+  return (
+    <div className="m-stop-page__header-feature mt-n5">
+      <span className="m-stop-page__icon c-icon__cr-zone">
+        Zone {zoneNumber}
+      </span>
+    </div>
+  );
 };
 
 export { CommuterRailZoneIcon };
