d1fb314aba275b2a3d6a174137294a9e5e206bc5
Cristen Jones <cjones3@mbta.com>
2023-04-10 12:17:37 -0400

    fixup(Predictions.PubSub): correction to comment

--- apps/predictions/lib/predictions_pub_sub.ex
+++ apps/predictions/lib/predictions_pub_sub.ex
@@ -2,7 +2,7 @@ defmodule Predictions.PredictionsPubSub do
   @moduledoc """
   Allow channels to subscribe to prediction streams, which are collected into a
   Map with keys representing route-stop-direction triads as
-  "<route-id>-<parent-stop-id>-<direction-id>"
+  "<route-id>:<parent-stop-id>:<direction-id>"
   """
 
   use GenServer
