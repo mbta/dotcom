ce9ce1dd413972164bbb1323b21d52f047d7f48a
Cristen Jones <cjones3@mbta.com>
2023-04-17 14:14:16 -0400

    refactor(DepartureCard): smoother directionId prop typing
    just iterating over 0 and 1 doesn't work without additional typing shenanigans -_-

--- apps/site/assets/ts/stop/components/DepartureCard.tsx
+++ apps/site/assets/ts/stop/components/DepartureCard.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 import { routeBgClass, busClass } from "../../helpers/css";
 import { breakTextAtSlash } from "../../helpers/text";
 import { isASilverLineRoute } from "../../models/route";
-import { DirectionId, Route, Stop } from "../../__v3api";
+import { Route, Stop } from "../../__v3api";
 import CRsvg from "../../../static/images/icon-commuter-rail-default.svg";
 import Bussvg from "../../../static/images/icon-bus-default.svg";
 import SubwaySvg from "../../../static/images/icon-subway-default.svg";
@@ -50,14 +50,18 @@ const DepartureCard = ({
       <div className={`departure-card__route ${routeBgClass(route)}`}>
         {renderSvg("c-svg__icon", routeToModeIcon(route), true)} {routeName}
       </div>
-      {Object.entries(route.direction_destinations).map(([direction_id]) => (
-        <DepartureTimes
-          key={`${route.id}-${direction_id}`}
-          route={route}
-          stop={stop}
-          directionId={(direction_id as unknown) as DirectionId}
-        />
-      ))}
+      <DepartureTimes
+        key={`${route.id}-0`}
+        route={route}
+        stop={stop}
+        directionId={0}
+      />
+      <DepartureTimes
+        key={`${route.id}-1`}
+        route={route}
+        stop={stop}
+        directionId={1}
+      />
     </li>
   );
 };
