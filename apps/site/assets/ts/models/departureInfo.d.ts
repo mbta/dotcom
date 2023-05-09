0ae785d40d026bb63086c080714ac313ae2e8bfa
kotva006 <ryan.kotval@gmail.com>
2023-05-09 11:14:40 -0500

    Added a comment

--- apps/site/assets/ts/models/departureInfo.d.ts
+++ apps/site/assets/ts/models/departureInfo.d.ts
@@ -1,6 +1,8 @@
 import { PredictionWithTimestamp } from "./perdictions";
 import { ScheduleWithTimestamp } from "./schedules";
 
+// Represents a schedule with its linked prediction
+// Has some helper state variables to avoid recalculation
 export interface DepartureInfo {
   prediction?: PredictionWithTimestamp;
   schedule?: ScheduleWithTimestamp;
