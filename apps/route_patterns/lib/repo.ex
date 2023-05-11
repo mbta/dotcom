192e97c2974d4f1c50e1fc7b255614c089abb74a
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-05-11 15:02:45 -0400

    fix(RoutePattern.Repo): Don't change stop relationship, filter canonical

--- apps/route_patterns/lib/repo.ex
+++ apps/route_patterns/lib/repo.ex
@@ -35,13 +35,13 @@ defmodule RoutePatterns.Repo do
   end
 
   def by_route_id(route_id, opts) do
-    opts =
-      opts
-      |> Keyword.put(:route, route_id)
-      |> Keyword.put(:sort, "typicality,sort_order")
-      |> Keyword.put(:include, "representative_trip.stops")
+    default_opts = [{:canonical, false}]
 
-    opts
+    default_opts
+    |> Keyword.merge(opts)
+    |> Keyword.put(:route, route_id)
+    |> Keyword.put(:sort, "typicality,sort_order")
+    |> Keyword.put(:include, "representative_trip.shape")
     |> cache(&api_all/1)
     |> Enum.sort(&reorder_mrts(&1, &2, route_id))
   end
