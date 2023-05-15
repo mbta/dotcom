a15eeef3876bc1142880f37a0a1fe9c46c355b7f
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2023-05-15 16:53:05 -0400

    test(StopMapRedesign): Placeholders for new StopMapForRoute component

--- apps/site/assets/ts/stop/__tests__/StopMapRedesignTest.tsx
+++ apps/site/assets/ts/stop/__tests__/StopMapRedesignTest.tsx
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
-import { Stop } from "../../__v3api";
-import StopMapRedesign from "../components/StopMapRedesign";
+import { DirectionId, Stop } from "../../__v3api";
+import StopMapRedesign, {
+  StopMapForRoute
+} from "../components/StopMapRedesign";
 import { newLatOrLon, newPolyline } from "./helpers";
+import useVehiclesChannel, { Vehicle } from "../../hooks/useVehiclesChannel";
 
 jest.mock("../../hooks/useMapConfig", () => ({
   __esModule: true,
@@ -37,5 +40,52 @@ describe("StopMapRedesign", () => {
       .querySelector("[aria-label='Map with stop']")
       ?.querySelectorAll(".leaflet-overlay-pane path");
     expect(mapPolylines).toHaveLength(lines.length);
+  });
+
+  describe("StopMapForRoute", () => {
+    jest.mock("useVehiclesChannel", () => ({
+      default: jest.fn(() => [])
+    }));
+
+    const vehicles: Vehicle[] = [
+      {
+        id: "y1799",
+        route_id: "39",
+        stop_id: "72",
+        trip_id: "25",
+        shape_id: "shape_1",
+        direction_id: 1 as DirectionId,
+        status: "STOPPED",
+        latitude: 2.2,
+        longitude: 1.1,
+        bearing: 140,
+        crowding: null
+      },
+      {
+        id: "y1800",
+        route_id: "39",
+        stop_id: "73",
+        trip_id: "25",
+        shape_id: "shape_1",
+        direction_id: 1 as DirectionId,
+        status: "STOPPED",
+        latitude: 2.4,
+        longitude: 1.3,
+        bearing: 141,
+        crowding: null
+      }
+    ];
+
+    beforeEach(() => {
+      (useVehiclesChannel as jest.Mock).mockReturnValue([]);
+    });
+
+    it("should render markers for each vehicle on the route", () => {});
+
+    it("should render the stop marker", () => {});
+    it("should render the route shape", () => {});
+
+    it("when a vehicle is selected, centers on that vehicle if it exists", () => {});
+    it("if the selected vehicle doesn't exist, centers on the stop marker", () => {});
   });
 });
