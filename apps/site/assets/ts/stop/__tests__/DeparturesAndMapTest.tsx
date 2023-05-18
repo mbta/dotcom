54a1d15968131e4430a8027d7514dda2b4951ea9
Isza <ceesaythedev@gmail.com>
2023-05-18 11:06:02 -0400

    test for feedback

--- apps/site/assets/ts/stop/__tests__/DeparturesAndMapTest.tsx
+++ apps/site/assets/ts/stop/__tests__/DeparturesAndMapTest.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import DeparturesAndMap from "../components/DeparturesAndMap";
 import { Stop } from "../../__v3api";
-import { mount } from "enzyme";
 import { RouteWithPolylines } from "../../hooks/useRoute";
 import { routeWithPolylines } from "./helpers";
+import * as useRoute from "../../hooks/useRoute";
+import { getByRole, render } from "@testing-library/react";
 
 const stop = {} as Stop;
-const testRoutesWithPolylines: RouteWithPolylines[] = [];
-
 describe("DeparturesAndMap", () => {
   it("should render", () => {
-    const wrapper = mount(
+    const testRoutesWithPolylines: RouteWithPolylines[] = [
+      routeWithPolylines("SomeBus", 3, 0),
+      routeWithPolylines("741", 3, 2),
+      routeWithPolylines("AnotherBus", 0, 0),
+      routeWithPolylines("Train1", 1, 3),
+      routeWithPolylines("Train2", 1, 4),
+      routeWithPolylines("Train3", 1),
+      routeWithPolylines("FerryRoute", 4, 0)
+    ];
+    jest.spyOn(useRoute, "useRoutesByStop").mockImplementation(() => {
+      return testRoutesWithPolylines;
+    });
+    let departuresAndMap = render(
       <DeparturesAndMap
         routes={[]}
         stop={stop}
@@ -18,6 +29,7 @@ describe("DeparturesAndMap", () => {
         routesWithPolylines={testRoutesWithPolylines}
       />
     );
-    expect(wrapper.find(".stop-routes-and-map").exists()).toBeTruthy();
+    const div = getByRole(departuresAndMap.baseElement, "div");
+    expect(div).toBeInTheDocument();
   });
 });
