c007ec046bf07e4d38847ed165d0a86778c47e13
kotva006 <ryan.kotval@gmail.com>
2023-05-23 17:16:10 -0500

    feat(DetourBadge): Display times along with detour status

--- apps/site/assets/ts/stop/__tests__/components/DepartureTimesTest.tsx
+++ apps/site/assets/ts/stop/__tests__/components/DepartureTimesTest.tsx
@@ -97,9 +97,8 @@ describe("DepartureTimes", () => {
     alertEffect     | expectedBadge
     ${"suspension"} | ${"Stop Closed"}
     ${"shuttle"}    | ${"Shuttle Service"}
-    ${"detour"}     | ${"Detour"}
   `(
-    `displays $expectedBadge when alert has effect $alertEffect`,
+    `displays $expectedBadge when high priority alert has effect $alertEffect`,
     ({ alertEffect, expectedBadge }) => {
       const schedules = [
         { trip: { direction_id: 0 } }
@@ -128,6 +127,56 @@ describe("DepartureTimes", () => {
       expect(screen.getByText(expectedBadge)).toBeDefined();
     }
   );
+
+  it("should display the detour badge with times if detour alert is present", () => {
+    const dateToCompare = new Date("2022-04-27T10:30:00-04:00");
+    jest.spyOn(predictionsChannel, "default").mockImplementation(() => {
+      return {
+        "Test 1": [
+          {
+            time: new Date("2022-04-27T11:15:00-04:00"),
+            trip: { id: "1", headsign: "Test 1" }
+          },
+          {
+            trip: { id: "2", headsign: "Test 1" },
+            time: new Date("2022-04-27T11:20:00-04:00")
+          }
+        ] as PredictionWithTimestamp[]
+      };
+    });
+    const schedules = [
+      {
+        trip: { id: "1", headsign: "Test 1" },
+        time: new Date("2022-04-27T11:15:00-04:00")
+      },
+      {
+        trip: { id: "2", headsign: "Test 1" },
+        time: new Date("2022-04-27T11:18:00-04:00")
+      }
+    ] as ScheduleWithTimestamp[];
+    const detourAlert = {
+      id: "123",
+      informed_entity: {
+        direction_id: [0]
+      },
+      effect: "detour"
+    };
+
+    render(
+      <DepartureTimes
+        route={route}
+        stop={stop}
+        directionId={0}
+        schedulesForDirection={schedules}
+        alertsForDirection={[detourAlert] as Alert[]}
+        overrideDate={dateToCompare}
+        onClick={() => {}}
+      />
+    );
+
+    expect(screen.getByText("Detour")).toBeDefined();
+    expect(screen.getByText("45 min")).toBeDefined();
+  });
 
   describe("getNextTwoTimes", () => {
     it("should return the next 2 departure infos times", () => {
