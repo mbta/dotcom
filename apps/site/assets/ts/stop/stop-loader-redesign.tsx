9fba8c5018658956a17519aba51ec3cd5ccd2b79
Cristen Jones <cjones3@mbta.com>
2023-06-08 10:50:03 -0400

    rename data attribute

--- apps/site/assets/ts/stop/stop-loader-redesign.tsx
+++ apps/site/assets/ts/stop/stop-loader-redesign.tsx
@@ -4,7 +4,7 @@ import StopPageRedesign from "./components/StopPageRedesign";
 
 const render = (): void => {
   const rootEl = document.getElementById("react-stop-redesign-root");
-  const stopId = rootEl?.dataset.stopId;
+  const stopId = rootEl?.dataset.mbtaStopId;
   if (!stopId) return;
   ReactDOM.render(<StopPageRedesign stopId={stopId} />, rootEl);
 };
