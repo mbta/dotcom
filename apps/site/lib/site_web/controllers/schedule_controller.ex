9780dbaf284a2be710aa08233e40c4ac5dba7e8e
Cristen Jones <cjones3@mbta.com>
2023-06-12 14:02:12 -0400

    refactor(StopPageDepartures): filter schedules in backend (#1628)

--- apps/site/lib/site_web/controllers/schedule_controller.ex
+++ apps/site/lib/site_web/controllers/schedule_controller.ex
@@ -1,6 +1,7 @@
 defmodule SiteWeb.ScheduleController do
   use SiteWeb, :controller
   alias Routes.Route
+  alias Schedules.Schedule
 
   plug(SiteWeb.Plugs.Route when action not in [:schedules_for_stop])
 
@@ -45,8 +46,38 @@ defmodule SiteWeb.ScheduleController do
   end
 
   @spec schedules_for_stop(Plug.Conn.t(), map) :: Plug.Conn.t()
-  def schedules_for_stop(conn, %{"stop_id" => stop_id}) do
-    schedules = Schedules.Repo.schedules_for_stop(stop_id, [])
+  def schedules_for_stop(conn, %{"stop_id" => stop_id} = params) do
+    schedules =
+      Schedules.Repo.schedules_for_stop(stop_id, [])
+      |> future_departures(conn, params)
+      |> omit_last_stop_departures(params)
+
     json(conn, schedules)
   end
+
+  defp future_departures(schedules, conn, %{"future_departures" => "true"}) do
+    now =
+      case conn.assigns do
+        %{"date_time" => date_time} -> date_time
+        _ -> Util.now()
+      end
+
+    # Only list schedules with departure_time in the future
+    schedules
+    |> Enum.filter(fn %Schedule{departure_time: dt} ->
+      dt && Util.time_is_greater_or_equal?(dt, now)
+    end)
+  end
+
+  defp future_departures(schedules, _, _), do: schedules
+
+  defp omit_last_stop_departures(schedules, %{"last_stop_departures" => "false"}) do
+    # Don't list schedules departing from the last stop
+    schedules
+    |> Enum.reject(fn %Schedule{departure_time: dt, last_stop?: is_last_stop} ->
+      dt && is_last_stop
+    end)
+  end
+
+  defp omit_last_stop_departures(schedules, _), do: schedules
 end
