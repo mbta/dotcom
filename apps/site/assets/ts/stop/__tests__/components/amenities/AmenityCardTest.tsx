97eceb20cd9faf4e194ae5f6454d12187ad3d12b
Cristen Jones <cjones3@mbta.com>
2023-06-14 12:54:52 -0400

    feat(AmenityModal): open modal, add example

--- apps/site/assets/ts/stop/__tests__/components/amenities/AmenityCardTest.tsx
+++ apps/site/assets/ts/stop/__tests__/components/amenities/AmenityCardTest.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import AmenityCard from "../../../components/amenities/AmenityCard";
+import AmenityCard, {
+  AmenityModal
+} from "../../../components/amenities/AmenityCard";
 
 describe("AmenityCard", () => {
-  it("should render the icon & title", () => {
+  it("should render the icon, title, content", () => {
     render(
       <AmenityCard
         headerText={"Amenity Title"}
         icon={<span data-testid="icon"></span>}
+        content={<div>Outer content</div>}
       />
     );
     expect(screen.getByText("Amenity Title")).toBeDefined();
     expect(screen.getByTestId("icon")).toBeDefined();
+    expect(screen.getByText("Outer content")).toBeDefined();
   });
 
-  it("should render children elements", () => {
+  it("should toggle children elements in a modal", () => {
     render(
-      <AmenityCard headerText={""} icon={<span></span>}>
-        <div>
-          <div>More content</div>
-        </div>
+      <AmenityCard
+        headerText={""}
+        icon={<span></span>}
+        content={<div>Outer content</div>}
+      >
+        <AmenityModal headerText="Inner header">
+          <div>
+            <div>Inner content</div>
+          </div>
+        </AmenityModal>
       </AmenityCard>
     );
-    expect(screen.getByText("More content")).toBeDefined();
+    expect(screen.queryByText("Inner header")).toBeNull();
+    expect(screen.queryByText("Inner content")).toBeNull();
+    screen.getByRole("button").click();
+    expect(screen.queryByText("Inner header")).toBeDefined();
+    expect(screen.queryByText("Inner content")).toBeDefined();
+    screen.getByRole("button", { name: "Close" }).click();
+    expect(screen.queryByText("Inner header")).toBeNull();
+    expect(screen.queryByText("Inner content")).toBeNull();
   });
 });
--- apps/site/assets/ts/stop/components/amenities/AmenityCard.tsx
+++ apps/site/assets/ts/stop/components/amenities/AmenityCard.tsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { ReactElement, useContext, useState } from "react";
 import renderFa from "../../../helpers/render-fa";
+import Modal from "../../../components/Modal";
+
+const AmenityModalContext = React.createContext({
+  closeModal: () => {}
+});
+
+export const AmenityModal = ({
+  headerText,
+  children
+}: {
+  headerText: string;
+  children?: JSX.Element[] | JSX.Element;
+}): ReactElement => {
+  const elementId = headerText.toLowerCase().replace(/[ _]/g, "-");
+  const { closeModal } = useContext(AmenityModalContext);
+  return (
+    <Modal
+      className="c-amenity-modal"
+      closeModal={closeModal}
+      ariaLabel={{ elementId }}
+    >
+      <h1 id={elementId} className="h4">
+        {headerText}
+      </h1>
+      {children}
+    </Modal>
+  );
+};
 
 const AmenityCard = ({
   headerText,
   icon,
-  content
+  content,
+  children
 }: {
   headerText: string;
   icon: JSX.Element;
   content?: JSX.Element[] | JSX.Element;
+  children?: JSX.Element[] | JSX.Element;
 }): JSX.Element => {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div
       className="c-descriptive-link justify-content-space-between"
@@ -28,9 +59,19 @@ const AmenityCard = ({
           </div>
         )}
       </div>
-      <button type="button" className="c-descriptive-link__caret-wrapper">
+      <button
+        type="button"
+        className="c-descriptive-link__caret-wrapper"
+        onClick={() => setModalOpen(true)}
+        disabled={!children}
+      >
         {renderFa("c-descriptive-link__caret", "fa-angle-right")}
       </button>
+      <AmenityModalContext.Provider
+        value={{ closeModal: () => setModalOpen(false) }}
+      >
+        {modalOpen && children}
+      </AmenityModalContext.Provider>
     </div>
   );
 };
