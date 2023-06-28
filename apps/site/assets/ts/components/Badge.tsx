ea771518b90c238f1bc5f7747ba7511ef9ae53de
Cristen Jones <cjones3@mbta.com>
2023-06-28 15:06:37 -0400

    feat(AmenityCard): add badge prop, improve styles (#1645)
    * fix(AmenityCard): use better children type
    
    `ReactNode` is more flexible, allows false and other child elements
    
    * feat(AmenityCard): add disabled state and styles
    
    when there's no modal content, add disabled state to the button and hide the caret icon
    
    * refactor(AmenityCard): separate badge from content
    
    - Badges shown on the amenity cards aren't supposed to be hidden in mobile like the rest of the card content is. So here we render it in a separate div before the rest of the content.
    - Sets display: inline-block so it doesn't take up the entire card width by default.
    
    * feat(Badge): support custom background-color CSS
    
    * feat(AmenityLink): reusable link
    
    * fixup: more AmenityCard style improvements

--- apps/site/assets/ts/components/Badge.tsx
+++ apps/site/assets/ts/components/Badge.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const Badge = ({
   text,
-  contextText
+  contextText,
+  bgClass
 }: {
   text: string;
   contextText?: string;
+  bgClass?: string;
 }): JSX.Element => {
   return (
     <div
-      className="u-error-background font-weight-bold ps-8 pe-8 fs-14"
+      className={`${bgClass ||
+        "u-error-background"} font-weight-bold ps-8 pe-8 fs-14`}
       style={{ borderRadius: "0.75rem" }}
     >
       {/* The purpose of this block is to have invisble text for screen readers */}
