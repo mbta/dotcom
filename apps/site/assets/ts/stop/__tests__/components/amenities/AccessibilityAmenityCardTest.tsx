f0fa2c1e06900cbcbd3b0dde2c96145ec0c32da6
Cristen Jones <cjones3@mbta.com>
2023-06-29 11:54:58 -0400

    feat(AccessibilityAmenityCard): add bus content
    and some improvements in response to feedback

--- apps/site/assets/ts/stop/__tests__/components/amenities/AccessibilityAmenityCardTest.tsx
+++ apps/site/assets/ts/stop/__tests__/components/amenities/AccessibilityAmenityCardTest.tsx
@@ -8,9 +8,15 @@ describe("AccessibilityAmenityCard", () => {
       <AccessibilityAmenityCard
         stopName=""
         accessibleFeatures={["accessible", "ramp"]}
+        isStation
       />
     );
     expect(screen.getByText("Accessibility")).toBeDefined();
+    expect(
+      screen.getByText(
+        "Learn more about the accessibility features at this station."
+      )
+    ).toBeDefined();
     screen.getByRole("button").click(); // open modal
     [
       "Report an accessibility issue",
@@ -20,11 +26,57 @@ describe("AccessibilityAmenityCard", () => {
     ].forEach(text => {
       expect(screen.getByRole("link", { name: text })).toBeDefined();
     });
+    expect(
+      screen.getByRole("heading", { name: "Station Features" })
+    ).toBeDefined();
   });
 
   it("should indicate absense of accessibility", () => {
-    render(<AccessibilityAmenityCard stopName="" accessibleFeatures={[]} />);
+    render(
+      <AccessibilityAmenityCard stopName="" accessibleFeatures={[]} isStation />
+    );
     expect(screen.getByText("Not accessible")).toBeDefined();
     expect(screen.getByText("This station is not accessible.")).toBeDefined();
+  });
+
+  it("should show different items for bus stops", () => {
+    render(
+      <AccessibilityAmenityCard
+        stopName="StopName"
+        accessibleFeatures={["accessible"]}
+        isStation={false}
+      />
+    );
+    expect(
+      screen.getByText(
+        "Learn more about the accessibility features at this stop."
+      )
+    ).toBeDefined();
+    screen.getByRole("button").click(); // open modal
+    [
+      "Report an accessibility issue",
+      "Learn more about bus accessibility",
+      "Plan an accessible trip",
+      "Visit the Mobility Center"
+    ].forEach(text => {
+      expect(screen.getByRole("link", { name: text })).toBeDefined();
+    });
+    expect(screen.getByRole("heading", { name: "Bus Features" })).toBeDefined();
+  });
+
+  it("should handle stations without extra features", () => {
+    render(
+      <AccessibilityAmenityCard
+        stopName=""
+        accessibleFeatures={["accessible"]}
+        isStation
+      />
+    );
+    screen.getByRole("button").click(); // open modal
+    expect(
+      screen.getByText(
+        "There are no additional accessibility features, but buses are always accessible to board."
+      )
+    ).toBeDefined();
   });
 });
