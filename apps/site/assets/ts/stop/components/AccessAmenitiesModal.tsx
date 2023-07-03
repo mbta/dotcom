feb345e6f429385dbd91727e03a8f2b171c4dd59
Isza <ceesaythedev@gmail.com>
2023-07-03 09:56:10 -0400

    use table

--- apps/site/assets/ts/stop/components/AccessAmenitiesModal.tsx
+++ apps/site/assets/ts/stop/components/AccessAmenitiesModal.tsx
@@ -26,36 +26,36 @@ const AccessAmenitiesModal = ({
         <AmenityModal headerText={`${facilityType} at ${stopName}`}>
           <Alerts alerts={alerts} />
           <h2>Eleavator Status</h2>
-          <div className="access-amenities-row facilities-list-header">
-            <h3>Elevator</h3>
-            <h3>Status</h3>
-          </div>
-          <div className="access-amenities-container">
+
+          <table className="access-amenities-table">
+            <tr className="access-amenities-header access-amenities-row">
+              <th className="access-amenities-name">Elevator</th>
+              <th className="status">Status</th>
+            </tr>
             {facilities?.map(facility => {
               return (
-                <div
+                <tr
                   key={facility.id}
                   className="access-amenities-row access-amenities-text"
                 >
-                  <div className="access-amenities-name">
+                  <td className="access-amenities-name">
                     {facility.attributes.short_name}
-                  </div>
+                  </td>
                   {hasFacilityAlert(facility.id, alerts) ? (
-                    <div>
+                    <td className="status">
                       <i className="fa-solid fa-circle amenity-status amenity-out" />
                       Out of Order
-                    </div>
+                    </td>
                   ) : (
-                    <div>
+                    <td className="status">
                       <i className="fa-solid fa-circle amenity-status amenity-working" />
                       Working
-                    </div>
+                    </td>
                   )}
-                </div>
+                </tr>
               );
             })}
-          </div>
-
+          </table>
           <AmenityLink
             url="/accessibility/trip-planning"
             text="Plan an accessible trip"
