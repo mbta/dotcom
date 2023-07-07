bb687a80f5482a7741019449a42ef3cebc6a81da
Isza <ceesaythedev@gmail.com>
2023-07-07 16:48:38 -0400

    typing

--- apps/site/assets/ts/stop/components/amenities/EscalatorsAmenityCard.tsx
+++ apps/site/assets/ts/stop/components/amenities/EscalatorsAmenityCard.tsx
@@ -29,7 +29,7 @@ const EscalatorsAmenityCard = ({
             stopName={stopName}
             alerts={alerts}
             facilities={escalatorFacilities}
-            facilityType="Escalators"
+            facilityType="Escalator"
           />
         )
       }
