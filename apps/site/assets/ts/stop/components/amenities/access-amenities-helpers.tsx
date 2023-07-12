a34949b58a2a820c006f75ab883552646d122ace
Isza <ceesaythedev@gmail.com>
2023-07-12 14:07:36 -0400

    use gray lightest on badge

--- apps/site/assets/ts/stop/components/amenities/access-amenities-helpers.tsx
+++ apps/site/assets/ts/stop/components/amenities/access-amenities-helpers.tsx
@@ -27,7 +27,7 @@ export const cardBadge = (
       backgroundClass = "u-error-background";
     }
     if (workingFacilities > 0 && workingFacilities < accessFacilities.length) {
-      backgroundClass = "gray-lightest";
+      backgroundClass = "u-bg--gray-lightest";
     }
     return (
       <Badge
