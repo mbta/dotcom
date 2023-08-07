addcc072ee7c56a21aca0178b370a3a8d4017b6f
Cristen Jones <cjones3@mbta.com>
2023-08-07 11:11:14 -0400

    fix: disable turbolinks on navigating to schedules

--- apps/site/assets/ts/stop/components/DepartureList.tsx
+++ apps/site/assets/ts/stop/components/DepartureList.tsx
@@ -57,6 +57,7 @@ const DepartureList = ({
           <a
             className="open-schedule"
             href={`../schedules/${route.id}/line?schedule_direction[direction_id]=${directionId}&schedule_direction[variant]=${tripForSelectedRoutePattern?.route_pattern_id}&schedule_finder[direction_id]=${directionId}&schedule_finder[origin]=${stop.id}`}
+            data-turbolinks="false"
           >
             View all schedules
           </a>
