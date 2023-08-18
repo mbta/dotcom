ba9d689365f8440d988f7295e13592c566bff29c
Erin Moore <erinmoorethefirst@gmail.com>
2023-08-18 08:53:27 -0400

    fix broken test

--- apps/alerts/test/parser_test.exs
+++ apps/alerts/test/parser_test.exs
@@ -66,7 +66,7 @@ defmodule Alerts.ParserTest do
                  effect: :delay,
                  updated_at: ~N[2016-06-20T16:09:29] |> Timex.to_datetime("Etc/GMT+4"),
                  description: "Affected routes: 18",
-                 priority: :high,
+                 priority: :low,
                  url: "www.mbta.com"
                }
     end
