ea279188518d2158a7a691d696e6bed63ae04d3b
Cristen Jones <cjones3@mbta.com>
2023-08-18 20:32:51 -0400

    feat(Predictions.Store): new GenServer managing predictions

---
+++ apps/predictions/lib/store.ex
@@ -0,0 +1,104 @@
+defmodule Predictions.Store do
+  @moduledoc """
+  Server for saving and editing predictions in an ETS table. Predictions are
+  added, removed, and updated by other processes via `update/2`. Predictions can
+  be retrieved using `fetch/1` for any combination of values specified of
+  `fetch_keys`.
+  """
+  use GenServer
+
+  alias Predictions.Prediction
+  alias Routes.Route
+  alias Schedules.Trip
+  alias Stops.Stop
+  alias Vehicles.Vehicle
+
+  @type fetch_keys :: [
+          prediction_id: Prediction.id_t(),
+          route: Route.id_t(),
+          stop: Stop.id_t(),
+          direction: 0 | 1,
+          trip: Trip.id_t(),
+          vehicle_id: Vehicle.id_t()
+        ]
+
+  @spec start_link(Keyword.t()) :: GenServer.on_start()
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @spec fetch(fetch_keys) :: [Prediction.t()]
+  def fetch(keys) do
+    GenServer.call(__MODULE__, {:fetch, keys})
+  end
+
+  @spec update(atom, [Prediction.t()]) :: :ok
+  def update(event, predictions) do
+    GenServer.cast(__MODULE__, {event, predictions})
+  end
+
+  # Server
+  @impl GenServer
+  def init(opts) do
+    table = :ets.new(Keyword.get(opts, :name, __MODULE__), [:public])
+    {:ok, table}
+  end
+
+  @impl true
+  def handle_cast({event, predictions}, table) when event in [:add, :update, :reset] do
+    _ = :ets.insert(table, Enum.map(predictions, &to_record/1))
+    {:noreply, table}
+  end
+
+  def handle_cast({:remove, predictions}, table) do
+    for id <- Enum.map(predictions, & &1.id) do
+      :ets.delete(table, id)
+    end
+
+    {:noreply, table}
+  end
+
+  def handle_cast(_, table), do: {:noreply, table}
+
+  @impl true
+  def handle_call({:fetch, keys}, _from, table) do
+    predictions = predictions_for_keys(table, keys)
+    {:reply, predictions, table}
+  end
+
+  @spec predictions_for_keys(:ets.table(), fetch_keys) :: [Prediction.t()]
+  defp predictions_for_keys(table, opts) do
+    match_pattern = {
+      Keyword.get(opts, :prediction_id, :_) || :_,
+      Keyword.get(opts, :route, :_) || :_,
+      Keyword.get(opts, :stop, :_) || :_,
+      Keyword.get(opts, :direction, :_) || :_,
+      Keyword.get(opts, :trip, :_) || :_,
+      Keyword.get(opts, :vehicle_id, :_) || :_,
+      :"$1"
+    }
+
+    :ets.select(table, [{match_pattern, [], [:"$1"]}])
+  end
+
+  defp to_record(
+         %Prediction{
+           id: id,
+           route: route,
+           stop: stop,
+           direction_id: direction,
+           trip: trip,
+           vehicle_id: vehicle_id
+         } = prediction
+       ) do
+    {
+      id,
+      if(route, do: route.id),
+      if(stop, do: stop.id),
+      direction,
+      if(trip, do: trip.id),
+      vehicle_id,
+      prediction
+    }
+  end
+end
---
+++ apps/predictions/test/store_test.exs
@@ -0,0 +1,102 @@
+defmodule Predictions.StoreTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  import Predictions.Store
+  alias Predictions.Prediction
+  alias Routes.Route
+  alias Schedules.Trip
+  alias Stops.Stop
+
+  setup_all do
+    _ = start_link(name: :test_store)
+    :ok = Predictions.Store.update(:add, base_predictions())
+    :ok
+  end
+
+  describe "fetch/1" do
+    test "fetches for key or keys" do
+      trip1_predictions = fetch(trip: "t1")
+      assert Enum.all?(trip1_predictions, &(&1.trip.id == "t1"))
+
+      other_predictions =
+        fetch(route: "purple", trip: "t4", stop: "s2", vehicle_id: "v3", direction: 1)
+
+      assert Enum.all?(
+               other_predictions,
+               &(&1.trip.id == "t4" && &1.route.id == "purple" && &1.stop.id == "s2" &&
+                   &1.vehicle_id == "v3" && &1.direction_id == 1)
+             )
+    end
+  end
+
+  describe "update/1" do
+    test "adds predictions" do
+      assert [] = fetch(prediction_id: "added")
+      update(:add, [%Prediction{id: "added"}])
+      assert [%Prediction{id: "added"}] = fetch(prediction_id: "added")
+    end
+
+    test "updates predictions" do
+      update(:add, [%Prediction{id: "new", stop: %Stop{id: "s1"}}])
+
+      assert [%Prediction{id: "new", stop: %Stop{id: "s1"}}] = fetch(prediction_id: "new")
+
+      update(:update, [%Prediction{id: "new", stop: %Stop{id: "s3"}}])
+
+      assert [%Prediction{id: "new", stop: %Stop{id: "s3"}}] = fetch(prediction_id: "new")
+    end
+
+    test "resets predictions" do
+      update(:add, [
+        %Prediction{id: "uno", stop: %Stop{id: "s100"}}
+      ])
+
+      update(:reset, [
+        %Prediction{id: "uno", trip: %Trip{id: "added-trip"}}
+      ])
+
+      assert [prediction] = fetch(prediction_id: "uno")
+
+      assert %Prediction{id: "uno", trip: %Trip{id: "added-trip"}} = prediction
+      refute prediction.stop == %Stop{id: "s100"}
+    end
+
+    test "removes predictions" do
+      update(:add, [%Prediction{id: "added"}])
+      assert [%Prediction{id: "added"}] = fetch(prediction_id: "added")
+      update(:remove, [%Prediction{id: "added"}])
+      assert [] = fetch(prediction_id: "added")
+    end
+  end
+
+  defp base_routes do
+    ["red", "orange", "yellow", "green", "blue", "purple"]
+    |> Enum.map(&%Route{id: &1})
+  end
+
+  defp base_trips do
+    ["t1", "t2", "t3", "t4", "t5"]
+    |> Enum.map(&%Trip{id: &1})
+  end
+
+  defp base_stops do
+    ["s1", "s2", "s3", "s4", "s5", "s6"]
+    |> Enum.map(&%Stop{id: &1})
+  end
+
+  defp base_predictions do
+    for r <- base_routes(),
+        t <- base_trips(),
+        s <- base_stops(),
+        v <- ["v1", "v2", "v3", "v4"],
+        d <- [0, 1],
+        do: %Prediction{
+          id: "prediction-#{r.id}-#{d}-#{s.id}-#{v}-#{t.id}",
+          route: r,
+          trip: t,
+          stop: s,
+          vehicle_id: v,
+          direction_id: d
+        }
+  end
+end
