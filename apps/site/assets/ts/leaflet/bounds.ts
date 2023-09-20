8fd2caa0858c09e379b7befd84d492f9cf42c831
Cristen Jones <cjones3@mbta.com>
2023-09-20 15:51:09 -0400

    hotfix(Leaflet): prevent nonsense bounds

--- apps/site/assets/ts/leaflet/bounds.ts
+++ apps/site/assets/ts/leaflet/bounds.ts
@@ -1,7 +1,8 @@
 import { LatLng, latLng, LatLngBounds, latLngBounds } from "leaflet";
 import { MapMarker as Marker } from "./components/__mapdata";
 
-const getBounds = (markers: Marker[]): LatLngBounds => {
+const getBounds = (markers: Marker[]): LatLngBounds | undefined => {
+  if (!markers.length) return undefined;
   const points: LatLng[] = markers.map(m => latLng(m.latitude, m.longitude));
   return latLngBounds(points);
 };
