958c3ff66f86607f872062142c2bdd753a36ed05
Cristen Jones <cjones3@mbta.com>
2023-09-22 16:00:18 -0400

    feat(StopPageRedesign): add an error boundary (#1754)
    * feat(StopPageRedesign): add an error boundary
    
    * chore: remove a few files from jest coverage

---
+++ apps/site/assets/ts/components/ErrorPage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FallbackRender } from "@sentry/react";
+
+// Adapted from the templates in SiteWeb.ErrorView
+// To be used in an ErrorBoundary
+const ErrorPage: FallbackRender = errorData => {
+  // eslint-disable-next-line no-console
+  console.error("from Sentry", JSON.stringify(errorData));
+  return (
+    <div className="container error-page">
+      <div className="row">
+        <div className="col-md-6">
+          <h1 className="mt-1">Oh no! We&#39;re experiencing delays.</h1>
+          <p className="error-paragraph u-bold">
+            Something went wrong on our end.
+          </p>
+          <div className="error-links">
+            Go to{" "}
+            <a className="c-call-to-action" href="/">
+              {`the MBTA's home page`}
+            </a>{" "}
+            or{" "}
+            <a className="c-call-to-action" href="/customer-support">
+              report an issue
+            </a>
+            .
+          </div>
+        </div>
+        <div className="col-md-6">
+          <img alt="" src="/images/error-bus.gif" className="w-100" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
