5e34d662dc6267e3de675eea4738280ae5542468
Cristen Jones <cjones3@mbta.com>
2023-10-19 15:49:46 -0400

    feat(Algolia.Query): build query params on backend
    - checks for valid index
    - adjusts query params based on index

---
+++ apps/algolia/lib/algolia/request.ex
@@ -0,0 +1,86 @@
+defmodule Algolia.Query.Request do
+  @moduledoc """
+  Algolia's REST API expects this format for each request
+  https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-preset-algolia/getAlgoliaResults/#param-queries-2
+
+  This module has some helpers to create requests for our indexes with some
+  specific default parameters.
+  """
+  @supported_indexes Algolia.Query.valid_indexes()
+
+  defstruct indexName: "",
+            query: "",
+            params: %{
+              "hitsPerPage" => 5,
+              "clickAnalytics" => true,
+              "facets" => ["*"],
+              "facetFilters" => [[]]
+            },
+            attributesToHighlight: ""
+
+  @type t :: %__MODULE__{
+          indexName: String.t(),
+          query: String.t(),
+          params: map(),
+          attributesToHighlight: String.t() | [String.t()]
+        }
+  @spec new(String.t(), String.t()) :: t()
+  def new(indexName, query) when indexName in @supported_indexes do
+    %__MODULE__{
+      indexName: indexName,
+      query: query,
+      attributesToHighlight: highlight(indexName)
+    }
+    |> with_hit_size(indexName)
+    |> with_facet_filters(indexName)
+  end
+
+  defp highlight("routes"), do: ["route.name", "route.long_name"]
+  defp highlight("stops"), do: "stop.name"
+  defp highlight("drupal"), do: "content_title"
+
+  defp with_hit_size(request, indexName) do
+    %__MODULE__{
+      request
+      | params: %{request.params | "hitsPerPage" => hit_size(indexName)}
+    }
+  end
+
+  defp hit_size("routes"), do: 5
+  defp hit_size("stops"), do: 2
+  defp hit_size("drupal"), do: 2
+
+  @spec with_facet_filters(t(), String.t()) :: t()
+  defp with_facet_filters(request, "drupal") do
+    %__MODULE__{
+      request
+      | params: %{
+          request.params
+          | "facetFilters" => [
+              [
+                "_content_type:page",
+                "_content_type:search_result",
+                "_content_type:diversion",
+                "_content_type:landing_page",
+                "_content_type:person",
+                "_content_type:project",
+                "_content_type:project_update"
+              ]
+            ]
+        }
+    }
+  end
+
+  defp with_facet_filters(request, _), do: request
+
+  def encode(%__MODULE__{} = request) do
+    request
+    |> Map.from_struct()
+    |> Map.update!(:params, &Algolia.Query.encode_params/1)
+    |> Map.new(fn {k, v} -> {to_string(k), v} end)
+    # The highlight tag values are needed for compatibility with Algolia's
+    # autocomplete.js library v1+
+    |> Map.put_new("highlightPreTag", "__aa-highlight__")
+    |> Map.put_new("highlightPostTag", "__/aa-highlight__")
+  end
+end
---
+++ apps/algolia/test/request_test.exs
@@ -0,0 +1,50 @@
+defmodule Algolia.Query.RequestTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+  alias Algolia.Query.Request
+
+  describe "new/2" do
+    test "doesn't work for invalid index" do
+      assert Request.new("drupal", "question")
+      assert_raise FunctionClauseError, fn -> Request.new("fakeindex", "question") end
+    end
+
+    test "includes query" do
+      search_string = "asfdsagfsadgsad"
+      assert %Request{query: ^search_string} = Request.new("drupal", search_string)
+    end
+
+    test "changes hitsPerPage param based on index" do
+      assert %Request{params: %{"hitsPerPage" => 5}} = Request.new("routes", "")
+      assert %Request{params: %{"hitsPerPage" => 2}} = Request.new("drupal", "")
+    end
+
+    test "changes attributesToHighlight param based on index" do
+      assert %Request{attributesToHighlight: "stop.name"} = Request.new("stops", "")
+
+      assert %Request{attributesToHighlight: ["route.name", "route.long_name"]} =
+               Request.new("routes", "")
+    end
+
+    test "changes facetFilters param based on index" do
+      assert %Request{params: %{"facetFilters" => [[]]}} = Request.new("routes", "")
+      assert %Request{params: %{"facetFilters" => facetFilters}} = Request.new("drupal", "")
+      refute facetFilters == [[]]
+    end
+  end
+
+  test "encode/1 returns a JSON-encodable map with encoded params" do
+    request = Request.new("drupal", "some special search")
+    encoded = Request.encode(request)
+
+    assert encoded == %{
+             "attributesToHighlight" => "content_title",
+             "highlightPostTag" => "__/aa-highlight__",
+             "highlightPreTag" => "__aa-highlight__",
+             "indexName" => "drupal",
+             "params" =>
+               "analytics=false&clickAnalytics=true&facetFilters=%5B%5B%22_content_type%3Apage%22%2C%22_content_type%3Asearch_result%22%2C%22_content_type%3Adiversion%22%2C%22_content_type%3Alanding_page%22%2C%22_content_type%3Aperson%22%2C%22_content_type%3Aproject%22%2C%22_content_type%3Aproject_update%22%5D%5D&facets=%5B%22*%22%5D&hitsPerPage=2",
+             "query" => "some special search"
+           }
+  end
+end
