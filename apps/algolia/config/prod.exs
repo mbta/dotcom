130476ea14fe9b086cfeffd36818b1f4436e15fa
Cristen Jones <cjones3@mbta.com>
2023-10-24 12:40:51 -0400

    chore: remove unused config

--- apps/algolia/config/prod.exs
+++ apps/algolia/config/prod.exs
@@ -1,10 +1,5 @@
 import Config
 
-config :algolia, :keys,
-  app_id: "${ALGOLIA_APP_ID}",
-  search: "${ALGOLIA_SEARCH_KEY}",
-  write: "${ALGOLIA_WRITE_KEY}"
-
 config :algolia, :click_analytics_url, "https://insights.algolia.io"
 config :algolia, :track_clicks?, true
 config :algolia, :track_analytics?, true
