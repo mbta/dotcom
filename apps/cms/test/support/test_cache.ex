b2d3fa9289a1995565c47ffddb44e1db81f4b38f
Anthony Shull <shull.anthony@gmail.com>
2023-12-20 17:30:28 -0600

    docs for test cache

--- apps/cms/test/support/test_cache.ex
+++ apps/cms/test/support/test_cache.ex
@@ -1,5 +1,7 @@
 defmodule CMS.TestCache do
-  use Nebulex.Cache,
-    otp_app: :cms,
-    adapter: Nebulex.Adapters.Local
+  @moduledoc """
+  This local Nebulex instance allows us to run tests w/out the need for Redis.
+  """
+
+  use Nebulex.Cache, otp_app: :cms, adapter: Nebulex.Adapters.Local
 end
