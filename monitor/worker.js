b5e8891c72c34ec42a88d377688bc7e5a148ab73
Anthony Shull <shull.anthony@gmail.com>
2024-01-25 12:37:13 -0600

    ...

--- monitor/worker.js
+++ monitor/worker.js
@@ -7,27 +7,25 @@ const client = new StatsD({
     prefix: 'dotcom.monitor.',
 });
 
-parentPort.on('message', _ => {
-    (async () => {
-        const { scenario } = require(workerData);
+parentPort.on('message', async _ => {
+    const { scenario } = require(workerData);
 
-        const browser = await chromium.launch();
-        const context = await browser.newContext();
-        const page = await context.newPage();
-    
-        const start = performance.now();
-    
-        await scenario.run({ page, baseURL: process.env.TARGET_URL });
-    
-        const end = performance.now();
-        const duration = end - start;
-    
-        const metric = scenario.name.toLowerCase().replace(/ /g, '.');
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-        client.gauge(metric, Math.floor(duration));
+    const start = performance.now();
 
-        parentPort.postMessage(`Scenario: "${scenario.name}" took ${Math.floor(duration)}ms`);
-    
-        await browser.close();
-    })();
+    await scenario.run({ page, baseURL: process.env.TARGET_URL });
+
+    const end = performance.now();
+    const duration = end - start;
+
+    const metric = scenario.name.toLowerCase().replace(/ /g, '.');
+
+    client.gauge(metric, Math.floor(duration));
+
+    parentPort.postMessage(`Scenario: "${scenario.name}" took ${Math.floor(duration)}ms`);
+
+    await browser.close();
 });
