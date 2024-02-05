4c141e6c133a31634077d9f7cc47cff04cddf7ca
kotva006 <ryan.kotval@gmail.com>
2024-02-05 10:53:15 -0600

    feat(SchedulePDFS): Change Additional Info Header, and add test

--- assets/ts/schedule/components/PDFSchedules.tsx
+++ assets/ts/schedule/components/PDFSchedules.tsx
@@ -3,6 +3,8 @@ import ExpandableBlock from "../../components/ExpandableBlock";
 import { SchedulePDF } from "./__schedule";
 import renderSvg from "../../helpers/render-svg";
 import pdfIcon from "../../../static/images/icon-pdf-default.svg";
+import { Route } from "../../__v3api";
+import { isACommuterRailRoute } from "../../models/route";
 
 const link = (pdf: SchedulePDF): ReactElement<HTMLElement> => (
   <a
@@ -20,12 +22,19 @@ const link = (pdf: SchedulePDF): ReactElement<HTMLElement> => (
 
 interface Props {
   pdfs: SchedulePDF[];
+  route: Route;
 }
 
-const PDFSchedules = ({ pdfs }: Props): ReactElement<HTMLElement> | null =>
+const PDFSchedules = ({
+  pdfs,
+  route
+}: Props): ReactElement<HTMLElement> | null =>
   pdfs.length > 0 ? (
     <ExpandableBlock
-      header={{ text: "PDF Schedules and Maps", iconSvgText: null }}
+      header={{
+        text: `PDF Schedules${isACommuterRailRoute(route) ? "" : " and Maps"}`,
+        iconSvgText: null
+      }}
       initiallyExpanded
       id="pdfs"
     >
--- assets/ts/schedule/components/__tests__/PDFSchedulesTest.tsx
+++ assets/ts/schedule/components/__tests__/PDFSchedulesTest.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { createReactRoot } from "../../../app/helpers/testUtils";
 import PDFSchedules from "../PDFSchedules";
+import { Route } from "../../../__v3api";
+import { render, screen } from "@testing-library/react";
 
 const pdfs = [
   {
@@ -10,14 +12,32 @@ const pdfs = [
   }
 ];
 
+const crRoute = {
+  type: 2
+} as Route;
+
+const railRoute = {
+  type: 0
+} as Route;
+
 it("it renders", () => {
   createReactRoot();
-  const tree = renderer.create(<PDFSchedules pdfs={pdfs} />).toJSON();
+  const tree = renderer
+    .create(<PDFSchedules pdfs={pdfs} route={railRoute} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it("it renders without pdfs", () => {
   createReactRoot();
-  const tree = renderer.create(<PDFSchedules pdfs={[]} />).toJSON();
+  const tree = renderer
+    .create(<PDFSchedules pdfs={[]} route={railRoute} />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it("Does not include the `and Maps` for CR routes", () => {
+  render(<PDFSchedules pdfs={pdfs} route={crRoute} />);
+  expect(screen.queryByText("PDF Schedules and Maps")).toBeNull();
+  expect(screen.getByText("PDF Schedules")).toBeInTheDocument();
 });
