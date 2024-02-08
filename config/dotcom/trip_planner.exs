7d70a588a5c9e4311380e35fe3200a881fd121a8
Cristen Jones <cjones3@mbta.com>
2024-02-08 12:42:55 -0500

    feat(OpenTripPlannerClient.Mock): add test stub

--- config/dotcom/trip_planner.exs
+++ config/dotcom/trip_planner.exs
@@ -3,6 +3,7 @@ import Config
 config :dotcom, TripPlanGeocode, module: TripPlan.Geocode
 
 if config_env() == :test do
+  config :dotcom, :trip_planner, OpenTripPlannerClient.Mock
 
   config :dotcom, TripPlanGeocode, module: TripPlan.Geocode.MockGeocode
 end
