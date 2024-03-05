f843b8fb2a68449365cb560fc87d13c1bd21429c
Cristen Jones <cjones3@mbta.com>
2024-03-05 14:22:24 -0500

    moving strings around

--- integration/e2e_tests/all-scenarios.spec.js
+++ integration/e2e_tests/all-scenarios.spec.js
@@ -8,9 +8,9 @@ const { fileToMetricName } = require("../utils");
 const filesPath = path.join(__dirname, "..", "scenarios");
 const files = fs.readdirSync(filesPath);
 
-const baseURL = process.env.PLAYWRIGHT_TEST_BASE_URL || (process.env.HOST
+const baseURL = process.env.HOST
   ? `https://${process.env.HOST}`
-  : "http://localhost:4001");
+  : "http://localhost:4001";
 
 files.forEach((file) => {
   test.describe("All scenarios", (_) => {
