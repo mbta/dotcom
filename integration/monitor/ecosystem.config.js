01a5394fc7ade50bfdfcebec7f7d325c81a57bda
Anthony Shull <shull.anthony@gmail.com>
2024-03-06 09:59:47 -0600

    add memory limits on processes and setup log rotation

--- integration/monitor/ecosystem.config.js
+++ integration/monitor/ecosystem.config.js
@@ -4,11 +4,15 @@ module.exports = {
       name: "all-health-checks",
       script: "./integration/monitor/all-health-checks.js",
       instances: 1,
+      max_memory_restart: "256M",
+      exec_mode : "cluster"
     },
     {
       name: "all-scenarios",
       script: "./integration/monitor/all-scenarios.js",
       instances: 1,
+      max_memory_restart: "1024M",
+      exec_mode : "cluster"
     },
   ],
 };
