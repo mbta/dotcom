7118d90407541dd883dd83ae4bb33e813a9f4083
Anthony Shull <shull.anthony@gmail.com>
2024-03-11 15:54:46 -0500

    cleaner test

--- test/dotcom/cache/multilevel_test.exs
+++ test/dotcom/cache/multilevel_test.exs
@@ -10,18 +10,21 @@ defmodule Dotcom.Cache.MultilevelTest do
   @cache Application.compile_env!(:dotcom, :cache)
 
   describe "flush_keys" do
-    test "deletes all keys from Redis" do
+    test "deletes all keys that match the pattern" do
       @cache.put("foo|bar", "baz")
       @cache.put("foo|baz", "bar")
+      @cache.put("bar|foo", "baz")
 
       expect(Redix.Mock, :start_link, fn _ -> {:ok, 0} end)
-      expect(Redix.Mock, :command, fn _, _ -> {:ok, [:stop, ["foo|bar", "foo|baz"]]} end)
+      expect(Redix.Mock, :command, fn _, _ -> {:ok, [1, ["foo|bar"]]} end)
+      expect(Redix.Mock, :command, fn _, _ -> {:ok, [:stop, ["foo|baz"]]} end)
       expect(Redix.Mock, :stop, fn _ -> :ok end)
 
       assert Dotcom.Cache.Multilevel.flush_keys("foo|*") == :ok
 
       assert @cache.get("foo|bar") == nil
       assert @cache.get("foo|baz") == nil
+      assert @cache.get("bar|foo") == "baz"
     end
   end
 end
