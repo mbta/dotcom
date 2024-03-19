87ca3436434ec08d1a0fbe3711563ec069f99a05
Anthony Shull <shull.anthony@gmail.com>
2024-03-19 13:15:21 -0500

    use blue line and check for predictions

--- integration/scenarios/search-for-a-subway-line.js
+++ integration/scenarios/search-for-a-subway-line.js
@@ -7,16 +7,17 @@ exports.scenario = async ({ page, baseURL }) => {
   // But, the enter key does not load the page. So, we have to click the first result.
   await page
     .locator("div.search-wrapper input#input")
-    .pressSequentially("Green Line");
+    .pressSequentially("Blue Line");
   await page.waitForSelector("ul#algolia-list");
   await page.locator("ul#algolia-list li:first-child a").click();
 
   await expect(page.locator("h1.schedule__route-name")).toHaveText(
-    "Green Line",
+    "Blue Line",
   );
   await page.waitForSelector("li.m-schedule-diagram__stop");
+  await page.waitForSelector("div.m-schedule-diagram__predictions");
   await expect
-    .poll(async () => page.locator("li.m-schedule-diagram__stop").count())
+    .poll(async () => page.locator("div.m-schedule-diagram__prediction-time").count())
     .toBeGreaterThan(1);
 
   await page.locator("a.alerts-tab").click();
