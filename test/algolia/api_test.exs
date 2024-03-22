c2870526434e6ab4ab283d35210a42577ffe70f2
Anthony Shull <shull.anthony@gmail.com>
2024-03-22 08:54:13 -0500

    flush cache on algolia test

--- test/algolia/api_test.exs
+++ test/algolia/api_test.exs
@@ -4,10 +4,21 @@ defmodule Algolia.ApiTest do
   import ExUnit.CaptureLog
   import Mox
 
+  setup :set_mox_global
+  setup :verify_on_exit!
+
   @failure_response Poison.encode!(%{"error" => "failure"})
   @request Poison.encode!(%{"requests" => [indexName: "*"]})
   @success_response Poison.encode!(%{"ok" => "success"})
   @url "http://localhost:9999"
+
+  setup do
+    cache = Application.get_env(:dotcom, :cache)
+
+    cache.flush()
+
+    :ok
+  end
 
   describe "action" do
     test "caches a successful response" do
