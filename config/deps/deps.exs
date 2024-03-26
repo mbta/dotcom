22db31383969e6052d68deda27da68bcfb0279c0
Cristen Jones <cjones3@mbta.com>
2024-03-26 16:23:22 -0400

    fix: use any AWS CLI config profile name

--- config/deps/deps.exs
+++ config/deps/deps.exs
@@ -3,11 +3,11 @@ import Config
 config :ex_aws,
   access_key_id: [
     {:system, "AWS_ACCESS_KEY_ID"},
-    {:awscli, "default", 30000}
+    {:awscli, :system, 30000}
   ],
   secret_access_key: [
     {:system, "AWS_SECRET_ACCESS_KEY"},
-    {:awscli, "default", 30000}
+    {:awscli, :system, 30000}
   ]
 
 config :hammer,
