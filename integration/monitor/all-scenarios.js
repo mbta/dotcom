2c0498ccb1aa41a31c7160d28cbc69b3e8bb4968
Anthony Shull <ashull@mbta.com>
2024-04-03 14:56:06 -0500

    Send images to sentry according to rate limit (#1955)
    * send images to sentry according to rate limit
    
    * empty line
    
    * doc formatting off
    
    * formatting
    
    * add sentry dep back
    
    * actually return...
    
    * only run from 6am to midnight
    
    * docs

--- integration/monitor/all-scenarios.js
+++ integration/monitor/all-scenarios.js
@@ -1,12 +1,38 @@
 const cron = require("node-cron");
 const fs = require("fs");
 const path = require("path");
+const Sentry = require("@sentry/node");
 const { Worker } = require("worker_threads");
 
 const { fileToMetricName } = require("../utils");
+const RateLimiter = require("./rate-limiter");
 
 const filesPath = path.join(__dirname, "..", "scenarios");
 
+/*
+ * Create a rate limiter that allows only one lease per hour.
+ */
+const rateLimiter = new RateLimiter(60 * 60 * 1000, 1);
+
+/*
+ * Initialize Sentry with the DSN and environment from the environment variables.
+ * Add a beforeSend callback that will only send events if the rate limiter allows it.
+ */
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.SENTRY_ENVIRONMENT,
+  beforeSend(event) {
+    if (process.env.SENTRY_ENVIRONMENT == "prod") {
+      return rateLimiter.lease() ? event : null;
+    }
+
+    return null;
+  }
+});
+
+/*
+ * Create a worker for each scenario file in the scenarios directory.
+ */
 const workers = fs.readdirSync(filesPath).map((file) => {
   const name = fileToMetricName(file);
   const worker = new Worker(path.join(__dirname, "worker.js"), {
@@ -16,7 +42,13 @@ const workers = fs.readdirSync(filesPath).map((file) => {
   return worker;
 });
 
-cron.schedule("* * * * *", (_) => {
+/*
+ * A task runs every minute on the minute between the hours of 6am and midnight EST.
+ * Spread out the worker runs over the minute to avoid overwhelming the system.
+ * If we receive a message from a worker, it means that there was a failure.
+ * Capture the exception with Sentry and attach a screenshot to the event.
+ */
+cron.schedule("* 5-23 * * *", (_) => {
   workers.forEach((worker, index) => {
     setTimeout(
       (_) => {
@@ -24,5 +56,16 @@ cron.schedule("* * * * *", (_) => {
       },
       (60000 / workers.length) * index,
     );
+
+    worker.on("message", ({exception, metric, screenshot}) => {
+      Sentry.getCurrentScope().addAttachment({
+        filename: `${metric}-${Date.now()}.jpeg`,
+        data: screenshot,
+      });
+
+      Sentry.captureException(exception);
+
+      Sentry.getCurrentScope().clearAttachments();
+    });
   });
 });
---
+++ integration/monitor/rate-limiter.js
@@ -0,0 +1,35 @@
+/**
+ * A simple rate limiter that allows a limited number of leases in a window of time.
+ * @constructor
+ * @param {number} window - The window of time in milliseconds in which to count leases.
+ * @param {number} limit - The maximum number of leases in the window. Default 1.
+ */
+class RateLimiter {
+
+  constructor(window, limit = 1) {
+    this.window = window;
+    this.limit = limit;
+
+    this.leases = 0;
+
+    setInterval(() => {
+      this.leases = 0;
+    }, window);
+  }
+
+  /**
+   * Attempt to acquire a lease. Returns true if a lease was acquired, false otherwise.
+   * @returns {boolean}
+   */
+  lease() {
+    if (this.leases < this.limit) {
+      this.leases += 1;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+module.exports = RateLimiter;
