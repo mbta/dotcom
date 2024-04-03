880a9e129d135b2b7b3e45ef6f3b7bc54bc43e86
Anthony Shull <shull.anthony@gmail.com>
2024-04-03 11:41:49 -0500

    docs

--- integration/monitor/all-scenarios.js
+++ integration/monitor/all-scenarios.js
@@ -43,7 +43,7 @@ const workers = fs.readdirSync(filesPath).map((file) => {
 });
 
 /*
- * A task runs every minute on the minute.
+ * A task runs every minute on the minute between the hours of 6am and midnight EST.
  * Spread out the worker runs over the minute to avoid overwhelming the system.
  * If we receive a message from a worker, it means that there was a failure.
  * Capture the exception with Sentry and attach a screenshot to the event.
