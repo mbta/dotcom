a1eb0461683a7d90c2f2d7b441ad5a103ee505e8
Cristen Jones <cjones3@mbta.com>
2024-04-12 17:08:56 -0400

    deps(mix): update open_trip_planner_client to 0.7

--- mix.exs
+++ mix.exs
@@ -107,7 +107,7 @@ defmodule DotCom.Mixfile do
       {:open_trip_planner_client,
        [
          github: "thecristen/open_trip_planner_client",
-         ref: "v0.6.3"
+         ref: "v0.7.0"
        ]},
       {:parallel_stream, "1.1.0"},
       # latest version 1.7.11
