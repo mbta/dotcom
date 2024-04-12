6359de8281f07c4bd1a63d58c8718767f76cc6b5
Cristen Jones <cjones3@mbta.com>
2024-04-12 12:29:56 -0400

    format

--- assets/js/fixedsticky.js
+++ assets/js/fixedsticky.js
@@ -7,11 +7,9 @@ export default function($) {
     $(".fixedsticky").fixedsticky("destroy");
     $(".fixedsticky").fixedsticky();
   }
-  window.addEventListener(
-    "load",
-    () => window.nextTick(fixedsticky),
-    { passive: true }
-  );
+  window.addEventListener("load", () => window.nextTick(fixedsticky), {
+    passive: true
+  });
   document.addEventListener(
     "shown.bs.collapse",
     () => window.nextTick(fixedsticky),
