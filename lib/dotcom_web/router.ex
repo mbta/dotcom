a412067503c44c477a644b8904bffed05fab7533
Cristen Jones <cjones3@mbta.com>
2024-04-17 16:15:49 -0400

    feat: redirect bad schedules URLs

--- lib/dotcom_web/router.ex
+++ lib/dotcom_web/router.ex
@@ -194,6 +194,7 @@ defmodule DotcomWeb.Router do
 
     get("/schedules/:route", ScheduleController, :show, as: :schedule)
     get("/schedules/:route/pdf", ScheduleController.Pdf, :pdf, as: :route_pdf)
+    get("/schedules/:route/*path", Plugs.ScheduleRedirector, [])
     get("/style-guide", StyleGuideController, :index)
     get("/style-guide/principles", Redirector, to: "/style-guide")
     get("/style-guide/about", Redirector, to: "/style-guide")
