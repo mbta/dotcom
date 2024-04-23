ddb282af1328d22a84acd5979d83a17e31393251
Anthony Shull <shull.anthony@gmail.com>
2024-04-23 09:00:05 -0500

    remove tests causing flakiness

--- test/mbta/api/stream_test.exs
+++ test/mbta/api/stream_test.exs
@@ -3,36 +3,6 @@ defmodule MBTA.Api.StreamTest do
 
   alias MBTA.Api.Stream
 
-  describe "build_options" do
-    test "includes api key" do
-      Application.put_env(:dotcom, :mbta_api, base_url: "foo", key: "bar")
-
-      opts = Stream.build_options(path: "/vehicles")
-      assert Keyword.get(opts, :url) =~ "/vehicles"
-      assert <<_::binary>> = Keyword.get(opts, :key)
-    end
-
-    test "throws error if mbta api base url is missing" do
-      Application.put_env(:dotcom, :mbta_api, base_url: nil, key: "foo")
-
-      assert_raise ArgumentError, "Missing required configuration for MBTA API", fn ->
-        Stream.build_options(path: "/vehicles")
-      end
-
-      Application.put_env(:dotcom, :mbta_api, base_url: "foo", key: "bar")
-    end
-
-    test "throws error if mbta api key is missing" do
-      Application.put_env(:dotcom, :mbta_api, base_url: "http://example.com", key: nil)
-
-      assert_raise ArgumentError, "Missing required configuration for MBTA API", fn ->
-        Stream.build_options(path: "/vehicles")
-      end
-
-      Application.put_env(:dotcom, :mbta_api, base_url: "foo", key: "bar")
-    end
-  end
-
   describe "start_link" do
     @tag :external
     test "handles api events" do
