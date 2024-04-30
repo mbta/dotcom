3e0680c073c10dc0b950e320c8bad32f64b6d82d
Anthony Shull <shull.anthony@gmail.com>
2024-04-30 11:11:32 -0500

    config check should suffice

--- lib/dotcom_web/controllers/static_file_controller.ex
+++ lib/dotcom_web/controllers/static_file_controller.ex
@@ -10,7 +10,7 @@ defmodule DotcomWeb.StaticFileController do
   end
 
   def send_file(conn) do
-    full_url = cms_api_base_url() <> conn.request_path
+    full_url = Application.get_env(:dotcom, :cms_api)[:base_url] <> conn.request_path
 
     forward_static_file(conn, full_url)
   end
@@ -21,13 +21,5 @@ defmodule DotcomWeb.StaticFileController do
     conn
     |> put_status(301)
     |> redirect(external: url)
-  end
-
-  defp cms_api_base_url() do
-    if config = Application.get_env(:dotcom, :cms_api) do
-      config[:base_url]
-    else
-      Application.get_env(:dotcom, :drupal)[:cms_root]
-    end
   end
 end
