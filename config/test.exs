09bce1c6bf9c41143d1addfc4f6f1d14a25197f9
kotva006 <ryan.kotval@gmail.com>
2024-05-07 08:16:15 -0500

    Moved config to test

--- config/test.exs
+++ config/test.exs
@@ -13,3 +13,10 @@ config :dotcom, :redix_pub_sub, Dotcom.Redix.PubSub.Mock
 config :dotcom, :req_module, Req.Mock
 
 config :dotcom, :trip_plan_feedback_cache, Dotcom.Cache.TestCache
+
+# Let test requests get routed through the :secure pipeline
+config :dotcom, :secure_pipeline,
+  force_ssl: [
+    host: nil,
+    rewrite_on: [:x_forwarded_proto]
+  ]
