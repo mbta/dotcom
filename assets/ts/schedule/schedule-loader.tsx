78d385c991636fa04699a355fc216f02e3fcbcc5
kotva006 <ryan.kotval@gmail.com>
2024-05-17 15:34:47 -0500

    refactor(Schedules): Consolidate schedules react trees into one tree

--- assets/ts/schedule/schedule-loader.tsx
+++ assets/ts/schedule/schedule-loader.tsx
@@ -6,14 +6,14 @@ import { updateInLocation } from "use-query-params";
 import Map from "./components/Map";
 import { SchedulePageData, SelectedOrigin } from "./components/__schedule";
 import { MapData } from "../leaflet/components/__mapdata";
-import { DirectionId } from "../__v3api";
+import { DirectionId, Route } from "../__v3api";
 import ScheduleLoader from "./components/ScheduleLoader";
 import {
   store,
   createScheduleStore,
   getCurrentState
 } from "./store/ScheduleStore";
-import { isABusRoute } from "../models/route";
+import { isABusRoute, isFerryRoute } from "../models/route";
 
 const renderMap = ({
   route_patterns: routePatternsByDirection,
@@ -147,11 +147,194 @@ const render = (): void => {
   } = schedulePageData;
 
   createScheduleStore(directionId);
-  renderAdditionalLineInformation(schedulePageData);
+  ReactDOM.render(
+    fun(schedulePageData),
+    document.getElementById("react-root-schedule-page")
+  );
+  // renderAdditionalLineInformation(schedulePageData);
 
-  if (!isEmpty(routePatterns)) {
-    renderDirectionOrMap(schedulePageData);
+  // if (!isEmpty(routePatterns)) {
+  //   renderDirectionOrMap(schedulePageData);
+  // }
+};
+
+// TODO figure out return type
+const getDirectionAndMap = (
+  schedulePageData: SchedulePageData
+): JSX.Element | undefined => {
+  const currentState = getCurrentState();
+  if (!!currentState && Object.keys(currentState).length !== 0) {
+    return (
+      <ScheduleLoader
+        component="SCHEDULE_DIRECTION"
+        schedulePageData={schedulePageData}
+        updateURL={updateURL}
+      />
+    );
+  } else {
+    <></>;
   }
+};
+
+const getScheduleFinder = (
+  schedulePageData: SchedulePageData
+): JSX.Element | undefined => {
+  return (
+    <ScheduleLoader
+      component="SCHEDULE_FINDER"
+      schedulePageData={schedulePageData}
+      updateURL={updateURL}
+    />
+  );
+};
+
+const getScheduleNote = (schedulePageData: SchedulePageData): JSX.Element => {
+  return (
+    <ScheduleLoader
+      component="SCHEDULE_NOTE"
+      schedulePageData={schedulePageData}
+      updateURL={updateURL}
+    />
+  );
+};
+
+const getAdditionalLineInfo = (
+  schedulePageData: SchedulePageData
+): JSX.Element => {
+  return (
+    <ScheduleLoader
+      component="ADDITIONAL_LINE_INFORMATION"
+      schedulePageData={schedulePageData}
+      updateURL={updateURL}
+    />
+  );
+};
+
+const getLineMap = (
+  channel: string,
+  mapImgSrc: string,
+  dynamicMapData: any,
+  schedulePageData: SchedulePageData
+): JSX.Element => {
+  const directionId = schedulePageData.direction_id;
+  const routePatterns = schedulePageData.route_patterns[directionId];
+  const defaultRoutePattern = routePatterns.slice(0, 1)[0];
+  const currentShapes = isABusRoute(schedulePageData.route)
+    ? [defaultRoutePattern.shape_id]
+    : routePatterns.map(pattern => pattern.shape_id);
+  const currentStops = defaultRoutePattern.stop_ids;
+  if (!channel) throw new Error("data-channel-id attribute not set");
+  const mapData: MapData = dynamicMapData as MapData;
+  return (
+    <>
+      <Map
+        data={mapData}
+        channel={channel}
+        currentShapes={currentShapes}
+        currentStops={currentStops}
+      />
+      <noscript>
+        <div className="line-map map m-schedule-line__map-static">
+          <img className="map-static map-static-img" srcSet={mapImgSrc}></img>
+        </div>
+      </noscript>
+    </>
+  );
+};
+
+const getPageTitle = (route: Route): string | null => {
+  switch (route.type) {
+    case 0:
+    case 1:
+      return "Stations & Departures";
+    case 4:
+      return null;
+    case 2:
+    default:
+      return "Schedules & Maps";
+  }
+};
+
+const getMetaJSON = (id: string): any => {
+  const metaContentString = getMetaString(id);
+  if (metaContentString) {
+    return JSON.parse(metaContentString);
+  }
+  return null;
+};
+
+const getMetaString = (id: string): string | undefined | null => {
+  const metaContent = document
+    .querySelector(`meta[name=${id}]`)
+    ?.getAttribute("content");
+
+  return metaContent;
+};
+
+const fun = (schedulePageData: SchedulePageData): JSX.Element => {
+  const route = schedulePageData.route;
+
+  const branches = getMetaJSON("branches");
+  const mapChannel = getMetaString("mapChannel");
+  const dynamicMapData = getMetaJSON("dynamicMapData");
+  const mapPdfUrl = getMetaString("mapPdfUrl");
+  const mapImageSrc = getMetaString("mapImageSrc");
+
+  const noBranches = branches.length === 0;
+
+  const width = noBranches ? "col-md-12" : "col-md-7";
+  const min_size = noBranches ? "" : "m-schedule-page__main-content__min-size";
+  const offset = noBranches
+    ? "col-md-offset-7 col-lg-offset-8"
+    : "col-lg-offset-1";
+  const ferry = isFerryRoute(route) ? "ferry" : "";
+  const title = getPageTitle(route);
+  return (
+    <>
+      <Provider store={store}>
+        <div className={width + " m-schedule-page__main-content " + min_size}>
+          <div className={"m-schedule-line__main-content " + ferry}>
+            {noBranches ? (
+              <>{"TODO: RENDER EMPTY"}</>
+            ) : (
+              <>
+                {title && <h2>{title}</h2>}
+                {getDirectionAndMap(schedulePageData)}
+                {isFerryRoute(route) && getScheduleFinder(schedulePageData)}
+                <div className="line-map-container">
+                  {!isFerryRoute(route) &&
+                    mapChannel &&
+                    mapImageSrc &&
+                    getLineMap(
+                      mapChannel,
+                      mapImageSrc,
+                      dynamicMapData,
+                      schedulePageData
+                    )}
+                </div>
+                {/* END branchs not empty rendering 8*/}
+              </>
+            )}
+          </div>
+        </div>
+        <div className="col-md-5 col-lg-4 col-lg-offset-1 m-schedule-page__schedule-finder-or-note">
+          {/* IF branches not empty continue rendering */}
+          {getScheduleNote(schedulePageData)}
+          {schedulePageData.schedule_note === null &&
+            !isFerryRoute(route) &&
+            getScheduleFinder(schedulePageData)}
+          {/* END branches not empty continue rendering */}
+        </div>
+        <div
+          className={`col-md-5 col-lg-4 ${offset} m-schedule-page__side-content`}
+        >
+          <div className="m-schedule-line__side-content">
+            {getAdditionalLineInfo(schedulePageData)}
+          </div>
+        </div>
+      </Provider>
+    </>
+  );
 };
 
 const onLoad = (): void => {
