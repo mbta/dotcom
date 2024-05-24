5861606b537cbccc510eb59b87c840f8a990ae35
Cristen Jones <cjones3@mbta.com>
2024-05-24 13:53:22 -0400

    feat(TimetableController): advanced override placement

--- lib/dotcom_web/controllers/schedule/timetable_controller.ex
+++ lib/dotcom_web/controllers/schedule/timetable_controller.ex
@@ -358,15 +358,23 @@ defmodule DotcomWeb.ScheduleController.TimetableController do
 
   # Some shuttle stops must be placed manually within the existing stops. Each
   # key is a scheduled stop ID from a shuttle, and the corresponding value is
-  # the name of the canonical rail stop it should be placed adjacent to.
+  # the name of the canonical rail stop it should be placed adjacent to in the
+  # inbound direction
   @shuttle_overrides %{
-    "14748" => "Lynn Interim",
-    "38671" => "Weymouth Landing/East Braintree",
-    "NHRML-0127-B" => "Reading",
-    "place-ER-0115" => "Swampscott",
-    "place-wondl" => "Lynn",
+    # Lynn busway is near Lynn Interim station
+    "14748" => {:after, "Lynn Interim"},
+    # Braintree busway
+    "38671" => {:after, "Weymouth Landing/East Braintree"},
+    # Anderson/Woburn is after Ballardvale on Haverhill line shuttle
+    "NHRML-0127-B" => {:after, "Ballardvale"},
+    # Lynn station
+    "place-ER-0115" => {:after, "Swampscott"},
+    # Wonderland station
+    "place-wondl" => {:after, "Lynn"},
     # Add Readville (canonically on other routes) back into the Providence timetable
-    "place-DB-0095" => "Route 128"
+    "place-DB-0095" => {:after, "Route 128"},
+    # Add Newton Highlands shuttle stop to Needham timetable
+    "place-newtn" => {:before, "Needham Heights"}
   }
   @shuttle_ids Map.keys(@shuttle_overrides)
 
@@ -477,15 +485,27 @@ defmodule DotcomWeb.ScheduleController.TimetableController do
   end
 
   defp merge_into_stop_list(new_stop, base_list, inbound?) do
-    next_stop_name = @shuttle_overrides[new_stop.id]
+    with {before_or_after, adjacent_name} <- @shuttle_overrides[new_stop.id],
+         index when not is_nil(index) <-
+           Enum.find_index(base_list, &match?(%Stops.Stop{name: ^adjacent_name}, &1)) do
+      position =
+        if inbound? do
+          # the after/before label is based on the inbound direction
+          case before_or_after do
+            :after -> index + 1
+            :before -> index
+          end
+        else
+          case before_or_after do
+            :after -> index
+            :before -> index + 1
+          end
+        end
 
-    case Enum.find_index(base_list, &match?(%Stop{name: ^next_stop_name}, &1)) do
-      nil ->
+      List.insert_at(base_list, position, new_stop)
+    else
+      _ ->
         base_list
-
-      index ->
-        base_list
-        |> List.insert_at(if(inbound?, do: index + 1, else: index), new_stop)
     end
   end
 
