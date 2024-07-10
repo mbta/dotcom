3e59338d6ce924c94622496338a3113e1e554aea
Cristen Jones <cjones3@mbta.com>
2024-07-10 09:51:07 -0400

    feat: basic basemap phoenix hook

--- assets/ts/phoenix-hooks/index.ts
+++ assets/ts/phoenix-hooks/index.ts
@@ -1,9 +1,11 @@
 import AlgoliaAutocomplete from "./algolia-autocomplete";
+import LeafletMap from "./leaflet-map";
 import ScrollIntoView from "./scroll-into-view";
 import TripPlannerForm from "./trip-planner-form";
 
 const Hooks = {
   AlgoliaAutocomplete,
+  LeafletMap,
   ScrollIntoView,
   TripPlannerForm
 };
