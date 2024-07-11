d0bd75882ff48adfc3792096a728775aabf47643
Anthony Shull <shull.anthony@gmail.com>
2024-07-11 13:26:12 -0500

    filter results by USA and appropriate states

--- lib/location_service/location_service.ex
+++ lib/location_service/location_service.ex
@@ -13,14 +13,18 @@ defmodule LocationService do
   @cache Application.compile_env!(:dotcom, :cache)
   @ttl :timer.hours(24)
 
+  @filter ~r/,\s(MA|NH|RI),\s/
+
   @behaviour LocationService.Behaviour
 
   @bias_options %{
     BiasPosition: [-71.0660, 42.3548],
+    FilterCountries: ["USA"],
     MaxResults: 50
   }
   @bounding_options %{
     FilterBBox: [-71.9380, 41.3193, -69.6189, 42.8266],
+    FilterCountries: ["USA"],
     MaxResults: 50
   }
 
@@ -30,9 +34,22 @@ defmodule LocationService do
   def autocomplete(text, limit, options) when 1 <= limit and limit <= 15 do
     Request.autocomplete(text, limit, options)
     |> Result.handle_response(%{search: text, limit: limit})
+    |> filter_results()
   end
 
   def autocomplete(_, _, _), do: {:error, :invalid_arguments}
+
+  defp filter_results({:ok, results}) do
+    results =
+      results
+      |> Enum.filter(fn result ->
+        Regex.match?(@filter, result.address)
+      end)
+
+    {:ok, results}
+  end
+
+  defp filter_results(result), do: result
 
   @decorate cacheable(cache: @cache, on_error: :nothing, opts: [ttl: @ttl])
   def geocode(address, options \\ @bounding_options) when is_binary(address) do
