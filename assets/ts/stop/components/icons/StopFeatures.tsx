d8811e4c7c518d84b865c51f5e6961b1bdadeb64
Cristen Jones <cjones3@mbta.com>
2024-08-28 12:28:35 -0400

    hotfix(StopFeatures): don't render mode icons for shuttle routes

--- assets/ts/stop/components/icons/StopFeatures.tsx
+++ assets/ts/stop/components/icons/StopFeatures.tsx
@@ -14,7 +14,11 @@ const StopFeatures = ({
 }): ReactElement<HTMLElement> => {
   return (
     <span className="m-stop-page__header-features mb-6">
-      <ModeIcons routes={routes} />
+      <ModeIcons
+        routes={routes.filter(
+          route => route.description !== "rail_replacement_bus"
+        )}
+      />
       <CommuterRailZoneIcon zoneNumber={stop.zone} />
       <AccessibilityIcon stop={stop} routes={routes} />
       <ParkingIcon stop={stop} />
