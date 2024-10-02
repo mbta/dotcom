02f0f4a0106a9c5d3eb73fc42258d198b95329ab
Josh Larson <theMutatedShrimp@gmail.com>
2024-10-02 14:26:49 -0400

    fix: Add images to alerts that are rendered on react pages (#2181)

--- assets/ts/__v3api.d.ts
+++ assets/ts/__v3api.d.ts
@@ -257,16 +257,18 @@ export type TimePeriodPairs = [string, string];
 export interface Alert {
   id: string;
   active_period: TimePeriodPairs[];
-  header: string;
-  informed_entity: InformedEntitySet;
-  effect: string;
-  severity: number;
-  lifecycle: Lifecycle;
-  updated_at: string;
-  description: string;
-  priority: Priority;
-  url: string | null;
   banner: string | null;
+  header: string;
+  description: string;
+  effect: string;
+  image?: string;
+  image_alternative_text?: string;
+  informed_entity: InformedEntitySet;
+  lifecycle: Lifecycle;
+  priority: Priority;
+  severity: number;
+  updated_at: string;
+  url: string | null;
 }
 
 interface DatesNotes {
