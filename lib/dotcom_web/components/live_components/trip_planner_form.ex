746c9b4099dad8f547ad649871fefdf28edf2256
Cristen Jones <cjones3@mbta.com>
2024-10-08 15:33:21 -0400

    chore(TripPlan.InputForm): change modes type from enum to map

--- lib/dotcom_web/components/live_components/trip_planner_form.ex
+++ lib/dotcom_web/components/live_components/trip_planner_form.ex
@@ -5,18 +5,16 @@ defmodule DotcomWeb.Components.LiveComponents.TripPlannerForm do
   use DotcomWeb, :live_component
 
   import DotcomWeb.ViewHelpers, only: [svg: 1]
-  import Phoenix.HTML.Form, only: [input_name: 2, input_value: 2, input_id: 2]
-
   import MbtaMetro.Components.Feedback
   import MbtaMetro.Components.InputGroup
+  import Phoenix.HTML.Form, only: [input_name: 2, input_value: 2, input_id: 2]
 
-  alias Dotcom.TripPlan.{InputForm, OpenTripPlanner}
+  alias Dotcom.TripPlan.{InputForm, InputForm.Modes, OpenTripPlanner}
 
-  @all_modes [:RAIL, :SUBWAY, :BUS, :FERRY]
   @form_defaults %{
     "datetime_type" => :now,
     "datetime" => NaiveDateTime.local_now(),
-    "modes" => @all_modes,
+    "modes" => InputForm.initial_modes(),
     "wheelchair" => true
   }
 
@@ -130,45 +128,18 @@ defmodule DotcomWeb.Components.LiveComponents.TripPlannerForm do
           <.fieldset legend="Modes">
             <.accordion>
               <:heading>
-                <%= selected_modes(input_value(@form, :modes)) %>
+                <%= Modes.selected_modes(input_value(f, :modes)) %>
               </:heading>
               <:content>
-                <div class="flex flex-col gap-1">
-                  <input
-                    type="checkbox"
-                    class="peer sr-only"
-                    name={input_name(@form, :modes) <> "[]"}
-                    value=""
-                    checked="true"
-                  />
-                  <label
-                    :for={
-                      {mode_name, mode_value} <- [
-                        {"Commuter Rail", :RAIL},
-                        {"Subway", :SUBWAY},
-                        {"Bus", :BUS},
-                        {"Ferry", :FERRY}
-                      ]
-                    }
-                    for={input_id(@form, :modes) <> "_#{mode_value}"}
-                    class="rounded border-solid border-2 border-transparent hover:bg-zinc-100 has-[:checked]:font-semibold py-1 px-2 mb-0"
-                  >
-                    <input
+                <div class="flex flex-col gap-05 px-2">
+                  <.inputs_for :let={f} field={f[:modes]}>
+                    <.input
+                      :for={subfield <- Modes.fields()}
                       type="checkbox"
-                      class="shrink-0 mr-2 rounded w-6 h-6 border-blue-500 rounded border-solid border-2 focus:border-blue-700 checked:border-blue-700 checked:bg-blue-700"
-                      id={input_id(@form, :modes)  <> "_#{mode_value}"}
-                      name={input_name(@form, :modes) <> "[]"}
-                      value={mode_value}
-                      checked={
-                        if(input_value(@form, :modes),
-                          do:
-                            mode_value in input_value(@form, :modes) ||
-                              "#{mode_value}" in input_value(@form, :modes)
-                        )
-                      }
+                      field={f[subfield]}
+                      label={Modes.mode_label(subfield)}
                     />
-                    <%= mode_name %>
-                  </label>
+                  </.inputs_for>
                 </div>
               </:content>
               <:extra :if={used_input?(f[:modes])}>
@@ -230,45 +201,5 @@ defmodule DotcomWeb.Components.LiveComponents.TripPlannerForm do
     result = OpenTripPlanner.plan(data)
     _ = on_submit.(result)
     result
-  end
-
-  defp mode_atom(mode) do
-    case mode do
-      :RAIL -> :commuter_rail
-      :SUBWAY -> :subway
-      :BUS -> :bus
-      :FERRY -> :ferry
-      other when is_binary(other) and other != "" -> String.to_atom(other)
-      _ -> :unknown
-    end
-  end
-
-  defp mode_name(mode) do
-    case mode_atom(mode) do
-      :unknown ->
-        ""
-
-      other ->
-        DotcomWeb.ViewHelpers.mode_name(other)
-    end
-  end
-
-  defp selected_modes(modes) when modes == @all_modes do
-    "All modes"
-  end
-
-  defp selected_modes([]), do: "No transit modes selected"
-  defp selected_modes(nil), do: "No transit modes selected"
-
-  defp selected_modes([mode]), do: mode_name(mode) <> " Only"
-  defp selected_modes([mode1, mode2]), do: mode_name(mode1) <> " and " <> mode_name(mode2)
-
-  defp selected_modes(modes) do
-    modes
-    |> Enum.map(&mode_name/1)
-    |> Enum.reject(&(&1 == ""))
-    |> Enum.intersperse(", ")
-    |> List.insert_at(-2, "and ")
-    |> Enum.join("")
   end
 end
--- test/dotcom/trip_plan/input_form_test.exs
+++ test/dotcom/trip_plan/input_form_test.exs
@@ -11,13 +11,13 @@ defmodule Dotcom.TripPlan.InputFormTest do
     "latitude" => "#{Faker.Address.latitude()}",
     "longitude" => "#{Faker.Address.longitude()}"
   }
-  @mode_params InputForm.valid_modes()
+  @mode_params InputForm.initial_modes()
   @params %{
     "from" => @from_params,
     "to" => @to_params,
     "datetime_type" => Faker.Util.pick(InputForm.time_types()) |> to_string(),
     "datetime" => Faker.DateTime.forward(4) |> to_string(),
-    "modes" => @mode_params |> Enum.map(&to_string/1),
+    "modes" => @mode_params,
     "wheelchair" => Faker.Util.pick(["true", "false"])
   }
 
@@ -25,6 +25,11 @@ defmodule Dotcom.TripPlan.InputFormTest do
     changeset = InputForm.changeset(%{})
     assert {_, [validation: :required]} = changeset.errors[:from]
     assert {_, [validation: :required]} = changeset.errors[:to]
+  end
+
+  test "mode required" do
+    changeset = InputForm.changeset(%{modes: nil})
+    assert {_, [validation: :required]} = changeset.errors[:modes]
   end
 
   describe "validate_params/1" do
@@ -67,6 +72,78 @@ defmodule Dotcom.TripPlan.InputFormTest do
       expected_error = InputForm.error_message(:from_to_same)
       assert {^expected_error, _} = changeset.errors[:to]
     end
+
+    test "at least one mode required" do
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "modes" => %{RAIL: false, BUS: false, FERRY: false, SUBWAY: false}
+        })
+
+      refute changeset.valid?
+
+      expected_error = InputForm.error_message(:modes)
+      assert {^expected_error, _} = changeset.errors[:modes]
+    end
+
+    test "adds datetime if using datetime_type == now" do
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "datetime_type" => "now",
+            "datetime" => nil
+        })
+
+      assert changeset.valid?
+      assert %DateTime{} = changeset.changes[:datetime]
+    end
+
+    test "datetime required if using datetime_type != now" do
+      expected_error = InputForm.error_message(:datetime)
+
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "datetime_type" => "arrive_by",
+            "datetime" => nil
+        })
+
+      refute changeset.valid?
+      assert {^expected_error, _} = changeset.errors[:datetime]
+
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "datetime_type" => "leave_at",
+            "datetime" => nil
+        })
+
+      refute changeset.valid?
+      assert {^expected_error, _} = changeset.errors[:datetime]
+    end
+
+    test "requires date to be in the future" do
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "datetime_type" => "arrive_by",
+            "datetime" => Faker.DateTime.forward(1)
+        })
+
+      assert changeset.valid?
+
+      expected_error = InputForm.error_message(:datetime)
+
+      changeset =
+        InputForm.validate_params(%{
+          @params
+          | "datetime_type" => "arrive_by",
+            "datetime" => Faker.DateTime.backward(1)
+        })
+
+      refute changeset.valid?
+      assert {^expected_error, _} = changeset.errors[:datetime]
+    end
   end
 
   describe "Location" do
@@ -87,6 +164,35 @@ defmodule Dotcom.TripPlan.InputFormTest do
         })
 
       assert changeset.changes.name == "#{lat}, #{lon}"
+    end
+  end
+
+  describe "Modes" do
+    test "selected_modes/1 summarizes two values" do
+      changeset =
+        InputForm.Modes.changeset(%InputForm.Modes{}, %{
+          "RAIL" => "true",
+          "SUBWAY" => "true",
+          "BUS" => "false",
+          "FERRY" => "false"
+        })
+
+      assert "Commuter Rail and Subway" = InputForm.Modes.selected_modes(changeset)
+    end
+
+    test "selected_modes/1 summarizes one value" do
+      data = %InputForm.Modes{RAIL: false, SUBWAY: false, BUS: true, FERRY: false}
+      assert "Bus Only" = InputForm.Modes.selected_modes(data)
+    end
+
+    test "selected_modes/1 summarizes many values" do
+      data = %InputForm.Modes{RAIL: true, SUBWAY: false, BUS: true, FERRY: true}
+      assert "Commuter Rail, Bus, and Ferry" = InputForm.Modes.selected_modes(data)
+    end
+
+    test "selected_modes/1 summarizes all values" do
+      data = %InputForm.Modes{RAIL: true, SUBWAY: true, BUS: true, FERRY: true}
+      assert "All modes" = InputForm.Modes.selected_modes(data)
     end
   end
 end
