411611142d0502fd898c3fbcc3f7da96b6fa1abb
Anthony Shull <shull.anthony@gmail.com>
2024-10-23 12:28:01 -0500

    add new pages

--- assets/js/mobile-app-banner.js
+++ assets/js/mobile-app-banner.js
@@ -1,28 +1,45 @@
-const isAlertsPage = () => {
-  const url = window.location.href;
-
-  return /\/alerts\/[\w.-]+/.test(url);
+const isAlertsPage = url => {
+  return /\/alerts/.test(url);
 };
 
 const isNotAndroid = () => {
   return !/Android/.test(navigator.userAgent);
 };
 
-const isSchedulePage = () => {
-  const url = window.location.href;
+const isNotCommuterRail = url => {
+  return !/\/schedules\/CR-/.test(url);
+};
 
+const isSchedulePage = url => {
   return /\/schedules\/[\w.-]+\/line/.test(url);
 };
 
-const isStopPage = () => {
-  const url = window.location.href;
-
+const isStopPage = url => {
   return /\/stops\/[\w.-]+/.test(url);
+};
+
+const isTransitNearMePage = url => {
+  return /\/transit-near-me/.test(url);
+};
+
+const is = url => {
+  return (
+    isAlertsPage(url) ||
+    isSchedulePage(url) ||
+    isStopPage(url) ||
+    isTransitNearMePage(url)
+  );
+};
+
+const isNot = url => {
+  return isNotAndroid() && isNotCommuterRail(url);
 };
 
 // Show the mobile app banner
 export default function mobileAppBanner() {
-  if (isNotAndroid() && (isAlertsPage() || isSchedulePage() || isStopPage())) {
+  const url = window.location.href;
+
+  if (is(url) && isNot(url)) {
     document.querySelector("#mobile-app-banner").style.display = "block";
   }
 }
