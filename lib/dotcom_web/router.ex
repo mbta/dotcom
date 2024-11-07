2370af3720cca09c4f1086f0015ce93962a4d873
Cristen Jones <cjones3@mbta.com>
2024-11-07 01:13:57 -0500

    organize routing

--- lib/dotcom_web/router.ex
+++ lib/dotcom_web/router.ex
@@ -226,11 +226,17 @@ defmodule DotcomWeb.Router do
     get("/vote", VoteController, :show)
   end
 
+
+  scope "/" do
+    storybook_assets()
+  end
+
   scope "/", DotcomWeb do
     import Phoenix.LiveDashboard.Router
 
     pipe_through([:browser, :browser_live, :basic_auth])
     live_dashboard("/dashboard")
+    live_storybook("/storybook", backend_module: Elixir.DotcomWeb.Storybook)
   end
 
   scope "/admin", DotcomWeb do
@@ -302,16 +308,6 @@ defmodule DotcomWeb.Router do
     get("/schedules_and_maps/*path", OldSiteRedirectController, :schedules_and_maps)
     get("/about_the_mbta/public_meetings", Redirector, to: "/events")
     get("/about_the_mbta/news_events", Redirector, to: "/news")
-  end
-
-  scope "/" do
-    storybook_assets()
-  end
-
-  scope "/", DotcomWeb do
-    # pipe_through([:secure, :browser, :browser_live, :basic_auth])
-    pipe_through(:browser)
-    live_storybook("/storybook", backend_module: Elixir.DotcomWeb.Storybook)
   end
 
   scope "/", DotcomWeb do
