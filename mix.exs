5437b3d2438d7f89ba8fc66aade057069528ffec
Cristen Jones <cjones3@mbta.com>
2024-11-15 16:34:47 -0500

    update metro

--- mix.exs
+++ mix.exs
@@ -105,7 +105,7 @@ defmodule DotCom.Mixfile do
       {:jason, "1.4.4", override: true},
       {:logster, "1.1.1"},
       {:mail, "0.4.1"},
-      {:mbta_metro, "0.0.56"},
+      {:mbta_metro, "0.0.58"},
       {:mock, "0.3.8", [only: :test]},
       {:mox, "1.2.0", [only: :test]},
       {:nebulex, "2.6.4"},
