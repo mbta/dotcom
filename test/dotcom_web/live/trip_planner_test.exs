f144a646cb0b18b63df0079cc023c042e9c7bc70
Josh Larson <jlarson@mbta.com>
2024-11-27 11:05:53 -0500

    tests: Add tests that validate itinerary-toggling behavior

--- test/dotcom_web/live/trip_planner_test.exs
+++ test/dotcom_web/live/trip_planner_test.exs
@@ -3,8 +3,38 @@ defmodule DotcomWeb.Live.TripPlannerTest do
 
   import Mox
   import Phoenix.LiveViewTest
+  import Test.Support.Factories.TripPlanner.TripPlanner, only: [limit_route_types: 1]
+
+  alias OpenTripPlannerClient.Test.Support.Factory, as: OtpFactory
 
   setup :verify_on_exit!
+
+  def stub_otp_results(itineraries) do
+    expect(OpenTripPlannerClient.Mock, :plan, fn _ ->
+      {:ok, %OpenTripPlannerClient.Plan{itineraries: itineraries}}
+    end)
+  end
+
+  def stub_populated_otp_results() do
+    # Uhhh the OTP factory will generate with any route_type value but our
+    # parsing will break with unexpected route types
+    itineraries =
+      OtpFactory.build_list(3, :itinerary)
+      |> Enum.map(&limit_route_types/1)
+
+    stub_otp_results(itineraries)
+  end
+
+  def update_trip_details(itinerary, trip_id: trip_id, start_time: start_time) do
+    updated_transit_leg =
+      itinerary.legs
+      |> Enum.at(1)
+      |> update_in([:trip, :gtfs_id], fn _ -> "ma_mbta_us:#{trip_id}" end)
+
+    itinerary
+    |> Map.update!(:legs, &List.replace_at(&1, 1, updated_transit_leg))
+    |> Map.put(:start, DateTime.new!(Date.utc_today(), start_time, "America/New_York"))
+  end
 
   test "Preview version behind basic auth", %{conn: conn} do
     conn = get(conn, ~p"/preview/trip-planner")
@@ -96,9 +126,7 @@ defmodule DotcomWeb.Live.TripPlannerTest do
         }
       }
 
-      expect(OpenTripPlannerClient.Mock, :plan, fn _ ->
-        {:ok, %OpenTripPlannerClient.Plan{itineraries: []}}
-      end)
+      stub_otp_results([])
 
       {:ok, view, _html} = live(conn, ~p"/preview/trip-planner?#{params}")
 
@@ -113,16 +141,6 @@ defmodule DotcomWeb.Live.TripPlannerTest do
 
       stub(Stops.Repo.Mock, :get, fn _ ->
         Test.Support.Factories.Stops.Stop.build(:stop)
-      end)
-
-      # Uhhh the OTP factory will generate with any route_type value but our
-      # parsing will break with unexpected route types
-      itineraries =
-        OpenTripPlannerClient.Test.Support.Factory.build_list(3, :itinerary)
-        |> Enum.map(&Test.Support.Factories.TripPlanner.TripPlanner.limit_route_types/1)
-
-      expect(OpenTripPlannerClient.Mock, :plan, fn _ ->
-        {:ok, %OpenTripPlannerClient.Plan{itineraries: itineraries}}
       end)
 
       %{
@@ -144,12 +162,16 @@ defmodule DotcomWeb.Live.TripPlannerTest do
     end
 
     test "starts out with no 'View All Options' button", %{conn: conn, params: params} do
+      stub_populated_otp_results()
+
       {:ok, view, _html} = live(conn, ~p"/preview/trip-planner?#{params}")
 
       refute render_async(view) =~ "View All Options"
     end
 
     test "clicking 'Details' button opens details view", %{conn: conn, params: params} do
+      stub_populated_otp_results()
+
       {:ok, view, _html} = live(conn, ~p"/preview/trip-planner?#{params}")
 
       render_async(view)
@@ -162,6 +184,8 @@ defmodule DotcomWeb.Live.TripPlannerTest do
       conn: conn,
       params: params
     } do
+      stub_populated_otp_results()
+
       {:ok, view, _html} = live(conn, ~p"/preview/trip-planner?#{params}")
 
       render_async(view)
@@ -170,6 +194,39 @@ defmodule DotcomWeb.Live.TripPlannerTest do
       view |> element("button", "View All Options") |> render_click()
 
       refute render_async(view) =~ "View All Options"
+    end
+
+    test "'Depart At' buttons toggle which itinerary to show", %{
+      conn: conn,
+      params: params
+    } do
+      base_itinerary =
+        OtpFactory.build(:itinerary)
+        |> limit_route_types()
+
+      # TODO: Right now, the headsign (which is what we actually want
+      # to show) is not available from OTP client, but we're rendering
+      # the trip ID in its place. Once the headsign is available, we
+      # should update these updates and the assertions below to use
+      # the headsign instead of the trip ID.
+      stub_otp_results([
+        update_trip_details(base_itinerary, trip_id: "trip_id_1", start_time: ~T[09:26:00]),
+        update_trip_details(base_itinerary, trip_id: "trip_id_2", start_time: ~T[10:46:00])
+      ])
+
+      {:ok, view, _html} = live(conn, ~p"/preview/trip-planner?#{params}")
+
+      render_async(view)
+
+      view |> element("button", "Details") |> render_click()
+
+      assert render_async(view) =~ "trip_id_1"
+      refute render_async(view) =~ "trip_id_2"
+
+      view |> element("button", "10:46AM") |> render_click()
+
+      assert render_async(view) =~ "trip_id_2"
+      refute render_async(view) =~ "trip_id_1"
     end
   end
 end
