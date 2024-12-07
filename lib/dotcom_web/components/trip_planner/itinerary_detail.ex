1eeedda8befbbb22414c358bcd6ce3e08ef4c942
Cristen Jones <cjones3@mbta.com>
2024-12-07 10:51:19 -0500

    feat: MBTA stops link to stop pages

--- lib/dotcom_web/components/trip_planner/itinerary_detail.ex
+++ lib/dotcom_web/components/trip_planner/itinerary_detail.ex
@@ -10,6 +10,7 @@ defmodule DotcomWeb.Components.TripPlanner.ItineraryDetail do
   import DotcomWeb.Components.TripPlanner.WalkingLeg, only: [walking_leg: 1]
 
   alias Routes.Route
+  alias Stops.Stop
   alias Dotcom.TripPlan.{PersonalDetail, TransitDetail}
 
   def itinerary_detail(
@@ -99,21 +100,39 @@ defmodule DotcomWeb.Components.TripPlanner.ItineraryDetail do
   end
 
   defp place(assigns) do
+    stop_url = stop_url(assigns.route, assigns.place.stop)
+
+    assigns =
+      assign(assigns, %{
+        stop_url: stop_url,
+        tag_name: if(stop_url, do: "a", else: "div")
+      })
+
     ~H"""
-    <div class="bg-gray-bordered-background px-3 py-2 rounded flex flex-nowrap items-center gap-2 w-full">
+    <.dynamic_tag
+      tag_name={@tag_name}
+      href={@stop_url}
+      class="bg-gray-bordered-background px-3 py-2 rounded flex flex-nowrap items-center gap-2 w-full hover:no-underline text-black"
+    >
       <.location_icon route={@route} class="grow-0 h-6 w-6" />
       <strong class="grow-0">{@place.name}</strong>
       <.icon
-        :if={!is_nil(@place.stop) and Stops.Stop.accessible?(@place.stop)}
+        :if={!is_nil(@place.stop) and Stop.accessible?(@place.stop)}
         type="icon-svg"
         name="icon-accessible-default"
         class="h-6 w-6 ml-0.5 grow-0"
         aria-hidden="true"
       />
       <time class="grow text-right">{format_time(@time)}</time>
-    </div>
+    </.dynamic_tag>
     """
   end
+
+  defp stop_url(%Route{external_agency_name: nil}, %Stop{} = stop) do
+    ~p"/stops/#{stop}"
+  end
+
+  defp stop_url(_, _), do: nil
 
   defp location_icon(%{route: %Route{}} = assigns) do
     icon_name =
