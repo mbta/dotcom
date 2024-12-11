9f119a2ea36ebdf2ac4fc908d56c7971f0842eec
Josh Larson <jlarson@mbta.com>
2024-12-11 13:12:58 -0500

    fix: De-duplicate start and end locations when they're next to transit segments

--- test/dotcom/trip_plan/leg_to_segment_helper_test.exs
+++ test/dotcom/trip_plan/leg_to_segment_helper_test.exs
@@ -20,4 +20,28 @@ defmodule Dotcom.TripPlan.LegToSegmentHelperTest do
                %Leg{mode: %PersonalDetail{}}
              ])
   end
+
+  test "does not prepend location if the first segment is transit" do
+    assert [
+             {:transit_segment, _},
+             {:walking_segment, _},
+             {:location_segment, _}
+           ] =
+             LegToSegmentHelper.legs_to_segments([
+               %Leg{mode: %TransitDetail{}},
+               %Leg{mode: %PersonalDetail{}}
+             ])
+  end
+
+  test "does not append location if the last segment is transit" do
+    assert [
+             {:location_segment, _},
+             {:walking_segment, _},
+             {:transit_segment, _}
+           ] =
+             LegToSegmentHelper.legs_to_segments([
+               %Leg{mode: %PersonalDetail{}},
+               %Leg{mode: %TransitDetail{}}
+             ])
+  end
 end
