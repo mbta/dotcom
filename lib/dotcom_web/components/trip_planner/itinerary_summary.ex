9158a463f47c1c691959631d68ac2af017688c82
Anthony Shull <shull.anthony@gmail.com>
2025-01-03 08:48:40 -0600

    make non accessible icon bigger to match accessible icon

--- lib/dotcom_web/components/trip_planner/itinerary_summary.ex
+++ lib/dotcom_web/components/trip_planner/itinerary_summary.ex
@@ -27,7 +27,7 @@ defmodule DotcomWeb.Components.TripPlanner.ItinerarySummary do
           <%= if @summary.accessible? do %>
             <.icon type="icon-svg" name="icon-accessible-small" class="h-3 w-3 mr-0.5" /> Accessible
           <% else %>
-            <.icon type="icon-svg" name="icon-not-accessible-small" class="h-3 w-3 mr-0.5" />
+            <.icon type="icon-svg" name="icon-not-accessible-small" class="h-4 w-4 mr-0.5" />
             May not be accessible
           <% end %>
           <.icon name="circle" class="h-0.5 w-0.5 mx-1" />
