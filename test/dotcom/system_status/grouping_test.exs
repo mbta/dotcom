a9796afcc35065522b195430f0cca9218af3f773
Josh Larson <jlarson@mbta.com>
2025-01-14 11:48:33 -0500

    Include "Station Closure" effect

--- test/dotcom/system_status/grouping_test.exs
+++ test/dotcom/system_status/grouping_test.exs
@@ -236,7 +236,7 @@ defmodule Dotcom.SystemStatus.GroupingTest do
                  route_id: "Orange",
                  sub_routes: [],
                  statuses: [
-                   %{status: "Delays", time: "8:30pm"},
+                   %{status: "Station Closure", time: "8:30pm"},
                    %{status: "Suspension", time: "10:00pm"}
                  ]
                }
@@ -244,7 +244,7 @@ defmodule Dotcom.SystemStatus.GroupingTest do
                Grouping.grouping(
                  [
                    Alert.build(:alert,
-                     effect: :delay,
+                     effect: :station_closure,
                      informed_entity: [InformedEntity.build(:informed_entity, route: "Orange")],
                      active_period: [future_active_period(~N[2025-01-09 20:30:00])]
                    ),
