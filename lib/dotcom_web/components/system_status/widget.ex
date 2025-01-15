9abe137bd685e7f7b1d875bda9b430d83d5bd0c8
Josh Larson <jlarson@mbta.com>
2025-01-15 12:16:00 -0500

    feat: Add Hi-Fi system status widget

---
+++ lib/dotcom_web/components/system_status/widget.ex
@@ -0,0 +1,135 @@
+defmodule DotcomWeb.Components.SystemStatus.Widget do
+  @moduledoc """
+  A component that renders the given `@statuses` in a table.
+  """
+
+  use DotcomWeb, :component
+
+  import DotcomWeb.Components.RoutePills
+  import DotcomWeb.Components.SystemStatus.StatusLabel
+
+  @route_ids ["Red", "Orange", "Green", "Blue"]
+
+  def system_status_widget(assigns) do
+    assigns = assigns |> assign(:rows, status_to_rows(assigns.routes_with_statuses))
+
+    ~H"""
+    <div class="px-5 py-4 border-[1px] border-gray-lightest rounded-lg w-96">
+      <div class="border-gray-lightest border-b-[1px] pl-2 pb-3 flex items-center gap-2">
+        <.icon type="icon-svg" name="icon-mode-subway-default" class="h-7 w-7" />
+        <span class="font-heading font-bold text-[1.75rem]">Subway Status</span>
+      </div>
+      <div
+        :for={row <- @rows}
+        class={[
+          "flex gap-2",
+          !row.style.short_bottom_border && "border-gray-lightest border-b-[1px]",
+          "hover:bg-brand-primary-lightest cursor-pointer group/row"
+        ]}
+      >
+        <div class={["pl-2 py-3", row.style.hide_route_pill && "invisible"]}>
+          <.route_pill_with_modifiers
+            route_id={row.route_info.route_id}
+            modifier_ids={row.route_info.branch_ids}
+            modifier_class="group-hover/row:ring-brand-primary-lightest"
+          />
+        </div>
+        <div class={[
+          "py-3 w-full",
+          row.style.short_bottom_border && "border-b-[1px] border-gray-lightest"
+        ]}>
+          <.status_label
+            status={row.status_entry.status}
+            prefix={row.status_entry.prefix}
+            plural={row.status_entry.plural}
+          />
+        </div>
+      </div>
+    </div>
+    """
+  end
+
+  def status_to_rows(data) do
+    @route_ids
+    |> Enum.map(&{&1, data |> Map.get(&1)})
+    |> Enum.flat_map(&rows_for_route/1)
+  end
+
+  defp rows_for_route({route_id, branches_with_statuses}) do
+    branches_with_statuses
+    |> Enum.flat_map(&rows_for_branch_group/1)
+    |> add_route_id(route_id)
+  end
+
+  defp rows_for_branch_group(%{branch_ids: branch_ids, status_entries: status_entries}) do
+    status_entries
+    |> rows_for_status_entries()
+    |> add_branch_ids(branch_ids)
+  end
+
+  defp rows_for_status_entries(status_entries) do
+    show_prefix = show_prefix?(status_entries)
+
+    status_entries
+    |> Enum.map(&row_for_status_entry(&1, show_prefix))
+    |> add_short_intermediate_borders()
+    |> show_first_route_pill()
+  end
+
+  defp row_for_status_entry(status_entry, show_prefix) do
+    %{status: status, time: time, multiple: multiple} = status_entry
+    prefix = if show_prefix, do: prefix(status_entry), else: nil
+
+    %{
+      route_info: %{},
+      status_entry: %{
+        status: status,
+        time: time,
+        plural: multiple,
+        prefix: prefix
+      },
+      style: %{
+        hide_route_pill: true,
+        short_bottom_border: false
+      }
+    }
+  end
+
+  defp add_branch_ids(rows, branch_ids) do
+    rows
+    |> Enum.map(fn row -> row |> put_in([:route_info, :branch_ids], branch_ids) end)
+  end
+
+  defp add_route_id(rows, route_id) do
+    rows
+    |> Enum.map(fn row -> row |> put_in([:route_info, :route_id], route_id) end)
+  end
+
+  defp show_first_route_pill([first_entry | rest_of_entries]) do
+    [
+      first_entry |> put_in([:style, :hide_route_pill], false)
+      | rest_of_entries
+    ]
+  end
+
+  defp add_short_intermediate_borders([entry]) do
+    [entry]
+  end
+
+  defp add_short_intermediate_borders([first_entry | rest_of_entries]) do
+    [
+      first_entry |> put_in([:style, :short_bottom_border], true)
+      | add_short_intermediate_borders(rest_of_entries)
+    ]
+  end
+
+  defp show_prefix?(status_entries) do
+    status_entries |> Enum.any?(&future?/1)
+  end
+
+  defp future?(%{time: {:future, _}}), do: true
+  defp future?(_), do: false
+
+  defp prefix(%{time: :current}), do: "Now"
+  defp prefix(%{time: {:future, time}}), do: Util.kitchen_downcase_time(time)
+end
--- lib/dotcom_web/live/system_status.ex
+++ lib/dotcom_web/live/system_status.ex
@@ -6,6 +6,10 @@ defmodule DotcomWeb.Live.SystemStatus do
 
   use DotcomWeb, :live_view
 
+  import DotcomWeb.Components.RoutePills
+  import DotcomWeb.Components.SystemStatus.StatusLabel
+  import DotcomWeb.Components.SystemStatus.Widget
+
   alias Dotcom.SystemStatus
 
   def render(assigns) do
@@ -18,21 +22,40 @@ defmodule DotcomWeb.Live.SystemStatus do
       |> assign(:alerts, alerts)
       |> assign(:statuses, statuses)
 
+    Widget
+
     ~H"""
     <h1>System Status</h1>
-    <div>
-      <.status :for={status <- @statuses} status={status} />
-    </div>
+    <.system_status_widget routes_with_statuses={@statuses} />
+
+    <h1>Examples</h1>
+    <.system_status_widget routes_with_statuses={fake_statuses_1()} />
+
     <h1>Alerts</h1>
     <div class="flex flex-col gap-2">
       <.alert :for={alert <- @alerts} alert={alert} />
     </div>
-    """
-  end
 
-  defp status(assigns) do
-    ~H"""
-    <pre>{inspect @status, pretty: true}</pre>
+    <h1>Misc Components</h1>
+    <h2>Status Labels</h2>
+    <div class="flex flex-col gap-2">
+      <.status_label status={:normal} />
+      <.status_label status={:shuttle} />
+      <.status_label status={:shuttle} plural />
+      <.status_label status={:shuttle} prefix="8:30pm" />
+      <.status_label status={:shuttle} prefix="Wed, Feb 12 - Fri, Feb 14" />
+      <.status_label status={:station_closure} />
+      <.status_label status={:delay} />
+    </div>
+
+    <h2>Route Pills</h2>
+    <div class="flex flex-col gap-2">
+      <.route_pill route_id="Blue" />
+      <.route_pill route_id="Green" />
+      <.route_pill route_id="Orange" />
+      <.route_pill route_id="Red" />
+      <.route_pill_with_modifiers route_id="Green" modifier_ids={["Green-B", "Green-C"]} />
+    </div>
     """
   end
 
@@ -48,5 +71,55 @@ defmodule DotcomWeb.Live.SystemStatus do
       </details>
     </details>
     """
+  end
+
+  defp fake_statuses_1() do
+    %{
+      "Blue" => [
+        %{
+          branch_ids: [],
+          status_entries: [%{time: :current, status: :normal, multiple: false}]
+        }
+      ],
+      "Orange" => [
+        %{
+          branch_ids: [],
+          status_entries: [
+            %{time: :current, status: :delay, multiple: false},
+            %{
+              time: {:future, Timex.now() |> Timex.set(hour: 20, minute: 30)},
+              status: :shuttle,
+              multiple: false
+            }
+          ]
+        }
+      ],
+      "Red" => [
+        %{
+          branch_ids: [],
+          status_entries: [%{time: :current, status: :normal, multiple: false}]
+        },
+        %{
+          branch_ids: ["Mattapan"],
+          status_entries: [%{time: :current, status: :suspension, multiple: false}]
+        }
+      ],
+      "Green" => [
+        %{
+          branch_ids: ["Green-D", "Green-E"],
+          status_entries: [
+            %{
+              time: {:future, Timex.now() |> Timex.set(hour: 18, minute: 0)},
+              status: :station_closure,
+              multiple: true
+            }
+          ]
+        },
+        %{
+          branch_ids: ["Green-B", "Green-C"],
+          status_entries: [%{time: :current, status: :normal, multiple: false}]
+        }
+      ]
+    }
   end
 end
