815934545e10e983eddecf7fe4e5fca27c43a3c4
Anthony Shull <shull.anthony@gmail.com>
2025-01-21 12:09:32 -0600

    remove tests

--- test/dotcom/trip_plan/anti_corruption_layer_test.exs
+++ test/dotcom/trip_plan/anti_corruption_layer_test.exs
@@ -1,54 +1,10 @@
 defmodule Dotcom.TripPlan.AntiCorruptionLayerTest do
   use ExUnit.Case
 
-  import Dotcom.TripPlan.AntiCorruptionLayer, only: [convert_old_action: 1, convert_old_params: 1]
+  import Dotcom.TripPlan.AntiCorruptionLayer, only: [convert_old_params: 1]
   import Mox
-  import Test.Support.Factories.LocationService.LocationService
 
   setup :verify_on_exit!
-
-  describe "convert_old_action/1" do
-    test "returns all defaults when no params are given" do
-      assert convert_old_action(%{}) == convert_old_action(%{"plan" => %{}})
-    end
-
-    test "returns params representing successfully geocoded result" do
-      query = Faker.Address.street_address()
-      geocoded_result = build(:address)
-
-      expect(LocationService.Mock, :geocode, fn ^query ->
-        {:ok, [geocoded_result]}
-      end)
-
-      assert convert_old_action(%{from: query}) == %{
-               "plan" => %{
-                 "from" => geocoded_result.formatted,
-                 "from_latitude" => geocoded_result.latitude,
-                 "from_longitude" => geocoded_result.longitude
-               }
-             }
-    end
-
-    test "returns all defaults when no address found" do
-      query = Faker.Address.street_address()
-
-      expect(LocationService.Mock, :geocode, fn _ ->
-        {:ok, []}
-      end)
-
-      assert convert_old_action(%{from: query}) == %{"plan" => %{}}
-    end
-
-    test "returns all defaults for geocoding error" do
-      query = Faker.Address.street_address()
-
-      expect(LocationService.Mock, :geocode, fn _ ->
-        {:error, :internal_error}
-      end)
-
-      assert convert_old_action(%{from: query}) == %{"plan" => %{}}
-    end
-  end
 
   describe "convert_old_params/1" do
     test "returns all defaults when no params are given" do
