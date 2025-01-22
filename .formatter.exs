c45999983e88dd5a03e544a558856ca0108a7bc6
Cristen Jones <cjones3@mbta.com>
2025-01-21 21:14:20 -0500

    configure styler

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 [
   import_deps: [:phoenix],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter, Styler],
   inputs: [
     "{config,lib,rel,test}/**/*.{heex,ex,eex,exs}",
     "*.{heex,ex,exs}"
