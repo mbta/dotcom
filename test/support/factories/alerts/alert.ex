957917135805d6d438de8a96d1413cc122d25da8
Josh Larson <jlarson@mbta.com>
2025-01-29 20:58:26 -0500

    cleanup: Add more alert factories for use in SystemStatus.SubwayTests

--- test/support/factories/alerts/alert.ex
+++ test/support/factories/alerts/alert.ex
@@ -6,7 +6,7 @@ defmodule Test.Support.Factories.Alerts.Alert do
   use ExMachina
 
   alias Alerts.{Alert, Priority}
-  alias Test.Support.Factories.Alerts.InformedEntitySet
+  alias Test.Support.Factories.Alerts.{InformedEntity, InformedEntitySet}
 
   def alert_factory do
     %Alert{
@@ -27,5 +27,43 @@ defmodule Test.Support.Factories.Alerts.Alert do
       updated_at: Timex.now(),
       url: Faker.Internet.url()
     }
+  end
+
+  def alert_for_route_factory(attrs) do
+    {route_id, attrs} = Map.pop(attrs, :route_id)
+
+    build(
+      :alert,
+      attrs
+      |> Map.put(
+        :informed_entity,
+        InformedEntitySet.build(:informed_entity_set,
+          entities: [InformedEntity.build(:informed_entity, route: route_id)]
+        )
+      )
+    )
+  end
+
+  def active_during(alert, time) do
+    %{alert | active_period: [{time_before(time), time_after(time)}]}
+  end
+
+  def active_starting_at(alert, start_time) do
+    %{alert | active_period: [{start_time, time_after(start_time)}]}
+  end
+
+  # Returns a random time during the day today before the time provided.
+  defp time_before(time) do
+    between(Timex.beginning_of_day(time), time)
+  end
+
+  # Returns a random time during the day today after the time provided.
+  defp time_after(time) do
+    between(time, Timex.end_of_day(time))
+  end
+
+  # Returns a random time between the times provided in the Eastern time zone.
+  defp between(time1, time2) do
+    Faker.DateTime.between(time1, time2) |> Timex.to_datetime("America/New_York")
   end
 end
