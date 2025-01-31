4542afc003e489e78c94529025486816056af705
Anthony Shull <ashull@mbta.com>
2025-01-31 13:37:50 -0600

    Show the mobile app banner for Android devices (#2350)
    * mobile app banner for android
    
    * docs
    
    * docs
    
    * empty line

---
+++ assets/js/mobile-app-banner.js
@@ -0,0 +1,52 @@
+const isAndroid = () => {
+  return /Android/.test(navigator.userAgent);
+};
+
+const isNotCommuterRail = url => {
+  return !/\/schedules\/CR-/.test(url);
+};
+
+const isNotFerry = url => {
+  return !/\/schedules\/Boat-/.test(url);
+};
+
+const isNotTripPlanner = url => {
+  return !/\/trip-planner/.test(url);
+};
+
+const isAlertsPage = url => {
+  return isNotCommuterRail(url) && isNotFerry(url) && /\/alerts/.test(url);
+};
+
+const isSchedulePage = url => {
+  return (
+    isNotCommuterRail(url) &&
+    isNotFerry(url) &&
+    /\/schedules\/[\w.-]+\//.test(url)
+  );
+};
+
+const isStopPage = url => {
+  return /\/stops\/[\w.-]+/.test(url);
+};
+
+const isIncludedPage = () => {
+  const url = window.location.href;
+
+  return [isAlertsPage, isSchedulePage, isStopPage, isNotTripPlanner].some(
+    fn => {
+      return fn(url);
+    }
+  );
+};
+
+/**
+ * If the user is on an Android device,
+ * and the page is one of the pages we want to show the banner on,
+ * show the banner.
+ */
+export default function mobileAppBanner() {
+  if (isAndroid() && isIncludedPage()) {
+    document.querySelector("#mobile-app-banner").style.display = "block";
+  }
+}
