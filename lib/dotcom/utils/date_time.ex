1831183e567f907e605d2ff7b3ad7ff9447eece9
Anthony Shull <shull.anthony@gmail.com>
2025-02-08 09:33:38 -0600

    some docs

--- lib/dotcom/utils/date_time.ex
+++ lib/dotcom/utils/date_time.ex
@@ -8,7 +8,7 @@ defmodule Dotcom.Utils.DateTime do
   use Timex
 
   @typedoc """
-  A range of time that can be open ended.
+  A range of time that can be open ended on either side.
   """
   @type time_range() :: {DateTime.t() | nil, DateTime.t() | nil}
 
@@ -25,9 +25,9 @@ defmodule Dotcom.Utils.DateTime do
   @spec now() :: DateTime.t()
   def now(), do: Timex.now(@timezone)
 
-  # Timex can give us ambiguous times during DST transitions.
-  # We choose the later time.
-  # In the **very** rare case that we are given an {:error, _} tuple, we default to now.
+  # Timex can give us ambiguous times during DST transitions; we choose the later time.
+  # In the *very* rare case that we are given an {:error, _} tuple, we default to now.
+  # In the even rarer case that we are given some other argument type, we also default to now.
   @spec coerce_ambiguous_time(DateTime.t() | Timex.AmbiguousDateTime.t() | {:error, term()}) :: DateTime.t()
   def coerce_ambiguous_time(%DateTime{} = date_time), do: date_time
   def coerce_ambiguous_time(%Timex.AmbiguousDateTime{after: later}), do: later
