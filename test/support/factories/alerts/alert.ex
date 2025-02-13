75829cb50de4829fc2b0753b61633e37558f89e4
Josh Larson <jlarson@mbta.com>
2025-02-13 10:37:54 -0500

    cleanup: Add more alert factories for use in SystemStatus.SubwayTests (#2347)
    * cleanup: Add more alert factories for use in SystemStatus.SubwayTests
    
    * fix: Use InformedEntitySet.new/1 rather than its factory
    
    ...in order to make sure that the informed entity set is consistent,
    since new/1 does some processing that gets bypassed by the factory

--- test/support/factories/alerts/alert.ex
+++ test/support/factories/alerts/alert.ex
@@ -5,8 +5,8 @@ defmodule Test.Support.Factories.Alerts.Alert do
 
   use ExMachina
 
-  alias Alerts.{Alert, Priority}
-  alias Test.Support.Factories.Alerts.InformedEntitySet
+  alias Alerts.{Alert, InformedEntitySet, Priority}
+  alias Test.Support.Factories
 
   def alert_factory do
     %Alert{
@@ -20,12 +20,50 @@ defmodule Test.Support.Factories.Alerts.Alert do
       header: Faker.Lorem.Shakespeare.king_richard_iii(),
       image: Faker.Internet.image_url(),
       image_alternative_text: Faker.Lorem.Shakespeare.king_richard_iii(),
-      informed_entity: InformedEntitySet.build(:informed_entity_set),
+      informed_entity: Factories.Alerts.InformedEntitySet.build(:informed_entity_set),
       lifecycle: Alert.lifecycles() |> Faker.Util.pick(),
       priority: Priority.priority_levels() |> Faker.Util.pick(),
       severity: :rand.uniform(10),
       updated_at: Timex.now(),
       url: Faker.Internet.url()
     }
+  end
+
+  def alert_for_route_factory(attrs) do
+    {route_id, attrs} = Map.pop(attrs, :route_id)
+
+    build(
+      :alert,
+      attrs
+      |> Map.put(
+        :informed_entity,
+        InformedEntitySet.new([
+          Factories.Alerts.InformedEntity.build(:informed_entity, route: route_id)
+        ])
+      )
+    )
+  end
+
+  def active_during(alert, time) do
+    %{alert | active_period: [{time_before(time), time_after(time)}]}
+  end
+
+  def active_starting_at(alert, start_time) do
+    %{alert | active_period: [{start_time, time_after(start_time)}]}
+  end
+
+  # Returns a random time during the day today before the time provided.
+  defp time_before(time) do
+    between(Timex.beginning_of_day(time), time)
+  end
+
+  # Returns a random time during the day today after the time provided.
+  defp time_after(time) do
+    between(time, Timex.end_of_day(time))
+  end
+
+  # Returns a random time between the times provided in the Eastern time zone.
+  defp between(time1, time2) do
+    Faker.DateTime.between(time1, time2) |> Timex.to_datetime("America/New_York")
   end
 end
