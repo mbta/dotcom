540908a9d961ff29c0a10556f1b66c22e83c4669
Cristen Jones <cjones3@mbta.com>
2025-02-18 22:18:09 -0500

    refactor(SystemStatus): support mocking alerts and datetime

--- lib/dotcom/system_status.ex
+++ lib/dotcom/system_status.ex
@@ -7,13 +7,17 @@ defmodule Dotcom.SystemStatus do
 
   alias Dotcom.SystemStatus
 
+  @alerts_repo Application.compile_env!(:dotcom, :repo_modules)[:alerts]
+  @date_time_module Application.compile_env!(:dotcom, :date_time_module)
+
   @doc """
   Returns a list of alerts that satisfy the following criteria:
   - They are for one of the subway or trolley lines (including Mattapan), and
   - They are either currently active, or will be later today
   """
+  @spec subway_alerts_for_today :: [Alerts.Alert.t()]
   def subway_alerts_for_today() do
-    subway_alerts_for_day(Timex.now())
+    subway_alerts_for_day(@date_time_module.now())
   end
 
   defp subway_alerts_for_day(datetime) do
@@ -27,7 +31,7 @@ defmodule Dotcom.SystemStatus do
       "Green-E",
       "Mattapan"
     ]
-    |> Alerts.Repo.by_route_ids(datetime)
+    |> @alerts_repo.by_route_ids(datetime)
     |> SystemStatus.Alerts.for_day(datetime)
     |> SystemStatus.Alerts.filter_relevant()
   end
@@ -35,7 +39,8 @@ defmodule Dotcom.SystemStatus do
   @doc """
   Returns a map indicating the subway status for each of the subway lines.
   """
+  @spec subway_status :: %{Routes.Route.id_t() => SystemStatus.Subway.status_entry_group()}
   def subway_status() do
-    subway_alerts_for_today() |> SystemStatus.Subway.subway_status(Timex.now())
+    subway_alerts_for_today() |> SystemStatus.Subway.subway_status(@date_time_module.now())
   end
 end
