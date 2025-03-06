b5fbdbcea6f0902cbaf76ce2d54514fe969aec68
Anthony Shull <shull.anthony@gmail.com>
2025-03-06 16:45:08 -0600

    test coverage

--- test/dotcom/stop_place_ids_test.exs
+++ test/dotcom/stop_place_ids_test.exs
@@ -4,9 +4,14 @@ defmodule Dotcom.StopPlaceIdsTest do
   import Dotcom.StopPlaceIds
 
   describe "stop_place_id/1" do
-    test "returns a place id" do
+    test "returns a string when the id exists" do
       # Exercise/Verify
-      assert stop_place_id("1") > is_binary()
+      assert stop_place_id("1") |> is_binary()
+    end
+
+    test "returns nil when the id does not exist" do
+      # Exercise/Verify
+      assert stop_place_id("FOOBARBAZ") |> is_nil()
     end
   end
 end
