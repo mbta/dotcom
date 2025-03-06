63f325eb8464ccb2bd6e2d28ae1ff8895e263458
Anthony Shull <shull.anthony@gmail.com>
2025-03-06 11:17:39 -0600

    format

--- lib/dotcom/stop_place_ids.ex
+++ lib/dotcom/stop_place_ids.ex
@@ -1,10 +1,10 @@
 defmodule Dotcom.StopPlaceIds do
-	@moduledoc "Generated file linking stop ids to Google place ids."
+  @moduledoc "Generated file linking stop ids to Google place ids."
 
-	@doc "Map a stop id to a Google place id."
-	@spec stop_place_id(Stop.id_t()) :: String.t() | nil
-	def stop_place_id("1"), do: "ChIJTUMeBjt644kR1Dsuni7KgH0"
-	def stop_place_id("10"), do: "ChIJGdvzT0F644kR_bz2iUbdVpw"
-	def stop_place_id("10000"), do: "ChIJmzbu3pxw44kRV0dzyb_s2P8"
-	def stop_place_id(_), do: nil
+  @doc "Map a stop id to a Google place id."
+  @spec stop_place_id(Stop.id_t()) :: String.t() | nil
+  def stop_place_id("1"), do: "ChIJTUMeBjt644kR1Dsuni7KgH0"
+  def stop_place_id("10"), do: "ChIJGdvzT0F644kR_bz2iUbdVpw"
+  def stop_place_id("10000"), do: "ChIJmzbu3pxw44kRV0dzyb_s2P8"
+  def stop_place_id(_), do: nil
 end
