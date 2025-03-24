4e6a93b795da42faa469dd5e2587c11c127cae90
Josh Larson <jlarson@mbta.com>
2025-03-24 16:04:15 -0400

    fix: Update `@spec`'s and update `@doc`'s

--- lib/dotcom/trip_plan/open_street_map_reconciler.ex
+++ lib/dotcom/trip_plan/open_street_map_reconciler.ex
@@ -8,11 +8,12 @@ defmodule Dotcom.TripPlan.OpenStreetMapReconciler do
   security guards. That said, plenty of people need to plan trips to
   the State House.
 
-  This module is responsible for reconcilinglocations that
-  aren't publicly accessible, but that should still be possible to
-  plan trips to and from, to a sensible lat/long that can be used in
-  trip plans, e.g. the public side of a security entrance.
+  This module is responsible for reconciling locations that aren't
+  publicly accessible, but that should still be possible to plan trips
+  to and from, to a sensible lat/long that can be used in trip plans,
+  e.g. the public side of a security entrance.
   """
+
   alias Dotcom.TripPlan.InputForm
 
   @state_house_location %{latitude: 42.35861, longitude: -71.06297}
@@ -48,29 +49,49 @@ defmodule Dotcom.TripPlan.OpenStreetMapReconciler do
     ]
   }
 
+  @doc """
+  Given a `Dotcom.TripPlan.InputForm`, checks the `to` and `from`
+  fields and reconciles them to a publicly-accessible location if
+  necessary.
+
+  See `reconcile_location/1` for more info.
+  """
   @spec reconcile(InputForm.t()) :: InputForm.t()
-  def reconcile(%{from: from, to: to} = data) do
-    %InputForm{data | from: reconcile_location(from), to: reconcile_location(to)}
+  @spec reconcile(arg) :: arg when arg: var
+  def reconcile(%{from: from, to: to} = form) do
+    %InputForm{form | from: reconcile_location(from), to: reconcile_location(to)}
   end
 
-  # @spec reconcile_location(InputForm.Location.t()) :: InputForm.Location.t()
+  def reconcile(form), do: form
+
+  @doc """
+  Given a `Dotcom.TripPlan.InputForm.Location`, reconciles it by
+  nudging it to a publicly-accessible location if necessary.
+
+  Currently, we're using this to reconcile locations in the
+  Massachusetts State House to instead have trips planned to or from
+  the publicly-accessible security booth just outside. We determine
+  that a location is in the State House by building a polygon around
+  the State House, and checking whether the lat/long pair is inside
+  that polygon.
+  """
+  @spec reconcile_location(InputForm.Location.t()) :: InputForm.Location.t()
+  @spec reconcile_location(nil) :: nil
+  def reconcile_location(nil), do: nil
+
   def reconcile_location(location) do
     if state_house?(location) do
-      %InputForm.Location{
-        location
-        | latitude: @state_house_location.latitude,
-          longitude: @state_house_location.longitude
-      }
+      location |> Map.merge(@state_house_location)
     else
       location
     end
   end
 
-  # @spec state_house?(InputForm.Location.t()) :: boolean()
+  @spec state_house?(InputForm.Location.t()) :: boolean()
   defp state_house?(%{latitude: latitude, longitude: longitude}) do
     Topo.intersects?(@state_house_geofence, %Geo.Point{coordinates: {longitude, latitude}})
   end
 
-  # @spec state_house_location() :: %{latitude: float(), longitude: float()}
+  @spec state_house_location() :: %{latitude: float(), longitude: float()}
   def state_house_location(), do: @state_house_location
 end
