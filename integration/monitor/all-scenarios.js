56151f5647eb1f12f38e989be051575129f3f80e
Cristen Jones <cjones3@mbta.com>
2025-03-25 20:32:49 -0400

    refactor(monitor): simplified all-scenarios script
    - remove our worker in favor of the ones automatically created by Playwright
    - remove our performance measuring (Playwright measures test duration itself)
    - use Playwright afterEach hook to log results & send screenshot to Sentry

--- integration/monitor/all-scenarios.js
+++ integration/monitor/all-scenarios.js
@@ -1,14 +1,7 @@
 const cron = require("node-cron");
 const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
 const Sentry = require("@sentry/node");
-const { Worker } = require("worker_threads");
-
-const { fileToMetricName } = require("../utils");
 const RateLimiter = require("./rate-limiter");
-
-const filesPath = path.join(__dirname, "..", "scenarios");
 
 /*
  * Create a rate limiter that allows only one lease per hour.
@@ -26,50 +19,15 @@ Sentry.init({
     if (process.env.SENTRY_ENVIRONMENT == "prod") {
       return rateLimiter.lease() ? event : null;
     }
-
     return null;
   }
 });
 
 /*
- * Create a worker for each scenario file in the scenarios directory.
- */
-const workers = fs.readdirSync(filesPath).map((file) => {
-  const name = fileToMetricName(file);
-  const worker = new Worker(path.join(__dirname, "worker.js"), {
-    workerData: { name, path: path.join(filesPath, file) },
-  });
-
-  worker.on("message", ({ exception, metric, screenshot }) => {
-    Sentry.getCurrentScope().addAttachment({
-      filename: `${metric}-${Date.now()}.jpeg`,
-      data: screenshot,
-    });
-
-    Sentry.captureException(exception);
-
-    Sentry.getCurrentScope().clearAttachments();
-  });
-
-  return worker;
-});
-
-/*
  * A task runs every minute between the hours of 5am and midnight EST.
- * Spread out the worker runs over the minute to avoid overwhelming the
- * system. If we receive a message from a worker, it means that there was a
- * failure. Capture the exception with Sentry and attach a screenshot to the
- * event.
  */
 cron.schedule("* 5-23 * * *", (_) => {
-  workers.forEach((worker, index) => {
-    setTimeout(
-      (_) => {
-        worker.postMessage(null);
-      },
-      (60 * 1000 / workers.length) * index,
-    );
-  });
+  exec("npx playwright test all-scenarios", (_error, _stdout, _stderr) => {});
 }, {
   scheduled: true,
   timezone: "America/New_York"
