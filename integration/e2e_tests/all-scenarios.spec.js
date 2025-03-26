b54f6eb573650f856e09f931f4594aa58f78f53e
Cristen Jones <cjones3@mbta.com>
2025-03-25 20:32:49 -0400

    refactor(monitor): simplified all-scenarios script
    - remove our worker in favor of the ones automatically created by Playwright
    - remove our performance measuring (Playwright measures test duration itself)
    - use Playwright afterEach hook to log results & send screenshot to Sentry

--- integration/e2e_tests/all-scenarios.spec.js
+++ integration/e2e_tests/all-scenarios.spec.js
@@ -1,36 +1,84 @@
 const fs = require("fs");
 const path = require("path");
-const { performance } = require("node:perf_hooks");
 const { test } = require("@playwright/test");
-
 const { fileToMetricName } = require("../utils");
+const Logger = require("node-json-logger");
+const StatsD = require("hot-shots");
+
+const prefix = "dotcom.monitor.";
+const client = new StatsD({ prefix });
+const logger = new Logger();
 
 const filesPath = path.join(__dirname, "..", "scenarios");
-const files = fs.readdirSync(filesPath);
-const tests = files.map(file => {
+const tests = fs.readdirSync(filesPath).map(file => {
   const filePath = path.join(filesPath, file);
   const { scenario } = require(filePath);
   const name = fileToMetricName(file);
   return [name, scenario];
 });
+
+/* 
+ * For each test, use its test info to get the duration and test errors
+ * Log the durations, and capture screenshots for failures
+ */
+async function LogMetricsAndScreenshots({ page }) {
+  const { duration, errors, title, retry, project } = test.info();
+  const metric = `${prefix}${title}`;
+
+  client.gauge(title, duration);
+  if (errors.length > 0) {
+    const screenshot = await page.screenshot({ fullPage: true });
+    await test.info().attach('screenshot', {
+      body: screenshot,
+      contentType: 'image/png',
+    });
+
+    /* 
+     * only log the error/send screenshot to Sentry on final fail, 
+     * not on every single retry
+     */
+    if (retry == project.retries) {
+      if (process.env.SENTRY_DSN) {
+        Sentry.getCurrentScope().addAttachment({
+          filename: `${metric}-${Date.now()}.jpeg`,
+          data: screenshot,
+        });
+      }
+      
+      errors.forEach((error) => {
+        logger.error({ metric, error: error.message });
+        if (process.env.SENTRY_DSN) {
+          Sentry.captureException(error);
+        }
+      });
+
+      if (process.env.SENTRY_DSN) {
+        Sentry.getCurrentScope().clearAttachments();
+      }
+    }
+  } else {
+    logger.info({ metric, duration });
+  }
+}
+
 const baseURL = process.env.HOST
   ? `https://${process.env.HOST}`
   : "http://localhost:4001";
 
-test.describe("All scenarios", { tag: "@scenarios" }, () => {
+test.use({ 
+  baseURL,
+  browserName: "chromium",
+  headless: true,
+  screenshot: "off",
+  trace: "off",
+  userAgent: "Playwright",
+  video: "off"
+});
+test.describe.configure({ mode: "parallel", retries: 2, timeout: 20000 });
+test.describe("All scenarios", { tag: "@scenarios" }, async () => {
+  test.afterEach(LogMetricsAndScreenshots);
   tests.forEach(([name, scenario]) => {
-    test(name, { tag: `@${name}` }, async ({ page }) => {
-      const start = performance.now();
-
-      await scenario({ page, baseURL });
-
-      const end = performance.now();
-      const duration = Math.floor(end - start);
-
-      test.info().annotations.push({
-        type: "performance",
-        description: `duration: ${duration}ms`,
-      });
-    });
+    test(name, { tag: `@${name}` }, scenario);
   });
 });
+
