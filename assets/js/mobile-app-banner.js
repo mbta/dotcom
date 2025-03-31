eb9b035348d0124a89144e31d360f15b214dd5f8
Anthony Shull <ashull@mbta.com>
2025-03-31 10:03:10 -0500

    Add a mobile banner (#2456)

---
+++ assets/js/mobile-app-banner.js
@@ -0,0 +1,59 @@
+const isAndroid = agent => {
+  return /Android/.test(agent);
+};
+
+const isIPhone = agent => {
+  return /iPhone/.test(agent);
+};
+
+const isMobile = agent => {
+  return isAndroid(agent) || isIPhone(agent);
+};
+
+const isNotFerry = url => {
+  return !/\/schedules\/Boat-/.test(url);
+};
+
+const isSchedulePage = url => {
+  return (
+    isNotFerry(url) &&
+    /\/schedules\/[\w.-]+\//.test(url)
+  );
+};
+
+const isStopPage = url => {
+  return /\/stops\/[\w.-]+/.test(url);
+};
+
+const isTransitNearMe = url => {
+  return /\/transit-near-me/.test(url);
+};
+
+const isIncludedPage = url => {
+  return [isSchedulePage, isStopPage, isTransitNearMe].some(fn => {
+    return fn(url);
+  });
+};
+  
+/**
+ * If the user is on a mobile device,
+ * and the page is one of the pages we want to show the banner on,
+ * show the banner.
+ *
+ * Set the particular text.
+ */
+export default function mobileAppBanner() {
+  const agent = navigator.userAgent;
+  const url = window.location.href;
+
+  if (isMobile(agent) && isIncludedPage(url)) {
+    const mobileAppBanner = document.querySelector("#mobile-app-banner");
+
+    if (mobileAppBanner) {
+      const text = isTransitNearMe(url) ? "find transit near you" : "track your trip";
+
+      mobileAppBanner.querySelector("span").textContent = text;
+      mobileAppBanner.style.display = "block";
+    }
+  }
+};
