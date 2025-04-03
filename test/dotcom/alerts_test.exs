29bf5db5bc6c530f1303722c870621e2646640b8
Anthony Shull <shull.anthony@gmail.com>
2025-04-03 17:49:35 -0500

    add time based tests

--- test/dotcom/alerts_test.exs
+++ test/dotcom/alerts_test.exs
@@ -40,6 +40,41 @@ defmodule Dotcom.AlertsTest do
       # Exercise/Verify
       refute diversion_alert?(alert)
     end
+
+    test "returns true when created at is before active period" do
+      # Setup
+      created_at = Timex.now()
+      active_period = [{Timex.shift(created_at, days: 1), Timex.shift(created_at, days: 2)}]
+
+      {effect, severity} = diversion_effects() |> Faker.Util.pick()
+
+      alert =
+        Factories.Alerts.Alert.build(:alert,
+          active_period: active_period,
+          created_at: created_at,
+          effect: effect,
+          severity: severity
+        )
+
+      assert diversion_alert?(alert)
+    end
+
+    test "returns false when created at is after active period" do
+      created_at = Timex.now()
+      active_period = [{Timex.shift(created_at, days: -2), Timex.shift(created_at, days: -1)}]
+
+      {effect, severity} = diversion_effects() |> Faker.Util.pick()
+
+      alert =
+        Factories.Alerts.Alert.build(:alert,
+          active_period: active_period,
+          created_at: created_at,
+          effect: effect,
+          severity: severity
+        )
+
+      refute diversion_alert?(alert)
+    end
   end
 
   describe "diversion_effects/0" do
