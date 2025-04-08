bbed89bdce41bc97409d7dcdaf26ca2e2a13c6f0
Anthony Shull <ashull@mbta.com>
2025-04-08 17:12:08 -0500

    Correctly order timetables (#2494)
    * tests
    
    * format
    
    * test
    
    * test
    
    * test
    
    * docs
    
    * docs

--- test/schedules/sort_test.exs
+++ test/schedules/sort_test.exs
@@ -5,20 +5,17 @@ defmodule SortTest do
   import Timex, only: [shift: 2]
 
   alias Schedules.Schedule
+  alias Schedules.Trip
 
   @now Timex.now()
 
   @schedules [
-    %Schedule{time: @now, trip: "t1", stop: "s1"},
-    %Schedule{time: shift(@now, minutes: 5), trip: "t1", stop: "s2"},
-    %Schedule{time: shift(@now, minutes: 10), trip: "t1", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t1", stop: "s4"},
-    %Schedule{time: shift(@now, minutes: 10), trip: "t3", stop: "s1"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t3", stop: "s2"},
-    %Schedule{time: shift(@now, minutes: 20), trip: "t3", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 25), trip: "t3", stop: "s4"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t2", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 20), trip: "t2", stop: "s4"}
+    %Schedule{departure_time: shift(@now, minutes: 1), trip: %Trip{id: "1"}},
+    %Schedule{departure_time: shift(@now, minutes: 2), trip: %Trip{id: "1"}},
+    %Schedule{departure_time: shift(@now, minutes: 5), trip: %Trip{id: "2"}},
+    %Schedule{departure_time: shift(@now, minutes: 6), trip: %Trip{id: "2"}},
+    %Schedule{departure_time: shift(@now, minutes: 3), trip: %Trip{id: "3"}},
+    %Schedule{departure_time: shift(@now, minutes: 4), trip: %Trip{id: "3"}}
   ]
 
   describe "sort_by_first_time/1" do
@@ -26,17 +23,17 @@ defmodule SortTest do
       result = sort_by_first_times(@schedules)
 
       for [first | _] = trip_list <- result do
-        assert Enum.all?(trip_list, &(&1.trip == first.trip))
+        assert Enum.all?(trip_list, &(&1.trip.id == first.trip.id))
       end
     end
 
-    test "sorts schedules by their time at the first shared stop" do
+    test "sorts schedules by their time at the first departure" do
       result = sort_by_first_times(@schedules)
 
       assert Enum.map(result, &List.first/1) == [
-               %Schedule{time: @now, trip: "t1", stop: "s1"},
-               %Schedule{time: shift(@now, minutes: 15), trip: "t2", stop: "s3"},
-               %Schedule{time: shift(@now, minutes: 10), trip: "t3", stop: "s1"}
+               %Schedule{departure_time: shift(@now, minutes: 1), trip: %Trip{id: "1"}},
+               %Schedule{departure_time: shift(@now, minutes: 3), trip: %Trip{id: "3"}},
+               %Schedule{departure_time: shift(@now, minutes: 5), trip: %Trip{id: "2"}}
              ]
     end
   end
