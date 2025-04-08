6c5e41bf988b77807306a0123dd6fc72cf13e239
Anthony Shull <shull.anthony@gmail.com>
2025-04-08 13:09:16 -0500

    tests

--- lib/schedules/sort.ex
+++ lib/schedules/sort.ex
@@ -3,7 +3,7 @@ defmodule Schedules.Sort do
   Handles sorting schedules.
   """
 
-  alias Schedules.{Schedule, Trip}
+  alias Schedules.Schedule
 
   @doc """
   Sorts a list of schedules into trip lists. Two lists are compared by checking the departure time at the
@@ -12,38 +12,38 @@ defmodule Schedules.Sort do
   @spec sort_by_first_times([Schedule.t()]) :: [[Schedule.t()]]
   def sort_by_first_times(schedules) do
     schedules
-    |> Enum.group_by(& &1.trip)
-    |> Enum.sort_by(&schedule_mapper/1, &schedule_sorter/2)
+    |> Enum.group_by(&(&1.trip.id))
+    |> Enum.sort_by(&mapper/1, &sorter/2)
     |> Enum.map(fn {_trip, schedules} -> schedules end)
   end
 
-  @spec schedule_sorter([Schedule.t()], [Schedule.t()]) :: boolean
-  defp schedule_sorter(left, right) do
-    {left_time, right_time} =
-      case first_matching_schedules(left, right) do
-        nil -> {List.first(left).time, List.first(right).time}
-        {%Schedule{time: left_time}, %Schedule{time: right_time}} -> {left_time, right_time}
-      end
-
-    Timex.before?(left_time, right_time)
-  end
-
-  @spec first_matching_schedules([Schedule.t()], [Schedule.t()]) ::
-          {Schedule.t(), Schedule.t()} | nil
-  def first_matching_schedules(left, right) do
-    left_stops = Map.new(left, fn schedule -> {schedule.stop, schedule} end)
-
-    case Enum.find(right, &Map.has_key?(left_stops, &1.stop)) do
-      nil ->
-        nil
-
-      matching_right_schedule ->
-        {left_stops[matching_right_schedule.stop], matching_right_schedule}
-    end
-  end
-
-  @spec schedule_mapper({Trip.t(), [Schedule.t()]}) :: [Schedule.t()]
-  defp schedule_mapper({_trip, schedules}) do
+  # Gets the first departure time for a list of schedules.
+  defp first_departure_time(schedules) do
     schedules
+    |> Enum.map(&(&1.departure_time))
+    |> Enum.reject(&is_nil/1)
+    |> Enum.sort(&subsorter/2)
+    |> List.first()
+  end
+
+  # Ignores the trip id and returns the schedules for sorting.
+  defp mapper({_, schedules}), do: schedules
+
+  # Sorts two schedule lists by comparing the first departure time of each.
+  defp sorter(a, b) do
+    a_departure_time = first_departure_time(a)
+
+    b_departure_time = first_departure_time(b)
+
+    subsorter(a_departure_time, b_departure_time)
+  end
+
+  # Compares two times.
+  # Handles the case where one or more are nil.
+  defp subsorter(nil, nil), do: true
+  defp subsorter(nil, _), do: false
+  defp subsorter(_, nil), do: true
+  defp subsorter(a, b) do
+    Timex.compare(a, b) < 1
   end
 end
--- test/schedules/sort_test.exs
+++ test/schedules/sort_test.exs
@@ -5,20 +5,21 @@ defmodule SortTest do
   import Timex, only: [shift: 2]
 
   alias Schedules.Schedule
+  alias Schedules.Trip
 
   @now Timex.now()
 
   @schedules [
-    %Schedule{time: @now, trip: "t1", stop: "s1"},
-    %Schedule{time: shift(@now, minutes: 5), trip: "t1", stop: "s2"},
-    %Schedule{time: shift(@now, minutes: 10), trip: "t1", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t1", stop: "s4"},
-    %Schedule{time: shift(@now, minutes: 10), trip: "t3", stop: "s1"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t3", stop: "s2"},
-    %Schedule{time: shift(@now, minutes: 20), trip: "t3", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 25), trip: "t3", stop: "s4"},
-    %Schedule{time: shift(@now, minutes: 15), trip: "t2", stop: "s3"},
-    %Schedule{time: shift(@now, minutes: 20), trip: "t2", stop: "s4"}
+    %Schedule{time: @now, trip: %Trip{id: "1"}, stop: "s1"},
+    %Schedule{time: shift(@now, minutes: 5), trip: %Trip{id: "1"}, stop: "s2"},
+    %Schedule{time: shift(@now, minutes: 10), trip: %Trip{id: "1"}, stop: "s3"},
+    %Schedule{time: shift(@now, minutes: 15), trip: %Trip{id: "1"}, stop: "s4"},
+    %Schedule{time: shift(@now, minutes: 10), trip: %Trip{id: "3"}, stop: "s1"},
+    %Schedule{time: shift(@now, minutes: 15), trip: %Trip{id: "3"}, stop: "s2"},
+    %Schedule{time: shift(@now, minutes: 20), trip: %Trip{id: "3"}, stop: "s3"},
+    %Schedule{time: shift(@now, minutes: 25), trip: %Trip{id: "3"}, stop: "s4"},
+    %Schedule{time: shift(@now, minutes: 15), trip: %Trip{id: "2"}, stop: "s3"},
+    %Schedule{time: shift(@now, minutes: 20), trip: %Trip{id: "2"}, stop: "s4"}
   ]
 
   describe "sort_by_first_time/1" do
@@ -34,9 +35,9 @@ defmodule SortTest do
       result = sort_by_first_times(@schedules)
 
       assert Enum.map(result, &List.first/1) == [
-               %Schedule{time: @now, trip: "t1", stop: "s1"},
-               %Schedule{time: shift(@now, minutes: 15), trip: "t2", stop: "s3"},
-               %Schedule{time: shift(@now, minutes: 10), trip: "t3", stop: "s1"}
+               %Schedule{time: @now, trip: %Trip{id: "1"}, stop: "s1"},
+               %Schedule{time: shift(@now, minutes: 15), trip: %Trip{id: "2"}, stop: "s3"},
+               %Schedule{time: shift(@now, minutes: 10), trip: %Trip{id: "3"}, stop: "s1"}
              ]
     end
   end
