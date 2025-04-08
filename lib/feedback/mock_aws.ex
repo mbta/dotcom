ad65ced97dfe51c5a2e3db684b75ec295686fc2c
Cristen Jones <cjones3@mbta.com>
2025-04-08 17:24:45 -0400

    fix(Feedback.MockAws): use right decoder (#2496)
    * fix(Feedback.MockAws): use right decoder
    
    * feat(Feedback.MockAws): just print email instead of logging
    
    (we don't show log level of info in dev anyway)

--- lib/feedback/mock_aws.ex
+++ lib/feedback/mock_aws.ex
@@ -2,14 +2,13 @@ defmodule Feedback.MockAws do
   @moduledoc """
   Mock AWS functions so that we aren't dependent on AWS in development.
   """
-  require Logger
 
-  alias Mail.Parsers.RFC2822
+  alias Mail.{Encoders.Base64, Parsers.RFC2822}
 
   def send_email(%{"RawMessage" => %{"Data" => raw_message}}) do
     _ =
       raw_message
-      |> Base.decode64!()
+      |> Base64.decode()
       |> RFC2822.parse()
       |> log_email()
 
@@ -17,17 +16,15 @@ defmodule Feedback.MockAws do
   end
 
   defp log_email(%Mail.Message{headers: %{"to" => to}, body: body}) do
-    Logger.info(fn ->
-      """
+    IO.puts("""
 
-      MOCK EMAIL
+    MOCK EMAIL
 
-      To: #{to}
+    To: #{to}
 
-      Body:
-      #{body}
+    Body:
+    #{body}
 
-      """
-    end)
+    """)
   end
 end
