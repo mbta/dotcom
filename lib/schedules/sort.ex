f8b998a4b0901e7a0cdc6f1f38b0a15341becdd6
Anthony Shull <shull.anthony@gmail.com>
2025-04-08 13:14:08 -0500

    format

--- lib/schedules/sort.ex
+++ lib/schedules/sort.ex
@@ -12,7 +12,7 @@ defmodule Schedules.Sort do
   @spec sort_by_first_times([Schedule.t()]) :: [[Schedule.t()]]
   def sort_by_first_times(schedules) do
     schedules
-    |> Enum.group_by(&(&1.trip.id))
+    |> Enum.group_by(& &1.trip.id)
     |> Enum.sort_by(&mapper/1, &sorter/2)
     |> Enum.map(fn {_trip, schedules} -> schedules end)
   end
@@ -20,7 +20,7 @@ defmodule Schedules.Sort do
   # Gets the first departure time for a list of schedules.
   defp first_departure_time(schedules) do
     schedules
-    |> Enum.map(&(&1.departure_time))
+    |> Enum.map(& &1.departure_time)
     |> Enum.reject(&is_nil/1)
     |> Enum.sort(&subsorter/2)
     |> List.first()
@@ -43,6 +43,7 @@ defmodule Schedules.Sort do
   defp subsorter(nil, nil), do: true
   defp subsorter(nil, _), do: false
   defp subsorter(_, nil), do: true
+
   defp subsorter(a, b) do
     Timex.compare(a, b) < 1
   end
