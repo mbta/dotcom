215166913338db277f168724e43e35a544eec802
Cristen Jones <cjones3@mbta.com>
2025-04-23 11:06:18 -0400

    format
    sorry, prettier doesn't like it

--- assets/ts/stop/components/amenities/AmenityCard.tsx
+++ assets/ts/stop/components/amenities/AmenityCard.tsx
@@ -21,7 +21,9 @@ export const AmenityModal = ({
       closeModal={closeModal}
       ariaLabel={{ elementId }}
     >
-      <h1 id={elementId} className="h4">{headerText}</h1>
+      <h1 id={elementId} className="h4">
+        {headerText}
+      </h1>
       {children}
     </Modal>
   );
