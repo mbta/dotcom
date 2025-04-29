fb2eca64e79422ba745a1a13a4b43ab0a630b95e
Anthony Shull <shull.anthony@gmail.com>
2025-04-29 11:38:01 -0500

    tests

---
+++ test/dotcom_web/components/stops/header_test.exs
@@ -0,0 +1,97 @@
+defmodule DotcomWeb.Components.Stops.HeaderTest do
+  use ExUnit.Case
+
+  import Phoenix.LiveViewTest
+
+  import DotcomWeb.Components.Stops.Header, only: [header: 1]
+
+  alias Routes.Route
+  alias Stops.Stop
+
+  describe "header/1" do
+    test "renders the accessibility icon when the stop is accessible" do
+      # Setup
+      stop = %Stop{accessibility: ["accessible"]}
+      routes_by_stop = []
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      assert html =~ "accessible"
+    end
+
+    test "renders the accessibility icon when it is a bus-only stop" do
+      # Setup
+      stop = %Stop{parent_id: nil, accessibility: []}
+      routes_by_stop = [%Route{type: 3}]
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      assert html =~ "accessible"
+    end
+
+    test "does not render the accessibility icon when the stop is not accessible" do
+      # Setup
+      stop = %Stop{accessibility: []}
+      routes_by_stop = [%Route{type: 1}]
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      refute html =~ "accessible"
+    end
+
+    test "renders the parking icon when parking lots are present" do
+      # Setup
+      stop = %Stop{parking_lots: [%{}]}
+      routes_by_stop = []
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      assert html =~ "Parking"
+    end
+
+    test "does not render the parking icon when no parking lots are present" do
+      # Setup
+      stop = %Stop{parking_lots: []}
+      routes_by_stop = []
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      refute html =~ "Parking"
+    end
+
+    test "renders the zone information when a stop has a zone" do
+      # Setup
+      zone = Faker.Pizza.topping()
+      stop = %Stop{zone: zone}
+      routes_by_stop = []
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      assert html =~ zone
+    end
+
+    test "does not render the zone information when none is present" do
+      # Setup
+      stop = %Stop{zone: nil}
+      routes_by_stop = []
+
+      # Exercise
+      html = render_component(&header/1, %{stop: stop, routes_by_stop: routes_by_stop})
+
+      # Verify
+      refute html =~ "Zone"
+    end
+  end
+end
