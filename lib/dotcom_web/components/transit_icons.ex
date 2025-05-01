0e9956680ff9c023811fd9eddd1dea8504fe294c
Anthony Shull <shull.anthony@gmail.com>
2025-05-01 08:27:21 -0500

    pr feedback

--- lib/dotcom_web/components/transit_icons.ex
+++ lib/dotcom_web/components/transit_icons.ex
@@ -12,9 +12,7 @@ defmodule DotcomWeb.Components.TransitIcons do
   """
   def accessibility(assigns) do
     ~H"""
-    <.badge class="bg-cobalt-30 h-6 !px-1.5 !min-w-6 max-w-6 !rounded-sm inline-flex" variant="square">
-      <.icon name="wheelchair" class="h-3.5 w-3.5 fill-white" aria-label="Accessible" />
-    </.badge>
+    <.icon type="icon-svg" name="icon-accessible-small" class="h-6 w-6" />
     """
   end
 
@@ -23,12 +21,9 @@ defmodule DotcomWeb.Components.TransitIcons do
   """
   def parking(assigns) do
     ~H"""
-    <.badge
-      class="bg-gray-light font-bold text-white max-h-6 !px-1.5 !min-w-6 !rounded-sm"
-      variant="square"
-    >
+    <div class="bg-gray-light text-white h-6 px-1.5 w-6 rounded-sm inline-flex items-center">
       <span arial-label="Parking">P</span>
-    </.badge>
+    </div>
     """
   end
 
@@ -37,12 +32,12 @@ defmodule DotcomWeb.Components.TransitIcons do
   """
   def zone(assigns) when not is_nil(assigns.stop.zone) do
     ~H"""
-    <.badge
-      class="border-[1px] border-commuter-rail text-commuter-rail max-h-6 !rounded-sm"
+    <div
+      class="border-[1px] border-commuter-rail text-commuter-rail max-h-6 rounded-sm px-1.5"
       variant="square"
     >
       Zone {@stop.zone}
-    </.badge>
+    </div>
     """
   end
 
