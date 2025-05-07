4c08e6fe8160fde3870b102d9df7966a69c8511c
Anthony Shull <shull.anthony@gmail.com>
2025-05-07 15:24:55 -0500

    backend logic working

---
+++ lib/dotcom/system_status/commuter_rail.ex
@@ -0,0 +1,44 @@
+defmodule Dotcom.SystemStatus.CommuterRail do
+  @moduledoc """
+  This module exists to provide a simple view into the status of the
+  commuter rail system, for each line, showing whether its running normally,
+  or whether there are alerts that impact service.
+  """
+
+  import Dotcom.Alerts, only: [service_impacting_alert?: 1]
+
+  alias Dotcom.SystemStatus
+  alias Routes.Route
+
+  @date_time_module Application.compile_env!(:dotcom, :date_time_module)
+  @repos_module Application.compile_env!(:dotcom, :repo_modules)
+
+  @alerts_repo @repos_module[:alerts]
+  @routes_repo @repos_module[:routes]
+
+  def commuter_rail_status() do
+    commuter_rail_routes()
+    |> Enum.map(fn route ->
+      {route.id, SystemStatus.service_today?(route)}
+    end)
+  end
+
+  def commuter_rail_alerts() do
+    commuter_rail_routes()
+    |> Enum.map(&commuter_rail_route_alerts/1)
+    |> Map.new()
+  end
+
+  defp commuter_rail_route_alerts(%Route{id: id} = route) do
+    [id]
+    |> @alerts_repo.by_route_ids(@date_time_module.now())
+    |> Enum.filter(&service_impacting_alert?/1)
+    |> Kernel.then(fn alerts -> {route.name, SystemStatus.alerts_to_statuses(alerts, @date_time_module.now())} end)
+  end
+
+  defp commuter_rail_routes() do
+    @routes_repo.all()
+    |> Enum.filter(&Routes.Route.commuter_rail?/1)
+    |> Enum.sort_by(& &1.sort_order)
+  end
+end
