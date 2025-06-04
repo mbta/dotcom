54a3e291a88d28f39c4075ce4e96f829bb78df54
Cristen Jones <cjones3@mbta.com>
2025-06-04 13:57:52 -0400

    fix: improve accessible trip plans to inaccessible stops

--- mix.exs
+++ mix.exs
@@ -121,7 +121,7 @@ defmodule DotCom.Mixfile do
       {:nebulex_redis_adapter, "2.4.2"},
       {
         :open_trip_planner_client,
-        [github: "mbta/open_trip_planner_client", tag: "v0.13.0"]
+        [github: "mbta/open_trip_planner_client", tag: "v0.14.0"]
       },
       {:parallel_stream, "1.1.0"},
       {:phoenix, "1.7.21"},
