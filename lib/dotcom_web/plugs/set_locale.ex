5a65073afa8cadbddbdea707cae7215a67b32633
Anthony Shull <shull.anthony@gmail.com>
2025-07-09 12:04:33 -0500

    handle session too

--- lib/dotcom_web/plugs/set_locale.ex
+++ lib/dotcom_web/plugs/set_locale.ex
@@ -24,12 +24,22 @@ defmodule DotcomWeb.Plugs.SetLocale do
     |> put_session(:locale, locale)
   end
 
-  # Check params, then cookie, and use the default if neither are present or supported.
+  # Check params, then session, then cookie, and use the default if none are present or supported.
   defp get_locale(conn) do
-    locale = get_locale_from_params(conn) || get_locale_from_cookie(conn)
+    locale =
+      get_locale_from_params(conn) ||
+        get_locale_from_session(conn) ||
+        get_locale_from_cookie(conn)
 
     if locale?(locale), do: locale, else: default_locale_code()
   end
+
+  # Destructure the cookies for the locale.
+  defp get_locale_from_cookie(%{cookies: %{"locale" => locale}}) do
+    locale
+  end
+
+  defp get_locale_from_cookie(_), do: nil
 
   # Destructure the params for the locale.
   defp get_locale_from_params(%{params: %{"locale" => locale}}) do
@@ -38,10 +48,8 @@ defmodule DotcomWeb.Plugs.SetLocale do
 
   defp get_locale_from_params(_), do: nil
 
-  # Destructure the cookies for the locale.
-  defp get_locale_from_cookie(%{cookies: %{"locale" => locale}}) do
-    locale
+  # Get the locale from the session, if present.
+  defp get_locale_from_session(conn) do
+    get_session(conn, :locale)
   end
-
-  defp get_locale_from_cookie(_), do: nil
 end
