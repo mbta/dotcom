903c6be18345f11d56f8cb4dcaf5f40bbaf6d69a
Anthony Shull <shull.anthony@gmail.com>
2025-07-10 09:38:28 -0500

    remove errors file we arent using

--- test/dotcom_web/gettext_test.exs
+++ test/dotcom_web/gettext_test.exs
@@ -1,9 +1,11 @@
 defmodule DotcomWeb.GettextTest do
   @moduledoc false
+
   use ExUnit.Case, async: true
 
   test "has additional locales established" do
     locales = Gettext.known_locales(DotcomWeb.Gettext)
+
     assert length(locales) > 1
   end
 end
