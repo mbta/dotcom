b4275edff5c0799d6065220725772d8401a2c81a
Anthony Shull <shull.anthony@gmail.com>
2025-07-10 09:41:27 -0500

    swap out translation test strings

--- lib/dotcom_web/controllers/health_controller.ex
+++ lib/dotcom_web/controllers/health_controller.ex
@@ -2,10 +2,13 @@ defmodule DotcomWeb.HealthController do
   @moduledoc """
   Simple controller to return 200 OK when the website is healthy.
   """
+
   use DotcomWeb, :controller
 
   def index(conn, _params) do
+    status = gettext("All clear, captain!")
+
     conn
-    |> send_resp(200, "")
+    |> send_resp(200, status)
   end
 end
