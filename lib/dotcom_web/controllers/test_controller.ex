2b1dd7364d94cc9b85de0a1b9c02337ddc9bf146
Anthony Shull <shull.anthony@gmail.com>
2025-07-15 15:23:30 -0500

    should pass gettext task

--- lib/dotcom_web/controllers/test_controller.ex
+++ lib/dotcom_web/controllers/test_controller.ex
@@ -7,20 +7,8 @@ defmodule DotcomWeb.TestController do
 
   use DotcomWeb, :controller
 
-  if Mix.env() === :dev do
-    1..10
-    |> Enum.to_list()
-    |> Enum.each(fn n ->
-      def star_wars_quote(unquote(n)) do
-        gettext(unquote(Faker.StarWars.quote()))
-      end
-    end)
-  end
-
-  def star_wars_quote(_), do: ""
-
   def index(conn, _params) do
-    status = ~t(Star Wars quote of the day) <> " -> " <> star_wars_quote(:rand.uniform(10))
+    status = ~t(All clear, Captain!)
 
     Logger.notice("#{__MODULE__}: #{status}")
 
