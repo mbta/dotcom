8919dcdaa808383dc069a5cf0fbb024ffda39ce3
Anthony Shull <shull.anthony@gmail.com>
2025-07-24 13:03:29 -0500

    round to 1/10

--- lib/mix/tasks/gettext/translate.ex
+++ lib/mix/tasks/gettext/translate.ex
@@ -134,7 +134,7 @@ defmodule Mix.Tasks.Gettext.Translate do
     if total < 1 do
       0.0
     else
-      (translated / total) |> Float.round(0)
+      (translated / total) |> Float.round(1)
     end
   end
 
