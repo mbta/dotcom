4345191a56453201f19d5d14b918704a04660612
Josh Larson <jlarson@mbta.com>
2025-07-26 15:06:16 -0400

    feat: Translate T-Alerts component

--- lib/dotcom_web/components/t_alerts.ex
+++ lib/dotcom_web/components/t_alerts.ex
@@ -8,23 +8,23 @@ defmodule DotcomWeb.Components.TAlerts do
   def t_alerts(assigns) do
     ~H"""
     <div class="m-alerts-ad">
-      <h3>Get T-Alerts</h3>
+      <h3>{~t(Get T-Alerts)}</h3>
       <div class="m-alerts-ad__content">
         <div class="m-alerts-ad__buttons">
           <p>
-            Tell us about your regular trips and receive text or email alerts that are relevant to you, at times when you want them.
+            {~t(Tell us about your regular trips and receive text or email alerts that are relevant to you, at times when you want them.)}
           </p>
           <a
             href="https://alerts.mbta.com/account/new"
             class="m-alerts-ad__link m-alerts-ad__link--create"
           >
-            Create an Account
+            {~t(Create an Account)}
           </a>
           <a
             href="https://alerts.mbta.com/login/new"
             class="m-alerts-ad__link m-alerts-ad__link--login"
           >
-            Sign in
+            {~t(Sign in)}
           </a>
         </div>
         <div class="m-alerts-ad__image">
