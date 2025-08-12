394b1d9105e7d1b42b0558c15badce48414d7f7c
Anthony Shull <shull.anthony@gmail.com>
2025-08-12 12:12:46 -0500

    docs

--- lib/dotcom/cache/get/subscriber.ex
+++ lib/dotcom/cache/get/subscriber.ex
@@ -1,5 +1,9 @@
 defmodule Dotcom.Cache.Get.Subscriber do
   @moduledoc """
+  Every node runs a subscriber.
+
+  Subscribers wait to hear from a publisher telling them what key they want a value for.
+  Subscribers look up their value and publish it back (if any value exists).
   """
 
   use GenServer
@@ -15,6 +19,7 @@ defmodule Dotcom.Cache.Get.Subscriber do
 
   @impl GenServer
   @doc """
+  Start by subscribing to the network-wide channel for publishing value requests.
   """
   def init(_) do
     Application.get_env(:dotcom, :redis_config)
@@ -25,12 +30,13 @@ defmodule Dotcom.Cache.Get.Subscriber do
   end
 
   @impl GenServer
-  @doc """
-  """
+  # Indicates a successful subscription.
   def handle_info({:redix_pubsub, _pid, _ref, :subscribed, %{channel: _}}, _) do
     {:noreply, nil}
   end
 
+  # Processes the request to get the node value.
+  # If the value is no present, it does nothing.
   def handle_info(
         {:redix_pubsub, _pid, _ref, :message, %{channel: @channel, payload: message}},
         _
@@ -39,22 +45,23 @@ defmodule Dotcom.Cache.Get.Subscriber do
 
     value = Enum.join(key, "|") |> @cache.get()
 
-    # If the value is nil we just don't respond.
     if value do
-      publish(sender, :erlang.term_to_binary(value))
+      publish(sender, value)
     end
 
     {:noreply, nil}
   end
 
+  # Sends the value back to the requestor's channel.
   defp publish(sender, value) do
     @redis.command([
       "PUBLISH",
       "#{@channel}:#{sender}",
-      value
+      :erlang.term_to_binary(value)
     ])
   end
 
+  # Subscribe to the network-wide channel.
   defp subscribe({:ok, pubsub}, channel) do
     @redix_pub_sub.subscribe(pubsub, channel, self())
   end
