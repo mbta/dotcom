9fa1a44dd0426f9250ec259fbd881f27f45cc952
Anthony Shull <shull.anthony@gmail.com>
2025-08-13 09:15:56 -0500

    remove unnecessary includes

--- lib/dotcom_web/controllers/page_controller.ex
+++ lib/dotcom_web/controllers/page_controller.ex
@@ -8,7 +8,6 @@ defmodule DotcomWeb.PageController do
 
   alias CMS.Partial.{
     Banner,
-    Paragraph,
     Teaser,
     WhatsHappeningItem
   }
@@ -55,14 +54,6 @@ defmodule DotcomWeb.PageController do
   def menu(conn, _params) do
     conn
     |> render("menu.html")
-  end
-
-  @spec fares(map) :: Paragraph.t() | nil
-  defp fares(query_params) do
-    case Repo.get_paragraph("paragraphs/multi-column/homepage-fares", query_params) do
-      {:error, _} -> nil
-      result -> result
-    end
   end
 
   @spec banner :: Banner.t() | nil
