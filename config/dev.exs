da3022aa8f80c9ee0448076a0e8479a535a662d6
Cristen Jones <cjones3@mbta.com>
2025-08-14 16:35:54 -0400

    feat(CMS.Api): configurable connection options

--- config/dev.exs
+++ config/dev.exs
@@ -2,6 +2,9 @@ import Config
 
 config :dotcom, :cache, Dotcom.Cache.Multilevel
 
+# Local development using custom SSL certs
+config :dotcom, :cms_api, connect_options: [transport_opts: [verify: :verify_none]]
+
 config :kino_live_component,
   css_path: "http://localhost:8090/app.css",
   endpoint: "http://localhost:4001/kino-live-component",
