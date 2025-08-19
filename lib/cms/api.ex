0874ea6760f94aeed69f9edffd41f98699401063
Cristen Jones <cjones3@mbta.com>
2025-08-19 16:36:49 -0400

    fix(CMS.Api): configurable connection options

--- lib/cms/api.ex
+++ lib/cms/api.ex
@@ -48,12 +48,21 @@ defmodule CMS.Api do
   def client(headers \\ []) do
     config = Application.get_env(:dotcom, :cms_api)
 
-    @req.new(
-      base_url: config[:base_url],
-      headers: config[:headers] ++ headers,
-      finch: if(is_nil(config[:connect_options]), do: Dotcom.Finch),
-      connect_options: config[:connect_options]
-    )
+    case config[:connect_options] do
+      nil ->
+        @req.new(
+          base_url: config[:base_url],
+          headers: config[:headers] ++ headers,
+          finch: Dotcom.Finch
+        )
+
+      connect_options ->
+        @req.new(
+          base_url: config[:base_url],
+          headers: config[:headers] ++ headers,
+          connect_options: connect_options
+        )
+    end
   end
 
   # Drupal's Redirect module forces all location header values to be
