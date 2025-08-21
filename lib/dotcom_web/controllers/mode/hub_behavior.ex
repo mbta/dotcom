d482dd3cea7ff948008c4d5497e400db0a73e5e7
Anthony Shull <shull.anthony@gmail.com>
2025-08-21 11:05:47 -0500

    Revert "get rid of warnings"
    This reverts commit 4094b7aded6ac150760b190ccf8a8a846879b2d7.

--- lib/dotcom_web/controllers/mode/hub_behavior.ex
+++ lib/dotcom_web/controllers/mode/hub_behavior.ex
@@ -1,7 +1,7 @@
 defmodule DotcomWeb.Mode.HubBehavior do
   @moduledoc "Macro and behaviour for mode hub pages."
 
-  use Phoenix.Controller, formats: [:html, :json]
+  use Phoenix.Controller, namespace: DotcomWeb
 
   import DotcomWeb.ControllerHelpers, only: [green_routes: 0]
   import DotcomWeb.Router.Helpers, only: [mode_path: 2]
