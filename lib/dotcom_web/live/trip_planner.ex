8ec3a4f2f129be0a8cea77e9c5df244d86d2b96e
Anthony Shull <shull.anthony@gmail.com>
2025-08-28 16:09:19 -0500

    combined legs working

--- lib/dotcom_web/live/trip_planner.ex
+++ lib/dotcom_web/live/trip_planner.ex
@@ -372,7 +372,7 @@ defmodule DotcomWeb.Live.TripPlanner do
 
     case plan do
       {:ok, itinerary_groups} ->
-        itinerary_groups
+        join_itinerary_groups_loops(itinerary_groups)
 
       error ->
         error
@@ -430,6 +430,57 @@ defmodule DotcomWeb.Live.TripPlanner do
     itinerary_groups
     |> itinerary_groups_to_itinerary_map(group_index, index)
     |> TripPlan.Map.get_points()
+  end
+
+  defp join_itinerary_groups_loops(itinerary_groups) do
+    itinerary_groups
+    |> Enum.slice(0, 1)
+    |> Enum.map(&join_itinerary_group_loops/1)
+  end
+
+  defp join_itinerary_group_loops(itinerary_group) do
+    itinerary_group
+    |> Map.update(:itineraries, [], &join_itineraries_loops/1)
+  end
+
+  defp join_itineraries_loops(itineraries) do
+    itineraries
+    |> Enum.map(&join_itinerary_loops/1)
+  end
+
+  defp join_itinerary_loops(itinerary) do
+    itinerary
+    |> Map.update(:legs, [], &join_leg_loops/1)
+  end
+
+  defp join_leg_loops([]), do: []
+
+  defp join_leg_loops([first]), do: [first]
+
+  defp join_leg_loops(legs) do
+    Enum.reduce(legs, [], fn curr, acc ->
+      if curr.interline_with_previous_leg do
+        last = List.last(acc)
+        all_but_last = if length(acc) === 1, do: [], else: Enum.slice(acc, -1, 1)
+
+        all_but_last ++ [merge_legs(last, curr)]
+      else
+        acc ++ [curr]
+      end
+    end)
+  end
+
+  defp merge_legs(first, second) do
+    second
+    |> Map.merge(first)
+    |> Map.put(:to, Map.get(second, :to))
+    |> Map.put(:end, Map.get(second, :end))
+    |> Map.put(:distance, Map.get(first, :distance, 0.0) + Map.get(second, :distance, 0.0))
+    |> Map.put(:duration, Map.get(first, :duration, 0.0) + Map.get(second, :duration, 0.0))
+    |> Map.put(
+      :intermediate_stops,
+      Map.get(first, :intermediate_stops, []) ++ Map.get(second, :intermediate_stops, [])
+    )
   end
 
   # For the from or to fields, get the underlying location data if changed
--- mix.exs
+++ mix.exs
@@ -129,7 +129,7 @@ defmodule DotCom.Mixfile do
       {:nebulex_redis_adapter, "2.4.2"},
       {
         :open_trip_planner_client,
-        [github: "mbta/open_trip_planner_client", tag: "v0.17.0"]
+        [github: "mbta/open_trip_planner_client", branch: "ags/add-interline-with-previous-leg"]
       },
       {:parallel_stream, "1.1.0"},
       {:phoenix, "1.8.0"},
