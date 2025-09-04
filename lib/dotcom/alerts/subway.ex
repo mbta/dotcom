1b18244875f71cb78587c8fee386726bfcc020f6
Anthony Shull <shull.anthony@gmail.com>
2025-09-04 14:56:17 -0500

    alert groups

--- lib/dotcom/alerts/subway.ex
+++ lib/dotcom/alerts/subway.ex
@@ -4,17 +4,18 @@ defmodule Dotcom.Alerts.Subway do
   """
 
   use Dotcom.Alerts.Group
+  use Dotcom.Gettext.Sigils
 
   import Dotcom.Alerts, only: [service_impacting_effects: 0]
 
   @impl Dotcom.Alerts.Group
   def effect_groups do
     [
-      {"Service", service_impacting_effects()},
-      {"Elevator & Escalator", [elevator_closure: 1, escalator_closure: 1]},
-      {"Bike", [bike_issue: 1]},
-      {"Parking", [parking_closure: 1, parking_issue: 1]},
-      {"Other", []}
+      {~t"Service", service_impacting_effects()},
+      {~t"Elevator & Escalator", [elevator_closure: 1, escalator_closure: 1]},
+      {~t"Bike", [bike_issue: 1]},
+      {~t"Parking", [parking_closure: 1, parking_issue: 1]},
+      {~t"Other", []}
     ]
   end
 end
