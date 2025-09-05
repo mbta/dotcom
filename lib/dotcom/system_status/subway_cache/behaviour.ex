74e3119db479d9e2c6b2c7d161c0b0367b2a8f4b
Josh Larson <jlarson@mbta.com>
2025-09-05 16:25:59 -0400

    fix: Dialyzer errors

--- lib/dotcom/system_status/subway_cache/behaviour.ex
+++ lib/dotcom/system_status/subway_cache/behaviour.ex
@@ -2,5 +2,7 @@ defmodule Dotcom.SystemStatus.SubwayCache.Behaviour do
   @moduledoc """
   A behaviour to capture the interface of `Dotcom.SystemStatus.SubwayCache`.
   """
-  @callback subway_status() :: %{Routes.Route.id_t() => SystemStatus.Subway.status_entry_group()}
+  @callback subway_status() :: %{
+              Routes.Route.id_t() => Dotcom.SystemStatus.Subway.status_entry_group()
+            }
 end
