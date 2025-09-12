2fc78b25fd0b1888f885dac605994193dce31e40
Cristen Jones <cjones3@mbta.com>
2025-09-12 10:34:50 -0400

    fix(Components.Alerts): format the more info link

--- lib/dotcom_web/components/alerts.ex
+++ lib/dotcom_web/components/alerts.ex
@@ -45,7 +45,7 @@ defmodule DotcomWeb.Components.Alerts do
           <%= if @url do %>
             <hr class="my-4 border-t-[1px] border-gray-lightest" />
             <p class="my-0">
-              For more information: <a href={@url}>{@url}</a>
+              For more information: {Alerts.URLParsingHelpers.replace_urls_with_links(@url)}
             </p>
           <% end %>
           <div class="c-alert-item__updated">
