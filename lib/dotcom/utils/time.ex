4d940fece113684f446b082f0a262ca52558554a
Anthony Shull <shull.anthony@gmail.com>
2025-10-23 13:40:30 -0500

    comments

--- lib/dotcom/utils/time.ex
+++ lib/dotcom/utils/time.ex
@@ -94,105 +94,129 @@ defmodule Dotcom.Utils.Time do
   """
   @spec format!(Timex.Types.valid_datetime(), atom()) :: String.t()
   def format!(datetime, style \\ :full_date)
-
+  # Example (2000-01-01 00:00 UTC): "January 1, 2000"
   def format!(datetime, :full_date) do
     Timex.format!(datetime, "{Mfull} {D}, {YYYY}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "Jan 1, 2000"
   def format!(datetime, :short_date) do
     Timex.format!(datetime, "{Mshort} {D}, {YYYY}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "January 1, 2000, 12:00 AM"
   def format!(datetime, :full_datetime) do
     Timex.format!(datetime, "{Mfull} {D}, {YYYY}, {h12}:{m} {AM}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "12 AM"
   def format!(datetime, :time_12h) do
     Timex.format!(datetime, "{h12} {AM}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "12:00 AM"
   def format!(datetime, :time_12h_with_minutes) do
     Timex.format!(datetime, "{h12}:{m} {AM}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "00:00"
   def format!(datetime, :time_24h) do
     Timex.format!(datetime, "{h24}:{m}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "2000-01-01T00:00:00Z"
   def format!(datetime, :iso) do
     Timex.format!(datetime, "{ISO:Extended}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "01/01/2000"
   def format!(datetime, :mm_dd_yyyy) do
     Timex.format!(datetime, "{0M}/{0D}/{YYYY}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "01/01/2000 00:00"
   def format!(datetime, :mm_dd_yyyy_time) do
     Timex.format!(datetime, "{0M}/{0D}/{YYYY} {h24}:{m}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "Saturday, January 1, 2000"
   def format!(datetime, :weekday_full_date) do
     Timex.format!(datetime, "{WDfull}, {Mfull} {D}, {YYYY}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "1"
   def format!(datetime, :strftime_day) do
     Timex.format!(datetime, "%-d", :strftime)
   end
 
+  # Example (2000-01-01 00:00 UTC): "Sat, Jan  1"
   def format!(datetime, :strftime_abbrev) do
     Timex.format!(datetime, "%a, %b %e", :strftime)
   end
 
+  # Example (2000-01-01 00:00 UTC): "January 2000"
   def format!(datetime, :mfull_year) do
     Timex.format!(datetime, "{Mfull} {YYYY}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "Jan"
   def format!(datetime, :mshort) do
     Timex.format!(datetime, "{Mshort}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "01"
   def format!(datetime, :zero_d) do
     Timex.format!(datetime, "{0D}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "2000-1-1 00:00"
   def format!(datetime, :yyyy_m_d_h24_m) do
     Timex.format!(datetime, "{YYYY}-{M}-{D} {h24}:{m}")
   end
 
   # Small additional atoms used in templates
+  # Example (2000-01-01 00:00 UTC): "12"
   def format!(datetime, :h12) do
     Timex.format!(datetime, "{h12}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "1/1/2000"
   def format!(datetime, :m_d_y) do
     Timex.format!(datetime, "{M}/{D}/{YYYY}")
   end
 
   # Additional atom mappings for patterns found in the codebase.
+  # Example (2000-01-01 00:00 UTC): "Saturday, January 1"
   def format!(datetime, :weekday_month_day) do
     Timex.format!(datetime, "{WDfull}, {Mfull} {D}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "00:00 AM"
   def format!(datetime, :h24_with_am) do
     Timex.format!(datetime, "{h24}:{m} {AM}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "2000-01-01"
   def format!(datetime, :yyyy_dash_0m_0d) do
     Timex.format!(datetime, "{YYYY}-{0M}-{0D}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "2000-01-01"
   def format!(datetime, :iso_date) do
     Timex.format!(datetime, "{ISOdate}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "20000101T000000Z"
   def format!(datetime, :ical_timestamp) do
     Timex.format!(datetime, "{YYYY}{0M}{0D}T{h24}{m}{s}Z")
   end
 
+  # Example (2000-01-01 00:00 UTC): "Jan 1 2000 00:00"
   def format!(datetime, :mshort_d_y_h24_m) do
     Timex.format!(datetime, "{Mshort} {D} {YYYY} {h24}:{m}")
   end
 
+  # Example (2000-01-01 00:00 UTC): "12:00 AM UTC"
   def format!(datetime, :time_12h_with_zone) do
     Timex.format!(datetime, "{h12}:{m} {AM} {Zabbr}")
   end
