a4c47819251e2355f9bbb93bfd9f0ce44e9f64f7
Cristen Jones <cjones3@mbta.com>
2025-10-28 09:54:29 -0400

    update the lib for real

--- mix.exs
+++ mix.exs
@@ -123,10 +123,7 @@ defmodule DotCom.Mixfile do
       {:logster, "1.1.1"},
       # reverted from 0.4
       {:mail, "0.3.1"},
-      {:mbta_metro,
-       github: "mbta/mbta_metro",
-       branch: "cbj/metro-with-tokens-and-without-tailwind",
-       runtime: false},
+      {:mbta_metro, "1.0.0", runtime: false},
       {:mock, "0.3.9", [only: :test]},
       {:mox, "1.2.0", [only: [:dev, :test]]},
       {:msgpack, "0.8.1"},
