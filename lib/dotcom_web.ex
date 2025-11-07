58aa3801d08eb616f3fba5efecb71f14ea273b39
Anthony Shull <shull.anthony@gmail.com>
2025-11-07 10:44:57 -0600

    remove more

--- lib/dotcom_web.ex
+++ lib/dotcom_web.ex
@@ -82,19 +82,6 @@ defmodule DotcomWeb do
       # Include shared imports and aliases for views
       unquote(view_helpers())
 
-      def track_template() do
-        if Application.get_env(:dotcom, :env) != :test do
-          {_, trace} = Process.info(self(), :current_stacktrace)
-
-          template =
-            trace
-            |> Enum.map(fn {_, _, _, [file: file, line: _]} -> "#{file}" end)
-            |> Enum.find(&Regex.match?(~r/.html(.eex|.heex)/, &1))
-
-          :telemetry.execute([:template, :track], %{}, %{template: template})
-        end
-      end
-
       @dialyzer :no_match
     end
   end
