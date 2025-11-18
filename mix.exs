bf05df98e9604210814f5c7bd1e1a511e83465a5
Cristen Jones <cjones3@mbta.com>
2025-11-18 17:00:46 -0500

    update metro again

--- mix.exs
+++ mix.exs
@@ -123,7 +123,7 @@ defmodule DotCom.Mixfile do
       {:logster, "1.1.1"},
       # reverted from 0.4
       {:mail, "0.3.1"},
-      {:mbta_metro, "1.1.1", runtime: false},
+      {:mbta_metro, "1.1.2", runtime: false},
       {:mock, "0.3.9", [only: :test]},
       {:mox, "1.2.0", [only: [:dev, :test]]},
       {:msgpack, "0.8.1"},
