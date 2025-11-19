6ddfe5d76baf4ca5338edfde5ad751a4b3f69489
Cristen Jones <cjones3@mbta.com>
2025-11-19 14:41:16 -0500

    hack - ignore prototype components from credo

--- .credo.exs
+++ .credo.exs
@@ -21,7 +21,12 @@
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
         #
-        excluded: [~r"/_build/", ~r"/deps/", ~r"/node_modules/"]
+        excluded: [
+          ~r"/_build/",
+          ~r"/deps/",
+          ~r"/node_modules/",
+          ~r"/lib/dotcom_web/components/prototype.ex"
+        ]
       },
       #
       # Load and configure plugins here:
