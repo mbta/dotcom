5d64ca13bb46e9e2e995d5a4bc027c06f96ed702
Cristen Jones <cjones3@mbta.com>
2025-11-21 09:24:02 -0500

    chore: merge SF components into LiveView file

--- lib/dotcom_web/live/schedule_finder_live.ex
+++ lib/dotcom_web/live/schedule_finder_live.ex
@@ -4,12 +4,15 @@ defmodule DotcomWeb.ScheduleFinderLive do
   for a given route/direction/stop.
   """
 
+  use DotcomWeb, :component
   use DotcomWeb, :live_view
 
-  import DotcomWeb.Components.ScheduleFinder
+  import CSSHelpers
 
   alias DotcomWeb.Components.Prototype
+  alias MbtaMetro.Components.SystemIcons
   alias Phoenix.LiveView
+  alias Routes.Route
 
   @routes_repo Application.compile_env!(:dotcom, :repo_modules)[:routes]
   @stops_repo Application.compile_env!(:dotcom, :repo_modules)[:stops]
@@ -49,5 +52,79 @@ defmodule DotcomWeb.ScheduleFinderLive do
   defp assign_stop(socket, params) do
     stop_id = Map.get(params, "stop")
     assign(socket, :stop, if(stop_id, do: @stops_repo.get(stop_id)))
+  end
+
+  # Schedule Finder components =================================================
+
+  attr :route, Route, required: true
+  attr :direction_id, :string, required: true
+
+  def route_banner(assigns) do
+    ~H"""
+    <div class={[route_to_class(@route), "font-heading p-md"]}>
+      <.link
+        class="text-current flex flex-col gap-sm hover:no-underline active:no-underline focus:text-current hover:text-current"
+        patch={~p"/schedules/#{@route}"}
+      >
+        <div class="flex items-center gap-xs font-bold">
+          <.route_banner_icon route={@route} class="shrink-0 -ml-xs" />
+          <span class="grow notranslate">{@route.name}</span>
+          <.icon
+            name="arrow-up-right-from-square"
+            aria-hidden
+            class="size-4 fill-current justify-self-end"
+          />
+        </div>
+        <div class="flex items-center gap-xs">
+          <.icon name="arrow-right" aria-hidden class="size-4 mr-xs fill-current" />
+          <span>
+            {@route.direction_names[@direction_id]}
+            <%= if @route.id != "Green" do %>
+              {~t"towards"}
+              <strong class="notranslate">{@route.direction_destinations[@direction_id]}</strong>
+            <% end %>
+          </span>
+        </div>
+      </.link>
+    </div>
+    """
+  end
+
+  attr :route, Routes.Route, required: true
+  attr :rest, :global
+
+  def route_banner_icon(assigns) do
+    mode = assigns.route |> Route.type_atom() |> atom_to_class()
+    line_name = assigns.route |> Route.icon_atom() |> atom_to_class()
+
+    assigns =
+      assign(assigns, %{
+        line_name: line_name,
+        mode: mode
+      })
+
+    ~H"""
+    <SystemIcons.mode_icon aria-hidden line={@line_name} mode={@mode} {@rest} />
+    """
+  end
+
+  attr :stop, Stops.Stop
+
+  def stop_banner(assigns) do
+    ~H"""
+    <.link
+      :if={@stop}
+      class="bg-gray-lightest p-md flex items-center gap-xs text-black hover:no-underline active:no-underline focus:text-black hover:text-black"
+      patch={~p"/stops/#{@stop}"}
+    >
+      <.icon
+        type="icon-svg"
+        name={if(@stop.station?, do: "mbta-logo", else: "icon-stop-default")}
+        class="size-5 fill-current"
+      />
+      <span class="grow font-bold font-heading">{@stop.name}</span>
+      <.icon name="arrow-up-right-from-square" class="size-4 fill-current" />
+    </.link>
+    """
   end
 end
