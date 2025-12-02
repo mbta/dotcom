4baf9775b540e4e7c3ab23df1881b147ceb2140b
Cristen Jones <cjones3@mbta.com>
2025-12-01 20:53:15 -0500

    fixup(RouteComponents): add size attribute

--- lib/dotcom_web/components/route_components.ex
+++ lib/dotcom_web/components/route_components.ex
@@ -10,16 +10,17 @@ defmodule DotcomWeb.RouteComponents do
 
   attr :rest, :global
   attr :route, Route, required: true
+  attr :size, :string, default: "default", values: ["default", "small"]
 
   def route_icon(%{route: %Route{type: 2}} = assigns) do
     ~H"""
-    <SystemIcons.mode_icon mode="commuter-rail" {@rest} />
+    <SystemIcons.mode_icon mode="commuter-rail" size={@size} {@rest} />
     """
   end
 
   def route_icon(%{route: %Route{type: 4}} = assigns) do
     ~H"""
-    <SystemIcons.mode_icon mode="ferry" {@rest} />
+    <SystemIcons.mode_icon mode="ferry" size={@size} {@rest} />
     """
   end
 
@@ -27,14 +28,14 @@ defmodule DotcomWeb.RouteComponents do
     case line_name(route_id) do
       nil ->
         ~H"""
-        <SystemIcons.route_icon name={@route.name} {@rest} />
+        <SystemIcons.route_icon name={@route.name} size={@size} {@rest} />
         """
 
       line_name ->
         assigns = assign(assigns, :line, line_name)
 
         ~H"""
-        <SystemIcons.route_icon line={@line} {@rest} />
+        <SystemIcons.route_icon line={@line} size={@size} {@rest} />
         """
     end
   end
