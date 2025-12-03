764282d0fe931303e7df3c9afbc7bd2ed1ed03b7
Josh Larson <jlarson@mbta.com>
2025-12-03 14:41:30 -0500

    tweak: Move `/daily-schedules` under `/preview`

--- lib/dotcom_web/router.ex
+++ lib/dotcom_web/router.ex
@@ -302,15 +302,6 @@ defmodule DotcomWeb.Router do
     end
   end
 
-  scope "/", DotcomWeb do
-    import Phoenix.LiveView.Router
-    pipe_through([:browser, :browser_live, :basic_auth_readonly])
-
-    live_session :internal, layout: {DotcomWeb.LayoutView, :live} do
-      live("/daily-schedules", Live.DailySchedules)
-    end
-  end
-
   scope "/preview", DotcomWeb do
     import Phoenix.LiveView.Router
     pipe_through([:browser, :browser_live, :basic_auth_readonly])
@@ -318,6 +309,7 @@ defmodule DotcomWeb.Router do
     live_session :default, layout: {DotcomWeb.LayoutView, :preview} do
       live "/system-status", Live.SystemStatus
       live "/schedules/:route_id/:direction_id", ScheduleFinderLive
+      live "/daily-schedules", Live.DailySchedules
     end
   end
 
