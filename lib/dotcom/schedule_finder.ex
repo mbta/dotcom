ee696ff5f6fe60447b42db5d2ef4f92b156707cd
Josh Larson <jlarson@mbta.com>
2025-12-09 15:06:02 -0500

    cleanup: Slight code rearranging, and explanatory comment

--- lib/dotcom/schedule_finder.ex
+++ lib/dotcom/schedule_finder.ex
@@ -168,9 +168,13 @@ defmodule Dotcom.ScheduleFinder do
     }
   end
 
+  # If a platform name is nil, then it's nil no matter the mode. (This
+  # way, clauses below don't have to worry about nil-checking
+  # `platform_name`.)
+  defp simplify(nil, _), do: nil
+
   # Don't show platform names for subway. We might make exceptions later (JFK?)
   defp simplify(_, vehicle_type) when vehicle_type in [0, 1], do: nil
-  defp simplify(nil, _), do: nil
 
   # For commuter rail every station has a platform, but most stations also only
   # have _one_ so we don't really need to show a platform name there either.
