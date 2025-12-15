949dd5e12ba797020c0d558045b0678e0aa6bfa1
Josh Larson <jlarson@mbta.com>
2025-12-15 15:41:51 -0500

    cleanup: Remove System/Subway Status preview page (#2840)

--- lib/dotcom_web/router.ex
+++ lib/dotcom_web/router.ex
@@ -303,7 +303,6 @@ defmodule DotcomWeb.Router do
 
     live_session :default, layout: {DotcomWeb.LayoutView, :preview} do
       live "/", PreviewLive
-      live "/system-status", Live.SystemStatus
       live "/schedules/:route_id/:direction_id", ScheduleFinderLive
     end
   end
