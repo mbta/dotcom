324efedd3381807a192d6eb9973f8d1820776e38
Josh Larson <jlarson@mbta.com>
2025-12-19 13:32:40 -0500

    tests: Validate that predictions override schedules

--- test/dotcom/schedule_finder/upcoming_departures_test.exs
+++ test/dotcom/schedule_finder/upcoming_departures_test.exs
@@ -126,6 +126,70 @@ defmodule Dotcom.ScheduleFinder.UpcomingDeparturesTest do
              ]
     end
 
+    test "favors prediction over schedules if both are present" do
+      # Setup
+      now = Dotcom.Utils.DateTime.now()
+
+      route = Factories.Routes.Route.build(Faker.Util.pick([:bus_route, :commuter_rail_route]))
+      route_id = route.id
+      stop_id = FactoryHelpers.build(:id)
+      direction_id = Faker.Util.pick([0, 1])
+
+      trip_id = FactoryHelpers.build(:id)
+
+      predicted_seconds_until_arrival = Faker.random_between(2 * 60, 59 * 60)
+      predicted_arrival_time = now |> DateTime.shift(second: predicted_seconds_until_arrival)
+
+      scheduled_arrival_time =
+        Generators.DateTime.random_time_range_date_time(
+          {predicted_arrival_time, ServiceDateTime.end_of_service_day(now)}
+        )
+
+      expect(Predictions.Repo.Mock, :all, 2, fn [
+                                                  route: ^route_id,
+                                                  direction_id: ^direction_id,
+                                                  include_terminals: true
+                                                ] ->
+        [
+          Factories.Predictions.Prediction.build(:prediction,
+            arrival_time: predicted_arrival_time,
+            stop: Factories.Stops.Stop.build(:stop, id: stop_id),
+            trip: Factories.Schedules.Trip.build(:trip, id: trip_id)
+          )
+        ]
+      end)
+
+      expect(Schedules.Repo.Mock, :by_route_ids, fn [^route_id],
+                                                    stop_ids: ^stop_id,
+                                                    direction_id: ^direction_id,
+                                                    date: date ->
+        assert date == ServiceDateTime.service_date(now)
+
+        [
+          Factories.Schedules.Schedule.build(:schedule,
+            arrival_time: scheduled_arrival_time,
+            time: scheduled_arrival_time,
+            stop: Factories.Stops.Stop.build(:stop, id: stop_id),
+            trip: Factories.Schedules.Trip.build(:trip, id: trip_id)
+          )
+        ]
+      end)
+
+      # Exercise
+      departures =
+        UpcomingDepartures.upcoming_departures(%{
+          direction_id: direction_id,
+          now: now,
+          route: route,
+          stop_id: stop_id
+        })
+
+      # Verify
+      assert departures |> Enum.map(& &1.arrival_status) == [
+               {:arrival_seconds, predicted_seconds_until_arrival}
+             ]
+    end
+
     test "does not include scheduled trips for subway" do
       # Setup
       now = Dotcom.Utils.DateTime.now()
