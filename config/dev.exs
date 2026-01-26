71910010b78f20245f7ed47106046d66af55ab70
Cristen Jones <cjones3@mbta.com>
2026-01-26 10:42:08 -0500

    chore: phoenix plugs to initialize at runtime

--- config/dev.exs
+++ config/dev.exs
@@ -14,3 +14,6 @@ config :phoenix_live_view,
   debug_heex_annotations: true,
   debug_attributes: true,
   enable_expensive_runtime_checks: true
+
+# Initialize plugs at runtime for faster development compilation
+config :phoenix, :plug_init_mode, :runtime
