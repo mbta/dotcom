cffd0af86dfb81bf9f5664012095a52e617a783a
Josh Larson <jlarson@mbta.com>
2026-02-10 14:46:13 -0500

    feedback: Use `beginning_of_year`/`end_of_year` instead of hard-coded dates

--- test/support/generators/date.ex
+++ test/support/generators/date.ex
@@ -3,12 +3,16 @@ defmodule Test.Support.Generators.Date do
   Factories to help generate/evaluate dates for testing.
   """
 
+  @date_time_module Application.compile_env!(:dotcom, :date_time_module)
+
   @doc "Generate a random date this decade"
   def random_date() do
-    beginning_of_time = ~N[2020-01-01 00:00:00]
-    end_of_time = ~N[2029-12-31 23:59:59]
+    now = @date_time_module.now()
 
-    date_generator_between({beginning_of_time, end_of_time}) |> Enum.take(1) |> List.first()
+    beginning_of_year = Date.new!(now.year, 1, 1)
+    end_of_year = Date.new!(now.year + 1, 1, 1)
+
+    date_generator_between({beginning_of_year, end_of_year}) |> Enum.take(1) |> List.first()
   end
 
   def date_generator_between({start, stop}) do
