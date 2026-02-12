074b68493a3036de497e04a6e79b963e9fcad1a2
Luc Vachon <lvachon1@mbta.com>
2026-02-12 15:04:43 -0500

    Removed empty line

--- test/dotcom_web/controllers/alert_controller_test.exs
+++ test/dotcom_web/controllers/alert_controller_test.exs
@@ -113,7 +113,6 @@ defmodule DotcomWeb.AlertControllerTest do
       rand_timeframe =  Faker.Util.pick(["current", "upcoming", "past"])
       alerts = get(conn, alert_path(conn, :show, :bus, alerts_timeframe: rand_timeframe))
       refute alerts |> Map.has_key?(:alerts_timeframe)
-
     end
   end
 
