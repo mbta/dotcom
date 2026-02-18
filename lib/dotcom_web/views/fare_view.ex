d8bfe621d08f3d0330a0ec4bd362c55c24393a58
Lucy Tan <ltan@mbta.com>
2026-02-18 11:04:16 -0500

    Also update cta link

--- lib/dotcom_web/views/fare_view.ex
+++ lib/dotcom_web/views/fare_view.ex
@@ -124,7 +124,7 @@ defmodule DotcomWeb.FareView do
     teaser =
       CMS.Repo.get_paragraph("/paragraphs/content-list/charliecard", conn.query_params)
       |> ContentList.fetch_teasers()
-    %{teaser | right_rail: false, header: nil} # Overridden to match styling on page
+    %{teaser | right_rail: false, header: nil, cta: %{behavior: "hide"}} # Overridden to match styling on page
     |> render_paragraph(conn)
   end
 end
