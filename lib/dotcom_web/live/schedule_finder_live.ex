b0b7c2f090985f3b19b30f52e78c029cb7f5de12
Luc Vachon <lvachon1@mbta.com>
2026-03-04 16:21:53 -0500

    Style guides recommend italics for ship names

--- lib/dotcom_web/live/schedule_finder_live.ex
+++ lib/dotcom_web/live/schedule_finder_live.ex
@@ -773,7 +773,7 @@ defmodule DotcomWeb.ScheduleFinderLive do
       </:additional_info>
 
       <:additional_info :if={@upcoming_departure.trip_details.vehicle_info.vehicle_name}>
-        {@upcoming_departure.trip_details.vehicle_info.vehicle_name}
+        <i>{@upcoming_departure.trip_details.vehicle_info.vehicle_name}</i>
       </:additional_info>
 
       <:time>
