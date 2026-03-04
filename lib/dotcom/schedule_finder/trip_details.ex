35bc25d0b11c326df4514d523439080273e068d1
Luc Vachon <lvachon1@mbta.com>
2026-03-04 15:00:55 -0500

    Found a cleaner way to get the mode

--- lib/dotcom/schedule_finder/trip_details.ex
+++ lib/dotcom/schedule_finder/trip_details.ex
@@ -89,30 +89,15 @@ defmodule Dotcom.ScheduleFinder.TripDetails do
     vehicle_id = Map.get(vehicle || %{}, :id, nil)
 
     mode =
-      if Enum.count(predicted_schedules) > 0 do
-        schedule = predicted_schedules |> Enum.at(0) |> Map.get(:schedule)
-        prediction = predicted_schedules |> Enum.at(0) |> Map.get(:prediction)
-
-        if(schedule) do
-          schedule
-          |> Map.get(:route)
-          |> Route.type_atom()
-        else
-          if(prediction) do
-            prediction
-            |> Map.get(:route)
-            |> Route.type_atom()
-          else
-            nil
-          end
-        end
+      if vehicle do
+        vehicle |> Map.get(:route_id) |> Routes.Repo.get() |> Route.type_atom()
       else
         nil
       end
 
     # Only add vehicle names for ferries (for now?)
     vehicle_info =
-      if !is_nil(vehicle) && mode == :ferry do
+      if mode == :ferry do
         vehicle_info(vehicle, predicted_schedules)
         |> Map.put(:vehicle_name, boat_name(vehicle_id))
       else
