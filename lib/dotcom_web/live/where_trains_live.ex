5f2fabc236d778ce01e9415108385b30213e7018
Luc Vachon <lvachon1@mbta.com>
2026-03-06 13:20:35 -0500

    Now it's on a map with icons

--- lib/dotcom_web/live/where_trains_live.ex
+++ lib/dotcom_web/live/where_trains_live.ex
@@ -1,14 +1,12 @@
 defmodule DotcomWeb.WhereTrainsLive do
-  alias DotcomWeb.WorldCupTimetable
   use DotcomWeb, :live_view
-  import CSSHelpers
-  alias Vehicles.Vehicle
 
-  @vehicles_repo Application.compile_env!(:dotcom, :repo_modules)[:vehicles]
+  @map_config Application.compile_env(:mbta_metro, :map)
+  @routes_repo Application.compile_env!(:dotcom, :repo_modules)[:routes]
 
   @impl LiveView
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(:map_config, @map_config)}
   end
 
   @impl LiveView
@@ -17,17 +15,40 @@ defmodule DotcomWeb.WhereTrainsLive do
     assigns = assigns |> assign(:vehicles, vehicles)
 
     ~H"""
-    <div class="container">
-      <.show_vehicle :for={vehicle <- @vehicles} assigns={assigns} vehicle={vehicle} />
-    </div>
+    <.live_component
+      module={MbtaMetro.Live.Map}
+      id="train-page-map"
+      class="h-[32rem] w-full"
+      config={@map_config}
+      icons={
+        @vehicles
+        |> Enum.map(
+          &%{
+            class: "size-5 cursor-pointer",
+            coordinates: [&1.longitude, &1.latitude],
+            name: icon_name(&1),
+            type: "system",
+            title: &1.id <> " on " <> &1.route_id
+          }
+        )
+      }
+    />
     """
   end
 
-  def show_vehicle(assigns) do
-    ~H"""
-    <div>
-      {@vehicle.id} serving {@vehicle.route_id} is at {@vehicle.longitude}, {@vehicle.latitude}
-    </div>
-    """
+  def icon_name(%{route_id: route_id}) do
+    mode = route_id |> @routes_repo.get() |> Routes.Route.type_atom()
+
+    case mode do
+      :commuter_rail -> "mode-commuter-rail-small"
+      :ferry -> "mode-ferry-small"
+      :subway -> "mode-subway-small"
+      :bus -> "mode-bus-small"
+      _ -> "mode-bus-small"
+    end
+  end
+
+  def icon_name(_) do
+    "mode-bus-small"
   end
 end
