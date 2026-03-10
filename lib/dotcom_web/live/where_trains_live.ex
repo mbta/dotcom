581494019438415512b47d740ed7b734b2a9e2e2
Luc Vachon <lvachon1@mbta.com>
2026-03-10 12:28:23 -0400

    Added mode filters

--- lib/dotcom_web/live/where_trains_live.ex
+++ lib/dotcom_web/live/where_trains_live.ex
@@ -5,16 +5,45 @@ defmodule DotcomWeb.WhereTrainsLive do
   @routes_repo Application.compile_env!(:dotcom, :repo_modules)[:routes]
 
   @impl LiveView
-  def mount(_params, _session, socket) do
-    {:ok, socket |> assign(:map_config, @map_config)}
+  def mount(params, _session, socket) do
+    show_bus = Map.get(params, "bus", "no") == "yes"
+    show_subway = Map.get(params, "subway", "no") == "yes"
+
+    {:ok,
+     socket
+     |> assign(:map_config, @map_config)
+     |> assign(:show_bus, show_bus)
+     |> assign(:show_subway, show_subway)}
   end
 
   @impl LiveView
   def render(assigns) do
-    vehicles = Vehicles.Repo.all()
+    vehicles =
+      Vehicles.Repo.all()
+      |> Enum.filter(fn %{route_id: route_id} ->
+        assigns.show_bus ||
+          (route_id |> @routes_repo.get() |> Routes.Route.type_atom() != :bus &&
+             (assigns.show_subway ||
+                route_id |> @routes_repo.get() |> Routes.Route.type_atom() != :subway))
+      end)
+
     assigns = assigns |> assign(:vehicles, vehicles)
 
     ~H"""
+    <div>
+      <a :if={!@show_bus} href={param_link(true, @show_subway)} title="Show busses">
+        Show busses
+      </a>
+      <a :if={@show_bus} href={param_link(false, @show_subway)} title="Show busses">
+        Hide busses
+      </a>
+      <a :if={!@show_subway} href={param_link(@show_bus, true)} title="Show subway">
+        Show subway
+      </a>
+      <a :if={@show_subway} href={param_link(@show_bus, false)} title="Show subway">
+        Hide subway
+      </a>
+    </div>
     <.live_component
       module={MbtaMetro.Live.Map}
       id="train-page-map"
@@ -34,6 +63,13 @@ defmodule DotcomWeb.WhereTrainsLive do
       }
     />
     """
+  end
+
+  def param_link(show_busses, show_subway) do
+    ~p"/preview/where-trains/?bus=" <>
+      if show_busses,
+        do: "yes",
+        else: "no" <> "&subway=" <> if(show_subway, do: "yes", else: "no")
   end
 
   def icon_name(%{route_id: route_id}) do
