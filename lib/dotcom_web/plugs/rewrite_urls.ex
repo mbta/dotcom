3d4db496dc21c6fb278beccc1f3222a12ae4f9cc
Luc Vachon <lvachon1@mbta.com>
2026-03-18 09:50:40 -0400

    Fixed redirect logic for "Green" line special case, cleaned up a bit of code.

--- lib/dotcom_web/plugs/rewrite_urls.ex
+++ lib/dotcom_web/plugs/rewrite_urls.ex
@@ -26,16 +26,24 @@ defmodule DotcomWeb.Plugs.RewriteUrls do
   end
 
   # Send SF1.0 URLS to the new SF2.0
-  defp rewrite_url(
-         %{
-           path_info: ["schedules", _, "line"],
-           params: %{
-             "route" => route_id,
-             "schedule_finder" => %{"direction_id" => direction_id, "origin" => stop_id}
-           }
-         } = conn
-       ) do
+  defp rewrite_url(%{
+         path_info: ["schedules", _, "line"],
+         params: %{
+           "route" => route_id,
+           "schedule_finder" => %{"direction_id" => direction_id, "origin" => stop_id}
+         }
+       }) do
     "/departures/?route_id=#{route_id}&direction_id=#{direction_id}&stop_id=#{stop_id}"
+  end
+
+  # "Green" line special case
+  defp rewrite_url(%{
+         path_info: ["schedules", "Green", "line"],
+         params: %{
+           "schedule_finder" => %{"direction_id" => direction_id, "origin" => stop_id}
+         }
+       }) do
+    "/departures/?route_id=Green&direction_id=#{direction_id}&stop_id=#{stop_id}"
   end
 
   defp rewrite_url(%{path_info: ["schedules", "Boat-F3" | _]} = conn) do
