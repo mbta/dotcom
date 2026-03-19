2c2446cbea6ad45a8cee53c76e6dcaee54ba6b91
Luc Vachon <lvachon1@mbta.com>
2026-03-19 10:23:10 -0400

    Initial color and text change commit, I need to tweak the colors though.

--- lib/dotcom_web/live/schedule_finder_live.ex
+++ lib/dotcom_web/live/schedule_finder_live.ex
@@ -778,14 +778,14 @@ defmodule DotcomWeb.ScheduleFinderLive do
               :if={Enum.count(upcoming_departure.trip_details.stops_before) > 0}
               class="group/details"
             >
-              <summary class="cursor-pointer">
+              <summary class="cursor-pointer bg-gray-lightest">
                 <.lined_list_item route={upcoming_departure.route} variant="none">
                   <div class="grow">
                     <span class="text-[0.75rem] underline group-open/details:hidden">
                       {~t"Show more stops"}
                     </span>
                     <span class="text-[0.75rem] underline hidden group-open/details:block">
-                      {~t"Hide more stops"}
+                      {~t"Show fewer stops"}
                     </span>
                   </div>
                   <div class="shrink-0">
@@ -795,7 +795,7 @@ defmodule DotcomWeb.ScheduleFinderLive do
               </summary>
               <.other_stop
                 :for={other_stop <- upcoming_departure.trip_details.stops_before}
-                class="border-t-xs border-gray-lightest"
+                class="border-t-xs border-gray-lighter bg-gray-lightest"
                 other_stop={other_stop}
                 route={upcoming_departure.route}
               />
