213ab669eb8d597adde22c98e12dd89c1fbc3359
Luc Vachon <lvachon1@mbta.com>
2026-03-26 12:56:59 -0400

    Refactored time format from ISO8601 in the wrong timezone to local date and time objects, updated a few functions to handle the new field and the new format.

--- lib/dotcom_web/components/world_cup_timetable/match_link.ex
+++ lib/dotcom_web/components/world_cup_timetable/match_link.ex
@@ -29,9 +29,9 @@ defmodule DotcomWeb.WorldCupTimetable.MatchLink do
     ~H"""
     <.link
       class="w-full p-sm rounded-lg border-xs border-charcoal-70 no-underline hover:bg-brand-primary-lightest text-black"
-      patch={~p"/preview/schedules/CR-WorldCup?#{[date: @date]}"}
+      patch={~p"/preview/schedules/CR-WorldCup?#{[date: Date.to_string(@date)]}"}
     >
-      <div class="font-bold">{@label} ({formatted_datetime(@date)})</div>
+      <div class="font-bold">{@label} ({formatted_datetime(@date, @time)})</div>
       <.teams selected={false} teams={@teams} />
     </.link>
     """
@@ -40,18 +40,16 @@ defmodule DotcomWeb.WorldCupTimetable.MatchLink do
   def selected_match_banner(assigns) do
     ~H"""
     <div class="w-full p-sm rounded-lg border-xs border-charcoal-70 no-underline bg-brand-primary text-white justify-between">
-      <div class="font-bold">{@label} ({formatted_datetime(@date)})</div>
+      <div class="font-bold">{@label} ({formatted_datetime(@date, @time)})</div>
       <.teams selected teams={@teams} />
     </div>
     """
   end
 
-  defp formatted_datetime(timestring) do
-    datetime = timestring |> DateTime.from_iso8601() |> Kernel.elem(1)
-
+  defp formatted_datetime(date, time) do
     gettext("%{date} at %{time}",
-      date: datetime |> Dotcom.Utils.Time.format!(:month_day_short),
-      time: datetime |> Util.narrow_time()
+      date: date |> Dotcom.Utils.Time.format!(:month_day_short),
+      time: time |> Util.narrow_time()
     )
   end
 
