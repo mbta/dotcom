55d1c8659eed221e7101164bf610f012f957eb50
Luc Vachon <lvachon1@mbta.com>
2026-04-09 15:42:16 -0400

    Updated homepage tabs to be accessible

--- assets/js/tabbed-nav.js
+++ assets/js/tabbed-nav.js
@@ -1,16 +1,25 @@
-const updateTabs = (tabType, navTabs, contentTabs) => e => {
+const updateTabs = (tabType, navTabs, contentTabs, updateContent=true) => e => {
+
   e.preventDefault();
+  e.stopPropagation();
 
   navTabs.forEach(tab => {
     const clickedTab = tab.dataset.tabType === tabType;
     tab.classList.toggle("active", clickedTab);
     tab.setAttribute("aria-selected", clickedTab);
+    tab.removeAttribute("aria-current")
+    if(clickedTab){
+      if(updateContent){tab.setAttribute("aria-current","page");}
+      tab.focus();
+    }
   });
 
-  contentTabs.forEach(tab => {
-    const clickedTabContentType = tab.dataset.tabContentType === tabType;
-    tab.classList.toggle("active", clickedTabContentType);
-  });
+  if(updateContent){
+    contentTabs.forEach(tab => {
+      const clickedTabContentType = tab.dataset.tabContentType === tabType;
+      tab.classList.toggle("active", clickedTabContentType);
+    });
+  }
 };
 
 const tabbedNavSetup = () => {
@@ -22,9 +31,19 @@ const tabbedNavSetup = () => {
     const callback = updateTabs(tabType, navTabs, contentTabs);
 
     item.addEventListener("click", callback);
-    item.addEventListener("keyup", e => {
+    item.addEventListener("keydown", e => {
       if (e.key === "Enter") {
         callback(e);
+      }
+      const selectedTab = Array.from(navTabs).findIndex(tab=>{tab.getAttribute("aria-selected")=="true"});
+      if(e.key === "ArrowLeft" && selectedTab > 0){
+        updateTabs(navTabs[selectedTab-1].dataset.tabType, navTabs, contentTabs, false)(e);
+      }
+      if(e.key === "ArrowRight" && selectedTab < navTabs.length - 1){
+        updateTabs(navTabs[selectedTab+1].dataset.tabType, navTabs, contentTabs, false)(e);
+      }
+      if(e.key === " "){
+        updateTabs(navTabs[selectedTab].dataset.tabType, navTabs, contentTabs, true)(e);
       }
     });
   });
