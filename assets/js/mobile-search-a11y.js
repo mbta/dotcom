6f5ee38180eecf47fa937efe47b595c74978c83e
Luc Vachon <lvachon1@mbta.com>
2026-04-29 15:47:50 -0400

    lint

--- assets/js/mobile-search-a11y.js
+++ assets/js/mobile-search-a11y.js
@@ -1,12 +1,11 @@
 export default () => {
-  const doResize = ()=>{
+  const doResize = () => {
     const mobileSearch = document.getElementsByClassName("m-menu__search")[0];
     const desktopSearch = document.getElementsByClassName("search-wrapper")[0];
 
-    mobileSearch.ariaHidden = (window.innerWidth > 543);
-    desktopSearch.ariaHidden = (window.innerWidth < 544);
-  }
-  window.addEventListener("resize", ()=>doResize());
-  window.addEventListener("load", ()=>doResize());
-  
-}
+    mobileSearch.ariaHidden = window.innerWidth > 543;
+    desktopSearch.ariaHidden = window.innerWidth < 544;
+  };
+  window.addEventListener("resize", () => doResize());
+  window.addEventListener("load", () => doResize());
+};
