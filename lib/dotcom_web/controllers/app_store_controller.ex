100d1d52092f801aa0e32376d8453f7d33460491
Emma Simon <esimon@mbta.com>
2026-04-29 11:12:15 -0400

    feat: Add app store redirects for mticket

--- lib/dotcom_web/controllers/app_store_controller.ex
+++ lib/dotcom_web/controllers/app_store_controller.ex
@@ -5,28 +5,38 @@ defmodule DotcomWeb.AppStoreController do
   """
   use DotcomWeb, :controller
 
-  @android_store_base_url "https://play.google.com/store/apps/details?id=com.mbta.tid.mbta_app"
-  @ios_store_base_url "https://apps.apple.com/app/apple-store/id6472726821"
-  @default_project_page "/goapp"
+  @go_android_store_base_url "https://play.google.com/store/apps/details?id=com.mbta.tid.mbta_app"
+  @go_ios_store_base_url "https://apps.apple.com/app/apple-store/id6472726821"
+  @go_default_project_page "/goapp"
+
+  @mticket_android_store_base_url "https://play.google.com/store/apps/details?id=com.mbta.mobileapp"
+  @mticket_ios_store_base_url "https://apps.apple.com/us/app/apple-store/id560487958"
+  @mticket_default_project_page "/#TODO"
 
   def redirect_mbta_go(conn, params) do
     conn
-    |> redirect_to_app_store(params)
+    |> redirect_to_app_store(params, @go_ios_store_base_url, @go_android_store_base_url, @go_default_project_page)
     |> halt
   end
 
-  defp redirect_to_app_store(conn, params) do
+  def redirect_mticket(conn, params) do
+    conn
+    |> redirect_to_app_store(params, @mticket_ios_store_base_url, @mticket_android_store_base_url, @mticket_default_project_page)
+    |> halt
+  end
+
+  defp redirect_to_app_store(conn, params, ios_url, android_url, desktop_url) do
     cond do
       Browser.ios?(conn) ->
         redirect(conn,
-          external: campaign_url(@ios_store_base_url, ios_params(params))
+          external: campaign_url(ios_url, ios_params(params))
         )
 
       Browser.android?(conn) ->
-        redirect(conn, external: campaign_url(@android_store_base_url, params))
+        redirect(conn, external: campaign_url(android_url, params))
 
       true ->
-        redirect(conn, to: campaign_url(@default_project_page, params))
+        redirect(conn, to: campaign_url(desktop_url, params))
     end
   end
 
--- test/dotcom_web/controllers/app_store_controller_test.exs
+++ test/dotcom_web/controllers/app_store_controller_test.exs
@@ -2,9 +2,7 @@ defmodule DotcomWeb.AppStoreControllerTest do
   use DotcomWeb.ConnCase, async: true
   import Test.Support.EnvHelpers
 
-  test "redirects to default project page by default, preserving params", %{conn: conn} do
-    reassign_env(:dotcom, :mbta_go_app, default_project_page: "/default_project_page")
-
+  test "redirects to default mbta go project page by default, preserving params", %{conn: conn} do
     conn =
       get(
         conn,
@@ -16,7 +14,7 @@ defmodule DotcomWeb.AppStoreControllerTest do
     assert redirected_to(conn, 302) =~ "/goapp?param_1=val_1"
   end
 
-  test "redirects to app store for ios browser", %{conn: conn} do
+  test "redirects to mbta go app store for ios browser", %{conn: conn} do
     conn =
       conn
       |> put_req_header(
@@ -37,7 +35,7 @@ defmodule DotcomWeb.AppStoreControllerTest do
     assert redirected_to =~ "?pt=fake%20pt&ct=fake%20ct&mt=fake-mt"
   end
 
-  test "redirects to app store for android browser", %{conn: conn} do
+  test "redirects to mbta go app store for android browser", %{conn: conn} do
     conn =
       conn
       |> put_req_header(
@@ -46,6 +44,61 @@ defmodule DotcomWeb.AppStoreControllerTest do
       )
       |> get(
         app_store_path(conn, :redirect_mbta_go, %{
+          "referrer" => "fake referrer",
+          "utm_source" => "fake utm source",
+          "utm_campaign" => "fake-utm-campaign"
+        })
+      )
+
+    redirected_to = redirected_to(conn, 302)
+    assert redirected_to =~ "https://play.google.com"
+
+    assert redirected_to =~
+             "&referrer=fake%20referrer&utm_campaign=fake-utm-campaign&utm_source=fake%20utm%20source"
+  end
+
+  test "redirects to default mticket project page by default, preserving params", %{conn: conn} do
+    conn =
+      get(
+        conn,
+        app_store_path(conn, :redirect_mticket, %{
+          "param_1" => "val_1"
+        })
+      )
+
+    assert redirected_to(conn, 302) =~ "/#TODO?param_1=val_1"
+  end
+
+  test "redirects to mticket app store for ios browser", %{conn: conn} do
+    conn =
+      conn
+      |> put_req_header(
+        "user-agent",
+        "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3"
+      )
+      |> get(
+        app_store_path(conn, :redirect_mticket, %{
+          "mt" => "fake-mt",
+          "ct" => "fake ct",
+          "pt" => "fake pt",
+          "extra" => "other param"
+        })
+      )
+
+    redirected_to = redirected_to(conn, 302)
+    assert redirected_to =~ "https://apps.apple.com"
+    assert redirected_to =~ "?pt=fake%20pt&ct=fake%20ct&mt=fake-mt"
+  end
+
+  test "redirects to mticket app store for android browser", %{conn: conn} do
+    conn =
+      conn
+      |> put_req_header(
+        "user-agent",
+        "Android SDK 1.5r3: Mozilla/5.0 (Linux; U; Android 1.5; de-; sdk Build/CUPCAKE) AppleWebkit/528.5+ (KHTML, like Gecko) Version/3.1.2 Mobile Safari/525.20.1"
+      )
+      |> get(
+        app_store_path(conn, :redirect_mticket, %{
           "referrer" => "fake referrer",
           "utm_source" => "fake utm source",
           "utm_campaign" => "fake-utm-campaign"
