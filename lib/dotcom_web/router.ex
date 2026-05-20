87550fa52c4919bcea59b850a4ca58d5e9bd5456
Luc Vachon <lvachon1@mbta.com>
2026-05-20 13:35:52 -0400

    Ferry [fix]: Add redirect from F1 to F2H pages (#3200)
    * Added a redirect from Boat-F1 /schedule/ pages to Boat-F2H timetable
    
    * Update lib/dotcom_web/router.ex
    
    Co-authored-by: Josh Larson <jlarson@mbta.com>
    
    ---------
    
    Co-authored-by: Josh Larson <jlarson@mbta.com>

--- lib/dotcom_web/router.ex
+++ lib/dotcom_web/router.ex
@@ -206,6 +206,9 @@ defmodule DotcomWeb.Router do
     # Redirect Foxboro line to World Cup Timetable Page for the World Cup (revert this later)
     get("/schedules/CR-Foxboro/*path_params", Redirector, to: "/schedules/bostonstadium")
 
+    # Redirect Boat-F1 to Boat-F2H until Boat-F1 can be unlisted
+    get("/schedules/Boat-F1/*path_params", Plugs.PathParamsRedirector, to: "/schedules/Boat-F2H")
+
     get("/", PageController, :index)
     get("/menu", PageController, :menu)
 
