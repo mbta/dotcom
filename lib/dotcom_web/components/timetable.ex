3814f4d6a8ebed484dddc5c1fe2cac44e2135a0a
Josh Larson <jlarson@mbta.com>
2026-06-03 09:55:33 -0400

    refactor: Split `<.timetable />` and `<.linear_timetable />` into separate components

--- lib/dotcom_web/components/timetable.ex
+++ lib/dotcom_web/components/timetable.ex
@@ -13,6 +13,170 @@ defmodule DotcomWeb.Components.Timetable do
   import DotcomWeb.ViewHelpers, only: [break_text_at_slash: 1, format_schedule_time: 1, svg: 1]
   import MbtaMetro.Components.Icon
 
+  def linear_timetable(assigns) do
+    ~H"""
+    <div id="timetable" class="m-timetable__table-container" data-sticky-container>
+      <table
+        class="m-timetable__table"
+        aria-label={"#{@direction_name} #{~t(timetable for)} #{@route.name}, #{@formatted_date}"}
+      >
+        <tr class="js-timetable-train-labels">
+          <th
+            scope="col"
+            class="m-timetable__cell m-timetable__cell--gray m-timetable__cell--first-column m-timetable__cell--first-column-header"
+            data-absolute
+          >
+            <div class="m-timetable__row-header">{~t(Stops)}</div>
+          </th>
+          <td class="hidden-no-js">
+            <div class="m-timetable__row-header"></div>
+          </td>
+          <th
+            :for={{schedule, index} <- Enum.with_index(@header_schedules)}
+            class="m-timetable__header-cell"
+            data-scroll-to={index == @offset}
+            scope="col"
+          >
+            <span class="sr-only left-0">{~t(Trip)}</span>
+            <span>{schedule.trip.name}</span>
+          </th>
+        </tr>
+
+        <tr :if={!ferry?(@route)}>
+          <th
+            scope="row"
+            class="m-timetable__cell m-timetable__cell--gray m-timetable__cell--first-column m-timetable__cell--first-column-header m-timetable__bike-icon-spacer"
+            data-absolute
+          >
+            <div class="m-timetable__row-header">
+              <span class="sr-only">{~t(Bicycles Allowed?)}</span>
+              {# this icon is set to visibility: hidden by CSS;
+              # it's needed to keep the absolutely positioned
+              # header cell height consistent with the cells
+              # that have icons in them.
+              svg("icon-bikes-default.svg")}
+            </div>
+          </th>
+          <td class="hidden-no-js">
+            <div class="m-timetable__row-header"></div>
+          </td>
+
+          <td :for={schedule <- @header_schedules} class="m-timetable__header-cell">
+            <%= if get_in(schedule, [Access.key(:trip, %{}), Access.key(:bikes_allowed?, false)]) do %>
+              <span
+                aria-hidden="true"
+                class="bicycles-allowed-icon"
+                data-toggle="tooltip"
+                title
+                data-original-title={~t"Bicycles allowed"}
+              >
+                {svg("icon-bikes-default.svg")}
+              </span>
+              <div class="sr-only">{~t(Bicycles allowed)}</div>
+            <% else %>
+              <div class="sr-only">{~t(Bicycles not allowed)}</div>
+            <% end %>
+            <.tooltip
+              :if={!is_nil(get_in(schedule, [Access.key(:trip, %{}), Access.key(:occupancy, nil)]))}
+              title={timetable_crowding_description(schedule.trip.occupancy)}
+              placement={:top}
+            >
+              <.icon
+                type="icon-svg"
+                name="icon-crowding"
+                class={"c-svg__icon c-icon__crowding c-icon__crowding--#{schedule.trip.occupancy} monochrome"}
+                aria-hidden="true"
+              />
+            </.tooltip>
+          </td>
+        </tr>
+
+        <%= for {stop, idx} <- @header_stops do %>
+          <% cell_background =
+            if rem(idx, 2) == 0 do
+              "white"
+            else
+              "gray"
+            end %>
+          <tr class={stop_row_class(idx)}>
+            <th
+              scope="row"
+              data-absolute
+              class={[
+                "js-tt-stop-name m-timetable__cell--first-column",
+                "m-timetable__cell m-timetable__cell--#{cell_background}"
+              ]}
+            >
+              <div class="m-timetable__row-header m-timetable__stop-name notranslate">
+                <.link navigate={~p"/stops/#{stop.id}"} class="m-timetable__stop-link">
+                  {break_text_at_slash(stop.name)}
+                </.link>
+                <div class="m-timetable__stop-icons">
+                  <%= if length(stop.parking_lots) > 0 do %>
+                    <.tooltip title={~t(Parking available)} placement={:top}>
+                      <.icon
+                        name="square-parking"
+                        class="size-4 fill-gray-light"
+                        aria-hidden="true"
+                      />
+                    </.tooltip>
+                  <% else %>
+                    <span class="sr-only">{~t(No parking)}</span>
+                  <% end %>
+
+                  <%= if Stops.Stop.accessible?(stop) do %>
+                    <.tooltip title={~t(Accessible)} placement={:top}>
+                      <.icon
+                        type="icon-svg"
+                        name="icon-accessible-default"
+                        class="size-4 fill-brand-primary"
+                        aria-hidden="true"
+                      />
+                    </.tooltip>
+                  <% else %>
+                    <%= if Stops.Stop.accessibility_known?(stop) do %>
+                      <span class="sr-only">{~t(Not accessible)}</span>
+                    <% else %>
+                      <span class="sr-only">{~t(May not be accessible)}</span>
+                    <% end %>
+                  <% end %>
+
+                  <%= if Alerts.Stop.match(@alerts, stop.id, route: @route.id, time: @date, direction_id: @direction_id) != [] do %>
+                    <.tooltip title={~t(Service alert or delay)} placement={:top}>
+                      <.icon name="triangle-exclamation" class="h-4 w-4" aria-hidden="true" />
+                    </.tooltip>
+                  <% end %>
+                </div>
+              </div>
+            </th>
+            <td class="js-tt-cell hidden-no-js" style="padding-left: 0.5rem">
+              <div class="m-timetable__row-header"></div>
+            </td>
+            <%= if !@linear_timetable? do %>
+              <td
+                :for={trip <- Enum.at(@timetable_schedules, idx)}
+                class="js-tt-cell m-timetable__cell px-lg"
+              >
+                {trip.time}
+              </td>
+            <% end %>
+            <%= for schedule <- @header_schedules, @linear_timetable? do %>
+              <.timetable_cell_wrapper
+                id={"#{stop.name}-#{schedule.trip.id}-tooltip"}
+                stop={stop}
+                trip_schedule={@trip_schedules[{schedule.trip.id, stop.id}]}
+                track_change={@track_changes[{schedule.trip.id, stop.id}]}
+                full_trip_message={@trip_messages[{schedule.trip.name}]}
+                trip_message={@trip_messages[{schedule.trip.name, stop.id}]}
+              />
+            <% end %>
+          </tr>
+        <% end %>
+      </table>
+    </div>
+    """
+  end
+
   def timetable(assigns) do
     ~H"""
     <div id="timetable" class="m-timetable__table-container" data-sticky-container>
