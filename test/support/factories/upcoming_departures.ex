da8ebc0623db694758cd3d4808d380feed88656b
Cristen Jones <cjones3@mbta.com>
2026-06-04 10:51:59 -0400

    improve the upcoming departure factory

--- test/support/factories/upcoming_departures.ex
+++ test/support/factories/upcoming_departures.ex
@@ -1,13 +1,19 @@
 defmodule Test.Support.Factories.UpcomingDepartures do
   use ExMachina
 
-  alias Test.Support.Factories
+  alias Test.Support.{Factories, FactoryHelpers}
 
   def upcoming_departure_factory do
     %Dotcom.UpcomingDepartures.UpcomingDeparture{
       arrival_status: {:arrival_minutes, Faker.Util.pick(1..60)},
+      crowding: Faker.Util.pick([:not_crowded, :some_crowding, :crowded]),
+      headsign: Faker.Address.city(),
+      last_trip?: false,
       route: Factories.Routes.Route.build(:route),
-      time: sequence(:time, &DateTime.from_unix!/1)
+      stop_sequence: Faker.Util.pick(0..200),
+      time: sequence(:time, &DateTime.from_unix!/1),
+      trip_id: FactoryHelpers.build(:id),
+      trip_name: Faker.Cat.name()
     }
   end
 end
