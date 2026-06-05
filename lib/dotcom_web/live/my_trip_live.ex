c3b4ae8131d6d2aea5c1ea411f72b4ec1a44853f
Luc Vachon <lvachon1@mbta.com>
2026-06-05 13:16:52 -0400

    Stuff

--- lib/dotcom_web/live/my_trip_live.ex
+++ lib/dotcom_web/live/my_trip_live.ex
@@ -4,17 +4,25 @@ defmodule DotcomWeb.MyTripLive do
   """
 
   use DotcomWeb, :live_view
-  @vehicle_repo Application.compile_env!(:dotcom, :repo_modules)[:vehicles]
+  @vehicles_repo Application.compile_env!(:dotcom, :repo_modules)[:vehicles]
   @routes_repo Application.compile_env!(:dotcom, :repo_modules)[:routes]
   # @stops_repo Application.compile_env!(:dotcom, :repo_modules)[:stops]
   @map_config Application.compile_env(:mbta_metro, :map)
 
   def mount(params, _session, socket) do
     trip_id = params |> Map.get("trip_id")
-    vehicle = @vehicle_repo.trip(trip_id)
+    # vehicle = @vehicle_repo.trip(trip_id)
 
-    route_id = params |> Map.get("route_id")
+    route_id = params |> Map.get("route_id", "Red")
     route = @routes_repo.get(route_id)
+    direction_id = params |> Map.get("direction_id", "0") |> String.to_integer()
+
+    vehicles =
+      @vehicles_repo.route(route_id, direction_id: direction_id)
+      |> Enum.sort_by(fn v -> v.latitude end)
+
+    offset = params |> Map.get("offset", "0") |> String.to_integer()
+    vehicle = vehicles |> Enum.at(offset)
 
     {:ok,
      socket
@@ -22,12 +30,22 @@ defmodule DotcomWeb.MyTripLive do
      |> assign(:vehicle, vehicle)
      |> assign(:route_id, route_id)
      |> assign(:route, route)
+     |> assign(:direction_id, direction_id)
+     |> assign(:offset, offset)
      |> assign(:map_config, @map_config)}
   end
 
   def render(%{vehicle: vehicle} = assigns) when not is_nil(vehicle) do
     ~H"""
-    <div>{@vehicle.id}: {@vehicle.latitude}, {@vehicle.longitude} @ {@vehicle.bearing}</div>
+    <div>
+      <a href={"/preview/my-trip?route_id=#{@route_id}&direction_id=#{@direction_id}&offset=#{@offset-1}"}>
+        Prev
+      </a>
+      <span>{@vehicle.id}: {@vehicle.latitude}, {@vehicle.longitude} @ {@vehicle.bearing}</span>
+      <a href={"/preview/my-trip?route_id=#{@route_id}&direction_id=#{@direction_id}&offset=#{@offset+1}"}>
+        Next
+      </a>
+    </div>
     <.live_component
       module={MbtaMetro.Live.Map}
       id="my-trip-map"
