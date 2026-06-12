3c756bc368c5298d6b57d5dc71c83ebf9067792e
Paul Swartz <pswartz@mbta.com>
2026-06-11 21:11:25 -0400

    refactor: extract shared stats logic

--- lib/dotcom_web/stats.ex
+++ lib/dotcom_web/stats.ex
@@ -28,22 +28,9 @@ defmodule DotcomWeb.Stats do
     status = metadata.conn.status
     duration = measurement[:duration]
 
-    Agent.update(__MODULE__, &update_state(&1, method, path, status, duration))
-  end
-
-  defp update_state(state, method, path, status, duration) do
-    if Kernel.get_in(state, [method, path, status]) do
-      Kernel.update_in(state, [method, path, status], fn count_and_sum ->
-        {count, sum} = count_and_sum
-        {count + 1, sum + duration}
-      end)
-    else
-      Kernel.put_in(
-        state,
-        [Access.key(method, %{}), Access.key(path, %{}), status],
-        {1, duration}
-      )
-    end
+    Agent.update(__MODULE__, fn state ->
+      Telemetry.Stats.record_stat(state, method, path, status, duration)
+    end)
   end
 
   @doc """
@@ -52,35 +39,9 @@ defmodule DotcomWeb.Stats do
   Resets the Agent state after dispatching the stats.
   """
   def dispatch_stats do
-    Enum.each(Agent.get(__MODULE__, & &1), &dispatch_method/1)
+    Agent.get(__MODULE__, & &1)
+    |> Telemetry.Stats.dispatch_stats([:dotcom_web, :request], :method)
 
     Agent.update(__MODULE__, fn _ -> %{} end)
-  end
-
-  defp dispatch_method(entry) do
-    {method, stats} = entry
-
-    Enum.each(stats, fn path_and_statuses ->
-      {path, statuses} = path_and_statuses
-
-      Enum.each(statuses, fn status_and_count_sum ->
-        {status, count_and_sum} = status_and_count_sum
-        {count, sum} = count_and_sum
-        dispatch_stat(method, path, status, count, sum)
-      end)
-    end)
-  end
-
-  defp dispatch_stat(method, path, status, count, sum) do
-    avg =
-      sum
-      |> Kernel.div(count)
-      |> System.convert_time_unit(:native, :millisecond)
-
-    :telemetry.execute([:dotcom_web, :request], %{count: count, avg: avg}, %{
-      method: method,
-      path: path,
-      status: status
-    })
   end
 end
