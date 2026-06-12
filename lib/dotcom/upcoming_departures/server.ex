321269a16497080810270e9c0c0888f8d7f67733
Paul Swartz <pswartz@mbta.com>
2026-06-11 22:20:46 -0400

    fix: use correct name when looking up UpcomingDepartures server

--- lib/dotcom/upcoming_departures/server.ex
+++ lib/dotcom/upcoming_departures/server.ex
@@ -15,7 +15,11 @@ defmodule Dotcom.UpcomingDepartures.Server do
   @upcoming_departures_module Application.compile_env!(:dotcom, :upcoming_departures_module)
 
   def start_link(params) do
-    GenServer.start_link(__MODULE__, params, name: {:global, {__MODULE__, params}})
+    GenServer.start_link(__MODULE__, params, name: server_name(params))
+  end
+
+  def server_name(params) do
+    {:global, {__MODULE__, params}}
   end
 
   @impl GenServer
