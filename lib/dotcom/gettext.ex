970eef4b780c2e1b88cd0cc5ecbc4d1c04a59e0d
Lily Barrett <lbarrett1@administrators-MacBook-Pro.local>
2026-07-01 10:37:36 -0400

    Code cleanup
    WIP
    
    Revert changes to cldr
    
    See if this works for CI
    
    Check on every file change

--- lib/dotcom/gettext.ex
+++ lib/dotcom/gettext.ex
@@ -5,41 +5,9 @@ defmodule Dotcom.Gettext do
 
   use Gettext.Backend,
     default_domain: "dotcom",
-    # default_locale: Application.compile_env!(:dotcom, :default_locale_code),
-    default_locale: :en,
-    # locales: Application.compile_env!(:dotcom, :locale_codes),
-    locales: Dotcom.Locales.locale_codes(),
+    default_locale: Application.compile_env!(:dotcom, :default_locale_code),
+    locales: Application.compile_env!(:dotcom, :locale_codes),
     otp_app: :dotcom,
     plural_forms: Dotcom.Gettext.Plural,
     priv: "priv/gettext"
 end
-
-# defmodule MyCharlieWeb.Gettext do
-#   @moduledoc """
-#   A module providing Internationalization with a gettext-based API.
-
-#   By using [Gettext](https://hexdocs.pm/gettext),
-#   your module gains a set of macros for translations, for example:
-
-#       use Gettext, backend: MyCharlieWeb.Gettext
-
-#       # Simple translation
-#       gettext("Here is the string to translate")
-
-#       # Plural translation
-#       ngettext("Here is the string to translate",
-#                "Here are the strings to translate",
-#                3)
-
-#       # Domain-based translation
-#       dgettext("errors", "Here is the error message to translate")
-
-#   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
-#   """
-#   use Gettext.Backend,
-#     otp_app: :my_charlie,
-#     default_locale: :en,
-#     locales: MyCharlie.Locales.locale_codes(),
-#     plural_forms: MyCharlieWeb.Gettext.Plural,
-#     priv: "priv/gettext"
-# end
