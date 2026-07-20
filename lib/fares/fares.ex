1b9f9be55228bcf72c462aceb36502055192eaf6
Josh Larson <jlarson@mbta.com>
2026-07-20 16:36:57 -0400

    fix(TripPlanner): Simplify ferry fares logic and fix some bugs (#3349)

--- lib/fares/fares.ex
+++ lib/fares/fares.ex
@@ -17,7 +17,55 @@ defmodule Fares do
   @express_routes ~w(170 325 326 352 354 426 428 434 450 459 501 502 503 504 505)
   @express_route_set MapSet.new(@express_routes)
 
-  # For the time being, these stops are ONLY served by the Winthrop Ferry
+  # This is a list of ferry zones, according to
+  # https://www.mbta.com/fares/ferry-fares, along with a list of which
+  # dock ID's belong to each zone. This is necessary for fare
+  # calculation, since ferry fares are based on the zones of the stops
+  # visited in a similar (though not identical) way to Commuter Rail.
+  @ferry_zones [
+    %{
+      zone: %{fare_name: :ferry_winthrop, zone_number: 1},
+      dock_ids: [
+        "Boat-Quincy",
+        "Boat-Winthrop"
+      ]
+    },
+    %{
+      zone: %{fare_name: :ferry_lynn, zone_number: 2},
+      dock_ids: [
+        "Boat-Blossom"
+      ]
+    },
+    %{
+      zone: %{fare_name: :commuter_ferry, zone_number: 5},
+      dock_ids: [
+        "Boat-George",
+        "Boat-Hingham",
+        "Boat-Hull"
+      ]
+    }
+  ]
+
+  # A "default" zone - docks that aren't listed above are assumed to
+  # be in the inner harbor, Zone 1A (which, for the sake of integer
+  # comparison, can be given a zone number of 0).
+  @inner_harbor_ferry_zone %{fare_name: :ferry_inner_harbor, zone_number: 0}
+
+  # The same data as in @ferry_zones, but arranged as a map of dock_id
+  # to zone, like
+  #   %{
+  #     "Boat-Quincy" => %{fare_name: :ferry_winthrop, zone_number: 1},
+  #     "Boat-Winthrop" => %{fare_name: :ferry_winthrop, zone_number: 1},
+  #     "Boat-Blossom" => %{fare_name: :ferry_lynn, zone_number: 2},
+  #     ...
+  #   }
+  # ... for easier lookup
+  @ferry_zones_by_dock @ferry_zones
+                       |> Enum.flat_map(fn %{zone: zone, dock_ids: dock_ids} ->
+                         dock_ids |> Enum.map(&{&1, zone})
+                       end)
+                       |> Map.new()
+
   @winthrop_ferry_stops [
     "Boat-Aquarium",
     "Boat-Fan",
@@ -49,10 +97,6 @@ defmodule Fares do
   @doc """
   Calculate the fare between a pair of stops.
   """
-
-  def fare_for_stops(:ferry, origin, destination, between) do
-    {:ok, calculate_ferry(origin, destination, between)}
-  end
 
   @spec fare_for_stops(
           :commuter_rail | :ferry,
@@ -97,18 +141,28 @@ defmodule Fares do
     {:interzone, "#{total_zones}"}
   end
 
-  def calculate_ferry(origin, destination, between)
-      when "Boat-Long" in [origin, destination] and "Boat-Logan" in [origin, destination]
-      when "Boat-Long" in [origin, destination] and "Boat-Lewis" in [origin, destination]
-      when "Boat-Long-North-5B" in [origin, destination] and "Boat-Lewis" in [origin, destination]
-      when origin in @loop_ferry_stops and destination in @loop_ferry_stops and
-             origin != destination
-      when origin in @winthrop_ferry_stops and destination in @winthrop_ferry_stops do
-    long_way_around_trip_logic(between)
-  end
-
-  def calculate_ferry(origin, destination, _) do
-    calculate_ferry(origin, destination)
+  # Ferry fares are based on zones, similar to Commuter Rail (and in
+  # fact have the same zone prices), but with two key differences:
+  #
+  # - Ferry trips can start and end in Zone 1A, while having
+  #   intermediate stops in zones further out. Fares are based on the
+  #   furthest-out zone that you visit, regardless of where you embark
+  #   or debark, so for instance, if you travel from Central Wharf to
+  #   Logan (both Zone 1A) via Quincy (Zone 1), then you pay the
+  #   higher Zone 1 fare.
+  #
+  # - Interzone fares are not considered. If you take the trip from
+  #   Hingham to Hull, then even though you've stayed in Zone 5, you
+  #   pay the Zone 5 fare, not the single-zone interzone fare.
+  #
+  # All this to say that the actual formula for a ferry fare is to
+  # pull the zones for all of the stops, origin, destination, and
+  # stops in between, and base the fare on the highest-priced zone.
+  def calculate_ferry(origin, destination, between) do
+    [origin, destination | between]
+    |> Enum.map(&Map.get(@ferry_zones_by_dock, &1, @inner_harbor_ferry_zone))
+    |> Enum.max_by(& &1.zone_number)
+    |> then(& &1.fare_name)
   end
 
   def calculate_ferry(origin, destination)
@@ -149,19 +203,6 @@ defmodule Fares do
 
   def calculate_ferry(_origin, _destination) do
     :commuter_ferry
-  end
-
-  def long_way_around_trip_logic(between) do
-    cond do
-      "Hingham" in between or "Hull" in between ->
-        :commuter_ferry
-
-      "Quincy" in between or "Winthrop Landing" in between ->
-        :ferry_winthrop
-
-      true ->
-        :ferry_inner_harbor
-    end
   end
 
   @spec silver_line_rapid_transit?(Route.id_t()) :: boolean
--- mix.exs
+++ mix.exs
@@ -152,7 +152,7 @@ defmodule DotCom.Mixfile do
       {:nebulex_redis_adapter, "2.4.2"},
       {
         :open_trip_planner_client,
-        [github: "mbta/open_trip_planner_client", tag: "v0.17.2"]
+        [github: "mbta/open_trip_planner_client", tag: "v0.18.0"]
       },
       {:parallel_stream, "1.1.0"},
       {:phoenix, "1.8.6", override: true},
