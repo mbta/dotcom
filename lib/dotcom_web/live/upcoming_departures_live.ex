87884109996f5438d10a18e58878fcd1804869bb
Luc Vachon <lvachon1@mbta.com>
2026-07-29 14:49:24 -0400

    Added logic to prevent crashes when the list of schedules returned is `nil`

--- lib/dotcom_web/live/upcoming_departures_live.ex
+++ lib/dotcom_web/live/upcoming_departures_live.ex
@@ -215,15 +215,22 @@ defmodule DotcomWeb.Live.UpcomingDeparturesLive do
     assign(socket, :last_trip_time, get_last_trip_time(socket.assigns, date))
   end
 
-  defp get_last_trip_time(assigns, date) do
-    [assigns.route_id]
-    |> @schedules_repo.by_route_ids(
-      direction_id: assigns.direction_id,
-      stop_ids: [assigns.stop_id],
-      date: date
-    )
-    |> List.last(%{})
-    |> Map.get(:time)
+  def get_last_trip_time(assigns, date) do
+    schedules =
+      [assigns.route_id]
+      |> @schedules_repo.by_route_ids(
+        direction_id: assigns.direction_id,
+        stop_ids: [assigns.stop_id],
+        date: date
+      )
+
+    if is_nil(schedules) do
+      nil
+    else
+      schedules
+      |> List.last(%{})
+      |> Map.get(:time)
+    end
   end
 
   attr :upcoming_departures, :any,
