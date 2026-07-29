54a251d297d68b43e19490d92d14898ad06d2044
Luc Vachon <lvachon1@mbta.com>
2026-07-29 13:49:18 -0400

    Replaced `random=0.6639563940...` with `r=???` where each ? is a random character from a to z.  This gives a 1:17576 chance of the bug occuring

--- lib/build_calendar.ex
+++ lib/build_calendar.ex
@@ -169,7 +169,7 @@ defmodule BuildCalendar do
     for date <- Date.range(first_day(shifted), last_day(shifted)) do
       %BuildCalendar.Day{
         date: date,
-        url: build_url(url_fn, date, today),
+        url: build_url(url_fn, date, today, selected),
         month_relation: month_relation(date, last_day_of_previous_month, last_day_of_this_month),
         selected?: date == selected,
         holiday?: MapSet.member?(holiday_set, date),
@@ -195,13 +195,26 @@ defmodule BuildCalendar do
     |> Date.end_of_week(:sunday)
   end
 
-  @spec build_url(url_fn, Date.t(), Date.t()) :: String.t()
-  defp build_url(url_fn, today, today) do
+  @spec build_url(url_fn, Date.t(), Date.t(), Date.t()) :: String.t()
+
+  defp build_url(url_fn, today, today, _) do
     url_fn.(date: nil, date_select: nil, shift: nil)
   end
 
-  defp build_url(url_fn, date, _) do
-    url_fn.(date: Date.to_iso8601(date), date_select: nil, shift: nil, random: :rand.uniform())
+  defp build_url(url_fn, date, _today, date) do
+    url_fn.(date: Date.to_iso8601(date), date_select: nil, shift: nil, r: rand_string(3))
+  end
+
+  defp build_url(url_fn, date, _, _) do
+    url_fn.(date: Date.to_iso8601(date), date_select: nil, shift: nil)
+  end
+
+  defp rand_string(length) do
+    0..(length - 1) |> Enum.to_list() |> Enum.map(fn _ -> rand_char() end) |> Enum.join()
+  end
+
+  defp rand_char() do
+    <<Enum.random(?a..?z)>>
   end
 
   @spec month_relation(Date.t(), Date.t(), Date.t()) :: __MODULE__.Day.month_relation()
