efb5b1f54273f1792cdc1370faa9621585d759a2
Luc Vachon <lvachon1@mbta.com>
2026-08-03 14:59:24 -0400

    QF | [WC Cleanup]: Revert these intercepts (#3378)
    * Removed the world cup intercepts/callouts on the south station stop page and the commuter rail alerts page.  Removed those components and some associated CSS.
    
    * Localize

--- lib/dotcom_web/components/components.ex
+++ lib/dotcom_web/components/components.ex
@@ -355,59 +355,6 @@ defmodule DotcomWeb.Components do
     """
   end
 
-  attr(:rest, :global, include: ~w(disabled))
-  attr(:class, :string, default: "")
-
-  @doc """
-  Same as above, but links to the timetable instead
-  """
-  def boston_stadium_intercept(assigns) do
-    ~H"""
-    <.descriptive_link
-      href="/schedules/bostonstadium"
-      class={@class}
-      {@rest}
-    >
-      <:title>
-        {~t(Taking the train to a World Cup match?)}
-      </:title>
-      <p class="c-descriptive-link__world-cup c-descriptive-link__boston-stadium">
-        {gettext("View %{timetable_link} for departure information.",
-          timetable_link: "<span class='underline font-medium'>Boston Stadium Train timetables</span>"
-        )
-        |> Phoenix.HTML.raw()}
-      </p>
-    </.descriptive_link>
-    """
-  end
-
-  attr(:rest, :global, include: ~w(disabled))
-  attr(:class, :string, default: "")
-
-  @doc """
-  Callout for commuter rail alerts during the world cup
-  """
-  def cr_alert_intercept(assigns) do
-    ~H"""
-    <.descriptive_link
-      href="/schedules/commuter-rail"
-      class={@class}
-      {@rest}
-    >
-      <:title>
-        {~t(Your Commuter Rail trip will be affected by the World Cup.)}
-      </:title>
-      <p class="c-descriptive-link__world-cup">
-        {gettext(
-          "Service changes are in effect June 6 – July 12. Check each day of your line’s %{timetable_link} for the most up-to-date schedule.",
-          timetable_link: "<span class='underline font-medium'>timetable</span>"
-        )
-        |> Phoenix.HTML.raw()}
-      </p>
-    </.descriptive_link>
-    """
-  end
-
   attr(:rest, :global)
   slot :inner_block, required: true
 
