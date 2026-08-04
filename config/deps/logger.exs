2028cd47adfbd79637733835f5be8c53e01a5576
Cristen Jones <cjones3@mbta.com>
2026-08-04 15:38:47 -0400

    chore: configure Logger overload handling

--- config/deps/logger.exs
+++ config/deps/logger.exs
@@ -14,6 +14,25 @@ if config_env() == :prod do
   config :logger, :default_formatter,
     format: "$dateT$time [$level]$levelpad node=$node $metadata$message\n",
     metadata: [:ip, :mbta_id, :request_id]
+
+  # Drop aggressively and early to prevent the message queue from growing large.
+  # Also enable terminating overloaded handlers based on memory or queue length.
+  # https://www.erlang.org/docs/28/apps/kernel/logger_chapter.html#overload_protection
+  config :logger, :default_handler,
+    config: [
+      # default
+      sync_mode_qlen: 10,
+      # lower than default of 200
+      drop_mode_qlen: 50,
+      # lower than default of 1000
+      flush_qlen: 200,
+      # changed from false
+      overload_kill_enable: true,
+      # lower than default of 20_000
+      overload_kill_qlen: 500,
+      # default
+      overload_kill_mem_size: 3_000_000
+    ]
 end
 
 if config_env() == :dev do
