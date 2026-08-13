2fe44f935b1f98bc287278f370230214c1b71a7b
Josh Larson <jlarson@mbta.com>
2026-08-13 14:28:33 -0400

    tests: Widen potential range for `Generators.Date.random_date/0`

--- test/support/generators/date.ex
+++ test/support/generators/date.ex
@@ -5,14 +5,14 @@ defmodule Test.Support.Generators.Date do
 
   @date_time_module Application.compile_env!(:dotcom, :date_time_module)
 
-  @doc "Generate a random date this decade"
+  @doc "Generate a random date in a 20-year span"
   def random_date() do
     now = @date_time_module.now()
 
-    beginning_of_year = Date.new!(now.year, 1, 1)
-    end_of_year = Date.new!(now.year + 1, 1, 1)
+    beginning_of_range = Date.new!(now.year - 10, 1, 1)
+    end_of_range = Date.new!(now.year + 10, 1, 1)
 
-    date_generator_between({beginning_of_year, end_of_year}) |> Enum.take(1) |> List.first()
+    date_generator_between({beginning_of_range, end_of_range}) |> Enum.take(1) |> List.first()
   end
 
   def date_generator_between({start, stop}) do
