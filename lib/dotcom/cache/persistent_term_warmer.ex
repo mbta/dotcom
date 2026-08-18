78e76fde8a90c38dd0f87e0fb4a04beac73324f4
Cristen Jones <cjones3@mbta.com>
2026-08-18 14:30:29 -0400

    feat: save rendered static markup for faster retrieval (#3426)

---
+++ lib/dotcom/cache/persistent_term_warmer.ex
@@ -0,0 +1,28 @@
+defmodule Dotcom.Cache.PersistentTermWarmer do
+  @moduledoc """
+  Functions to trigger at application startup to warm caches that are stored in
+  :persistent_term.
+
+  By starting these processes at application startup, we can ensure that the
+  caches are warmed before any user-facing requests are handled. This is
+  important for performance and to avoid cache misses on the first request.
+  """
+
+  def warm_homepage do
+    # Locale-independent — called once
+    DotcomWeb.LayoutView.hidden_icons()
+
+    # Per-locale — called once per locale (8 locales)
+    # Handled inside a Task because the locale is set per-process,
+    # thus does not change locale for user-facing requests.
+    for %{code: code} <- Dotcom.Locales.locales() do
+      Task.async(fn ->
+        Dotcom.Locales.set_locale(code)
+
+        # All of these should use the locale code in its cache key.
+        DotcomWeb.LayoutView.top_tier_nav(code)
+      end)
+    end
+    |> Task.await_many()
+  end
+end
