9d6fd82bcce9519009650d21c0d4b91e23ad8129
Josh Larson <jlarson@mbta.com>
2026-09-08 10:46:39 -0400

    chore: Update the docs

--- lib/dotcom_web/hooks/assign_route.ex
+++ lib/dotcom_web/hooks/assign_route.ex
@@ -6,14 +6,18 @@ defmodule DotcomWeb.Hooks.AssignRoute do
 
   @routes_repo Application.compile_env!(:dotcom, :repo_modules)[:routes]
 
+  @doc """
+  Uses the route ID in the params to look up the corresponding
+  route, and assigns that route to the socket as `:route`.
+
+  If the route doesn't exist, then raises a
+  `DotcomWeb.NotFoundError` in order to get
+  `DotcomWeb.Router.handle_errors/2` to render the 404 page.
+  """
   def on_mount(:default, %{"route_id" => route_id}, _session, socket) do
     case @routes_repo.get(route_id) do
-      nil ->
-        # Raising this error will render the 404 page
-        raise DotcomWeb.NotFoundError
-
-      route ->
-        {:cont, assign(socket, :route, route)}
+      nil -> raise DotcomWeb.NotFoundError
+      route -> {:cont, assign(socket, :route, route)}
     end
   end
 end
