866c46ac14b82ce5db8c45beae44a4774685002d
Cristen Jones <cjones3@mbta.com>
2026-09-15 16:26:44 -0400

    feat(health): add OTP health check (#3498)

--- lib/dotcom_web/controllers/health_controller.ex
+++ lib/dotcom_web/controllers/health_controller.ex
@@ -4,8 +4,32 @@ defmodule DotcomWeb.HealthController do
   """
   use DotcomWeb, :controller
 
+  @cache Application.compile_env!(:dotcom, :cache)
+  @otp_module Application.compile_env!(:dotcom, :otp_module)
+
   def index(conn, _params) do
     conn
     |> send_resp(200, "")
+  end
+
+  def open_trip_planner(conn, _params) do
+    result = check_otp()
+    status = if result.healthy?, do: 200, else: 503
+
+    conn
+    |> put_resp_header("cache-control", "max-age=60, public")
+    |> send_resp(status, "")
+  end
+
+  defp check_otp do
+    case @cache.get(:otp_health) do
+      nil ->
+        result = %{healthy?: @otp_module.healthy?()}
+        @cache.put(:otp_health, result, ttl: :timer.minutes(1))
+        result
+
+      health ->
+        health
+    end
   end
 end
