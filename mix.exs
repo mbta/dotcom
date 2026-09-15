e14c3aa0d0d0a92feac7cd59b2a10a59482042a3
Cristen Jones <cjones3@mbta.com>
2026-09-14 21:00:01 -0400

    deps: update open_trip_planner_client

--- mix.exs
+++ mix.exs
@@ -151,7 +151,7 @@ defmodule DotCom.Mixfile do
       {:nebulex_redis_adapter, "2.4.2"},
       {
         :open_trip_planner_client,
-        [github: "mbta/open_trip_planner_client", tag: "v0.20.0"]
+        [github: "mbta/open_trip_planner_client", tag: "v0.21.0"]
       },
       {:parallel_stream, "1.1.0"},
       {:phoenix, "1.8.6", override: true},
