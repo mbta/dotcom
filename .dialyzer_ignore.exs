2d9ebc7c5a1a19daf66ef8305a5e4ee2eae68097
Josh Larson <jlarson@mbta.com>
2026-09-15 16:38:34 -0400

    cleanup: Remove `opaque`-ignoring stuff from `.dialyzer_ignore` (#3503)
    The `no_opaque` flag in `mix.exs` appears to do this for us already.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,20 +1,4 @@
-all_files = Dotcom.Utils.File.list_all_files("lib")
-
-# Ignore all call_without_opaque
-call_without_opaque =
-  all_files
-  |> Enum.map(fn path ->
-    {path, :call_without_opaque}
-  end)
-
-# Ignore all contract_with_opaque
-contract_with_opaque =
-  all_files
-  |> Enum.map(fn path ->
-    {path, :contract_with_opaque}
-  end)
-
-manual = [
+[
   {"lib/alerts/parser.ex", :unknown_type},
   {"lib/algolia/object.ex", :no_return},
   {"lib/algolia/routes.ex", :call},
@@ -81,5 +65,3 @@ manual = [
   {"test/support/integration_helpers.ex", :unknown_function},
   {"test/support/page_helpers.ex", :guard_fail}
 ]
-
-call_without_opaque ++ contract_with_opaque ++ manual
