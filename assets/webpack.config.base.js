1419a0360c97f43992f596bcd9f62f40f8d71947
Luc Vachon <lvachon1@mbta.com>
2026-09-21 13:34:54 -0400

    Updating sass loader to include the maplibre css in the dist package maybe?  I noticed it was missing from the broken versions node_modules

--- assets/webpack.config.base.js
+++ assets/webpack.config.base.js
@@ -137,6 +137,7 @@ module.exports = {
                 loadPaths: [
                   "node_modules/bootstrap",
                   "node_modules/@fortawesome/fontawesome-free",
+                  "node_modules/maplibre-gl",
                   "vendor"
                 ],
                 style: "compressed",
